4ab981c55b145157926b97303d4d231572c20eb6
Yoyoyopo5 <118655115+Yoyoyopo5@users.noreply.github.com>
2024-09-29 21:13:56 -0500

    Add Snooze Next Ad API

---
+++ TwitchySharp.Api/Helix/Channels/Ads/Requests/SnoozeNextAdRequest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using TwitchySharp.Api.Helix.Channels.Ads.Responses;
+using TwitchySharp.Api.Authorization;
+
+namespace TwitchySharp.Api.Helix.Channels.Ads.Requests;
+/// <summary>
+/// If available, pushes back the timestamp of the upcoming automatic mid-roll ad by 5 minutes. 
+/// This endpoint duplicates the snooze functionality in the creator dashboard’s Ads Manager.
+/// The channel must be live and have an upcoming scheduled ad break.
+/// Requires a user access token with <see cref="Scope.ChannelManageAds"/>.
+/// See <see href="https://dev.twitch.tv/docs/api/reference/#snooze-next-ad">snooze next ad</see> for more information.
+/// </summary>
+/// <param name="clientId">The client id of the application.</param>
+/// <param name="accessToken">A user access token with <see cref="Scope.ChannelManageAds"/>.</param>
+/// <param name="broadcasterId">The user id of the channel to snooze an ad on. This must be the same user that provided the <paramref name="accessToken"/></param>
+public class SnoozeNextAdRequest(string clientId, string accessToken, string broadcasterId)
+    : HelixApiRequest<SnoozeNextAdResponse>(
+        "/channels/ads/schedule/snooze" + $"broadcaster_id={broadcasterId}", 
+        clientId, 
+        accessToken
+        )
+{
+    public override HttpMethod Method => HttpMethod.Post;
+}
