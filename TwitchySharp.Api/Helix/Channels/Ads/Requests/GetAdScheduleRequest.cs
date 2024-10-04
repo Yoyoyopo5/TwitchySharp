91b053e268d9909e30bbc18cecb427dadc44bd3c
Yoyoyopo5 <yoyoyopo5@DESKTOP-IT4LOU9>
2024-10-04 03:51:32 -0500

    Formatting change in documentation

--- TwitchySharp.Api/Helix/Channels/Ads/Requests/GetAdScheduleRequest.cs
+++ TwitchySharp.Api/Helix/Channels/Ads/Requests/GetAdScheduleRequest.cs
@@ -8,8 +8,10 @@ namespace TwitchySharp.Api.Helix.Channels.Ads.Requests;
 /// <summary>
 /// This endpoint returns ad schedule related information, including snooze, when the last ad was run, when the next ad is scheduled, and if the channel is currently in pre-roll free time. 
 /// Note that a new ad cannot be run until 8 minutes after running a previous ad.
-/// Requires a user access token with <see cref="Scope.ChannelReadAds"/>.
+/// <br/>
 /// See <see href="https://dev.twitch.tv/docs/api/reference/#get-ad-schedule">get ad schedule</see> for more information.
+/// <br/>
+/// Requires a user access token with <see cref="Scope.ChannelReadAds"/>.
 /// </summary>
 /// <param name="clientId">The client id of the application.</param>
 /// <param name="accessToken">A user access token with <see cref="Scope.ChannelReadAds"/>.</param>
