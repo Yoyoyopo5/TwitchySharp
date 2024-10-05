aa29cb1b5eef88fb11b8f45c545be776dcd3e5d1
Yoyoyopo5 <yoyoyopo5@DESKTOP-IT4LOU9>
2024-10-05 02:21:27 -0500

    Add Get Bits Leaderboard API

---
+++ TwitchySharp.Api/Helix/Bits/Requests/GetBitsLeaderboardRequest.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TwitchySharp.Helpers;
+using TwitchySharp.Api.Authorization;
+
+namespace TwitchySharp.Api.Helix.Bits;
+/// <summary>
+/// Gets the Bits leaderboard for the authenticated broadcaster.
+/// <br/>
+/// See <see href="https://dev.twitch.tv/docs/api/reference/#get-bits-leaderboard">get bits leaderboard</see> for more information.
+/// <br/>
+/// Requires a user access token that includes <see cref="Scope.BitsRead"/>.
+/// </summary>
+/// <param name="clientId">The client id of the application.</param>
+/// <param name="accessToken">A user access token that includes <see cref="Scope.BitsRead"/>.</param>
+/// <param name="count">The number of results (leaderboard entries) to return. The minimum count is 1 and the maximum is 100. The default is 10.</param>
+/// <param name="period">The time period over which data is aggregated.</param>
+/// <param name="StartedAt">
+/// The start date used for determining the aggregation period. 
+/// Specify this parameter only if you specify <paramref name="period"/>. The start date is ignored if <paramref name="period"/> is <see cref="LeaderboardPeriod.All"/>.
+/// </param>
+/// <param name="userId">
+/// The user ID of a user that has cheered bits in the channel. 
+/// If count is greater than 1, the response may include users ranked above and below the specified user. 
+/// To get the leaderboard’s top leaders, set this to <see langword="null"/>.
+/// </param>
+public class GetBitsLeaderboardRequest(
+    string clientId,
+    string accessToken,
+    int? count = null,
+    LeaderboardPeriod? period = null,
+    DateTimeOffset? StartedAt = null,
+    string? userId = null
+    )
+    : HelixApiRequest<GetBitsLeaderboardResponse>(
+        "/bits/leaderboard" + (new Dictionary<string, string?>() 
+        {
+            { "count", count?.ToString() },
+            { "period", period?.Value },
+            { "started_at", StartedAt?.UtcDateTime.AddHours(8).ToString("yyyy-MM-dd'T'HH:mm:ssZ") }, // This is kind of fucked, but we have to add 8 hours to the time because Twitch converts it to PST (-8:00).
+            { "user_id", userId }
+        }.ToHttpQueryString()),
+        clientId,
+        accessToken
+        );
+
+public record LeaderboardPeriod : ValueBackedEnum<string>
+{
+    /// <summary>
+    /// A day spans from 00:00:00 on the day specified in StartedAt and runs through 00:00:00 of the next day.
+    /// </summary>
+    public static LeaderboardPeriod Day { get; } = new("day");
+    /// <summary>
+    /// A week spans from 00:00:00 on the Monday of the week specified in StartedAt and runs through 00:00:00 of the next Monday.
+    /// </summary>
+    public static LeaderboardPeriod Week { get; } = new("week");
+    /// <summary>
+    /// A month spans from 00:00:00 on the first day of the month specified in StartedAt and runs through 00:00:00 of the first day of the next month.
+    /// </summary>
+    public static LeaderboardPeriod Month { get; } = new("month");
+    /// <summary>
+    /// A year spans from 00:00:00 on the first day of the year specified in StartedAt and runs through 00:00:00 of the first day of the next year.
+    /// </summary>
+    public static LeaderboardPeriod Year { get; } = new("year");
+    /// <summary>
+    /// Default. The lifetime of the broadcaster's channel.
+    /// </summary>
+    public static LeaderboardPeriod All { get; } = new("all");
+    private LeaderboardPeriod(string period) : base(period) { }
+}
---
+++ TwitchySharp.Api/Helix/Bits/Responses/GetBitsLeaderboardResponse.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json.Serialization;
+using TwitchySharp.Api.Models;
+
+namespace TwitchySharp.Api.Helix.Bits;
+/// <summary>
+/// Contains information about the bits leaderboard for a broadcaster.
+/// </summary>
+public record GetBitsLeaderboardResponse
+{
+    /// <summary>
+    /// A list of leaderboard leaders. 
+    /// The leaders are returned in rank order by how much they’ve cheered. 
+    /// The array is empty if nobody has cheered bits.
+    /// </summary>
+    [JsonInclude, JsonRequired]
+    public BitsLeaderboardData[] Data { get; private set; } = [];
+    /// <summary>
+    /// The reporting window’s start and end dates. 
+    /// The dates are calculated by using the StartedAt and Period request parameters. 
+    /// If you didn't specify the StartedAt query parameter, the dates are null.
+    /// </summary>
+    [JsonInclude, JsonRequired]
+    public DateTimeOffsetRange DateRange { get; private set; }
+    /// <summary>
+    /// The number of ranked users in <see cref="Data"/>.
+    /// This is the value in the Count request parameter or the total number of entries on the leaderboard, whichever is less.
+    /// </summary>
+    [JsonInclude, JsonRequired]
+    public int Total { get; private set; }
+}
+
+/// <summary>
+/// Contains information about a single entry (user) on a bits leaderboard.
+/// </summary>
+public record BitsLeaderboardData
+{
+    /// <summary>
+    /// The user's user id.
+    /// </summary>
+    [JsonInclude, JsonRequired]
+    public string UserId { get; private set; } = string.Empty;
+    /// <summary>
+    /// The user’s login name (username).
+    /// </summary>
+    [JsonInclude, JsonRequired]
+    public string UserLogin { get; private set; } = string.Empty;
+    /// <summary>
+    /// The user’s display name.
+    /// </summary>
+    [JsonInclude, JsonRequired]
+    public string UserName { get; private set; } = string.Empty;
+    /// <summary>
+    /// The user’s position on the leaderboard.
+    /// </summary>
+    [JsonInclude, JsonRequired]
+    public int Rank { get; private set; }
+    /// <summary>
+    /// The number of Bits the user has cheered.
+    /// </summary>
+    [JsonInclude, JsonRequired]
+    public int Score { get; private set; }
+}
