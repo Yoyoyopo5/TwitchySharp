8f774f105232a9398608c764f34c9d653d7a9a75
Yoyoyopo5 <yoyoyopo5@DESKTOP-IT4LOU9>
2024-10-05 09:25:51 -0500

    Add snake case enum helper

---
+++ TwitchySharp.Helpers/SnakeCaseJsonStringEnumConverter.cs
@@ -0,0 +1,8 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace TwitchySharp.Helpers;
+public class SnakeCaseJsonStringEnumConverter() : JsonStringEnumConverter(JsonNamingPolicy.SnakeCaseLower); // Need to include because this version doesn't allow passing params to JsonConverter
