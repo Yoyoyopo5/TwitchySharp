e4ecc8e35ded3cbd71dd657e3c4966a98491fa49
Yoyoyopo5 <118655115+Yoyoyopo5@users.noreply.github.com>
2024-10-05 02:21:17 -0500

    Allow empty strings for DateTimeOffsetRanges

--- TwitchySharp.Api/Models/DateTimeOffsetRange.cs
+++ TwitchySharp.Api/Models/DateTimeOffsetRange.cs
@@ -2,12 +2,13 @@
 using System.Collections.Generic;
 using System.Text;
 using System.Text.Json.Serialization;
+using TwitchySharp.Helpers;
 
 namespace TwitchySharp.Api.Models;
 public record struct DateTimeOffsetRange
 {
-    [JsonInclude, JsonRequired]
-    public DateTimeOffset StartedAt { get; private set; }
-    [JsonInclude, JsonRequired]
-    public DateTimeOffset EndedAt { get; private set; }
+    [JsonInclude, JsonRequired, JsonConverter(typeof(EmptyDateTimeOffsetConverter))]
+    public DateTimeOffset? StartedAt { get; private set; }
+    [JsonInclude, JsonRequired, JsonConverter(typeof(EmptyDateTimeOffsetConverter))]
+    public DateTimeOffset? EndedAt { get; private set; }
 }
