23f591a327aec85fbfc6acef10014d386f950e8b
Yoyoyopo5 <118655115+Yoyoyopo5@users.noreply.github.com>
2024-10-07 09:37:26 -0500

    Add Get Channel Editors API

---
+++ TwitchySharp.Api/Helix/Channels/Requests/GetChannelEditorsRequest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TwitchySharp.Helpers;
+using TwitchySharp.Api.Authorization;
+
+namespace TwitchySharp.Api.Helix.Channels;
+/// <summary>
+/// Gets the broadcaster’s list editors.
+/// <br/>
+/// See <see href="https://dev.twitch.tv/docs/api/reference/#get-channel-editors">get channel editors</see> for more information.
+/// <br/>
+/// Requires a user access token with <see cref="Scope.ChannelReadEditors"/>.
+/// </summary>
+/// <param name="clientId">The client id of the application.</param>
+/// <param name="accessToken">A user access token with <see cref="Scope.ChannelReadEditors"/>.</param>
+/// <param name="broadcasterId">
+/// The user ID of the broadcaster that owns the channel. 
+/// This ID must match the user ID in the <paramref name="accessToken"/>.
+/// </param>
+public class GetChannelEditorsRequest(string clientId, string accessToken, string broadcasterId)
+    : HelixApiRequest<GetChannelEditorsResponse>(
+        "/channels/editors" + 
+        new HttpQueryParameters()
+            .Add("broadcaster_id", broadcasterId), 
+        clientId, 
+        accessToken
+        );
