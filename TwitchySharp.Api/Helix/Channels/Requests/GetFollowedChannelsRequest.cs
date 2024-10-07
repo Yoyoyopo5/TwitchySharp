c74dc20bbea233ee37d94b762c5bebcc3816541e
Yoyoyopo5 <118655115+Yoyoyopo5@users.noreply.github.com>
2024-10-07 11:56:28 -0500

    Add Get Followed Channels API

---
+++ TwitchySharp.Api/Helix/Channels/Requests/GetFollowedChannelsRequest.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TwitchySharp.Helpers;
+using TwitchySharp.Api.Authorization;
+using TwitchySharp.Api.Models;
+
+namespace TwitchySharp.Api.Helix.Channels;
+/// <summary>
+/// Gets a list of broadcasters that the specified user follows. 
+/// You can also use this endpoint to see whether a user follows a specific broadcaster.
+/// <br/>
+/// See <see href="https://dev.twitch.tv/docs/api/reference/#get-followed-channels">get followed channels</see> for more information.
+/// <br/>
+/// Requires a user access token with <see cref="Scope.UserReadFollows"/>.
+/// </summary>
+/// <param name="clientId">The client id of the application.</param>
+/// <param name="accessToken">A user access token with <see cref="Scope.UserReadFollows"/>.</param>
+/// <param name="userId">
+/// The user ID of the user to get follows for. 
+/// This ID must match the user ID in the <paramref name="accessToken"/>.
+/// </param>
+/// <param name="broadcasterId">
+/// A broadcaster’s user ID. 
+/// Use this parameter to see whether the user follows this broadcaster. 
+/// If specified, the response contains this broadcaster if the user follows them. 
+/// If not specified, the response contains all broadcasters that the user follows.
+/// </param>
+/// <param name="first">
+/// The maximum number of items to return per page in the response. 
+/// The minimum page size is 1 item per page and the maximum is 100. 
+/// The default is 20.
+/// </param>
+/// <param name="after">
+/// The cursor used to get the next page of results. 
+/// The <see cref="Pagination"/> property in the response contains the cursor’s value.
+/// </param>
+public class GetFollowedChannelsRequest(
+    string clientId, 
+    string accessToken, 
+    string userId, 
+    string? broadcasterId = null, 
+    int? first = null, 
+    string? after = null
+    )
+    : HelixApiRequest<GetFollowedChannelsResponse>(
+        "/channels/followed" + 
+        new HttpQueryParameters()
+            .Add("user_id", userId)
+            .Add("broadcaster_id", broadcasterId)
+            .Add("first", first?.ToString())
+            .Add("after", after),
+        clientId,
+        accessToken
+        );
---
+++ TwitchySharp.Api/Helix/Channels/Responses/GetFollowedChannelsResponse.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json.Serialization;
+using TwitchySharp.Api.Models;
+
+namespace TwitchySharp.Api.Helix.Channels;
+/// <summary>
+/// Contains the list of a user's followed channels.
+/// </summary>
+public record GetFollowedChannelsResponse
+{
+    /// <summary>
+    /// The list of broadcasters that the user follows. 
+    /// The list is in descending order by the <see cref="FollowedChannel.FollowedAt"/> property (with the most recently followed broadcaster first). 
+    /// The list is empty if the user doesn’t follow anyone.
+    /// </summary>
+    [JsonInclude, JsonRequired]
+    public FollowedChannel[] Data { get; private set; } = [];
+    /// <summary>
+    /// Contains the information used to page through the list of results. 
+    /// The <see cref="Pagination.Cursor"/> is null if there are no more pages left to page through.
+    /// </summary>
+    [JsonInclude, JsonRequired]
+    public Pagination Pagination { get; private set; } = new();
+    /// <summary>
+    /// The total number of broadcasters that the user follows. 
+    /// As someone pages through the list, the number may change as the user follows or unfollows broadcasters.
+    /// </summary>
+    [JsonInclude, JsonRequired]
+    public int Total { get; private set; }
+}
+
+public record FollowedChannel
+{
+    /// <summary>
+    /// The user ID of the the broadcaster that this user is following.
+    /// </summary>
+    [JsonInclude, JsonRequired]
+    public string BroadcasterId { get; private set; } = string.Empty;
+    /// <summary>
+    /// The broadcaster’s login name (username).
+    /// </summary>
+    [JsonInclude, JsonRequired]
+    public string BroadcasterLogin { get; private set; } = string.Empty;
+    /// <summary>
+    /// The broadcaster’s display name.
+    /// </summary>
+    [JsonInclude, JsonRequired]
+    public string BroadcasterName { get; private set; } = string.Empty;
+    /// <summary>
+    /// The time when the user started following the broadcaster.
+    /// </summary>
+    [JsonInclude, JsonRequired]
+    public DateTimeOffset FollowedAt { get; private set; }
+}
