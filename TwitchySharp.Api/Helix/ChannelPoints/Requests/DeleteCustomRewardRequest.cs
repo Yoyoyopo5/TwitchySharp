8e2118d26c8f3c8c1b202e382acd1cd3d86eff29
Yoyoyopo5 <yoyoyopo5@DESKTOP-IT4LOU9>
2024-10-13 17:16:01 -0500

    Add Delete Custom Reward API

---
+++ TwitchySharp.Api/Helix/ChannelPoints/Requests/DeleteCustomRewardRequest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using TwitchySharp.Helpers;
+using TwitchySharp.Api.Authorization;
+
+namespace TwitchySharp.Api.Helix.ChannelPoints;
+/// <summary>
+/// Deletes a custom reward that the broadcaster created. 
+/// The app used to create the reward is the only app that may delete it.
+/// If the reward’s redemption status is UNFULFILLED at the time the reward is deleted, its redemption status is marked as FULFILLED.
+/// <br/>
+/// See <see href="https://dev.twitch.tv/docs/api/reference/#delete-custom-reward">delete custom reward</see> for more information.
+/// <br/>
+/// Requires a user access token with <see cref="Scope.ChannelManageRedemptions"/>.
+/// </summary>
+/// <param name="clientId">The client id of the application.</param>
+/// <param name="accessToken">A user access token with <see cref="Scope.ChannelManageRedemptions"/>.</param>
+/// <param name="broadcasterId">
+/// The user id of the broadcaster that created the custom reward. 
+/// This must be the same user that created the <paramref name="accessToken"/>.
+/// </param>
+/// <param name="rewardId">The ID of the custom reward to delete.</param>
+public class DeleteCustomRewardRequest(string clientId, string accessToken, string broadcasterId, string rewardId)
+    : HelixApiRequest<DeleteCustomRewardResponse>(
+        "/channel_points/custom_rewards" +
+        new HttpQueryParameters()
+            .Add("broadcaster_id", broadcasterId)
+            .Add("id", rewardId),
+        clientId,
+        accessToken
+        )
+{
+    public override HttpMethod Method => HttpMethod.Delete;
+}
---
+++ TwitchySharp.Api/Helix/ChannelPoints/Responses/DeleteCustomRewardResponse.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwitchySharp.Api.Helix.ChannelPoints;
+/// <summary>
+/// Contains no data.
+/// </summary>
+public record DeleteCustomRewardResponse { }
