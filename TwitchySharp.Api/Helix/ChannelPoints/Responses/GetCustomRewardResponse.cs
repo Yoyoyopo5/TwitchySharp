b30a8b7db0195b487979c80ba75a07d82e8c0946
Yoyoyopo5 <yoyoyopo5@DESKTOP-IT4LOU9>
2024-10-13 19:18:06 -0500

    Add Get Custom Reward API and create shared model for rewards APIs

---
+++ TwitchySharp.Api/Helix/ChannelPoints/Responses/GetCustomRewardResponse.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwitchySharp.Api.Helix.ChannelPoints;
+/// <summary>
+/// Contains a list of custom rewards.
+/// </summary>
+public record GetCustomRewardResponse
+{
+    /// <summary>
+    /// A list of custom rewards. 
+    /// The list is in ascending order by id. 
+    /// If the broadcaster hasn’t created custom rewards, the list is empty.
+    /// </summary>
+    public required CustomChannelPointsReward[] Data { get; init; }
+}
