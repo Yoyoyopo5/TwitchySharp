c45218cb1a92a0b9a6cb14d496056d244c2831a7
Yoyoyopo5 <118655115+Yoyoyopo5@users.noreply.github.com>
2024-10-15 11:43:22 -0500

    Add Update Redemption Status API

---
+++ TwitchySharp.Api/Helix/ChannelPoints/Responses/UpdateRedemptionStatusResponse.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwitchySharp.Api.Helix.ChannelPoints;
+/// <summary>
+/// Contains information on updated reward redemptions.
+/// </summary>
+public record UpdateRedemptionStatusResponse
+{
+    /// <summary>
+    /// A list containing the single redemption that was updated.
+    /// </summary>
+    public required CustomRewardRedemption[] Data { get; init; }
+}
