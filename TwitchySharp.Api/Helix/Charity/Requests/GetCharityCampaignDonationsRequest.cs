2d4c308343472c00898069f34b9a029483e99d85
Yoyoyopo5 <118655115+Yoyoyopo5@users.noreply.github.com>
2024-10-19 19:59:40 -0500

    Add Get Charity Campaign Donations API

---
+++ TwitchySharp.Api/Helix/Charity/Requests/GetCharityCampaignDonationsRequest.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TwitchySharp.Helpers;
+using TwitchySharp.Api.Authorization;
+using TwitchySharp.Api.Models;
+
+namespace TwitchySharp.Api.Helix.Charity;
+/// <summary>
+/// Gets the list of donations that users have made to the broadcaster’s active charity campaign.
+/// <br/>
+/// See <see href="https://dev.twitch.tv/docs/api/reference/#get-charity-campaign-donations">get charity campaign donations</see> for more information.
+/// <br/>
+/// Requires a user access token that includes <see cref="Scope.ChannelReadCharity"/>.
+/// </summary>
+/// <param name="clientId">The client id of the application.</param>
+/// <param name="accessToken">A user access token that includes <see cref="Scope.ChannelReadCharity"/>.</param>
+/// <param name="broadcasterId">
+/// The user id of the broadcaster that you want to get charity donations for. 
+/// This must be the same user that created the <paramref name="accessToken"/>.
+/// </param>
+/// <param name="first">
+/// The maximum number of items to return per page in the response. 
+/// The minimum page size is 1 item per page and the maximum is 100. 
+/// The default is 20.
+/// </param>
+/// <param name="after">
+/// The cursor used to get the next page of results. 
+/// The <see cref="Pagination"/> property in the response contains the cursor’s value.
+/// </param>
+public class GetCharityCampaignDonationsRequest(string clientId, string accessToken, string broadcasterId, int? first = null, string? after = null)
+    : HelixApiRequest<GetCharityCampaignDonationsResponse>(
+        "/charity/donations" +
+        new HttpQueryParameters()
+            .Add("broadcaster_id", broadcasterId)
+            .Add("first", first?.ToString())
+            .Add("after", after),
+        clientId,
+        accessToken
+        );
---
+++ TwitchySharp.Api/Helix/Charity/Response/GetCharityCampaignDonationsResponse.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TwitchySharp.Api.Models;
+
+namespace TwitchySharp.Api.Helix.Charity;
+/// <summary>
+/// Contains a list of donations for a charity campaign.
+/// </summary>
+public record GetCharityCampaignDonationsResponse
+{
+    /// <summary>
+    /// A list that contains the donations that users have made to the broadcaster’s charity campaign. 
+    /// The list is empty if the broadcaster is not currently running a charity campaign; the donation information is not available after the campaign ends.
+    /// </summary>
+    public required CharityDonation[] Data { get; init; }
+    /// <summary>
+    /// An object that contains the information used to page through the list of results. 
+    /// The <see cref="Pagination.Cursor"/> property is <see langword="null"/> if there are no more pages left to page through.
+    /// </summary>
+    public required Pagination Pagination { get; init; }
+}
+
+/// <summary>
+/// Contains information about a specific donation to a charity campaign.
+/// </summary>
+public record CharityDonation
+{
+    /// <summary>
+    /// The unique id of the specific donation.
+    /// </summary>
+    public required string Id { get; init; }
+    /// <summary>
+    /// The id of the charity campaign this donation belongs to.
+    /// </summary>
+    public required string CampaignId { get; init; }
+    /// <summary>
+    /// The user id of the user that made the donation.
+    /// </summary>
+    public required string UserId { get; init; }
+    /// <summary>
+    /// The user login (username) of the user that made the donation.
+    /// </summary>
+    public required string UserLogin { get; init; }
+    /// <summary>
+    /// The display name of the user that made the donation.
+    /// </summary>
+    public required string UserName { get; init; }
+    /// <summary>
+    /// Contains information on the amount of money the user donated.
+    /// </summary>
+    public required CharityAmount Amount { get; init; }
+}
