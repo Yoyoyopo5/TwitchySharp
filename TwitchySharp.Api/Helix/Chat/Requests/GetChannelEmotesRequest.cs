a458392d4095b706924781a25413a62bc7cd3e5d
Yoyoyopo5 <yoyoyopo5@DESKTOP-IT4LOU9>
2024-10-29 16:22:51 -0500

    Add Get Channel Emotes API

---
+++ TwitchySharp.Api/Helix/Chat/Requests/GetChannelEmotesRequest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TwitchySharp.Helpers;
+
+namespace TwitchySharp.Api.Helix.Chat;
+/// <summary>
+/// Gets the broadcaster’s list of custom emotes. 
+/// Broadcasters create these custom emotes for users who subscribe to or follow the channel or cheer Bits in the channel’s chat window.
+/// For information about the custom emotes, see <see href="https://help.twitch.tv/s/article/subscriber-emote-guide">subscriber emotes</see>, <see href="https://help.twitch.tv/s/article/custom-bit-badges-guide?language=bg#slots">Bits tier emotes</see>, and <see href="https://blog.twitch.tv/en/2021/06/04/kicking-off-10-years-with-our-biggest-emote-update-ever/">follower emotes</see>.
+/// <br/>
+/// <b>NOTE:</b> With the exception of custom follower emotes, users may use custom emotes in any Twitch chat.
+/// </summary>
+/// <param name="clientId">The client id of the application.</param>
+/// <param name="accessToken">An app or user access token.</param>
+/// <param name="broadcasterId">The user id of the broadcaster whose emotes you want to get.</param>
+public class GetChannelEmotesRequest(string clientId, string accessToken, string broadcasterId)
+    : HelixApiRequest<GetChannelEmotesResponse>(
+        "/chat/emotes" +
+        new HttpQueryParameters()
+            .Add("broadcaster_id", broadcasterId),
+        clientId,
+        accessToken
+        );
---
+++ TwitchySharp.Api/Helix/Chat/Responses/GetChannelEmotesResponse.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace TwitchySharp.Api.Helix.Chat;
+/// <summary>
+/// Contains a list of custom emotes.
+/// </summary>
+public record GetChannelEmotesResponse
+{
+    /// <summary>
+    /// The list of emotes that the specified broadcaster created. 
+    /// If the broadcaster hasn't created custom emotes, the list is empty.
+    /// </summary>
+    public required ChannelEmote[] Data { get; init; }
+    /// <summary>
+    /// A templated URL. 
+    /// Use the values from the <see cref="ChannelEmote.Id"/>, <see cref="ChannelEmote.Format"/>, <see cref="ChannelEmote.Scale"/>, and <see cref="ChannelEmote.ThemeMode"/> properties to replace the like-named placeholder strings in the templated URL to create a CDN (content delivery network) URL that you use to fetch the emote. 
+    /// For information about what the template looks like and how to use it to fetch emotes, see <see href="https://dev.twitch.tv/docs/chat/send-receive-messages/#cdn-template">Emote CDN URL format</see>. 
+    /// You should use this template instead of using the URLs in the images object.
+    /// </summary>
+    public required string Template { get; init; }
+}
+
+public record ChannelEmote
+{
+    /// <summary>
+    /// An ID that identifies this emote.
+    /// </summary>
+    public required string Id { get; init; }
+    /// <summary>
+    /// The name of the emote. 
+    /// This is the name that viewers type in the chat window to get the emote to appear.
+    /// </summary>
+    public required string Name { get; init; }
+    /// <summary>
+    /// The image URLs for the emote. 
+    /// These image URLs always provide a static, non-animated emote image with a light background.
+    /// <b>NOTE:</b> You should use the templated URL in the template field to fetch the image instead of using these URLs.
+    /// </summary>
+    public required EmoteImage Images { get; init; }
+    /// <summary>
+    /// The subscriber tier at which the emote is unlocked. 
+    /// This field contains the tier information only if <see cref="EmoteType"/> is set to <see cref="EmoteType.Subscriptions"/>, otherwise, it's an empty string.
+    /// </summary>
+    public required string Tier { get; init; }
+    /// <summary>
+    /// The type of emote.
+    /// </summary>
+    [JsonConverter(typeof(JsonStringEnumConverter))]
+    public required EmoteType EmoteType { get; init; }
+    /// <summary>
+    /// An ID that identifies the emote set that the emote belongs to.
+    /// </summary>
+    public required string EmoteSetId { get; init; }
+    /// <summary>
+    /// The formats that the emote is available in. 
+    /// For example, if the emote is available only as a static PNG, the array contains only <see cref="EmoteFormat.Static"/>. 
+    /// But if the emote is available as a static PNG and an animated GIF, the array contains <see cref="EmoteFormat.Static"/> and <see cref="EmoteFormat.Animated"/>.
+    /// </summary>
+    public required EmoteFormat[] Format { get; init; } // It would be cool to write a converter that can convert an array of strings into a Flags enum instead of an enum array.
+    /// <summary>
+    /// The sizes that the emote is available in. 
+    /// For example, if the emote is available in small and medium sizes, the array contains 1.0 and 2.0. 
+    /// Possible sizes are: 
+    /// 1.0: A small version (28px x 28px) is available.
+    /// 2.0: A medium version (56px x 56px) is available.
+    /// 3.0: A large version (112px x 112px) is available.
+    /// </summary>
+    public required string[] Scale { get; init; }
+    /// <summary>
+    /// The background themes that the emote is available in.
+    /// </summary>
+    public required EmoteTheme[] ThemeMode { get; init; }
+}
+
+/// <summary>
+/// Contains URLs for an emote's images.
+/// Note that these are always static emote images with a light background theme.
+/// </summary>
+public record EmoteImage
+{
+    /// <summary>
+    /// A URL to the small version (28px x 28px) of the emote.
+    /// </summary>
+    public required string Url1x { get; init; }
+    /// <summary>
+    /// A URL to the medium version (56px x 56px) of the emote.
+    /// </summary>
+    public required string Url2x { get; init; }
+    /// <summary>
+    /// A URL to the large version (112px x 112px) of the emote.
+    /// </summary>
+    public required string Url4x { get; init; }
+}
+
+public enum EmoteType
+{
+    /// <summary>
+    /// A custom Bits tier emote.
+    /// </summary>
+    Bitstier,
+    /// <summary>
+    /// A custom follower emote.
+    /// </summary>
+    Follower,
+    /// <summary>
+    /// A custom subscriber emote.
+    /// </summary>
+    Subscriptions
+}
+
+[JsonConverter(typeof(JsonStringEnumConverter))] // So we can use array of strings to enum array
+public enum EmoteFormat
+{
+    Animated,
+    Static
+}
+
+[JsonConverter(typeof(JsonStringEnumConverter))] // So we can use array of strings to enum array
+public enum EmoteTheme
+{
+    Dark,
+    Light
+}
