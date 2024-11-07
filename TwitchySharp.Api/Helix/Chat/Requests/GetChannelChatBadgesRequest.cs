d024935beca23445e5c89b054e92c01b1df0ae35
Yoyoyopo5 <118655115+Yoyoyopo5@users.noreply.github.com>
2024-11-07 17:26:27 -0600

    Add link to summary

--- TwitchySharp.Api/Helix/Chat/Requests/GetChannelChatBadgesRequest.cs
+++ TwitchySharp.Api/Helix/Chat/Requests/GetChannelChatBadgesRequest.cs
@@ -10,6 +10,8 @@ namespace TwitchySharp.Api.Helix.Chat;
 /// Gets the broadcaster’s list of custom chat badges. 
 /// The list is empty if the broadcaster hasn’t created custom chat badges. 
 /// For information about custom badges, see <see href="https://help.twitch.tv/s/article/subscriber-badge-guide">subscriber badges</see> and <see href="https://help.twitch.tv/s/article/custom-bit-badges-guide">Bits badges</see>.
+/// <br/>
+/// See <see href="https://dev.twitch.tv/docs/api/reference/#get-channel-chat-badges">get channel chat badges</see> for more information.
 /// </summary>
 /// <param name="clientId">The client id of the application.</param>
 /// <param name="accessToken">An app or user access token.</param>
