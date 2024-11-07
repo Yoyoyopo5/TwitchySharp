ce7598abd27cc7a230888bfc93195268330f82c1
Yoyoyopo5 <118655115+Yoyoyopo5@users.noreply.github.com>
2024-11-07 17:26:40 -0600

    Add Get Global Chat Badges API

---
+++ TwitchySharp.Api/Helix/Chat/Requests/GetGlobalChatBadgesRequest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwitchySharp.Api.Helix.Chat;
+/// <summary>
+/// Gets Twitch’s list of chat badges, which users may use in any channel’s chat room. 
+/// For information about chat badges, see <see href="https://help.twitch.tv/s/article/twitch-chat-badges-guide">Twitch Chat Badges Guide</see>.
+/// <br/>
+/// See <see href="https://dev.twitch.tv/docs/api/reference/#get-global-chat-badges">get global chat badges</see> for more information.
+/// </summary>
+/// <param name="clientId">The client id of the application.</param>
+/// <param name="accessToken">An app or user access token.</param>
+public class GetGlobalChatBadgesRequest(string clientId, string accessToken)
+    : HelixApiRequest<GetGlobalChatBadgesResponse>(
+        "/chat/badges/global",
+        clientId,
+        accessToken
+        );
---
+++ TwitchySharp.Api/Helix/Chat/Responses/GetGlobalChatBadgesResponse.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwitchySharp.Api.Helix.Chat;
+/// <summary>
+/// Contains a list of global Twitch chat badges.
+/// </summary>
+public record GetGlobalChatBadgesResponse
+{
+    /// <summary>
+    /// The list of chat badges. 
+    /// The list is sorted in ascending order by <see cref="ChatBadgeSet.SetId"/>.
+    /// </summary>
+    public required ChatBadgeSet[] Data { get; init; }
+}
