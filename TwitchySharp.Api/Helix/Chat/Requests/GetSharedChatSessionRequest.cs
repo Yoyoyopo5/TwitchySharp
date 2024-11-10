031c02b4eb9b53b5715ee659d9f494dffe628cda
Yoyoyopo5 <yoyoyopo5@DESKTOP-IT4LOU9>
2024-11-10 02:56:49 -0600

    Add Get Shared Chat Session API

---
+++ TwitchySharp.Api/Helix/Chat/Requests/GetSharedChatSessionRequest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TwitchySharp.Helpers;
+
+namespace TwitchySharp.Api.Helix.Chat;
+/// <summary>
+/// Retrieves the active shared chat session for a channel.
+/// </summary>
+/// <param name="clientId">The client id of the application.</param>
+/// <param name="accessToken">An app or user access token.</param>
+/// <param name="broadcasterId">The user id of the broadcaster whose shared chat you want to get.</param>
+public class GetSharedChatSessionRequest(string clientId, string accessToken, string broadcasterId)
+    : HelixApiRequest<GetSharedChatSessionResponse>(
+        "/shared_chat/session" +
+        new HttpQueryParameters()
+            .Add("broadcaster_id", broadcasterId),
+        clientId,
+        accessToken
+        );
---
+++ TwitchySharp.Api/Helix/Chat/Responses/GetSharedChatSessionResponse.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwitchySharp.Api.Helix.Chat;
+/// <summary>
+/// Contains a list of shared chat sessions.
+/// </summary>
+public record GetSharedChatSessionResponse
+{
+    /// <summary>
+    /// A list containing the single shared chat session.
+    /// </summary>
+    public required SharedChatSession[] Data { get; init; }
+}
+
+/// <summary>
+/// Contains information about a shared chat session.
+/// </summary>
+public record SharedChatSession
+{
+    /// <summary>
+    /// The unique id for the shared chat session.
+    /// </summary>
+    public required string SessionId { get; init; }
+    /// <summary>
+    /// The user id of the host broadcaster.
+    /// </summary>
+    public required string HostBroadcasterId { get; init; }
+    /// <summary>
+    /// The list of participant broadcasters in the shared chat session.
+    /// </summary>
+    public required SharedChatParticipant[] Participants { get; init; }
+    /// <summary>
+    /// The date and time when the session was created.
+    /// </summary>
+    public required DateTimeOffset CreatedAt { get; init; }
+    /// <summary>
+    /// The date and time when the session was last updated.
+    /// </summary>
+    public required DateTimeOffset UpdatedAt { get; init; }
+}
+
+/// <summary>
+/// Contains information about a broadcaster in a shared chat session.
+/// </summary>
+public record SharedChatParticipant
+{
+    /// <summary>
+    /// The user id of the participant broadcaster.
+    /// </summary>
+    public required string BroadcasterId { get; init; }
+}
