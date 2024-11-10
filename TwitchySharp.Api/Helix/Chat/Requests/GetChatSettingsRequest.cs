ca58ba6b3f2863d722b792e02f4723b2ddf4a5e8
Yoyoyopo5 <yoyoyopo5@DESKTOP-IT4LOU9>
2024-11-10 02:47:16 -0600

    Add Get Chat Settings API

---
+++ TwitchySharp.Api/Helix/Chat/Requests/GetChatSettingsRequest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TwitchySharp.Helpers;
+
+namespace TwitchySharp.Api.Helix.Chat.Requests;
+/// <summary>
+/// Gets the broadcaster’s chat settings.
+/// For an overview of chat settings, see <see href="https://help.twitch.tv/s/article/chat-commands#AllMods">Chat Commands for Broadcasters and Moderators</see> and <see href="https://help.twitch.tv/s/article/setting-up-moderation-for-your-twitch-channel#modpreferences">Moderator Preferences</see>.
+/// </summary>
+/// <param name="clientId">The client id of the application.</param>
+/// <param name="accessToken">An app or user access token.</param>
+/// <param name="broadcasterId">The user id of the broadcaster whose chat settings you want to get.</param>
+/// <param name="moderatorId">
+/// The user id of the broadcaster or one of the broadcaster's moderators.
+/// <br/>
+/// This parameter is only required if you want to include the <see cref="ChatSettings.NonModeratorChatDelay"/> and <see cref="ChatSettings.NonModeratorChatDelayDuration"/> in the response.
+/// <br/>
+/// If specified, this must be the same user that created the <paramref name="accessToken"/>.
+/// </param>
+public class GetChatSettingsRequest(string clientId, string accessToken, string broadcasterId, string? moderatorId = null)
+    : HelixApiRequest<GetChatSettingsResponse>(
+        "/chat/settings" +
+        new HttpQueryParameters()
+            .Add("broadcaster_id", broadcasterId)
+            .Add("moderator_id", moderatorId),
+        clientId,
+        accessToken
+        );
---
+++ TwitchySharp.Api/Helix/Chat/Responses/GetChatSettingsResponse.cs
@@ -0,0 +1,89 @@
+﻿using OneOf.Types;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TwitchySharp.Api.Authorization;
+
+namespace TwitchySharp.Api.Helix.Chat;
+/// <summary>
+/// Contains a list of chat settings.
+/// </summary>
+public record GetChatSettingsResponse
+{
+    /// <summary>
+    /// The list of chat settings. 
+    /// The list contains a single object with all the settings.
+    /// </summary>
+    public required ChatSettings[] Data { get; init; }
+}
+
+/// <summary>
+/// Contains chat setttings.
+/// </summary>
+public record ChatSettings
+{
+    /// <summary>
+    /// The user id of the broadcaster who has the chat settings.
+    /// </summary>
+    public required string BroadcasterId { get; init; }
+    /// <summary>
+    /// Determines whether chat messages must contain only emotes. 
+    /// Is <see langword="true"/> if chat messages may contain only emotes; otherwise, <see langword="false"/>.
+    /// </summary>
+    public required bool EmoteMode { get; init; }
+    /// <summary>
+    /// Determines whether the broadcaster restricts the chat room to followers only. 
+    /// Is <see langword="true"/> if the broadcaster restricts the chat room to followers only; otherwise, <see langword="false"/>.
+    /// See <see cref="FollowerModeDuration"/> for how long users must follow the broadcaster before being able to participate in the chat room.
+    /// </summary>
+    public required bool FollowerMode { get; init; }
+    /// <summary>
+    /// The length of time, in <b>minutes</b>, that users must follow the broadcaster before being able to participate in the chat room. 
+    /// Is <see langword="null"/> if <see cref="FollowerMode"/> is <see langword="false"/>.
+    /// </summary>
+    public int? FollowerModeDuration { get; init; }
+    /// <summary>
+    /// The moderator’s user id that was used in the request. 
+    /// This property is <see langword="null"/> if the request specifies a user access token that includes <see cref="Scope.ModeratorReadChatSettings"/>.
+    /// </summary>
+    public string? ModeratorId { get; init; }
+    /// <summary>
+    /// Determines whether the broadcaster adds a short delay before chat messages appear in the chat room. 
+    /// This gives chat moderators and bots a chance to remove them before viewers can see the message. 
+    /// See <see cref="NonModeratorChatDelayDuration"/> for the length of the delay. Is <see langword="true"/> if the broadcaster applies a delay; otherwise, <see langword="false"/>. 
+    /// <br/>
+    /// This property is <see langword="null"/> unless the request specifies a user access token that includes <see cref="Scope.ModeratorReadChatSettings"/> and the user in the moderator_id query parameter is one of the broadcaster’s moderators.
+    /// </summary>
+    public bool? NonModeratorChatDelay { get; init; }
+    /// <summary>
+    /// The amount of time, in <b>seconds</b>, that messages are delayed before appearing in chat. 
+    /// Is <see langword="null"/> if <see cref="NonModeratorChatDelay"/> is <see langword="false"/>.
+    /// <br/>
+    /// This property is <see langword="null"/> unless the request specifies a user access token that includes the <see cref="Scope.ModeratorReadChatSettings"/> scope and the user in the moderator_id query parameter is one of the broadcaster’s moderators.
+    /// </summary>
+    public int? NonModeratorChatDelayDuration { get; init; }
+    /// <summary>
+    /// Determines whether the broadcaster limits how often users in the chat room are allowed to send messages.
+    /// Is <see langword="true"/> if the broadcaster applies a delay; otherwise, <see langword="false"/>. 
+    /// See <see cref="SlowModeWaitTime"/> for the delay.
+    /// </summary>
+    public required bool SlowMode { get; init; }
+    /// <summary>
+    /// The amount of time, in <b>seconds</b>, that users must wait between sending messages. 
+    /// Is <see langword="null"/> if <see cref="SlowMode"/> is <see langword="false"/>.
+    /// </summary>
+    public int? SlowModeWaitTime { get; init; }
+    /// <summary>
+    /// Determines whether only users that subscribe to the broadcaster’s channel may talk in the chat room.
+    /// Is <see langword="true"/> if the broadcaster restricts the chat room to subscribers only; otherwise, <see langword="false"/>.
+    /// </summary>
+    public required bool SubscriberMode { get; init; }
+    /// <summary>
+    /// Determines whether the broadcaster requires users to post only unique messages in the chat room.
+    /// Is <see langword="true"/> if the broadcaster requires unique messages only; otherwise, <see langword="false"/>.
+    /// </summary>
+    public required bool UniqueChatMode { get; init; }
+}
