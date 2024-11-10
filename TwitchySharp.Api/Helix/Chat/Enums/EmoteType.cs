d3d97604461009faa4438e44c021042209c12196
Yoyoyopo5 <118655115+Yoyoyopo5@users.noreply.github.com>
2024-11-10 04:24:35 -0600

    Add new emote types

--- TwitchySharp.Api/Helix/Chat/Enums/EmoteType.cs
+++ TwitchySharp.Api/Helix/Chat/Enums/EmoteType.cs
@@ -5,6 +5,10 @@
 public enum EmoteType
 {
     /// <summary>
+    /// No emote type was assigned to this emote.
+    /// </summary>
+    None,
+    /// <summary>
     /// A custom Bits tier emote.
     /// </summary>
     Bitstier,
@@ -15,5 +19,45 @@ public enum EmoteType
     /// <summary>
     /// A custom subscriber emote.
     /// </summary>
-    Subscriptions
+    Subscriptions,
+    /// <summary>
+    /// An emote granted by using channel points.
+    /// </summary>
+    ChannelPoints,
+    /// <summary>
+    /// An emote granted to the user through a special event.
+    /// </summary>
+    Rewards,
+    /// <summary>
+    /// An emote granted for participation in a Hype Train.
+    /// </summary>
+    HypeTrain,
+    /// <summary>
+    /// An emote granted for linking an Amazon Prime account.
+    /// </summary>
+    Prime,
+    /// <summary>
+    /// An emote granted for having Twitch Turbo.
+    /// </summary>
+    Turbo,
+    /// <summary>
+    /// Emoticons supported by Twitch.
+    /// </summary>
+    Smilies,
+    /// <summary>
+    /// An emote accessible by everyone.
+    /// </summary>
+    Globals,
+    /// <summary>
+    /// Emotes related to Overwatch League 2019.
+    /// </summary>
+    Owl2019,
+    /// <summary>
+    /// Emotes granted by enabling two-factor authentication on an account.
+    /// </summary>
+    TwoFactor,
+    /// <summary>
+    /// Emotes that were granted for only a limited time.
+    /// </summary>
+    LimitedTime
 }
