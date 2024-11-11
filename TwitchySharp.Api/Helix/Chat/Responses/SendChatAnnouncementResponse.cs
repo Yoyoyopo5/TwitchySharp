a85fcab633f8a4f1ff6ff1b513b5a917f6aa0823
Yoyoyopo5 <118655115+Yoyoyopo5@users.noreply.github.com>
2024-11-11 08:40:24 -0600

    Add Send Chat Announcement API

---
+++ TwitchySharp.Api/Helix/Chat/Responses/SendChatAnnouncementResponse.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwitchySharp.Api.Helix.Chat;
+/// <summary>
+/// Empty response.
+/// </summary>
+public record SendChatAnnouncementResponse { }
