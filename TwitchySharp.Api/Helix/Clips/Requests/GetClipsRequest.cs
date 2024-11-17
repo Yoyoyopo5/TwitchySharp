b32135ab8dc0ddf73dd340746c04ff4cdda5decf
Yoyoyopo5 <118655115+Yoyoyopo5@users.noreply.github.com>
2024-11-17 09:14:29 -0600

    Add Get Clips API

---
+++ TwitchySharp.Api/Helix/Clips/Requests/GetClipsRequest.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TwitchySharp.Helpers;
+
+namespace TwitchySharp.Api.Helix.Clips;
+/// <summary>
+/// Gets one or more video clips that were captured from streams. For information about clips, see <see href="https://help.twitch.tv/s/article/how-to-use-clips">How to use clips</see>.
+/// <br/>
+/// See <see href="https://dev.twitch.tv/docs/api/reference/#get-clips">get clips</see> for more information.
+/// </summary>
+/// <remarks>
+/// The <paramref name="id"/>, <paramref name="gameId"/>, and <paramref name="broadcasterId"/> parameters are mutually exclusive.
+/// <br/>
+/// Requires an app or user access token.
+/// </remarks>
+/// <param name="clientId">The client id of the application.</param>
+/// <param name="accessToken">An app or user access token.</param>
+/// <param name="broadcasterId"> 	
+/// The user id of the broadcaster whose video clips you want to get. 
+/// Use this parameter to get clips that were captured from the broadcaster’s streams.
+/// This parameter is mutually exclusive with <paramref name="gameId"/> and <paramref name="id"/>.
+/// </param>
+/// <param name="gameId">
+/// The game id of the game whose clips you want to get. 
+/// Use this parameter to get clips that were captured from streams that were playing this game.
+/// This parameter is mutually exclusive with <paramref name="broadcasterId"/> and <paramref name="id"/>.
+/// </param>
+/// <param name="id">
+/// The clip id of the clip to get. 
+/// You may specify a maximum of 100 IDs. 
+/// The API ignores duplicate IDs and IDs that aren’t found.
+/// This parameter is mutually exclusive with <paramref name="broadcasterId"/> and <paramref name="gameId"/>.
+/// </param>
+/// <param name="startedAt">
+/// The start date used to filter clips. 
+/// The API returns only clips within the start and end date window.
+/// </param>
+/// <param name="endedAt">
+/// The end date used to filter clips. 
+/// If not specified, the time window is the start date plus one week.
+/// </param>
+/// <param name="first">
+/// The maximum number of clips to return per page in the response. 
+/// The minimum page size is 1 clip per page and the maximum is 100. 
+/// The default is 20.
+/// </param>
+/// <param name="before">
+/// The cursor used to get the previous page of results. 
+/// The <see cref="GetClipsResponse.Pagination"/> property in the response contains the cursor’s value. 
+/// </param>
+/// <param name="after">
+/// The cursor used to get the next page of results. 
+/// The <see cref="GetClipsResponse.Pagination"/> property in the response contains the cursor’s value.
+/// </param>
+/// <param name="isFeatured">
+/// Determines whether the response includes featured clips. 
+/// If <see langword="true"/>, returns only clips that are featured. 
+/// If <see langword="false"/>, returns only clips that aren’t featured. 
+/// All clips are returned if this parameter is <see langword="null"/>.
+/// </param>
+public class GetClipsRequest(
+    string clientId, 
+    string accessToken, 
+    string? broadcasterId = null,
+    string? gameId = null,
+    string? id = null,
+    DateTimeOffset? startedAt = null,
+    DateTimeOffset? endedAt = null,
+    int? first = null,
+    string? before = null,
+    string? after = null,
+    bool? isFeatured = null
+    )
+    : HelixApiRequest<GetClipsResponse>(
+        "/clips" +
+        new HttpQueryParameters()
+            .Add("id", id)
+            .Add("broadcaster_id", id is null ? broadcasterId : null)
+            .Add("game_id", id is null && broadcasterId is null ? gameId : null) // Should we remove the mutual exclusivity guard and allow the API to return the error if more than one is set? This may be unexpected behavior.
+            .Add("started_at", startedAt?.UtcDateTime.Date.ToString("yyyy-MM-dd'T'HH:mm:ssZ"))
+            .Add("ended_at", endedAt?.UtcDateTime.Date.ToString("yyyy-MM-dd'T'HH:mm:ssZ"))
+            .Add("first", first?.ToString())
+            .Add("before", before)
+            .Add("after", after)
+            .Add("is_featured", isFeatured?.ToString()),
+        clientId,
+        accessToken
+        );
---
+++ TwitchySharp.Api/Helix/Clips/Responses/GetClipsResponse.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TwitchySharp.Api.Models;
+
+namespace TwitchySharp.Api.Helix.Clips;
+/// <summary>
+/// Contains a list of Twitch clips.
+/// </summary>
+public record GetClipsResponse
+{
+    /// <summary>
+    /// The list of video clips.
+    /// For clips returned by GameId or BroadcasterId, the list is in descending order by view count. 
+    /// For lists returned by id, the list is in the same order as the input ids.
+    /// </summary>
+    public required TwitchClip[] Data { get; init; }
+    /// <summary>
+    /// The information used to page through the list of results. 
+    /// The <see cref="Pagination.Cursor"/> is <see langword="null"/> if there are no more pages left to page through.
+    /// </summary>
+    public required Pagination Pagination { get; init; }
+}
+
+/// <summary>
+/// Contains information about a specific Twitch clip.
+/// </summary>
+public record TwitchClip
+{
+    /// <summary>
+    /// An id that uniquely identifies the clip.
+    /// </summary>
+    public required string Id { get; init; }
+    /// <summary>
+    /// A URL to the clip.
+    /// </summary>
+    public required string Url { get; init; }
+    /// <summary>
+    /// A URL that you can use in an iframe to embed the clip (see <see href="https://dev.twitch.tv/docs/embed/video-and-clips/">Embedding Video and Clips</see>).
+    /// </summary>
+    public required string EmbedUrl { get; init; }
+    /// <summary>
+    /// The user id of the broadcaster that the video was clipped from.
+    /// </summary>
+    public required string BroadcasterId { get; init; }
+    /// <summary>
+    /// The broadcaster’s display name.
+    /// </summary>
+    public required string BroadcasterName { get; init; }
+    /// <summary>
+    /// The user id of the user who created the clip.
+    /// </summary>
+    public required string CreatorId { get; init; }
+    /// <summary>
+    /// The creator's display name.
+    /// </summary>
+    public required string CreatorName { get; init; }
+    /// <summary>
+    /// An ID that identifies the video that the clip came from. 
+    /// This is an empty string if the video is not available.
+    /// </summary>
+    public required string VideoId { get; init; }
+    /// <summary>
+    /// The ID of the game that was being played when the clip was created.
+    /// </summary>
+    public required string GameId { get; init; }
+    /// <summary>
+    /// The ISO 639-1 two-letter language code that the broadcaster broadcasts in. 
+    /// For example, en for English. 
+    /// The value is other if the broadcaster uses a language that Twitch doesn’t support.
+    /// </summary>
+    public required string Language { get; init; }
+    /// <summary>
+    /// The title of the clip.
+    /// </summary>
+    public required string Title { get; init; }
+    /// <summary>
+    /// The number of times the clip has been viewed.
+    /// </summary>
+    public required int ViewCount { get; init; }
+    /// <summary>
+    /// The date and time of when the clip was created.
+    /// </summary>
+    public DateTimeOffset CreatedAt { get; init; }
+    /// <summary>
+    /// A URL to a thumbnail image of the clip.
+    /// </summary>
+    public required string ThumbnailUrl { get; init; }
+    /// <summary>
+    /// The length of the clip, in <b>seconds</b>. Precision is 0.1.
+    /// </summary>
+    public required double Duration { get; init; }
+    /// <summary>
+    /// The zero-based offset, in <b>seconds</b>, to where the clip starts in the video (VOD). 
+    /// Is <see langword="null"/> if the video is not available or hasn’t been created yet from the live stream.
+    /// <br/>
+    /// Note that there’s a delay between when a clip is created during a broadcast and when the offset is set. 
+    /// During the delay period, this property is <see langword="null"/>. The delay is indeterminant but is typically minutes long.
+    /// </summary>
+    public int? VodOffset { get; init; }
+    /// <summary>
+    /// Indicates if the clip is featured or not.
+    /// </summary>
+    public bool IsFeatured { get; init; }
+}
