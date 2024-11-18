2858ff881c3203acaf886613334884ca2ae2731e
Yoyoyopo5 <yoyoyopo5@DESKTOP-IT4LOU9>
2024-11-18 07:01:12 -0600

    Add Get Conduits API

---
+++ TwitchySharp.Api/Helix/Conduits/Requests/GetConduitsRequest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwitchySharp.Api.Helix.Conduits;
+/// <summary>
+/// Gets the conduits for a client ID.
+/// <br/>
+/// See <see href="https://dev.twitch.tv/docs/api/reference/#get-conduits">get conduits</see> for more information.
+/// </summary>
+/// <param name="clientId">The client id of the application. This will be the application to get conduits for.</param>
+/// <param name="accessToken">An app access token.</param>
+public class GetConduitsRequest(string clientId, string accessToken)
+    : HelixApiRequest<GetConduitsResponse>(
+        "/eventsub/conduits",
+        clientId,
+        accessToken
+        );
---
+++ TwitchySharp.Api/Helix/Conduits/Responses/GetConduitsResponse.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwitchySharp.Api.Helix.Conduits;
+/// <summary>
+/// Contains a list of conduits.
+/// </summary>
+public record GetConduitsResponse
+{
+    /// <summary>
+    /// The list of conduits.
+    /// </summary>
+    public required Conduit[] Data { get; init; }
+}
+
+/// <summary>
+/// Contains data about a specific conduit.
+/// </summary>
+public record Conduit
+{
+    /// <summary>
+    /// The id of the conduit.
+    /// </summary>
+    public required string Id { get; init; }
+    /// <summary>
+    /// The number of shards associated with this conduit.
+    /// </summary>
+    public required int ShardCount { get; init; }
+}
