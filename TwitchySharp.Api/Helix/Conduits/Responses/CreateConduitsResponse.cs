1640f203f01c8d8ccc2bf7326616fe235140bb5c
Yoyoyopo5 <118655115+Yoyoyopo5@users.noreply.github.com>
2024-11-18 09:50:07 -0600

    Add Create Conduits API

---
+++ TwitchySharp.Api/Helix/Conduits/Responses/CreateConduitsResponse.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwitchySharp.Api.Helix.Conduits;
+/// <summary>
+/// Contains a list of created conduits.
+/// </summary>
+public record CreateConduitsResponse
+{
+    /// <summary>
+    /// The list of created conduits.
+    /// </summary>
+    public required Conduit[] Data { get; init; }
+}
