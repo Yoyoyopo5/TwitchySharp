e9bc34a2a62cc9d2b63b4118d6811794ebff0f4d
Yoyoyopo5 <yoyoyopo5@DESKTOP-IT4LOU9>
2024-11-19 03:35:28 -0600

    Add Update Conduit API

---
+++ TwitchySharp.Api/Helix/Conduits/Responses/UpdateConduitResponse.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwitchySharp.Api.Helix.Conduits;
+/// <summary>
+/// Contains a list of updated conduits.
+/// </summary>
+public record UpdateConduitResponse
+{
+    /// <summary>
+    /// A list of updated conduits.
+    /// </summary>
+    public required Conduit[] Data { get; init; }
+}
