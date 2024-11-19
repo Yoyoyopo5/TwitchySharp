2a53e499c85e622f4bc6bf7d895c22ff19dbd856
Yoyoyopo5 <yoyoyopo5@DESKTOP-IT4LOU9>
2024-11-19 11:34:18 -0600

    Add Get Extension Configuration Segment API

---
+++ TwitchySharp.Api/Helix/Extensions/Responses/GetExtensionConfigurationSegmentResponse.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace TwitchySharp.Api.Helix.Extensions;
+/// <summary>
+/// Contains a list of extension configuration segment data.
+/// </summary>
+public record GetExtensionConfigurationSegmentResponse
+{
+    /// <summary>
+    /// The list of requested configuration segments. 
+    /// The list is returned in the same order that you specified the list of segments in the request.
+    /// </summary>
+    public required ExtensionConfigurationSegment[] Data { get; init; }
+}
+
+/// <summary>
+/// Contains information about a specific extension configuration segment.
+/// </summary>
+public record ExtensionConfigurationSegment
+{
+    /// <summary>
+    /// The type of segment.
+    /// </summary>
+    [JsonConverter(typeof(JsonStringEnumConverter<ExtensionConfigurationSegmentType>))]
+    public required ExtensionConfigurationSegmentType Segment { get; init; }
+    /// <summary>
+    /// The user id of the broadcaster that installed the extension. 
+    /// This is <see langword="null"/> if <see cref="Segment"/> is set to <see cref="ExtensionConfigurationSegmentType.Global"/>.
+    /// </summary>
+    public string? BroadcasterId { get; init; }
+    /// <summary>
+    /// The contents of the segment. 
+    /// This string may be a plain-text string or a string-encoded JSON object.
+    /// </summary>
+    public required string Content { get; init; }
+    /// <summary>
+    /// The version number that identifies this definition of the segment’s data.
+    /// </summary>
+    public required string Version { get; init; }
+}
+
+/// <summary>
+/// Represents types of extension configuration segments.
+/// </summary>
+public enum ExtensionConfigurationSegmentType
+{
+    Broadcaster,
+    Developer,
+    Global
+}
