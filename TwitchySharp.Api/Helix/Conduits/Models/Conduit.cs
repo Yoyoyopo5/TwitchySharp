9bd412f61fcfa18308560b565dc1ee9aaf651b5e
Yoyoyopo5 <yoyoyopo5@DESKTOP-IT4LOU9>
2024-11-19 03:57:31 -0600

    Move model

---
+++ TwitchySharp.Api/Helix/Conduits/Models/Conduit.cs
@@ -0,0 +1,16 @@
+﻿namespace TwitchySharp.Api.Helix.Conduits.Models;
+
+/// <summary>
+/// Contains data about a specific conduit.
+/// </summary>
+public record Conduit
+{
+    /// <summary>
+    /// The id of the conduit.
+    /// </summary>
+    public required string Id { get; init; }
+    /// <summary>
+    /// The number of shards associated with this conduit.
+    /// </summary>
+    public required int ShardCount { get; init; }
+}
