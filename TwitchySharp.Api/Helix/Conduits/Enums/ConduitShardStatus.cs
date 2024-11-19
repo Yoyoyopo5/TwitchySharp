e73527f035584bfa9c8152b7b8db7743e07d35b6
Yoyoyopo5 <118655115+Yoyoyopo5@users.noreply.github.com>
2024-11-19 04:10:59 -0600

    Add Get Conduit Shards API

---
+++ TwitchySharp.Api/Helix/Conduits/Enums/ConduitShardStatus.cs
@@ -0,0 +1,53 @@
+﻿namespace TwitchySharp.Api.Helix.Conduits;
+
+/// <summary>
+/// Represents the status of a conduit shard.
+/// </summary>
+public enum ConduitShardStatus
+{
+    /// <summary>
+    /// The shard is enabled and working.
+    /// </summary>
+    Enabled,
+    /// <summary>
+    /// The shard is pending verification of the specified callback URL.
+    /// </summary>
+    WebhookCallbackVerificationPending,
+    /// <summary>
+    /// The specified callback URL failed verification.
+    /// </summary>
+    WebhookCallbackVerificationFailed,
+    /// <summary>
+    /// The notification delivery failure rate was too high.
+    /// </summary>
+    NotificationFailuresExceeded,
+    /// <summary>
+    /// The client closed the connection.
+    /// </summary>
+    WebsocketDisconnected,
+    /// <summary>
+    /// The client failed to respond to a ping message.
+    /// </summary>
+    WebsocketFailedPingPong,
+    /// <summary>
+    /// The client sent a non-pong message. 
+    /// Clients may only send pong messages (and only in response to a ping message).
+    /// </summary>
+    WebsocketReceivedInboundTraffic,
+    /// <summary>
+    /// The Twitch WebSocket server experienced an unexpected error.
+    /// </summary>
+    WebsocketInternalError,
+    /// <summary>
+    /// The Twitch WebSocket server timed out writing the message to the client.
+    /// </summary>
+    WebsocketNetworkTimeout,
+    /// <summary>
+    /// The Twitch WebSocket server experienced a network error writing the message to the client.
+    /// </summary>
+    WebsocketNetworkError,
+    /// <summary>
+    /// The client failed to reconnect to the Twitch WebSocket server within the required time after a Reconnect Message.
+    /// </summary>
+    WebsocketFailedToReconnect
+}
---
+++ TwitchySharp.Api/Helix/Conduits/Requests/GetConduitShardsRequest.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TwitchySharp.Helpers;
+using TwitchySharp.Api.Models;
+using Minerals.StringCases;
+
+namespace TwitchySharp.Api.Helix.Conduits;
+/// <summary>
+/// Gets a lists of all shards for a conduit.
+/// <br/>
+/// See <see href="https://dev.twitch.tv/docs/api/reference/#get-conduit-shards">get conduit shards</see> for more information.
+/// </summary>
+/// <remarks>
+/// Requires an app access token.
+/// </remarks>
+/// <param name="clientId">The client id of the application.</param>
+/// <param name="accessToken">An app access token.</param>
+/// <param name="conduitId">The conduit id of the conduit you want to get shards for.</param>
+/// <param name="status">Status to filter returned shards by.</param>
+/// <param name="after">
+/// The cursor used to get the next page of results. 
+/// The <see cref="Pagination"/> in the response contains the cursor’s value.
+/// </param>
+public class GetConduitShardsRequest(
+    string clientId, 
+    string accessToken, 
+    string conduitId, 
+    ConduitShardStatus? status = null, 
+    string? after = null
+    )
+    : HelixApiRequest<GetConduitShardsResponse>(
+        "eventsub/conduits/shards" +
+        new HttpQueryParameters()
+            .Add("conduit_id", conduitId)
+            .Add("status", status?.ToString().ToSnakeCase())
+            .Add("after", after),
+        clientId,
+        accessToken
+        );
