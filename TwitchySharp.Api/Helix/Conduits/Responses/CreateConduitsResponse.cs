6edaf677cc34cc24d96a99ae48c8d56154057f55
Yoyoyopo5 <118655115+Yoyoyopo5@users.noreply.github.com>
2024-11-19 03:57:31 -0600

    Move model

--- TwitchySharp.Api/Helix/Conduits/Responses/CreateConduitsResponse.cs
+++ TwitchySharp.Api/Helix/Conduits/Responses/CreateConduitsResponse.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TwitchySharp.Api.Helix.Conduits.Models;
 
 namespace TwitchySharp.Api.Helix.Conduits;
 /// <summary>
--- TwitchySharp.Api/Helix/Conduits/Responses/GetConduitsResponse.cs
+++ TwitchySharp.Api/Helix/Conduits/Responses/GetConduitsResponse.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TwitchySharp.Api.Helix.Conduits.Models;
 
 namespace TwitchySharp.Api.Helix.Conduits;
 /// <summary>
@@ -14,19 +15,4 @@ public record GetConduitsResponse
     /// The list of conduits.
     /// </summary>
     public required Conduit[] Data { get; init; }
-}
-
-/// <summary>
-/// Contains data about a specific conduit.
-/// </summary>
-public record Conduit
-{
-    /// <summary>
-    /// The id of the conduit.
-    /// </summary>
-    public required string Id { get; init; }
-    /// <summary>
-    /// The number of shards associated with this conduit.
-    /// </summary>
-    public required int ShardCount { get; init; }
 }
