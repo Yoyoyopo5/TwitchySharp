bc15fe00129076da50d3b9d48894f8d6dc3d7bf6
Yoyoyopo5 <118655115+Yoyoyopo5@users.noreply.github.com>
2024-11-20 16:28:03 -0600

    Add json converter for ValueBackedEnum

--- TwitchySharp.Helpers/ValueBackedEnum.cs
+++ TwitchySharp.Helpers/ValueBackedEnum.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 
 namespace TwitchySharp.Helpers;
 /// <summary>
@@ -15,4 +17,23 @@ public record ValueBackedEnum<T>
     }
     public override string ToString() => Value?.ToString() ?? string.Empty;
     public static implicit operator T(ValueBackedEnum<T> a) => a.Value;
+}
+
+/// <summary>
+/// Used to serialize classes based on <see cref="ValueBackedEnum{T}"/>
+/// </summary>
+/// <typeparam name="TValueBackedEnum"></typeparam>
+/// <typeparam name="T"></typeparam>
+public class ValueBackedEnumJsonConverter<TValueBackedEnum, T> : JsonConverter<TValueBackedEnum>
+    where TValueBackedEnum : ValueBackedEnum<T>
+{
+    public override TValueBackedEnum? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    {
+        return (TValueBackedEnum?)Activator.CreateInstance(typeToConvert, JsonSerializer.Deserialize<T>(ref reader, options));
+    }
+
+    public override void Write(Utf8JsonWriter writer, TValueBackedEnum value, JsonSerializerOptions options)
+    {
+        JsonSerializer.Serialize<T>(writer, value.Value, options);
+    }
 }
