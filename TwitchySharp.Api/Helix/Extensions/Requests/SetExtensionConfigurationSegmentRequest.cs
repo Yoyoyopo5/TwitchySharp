05c560cc03d9824e41e942aa26cd5a93f9cc4ea6
Yoyoyopo5 <yoyoyopo5@DESKTOP-IT4LOU9>
2024-11-20 07:38:47 -0600

    Add Set Extension Configuration Segment API

---
+++ TwitchySharp.Api/Helix/Extensions/Requests/SetExtensionConfigurationSegmentRequest.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using TwitchySharp.Helpers;
+
+namespace TwitchySharp.Api.Helix.Extensions;
+/// <summary>
+/// Updates a configuration segment. 
+/// The segment is limited to 5 KB. 
+/// Extensions that are active on a channel do not receive the updated configuration.
+/// <br/>
+/// See <see href="https://dev.twitch.tv/docs/api/reference/#set-extension-configuration-segment">set extension configuration segment</see> for more information.
+/// </summary>
+/// <remarks>
+/// <b>Rate Limits:</b> You may update the configuration a maximum of 20 times per minute.
+/// Requires a signed JWT created by an Extension Backend Service.
+/// </remarks>
+/// <param name="clientId">The client id of the application.</param>
+/// <param name="accessToken">
+/// A signed JWT created by an Extension Backend Service.
+/// For signing requirements, see <see href="https://dev.twitch.tv/docs/extensions/building/#signing-the-jwt">Signing the JWT</see>.
+/// The signed JWT must include the role, user_id, and exp fields (see <see href="https://dev.twitch.tv/docs/extensions/reference/#jwt-schema">JWT Schema</see>). The role field must be set to external.
+/// </param>
+/// <param name="data">
+/// Data used to set the configuration.
+/// </param>
+public class SetExtensionConfigurationSegmentRequest(string clientId, string accessToken, SetExtensionConfigurationSegmentRequestData data)
+    : HelixApiRequest<SetExtensionConfigurationSegmentResponse, SetExtensionConfigurationSegmentRequestData>(
+        "/extensions/configurations",
+        clientId,
+        accessToken,
+        data
+        )
+{
+    public override HttpMethod Method => HttpMethod.Put;
+}
+
+/// <summary>
+/// Contains data used to set an extension configuration segment.
+/// </summary>
+public record SetExtensionConfigurationSegmentRequestData
+{
+    /// <summary>
+    /// The id of the extension to update.
+    /// </summary>
+    public required string ExtensionId { get; set; }
+    /// <summary>
+    /// The configuration segment to update.
+    /// </summary>
+    [JsonConverter(typeof(SnakeCaseLowerJsonStringEnumConverter<ExtensionConfigurationSegmentType>))]
+    public required ExtensionConfigurationSegmentType Segment { get; set; }
+    /// <summary>
+    /// The user id of the broadcaster that installed the extension.
+    /// Include this property only if the <see cref="Segment"/> is set to <see cref="ExtensionConfigurationSegmentType.Developer"/> or <see cref="ExtensionConfigurationSegmentType.Broadcaster"/>.
+    /// </summary>
+    public string? BroadcasterId { get; set; }
+    /// <summary>
+    /// The contents of the segment.
+    /// This may be in plain-text or string-encoded JSON.
+    /// </summary>
+    public string? Content { get; set; }
+    /// <summary>
+    /// The version number that identifies this definition of the segment’s data. 
+    /// If not specified, the latest definition is updated.
+    /// </summary>
+    public string? Version { get; set; }
+}
