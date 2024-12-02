5cf7bdb6a0fba296b5dbe2d184e430be9868912b
Yoyoyopo5 <yoyoyopo5@DESKTOP-IT4LOU9>
2024-12-02 15:03:56 -0600

    Add Create Extension Secret API

---
+++ TwitchySharp.Api/Helix/Extensions/Responses/CreateExtensionSecretResponse.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwitchySharp.Api.Helix.Extensions;
+/// <summary>
+/// Contains a list of newly added extension secrets.
+/// </summary>
+public record CreateExtensionSecretResponse
+{
+    /// <summary>
+    /// The list of newly created secrets, grouped by version.
+    /// </summary>
+    public required ExtensionSecretsVersion[] Data { get; init; }
+}
