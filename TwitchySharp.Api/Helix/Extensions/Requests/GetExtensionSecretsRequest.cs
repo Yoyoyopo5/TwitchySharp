0e3c016f4d513cce730d865c043e64b9783ab0ee
Yoyoyopo5 <118655115+Yoyoyopo5@users.noreply.github.com>
2024-12-02 14:55:51 -0600

    Add Get Extension Secrets API

---
+++ TwitchySharp.Api/Helix/Extensions/Requests/GetExtensionSecretsRequest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TwitchySharp.Helpers;
+
+namespace TwitchySharp.Api.Helix.Extensions;
+/// <summary>
+/// Gets an extension's list of shared secrets.
+/// <br/>
+/// See <see href="https://dev.twitch.tv/docs/api/reference/#get-extension-secrets">get extension secrets</see> for more information.
+/// </summary>
+/// <remarks>
+/// Requires a signed JSON Web Token (JWT) created by an EBS. 
+/// For signing requirements, see <see href="https://dev.twitch.tv/docs/extensions/building/#signing-the-jwt">Signing the JWT</see>. 
+/// The signed JWT must include the role, user_id, and exp fields (see <see href="https://dev.twitch.tv/docs/extensions/reference/#jwt-schema">JWT Schema</see>). 
+/// The role field must be set to external.
+/// </remarks>
+/// <param name="clientId">The client id of the application.</param>
+/// <param name="accessToken">A signed JWT created by an EBS.</param>
+/// <param name="extensionId">The id of the extension whose shared secrets you want to get.</param>
+public class GetExtensionSecretsRequest(string clientId, string accessToken, string extensionId)
+    : HelixApiRequest<GetExtensionSecretsResponse>(
+        "/extensions/jwt/secrets" +
+        new HttpQueryParameters()
+            .Add("extension_id", extensionId),
+        clientId,
+        accessToken
+        );
---
+++ TwitchySharp.Api/Helix/Extensions/Responses/GetExtensionSecretsResponse.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwitchySharp.Api.Helix.Extensions;
+/// <summary>
+/// Contains a list of an extension's shared secrets, grouped by version.
+/// </summary>
+public record GetExtensionSecretsResponse
+{
+    /// <summary>
+    /// The list of shared extension secrets, grouped by version.
+    /// </summary>
+    public required ExtensionSecretsVersion[] Data { get; init; }
+}
+
+/// <summary>
+/// Contains a group of extension secrets that have a specific version.
+/// </summary>
+public record ExtensionSecretsVersion
+{
+    /// <summary>
+    /// The version number that identifies this definition of the secret’s data.
+    /// </summary>
+    public required int FormatVersion { get; init; }
+    /// <summary>
+    /// The list of secrets.
+    /// </summary>
+    public required ExtensionSecret[] Secrets { get; init; }
+}
+
+/// <summary>
+/// Contains information about a specific extension secret.
+/// </summary>
+public record ExtensionSecret
+{
+    /// <summary>
+    /// The raw secret that you use with JWT encoding.
+    /// </summary>
+    public required string Content { get; init; }
+    /// <summary>
+    /// The date and time that you may begin using this secret to sign a JWT.
+    /// </summary>
+    public required DateTimeOffset ActiveAt { get; init; }
+    /// <summary>
+    /// The date and time when you must stop using this secret to decode a JWT.
+    /// </summary>
+    public required DateTimeOffset ExpiresAt { get; init; }
+}
