b05d9e12ad7aa39e056709e308e9f7c67c3f2740
Yoyoyopo5 <118655115+Yoyoyopo5@users.noreply.github.com>
2024-12-16 17:57:10 -0600

    Add Get Games API

---
+++ TwitchySharp.Api/Helix/Games/Responses/GetGamesResponse.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwitchySharp.Api.Helix.Games;
+/// <summary>
+/// Contains a list of Twitch categories.
+/// </summary>
+public record GetGamesResponse
+{
+    /// <summary>
+    /// The list of categories and games.
+    /// </summary>
+    public required Game[] Data { get; init; }
+}
