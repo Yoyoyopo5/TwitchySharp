f53fec16ad398eb8271f7057d42acdbbda72f8cd
Yoyoyopo5 <yoyoyopo5@DESKTOP-IT4LOU9>
2024-12-18 13:28:06 -0600

    Add Get Channel Guest Star Settings API

---
+++ TwitchySharp.Api/Helix/GuestStar/Requests/GetChannelGuestStarSettingsRequest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TwitchySharp.Helpers;
+using TwitchySharp.Api.Authorization;
+
+namespace TwitchySharp.Api.Helix.GuestStar;
+/// <summary>
+/// <b>BETA</b>
+/// <br/>
+/// Gets the channel settings for configuration of the Guest Star feature for a particular host.
+/// <br/>
+/// See <see href="https://dev.twitch.tv/docs/api/reference/#get-channel-guest-star-settings">get channel guest star settings</see> for more information.
+/// </summary>
+/// <remarks>
+/// Requires a user access token that includes one of <see cref="Scope.ChannelReadGuestStar"/>, <see cref="Scope.ChannelManageGuestStar"/>, <see cref="Scope.ModeratorReadGuestStar"/>, or <see cref="Scope.ModeratorManageGuestStar"/>.
+/// </remarks>
+/// <param name="clientId">The client id of the application.</param>
+/// <param name="accessToken">A user access token that includes one of <see cref="Scope.ChannelReadGuestStar"/>, <see cref="Scope.ChannelManageGuestStar"/>, <see cref="Scope.ModeratorReadGuestStar"/>, or <see cref="Scope.ModeratorManageGuestStar"/>.</param>
+/// <param name="broadcasterId">The user id of the broadcaster to get Guest Star settings for.</param>
+/// <param name="moderatorId">
+/// The user id of the broadcaster or a moderator in the broadcaster's chat.
+/// This must be the same user that created the <paramref name="accessToken"/>.
+/// </param>
+public class GetChannelGuestStarSettingsRequest(string clientId, string accessToken, string broadcasterId, string moderatorId)
+    : HelixApiRequest<GetChannelGuestStarSettingsResponse>(
+        "/guest_star/channel_settings" +
+        new HttpQueryParameters()
+            .Add("broadcaster_id", broadcasterId)
+            .Add("moderator_id", moderatorId),
+        clientId,
+        accessToken
+        );
---
+++ TwitchySharp.Api/Helix/GuestStar/Responses/GetChannelGuestStarSettingsResponse.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using TwitchySharp.Helpers;
+
+namespace TwitchySharp.Api.Helix.GuestStar;
+/// <summary>
+/// Contains a list containing the channel's Guest Star settings.
+/// </summary>
+public record GetChannelGuestStarSettingsResponse
+{
+    /// <summary>
+    /// Contains a single entry of the channel's Guest Star settings.
+    /// </summary>
+    public required ChannelGuestStarSettings[] Data { get; init; }
+}
+
+/// <summary>
+/// Contains information about a channel's Guest Star settings.
+/// </summary>
+public record ChannelGuestStarSettings
+{
+    /// <summary>
+    /// Determines if Guest Star moderators have access to control whether a guest is live once assigned to a slot.
+    /// </summary>
+    public required bool IsModeratorSendLiveEnabled { get; init; }
+    /// <summary>
+    /// Number of slots the Guest Star call interface will allow the host to add to a call. 
+    /// Ranges between 1 and 6.
+    /// </summary>
+    public required int SlotCount { get; init; }
+    /// <summary>
+    /// Determines if Browser Sources subscribed to sessions on this channel should output audio.
+    /// </summary>
+    public required bool IsBrowserSourceAudioEnabled { get; init; }
+    /// <summary>
+    /// Determines how the guests within a session should be laid out within the browser source.
+    /// </summary>
+    [JsonConverter(typeof(SnakeCaseUpperJsonStringEnumConverter<GuestStarGroupLayout>))]
+    public required GuestStarGroupLayout GroupLayout { get; init; }
+    /// <summary>
+    /// A token used to generate browser source URLs.
+    /// </summary>
+    public required string BrowserSourceToken { get; init; }
+}
+
+/// <summary>
+/// Possible values for how guests should be laid out within a browser source in a Guest Star session.
+/// </summary>
+public enum GuestStarGroupLayout
+{
+    /// <summary>
+    /// All live guests are tiled within the browser source with the same size.
+    /// </summary>
+    TiledLayout,
+    /// <summary>
+    /// All live guests are tiled within the browser source with the same size. 
+    /// If there is an active screen share, it is sized larger than the other guests.
+    /// </summary>
+    ScreenshareLayout
+}
