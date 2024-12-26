b1d44f26d64d9e8f70703660fb75a537b5dab99f
Yoyoyopo5 <Yoyoyopo5@DESKTOP-26KQ6JN>
2024-12-26 17:01:19 -0600

    Move enum

---
+++ TwitchySharp.Api/Helix/GuestStar/Enums/GuestStarGroupLayout.cs
@@ -0,0 +1,25 @@
+﻿namespace TwitchySharp.Api.Helix.GuestStar;
+
+/// <summary>
+/// Possible values for how guests should be laid out within a browser source in a Guest Star session.
+/// </summary>
+public enum GuestStarGroupLayout
+{
+    /// <summary>
+    /// All live guests are tiled within the browser source with the same size.
+    /// </summary>
+    TiledLayout,
+    /// <summary>
+    /// All live guests are tiled within the browser source with the same size. 
+    /// If there is an active screen share, it is sized larger than the other guests.
+    /// </summary>
+    ScreenshareLayout,
+    /// <summary>
+    /// All live guests are arranged in a horizontal bar within the browser source.
+    /// </summary>
+    HorizontalLayout,
+    /// <summary>
+    /// All live guests are arranged in a vertical bar within the browser source.
+    /// </summary>
+    VerticalLayout
+}
