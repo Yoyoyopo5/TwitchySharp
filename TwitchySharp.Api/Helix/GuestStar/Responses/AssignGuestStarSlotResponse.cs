03d7c61afc5075216bb0aba566e51dbe2074e204
Yoyoyopo5 <Yoyoyopo5@DESKTOP-26KQ6JN>
2024-12-26 23:40:55 -0600

    Add Assign Guest Star Slot API

---
+++ TwitchySharp.Api/Helix/GuestStar/Responses/AssignGuestStarSlotResponse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwitchySharp.Api.Helix.GuestStar;
+/// <summary>
+/// Empty response.
+/// </summary>
+/// <remarks>
+/// Dev Note: Docs are ambiguous on this response. Please update during integration testing.
+/// </remarks>
+public record AssignGuestStarSlotResponse { }
