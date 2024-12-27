d1de4174668d734eb052e0035c067e8a297dd0ac
Yoyoyopo5 <Yoyoyopo5@DESKTOP-26KQ6JN>
2024-12-26 21:24:19 -0600

    Add End Guest Star Session API

---
+++ TwitchySharp.Api/Helix/GuestStar/Requests/EndGuestStarSessionRequest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using TwitchySharp.Helpers;
+using TwitchySharp.Api.Authorization;
+
+namespace TwitchySharp.Api.Helix.GuestStar;
+/// <summary>
+/// BETA
+/// <br/>
+/// Ends a Guest Star session on behalf of the broadcaster. 
+/// Performs the same action as if the host clicked the “End Call” button in the Guest Star UI.
+/// <br/>
+/// See <see href="https://dev.twitch.tv/docs/api/reference/#end-guest-star-session">end Guest Star session</see> for more information.
+/// </summary>
+/// <remarks>
+/// Requires a user access token that includes <see cref="Scope.ChannelManageGuestStar"/>.
+/// </remarks>
+/// <param name="clientId">The client id of the application.</param>
+/// <param name="accessToken">A user access token that includes <see cref="Scope.ChannelManageGuestStar"/>.</param>
+/// <param name="broadcasterId">
+/// The user id of the broadcaster to end a Guest Star session for.
+/// This must be the same user that created the <paramref name="accessToken"/>.
+/// </param>
+/// <param name="sessionId">The id of the Guest Star session to end.</param>
+public class EndGuestStarSessionRequest(string clientId, string accessToken, string broadcasterId, string sessionId)
+    : HelixApiRequest<EndGuestStarSessionResponse>(
+        "/guest_star/session" +
+        new HttpQueryParameters()
+            .Add("broadcaster_id", broadcasterId)
+            .Add("session_id", sessionId),
+        clientId,
+        accessToken
+        )
+{
+    public override HttpMethod Method => HttpMethod.Delete;
+}
---
+++ TwitchySharp.Api/Helix/GuestStar/Responses/EndGuestStarSessionResponse.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwitchySharp.Api.Helix.GuestStar;
+/// <summary>
+/// Contains details about the session that was ended.
+/// </summary>
+public record EndGuestStarSessionResponse
+{
+    /// <summary>
+    /// Contains a single entry of a summary of the session details when the session was ended.
+    /// </summary>
+    public required GuestStarSession[] Data { get; init; }
+}
