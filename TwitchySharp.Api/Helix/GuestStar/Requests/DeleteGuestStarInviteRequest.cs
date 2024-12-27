e38ab4e774bc57fb687a6b531c10ce594b024e3e
Yoyoyopo5 <Yoyoyopo5@DESKTOP-26KQ6JN>
2024-12-26 23:00:27 -0600

    Add Delete Guest Star Invite API

---
+++ TwitchySharp.Api/Helix/GuestStar/Requests/DeleteGuestStarInviteRequest.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using TwitchySharp.Helpers;
+using TwitchySharp.Api.Authorization;
+
+namespace TwitchySharp.Api.Helix.GuestStar;
+/// <summary>
+/// BETA
+/// <br/>
+/// Revokes a previously sent invite for a Guest Star session.
+/// <br/>
+/// See <see href="https://dev.twitch.tv/docs/api/reference/#delete-guest-star-invite">delete Guest Star invite</see> for more information.
+/// </summary>
+/// <remarks>
+/// Requires a user access token that includes <see cref="Scope.ChannelManageGuestStar"/> or <see cref="Scope.ModeratorManageGuestStar"/>.
+/// </remarks>
+/// <param name="clientId">The client id of the application.</param>
+/// <param name="accessToken">A user access token that includes <see cref="Scope.ChannelManageGuestStar"/> or <see cref="Scope.ModeratorManageGuestStar"/>.</param>
+/// <param name="broadcasterId">The user id of the broadcaster hosting the Guest Star session.</param>
+/// <param name="moderatorId">
+/// The user id of the broadcaster or a moderator of the broadcaster's channel.
+/// This must be the same user that created the <paramref name="accessToken"/>.
+/// </param>
+/// <param name="sessionId">The id of the Guest Star session that the invite was created for.</param>
+/// <param name="guestId">The user id of the user to revoke the invite for.</param>
+public class DeleteGuestStarInviteRequest(
+    string clientId, 
+    string accessToken, 
+    string broadcasterId, 
+    string moderatorId, 
+    string sessionId, 
+    string guestId
+    )
+    : HelixApiRequest<DeleteGuestStarInviteResponse>(
+        "/guest_star/invites" +
+        new HttpQueryParameters()
+            .Add("broadcaster_id", broadcasterId)
+            .Add("moderator_id", moderatorId)
+            .Add("session_id", sessionId)
+            .Add("guest_id", guestId),
+        clientId,
+        accessToken
+        )
+{
+    public override HttpMethod Method => HttpMethod.Delete;
+}
