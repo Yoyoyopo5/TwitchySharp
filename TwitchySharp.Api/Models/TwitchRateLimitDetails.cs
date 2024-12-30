02ec9a4adca6284e71ccb91298a557196a787b93
Yoyoyopo5 <Yoyoyopo5@DESKTOP-26KQ6JN>
2024-12-29 22:27:32 -0600

    Add rate limit helper extension and model

---
+++ TwitchySharp.Api/Models/TwitchRateLimitDetails.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace TwitchySharp.Api.Models;
+
+/// <summary>
+/// Contains information about current Twitch rate limit usage.
+/// This data is reported once for each call to the Twitch API.
+/// <br/>
+/// See <see href="https://dev.twitch.tv/docs/api/guide/#twitch-rate-limits">Twitch Rate Limits</see> for more information.
+/// </summary>
+/// <param name="Limit">The maximum amount of points that can be in your bucket.</param>
+/// <param name="Remaining">The number of points currently in your bucket (after the last request).</param>
+/// <param name="Reset">The date and time when your bucket is reset to full.</param>
+public readonly record struct TwitchRateLimitDetails(int Limit, int Remaining, DateTimeOffset Reset);
