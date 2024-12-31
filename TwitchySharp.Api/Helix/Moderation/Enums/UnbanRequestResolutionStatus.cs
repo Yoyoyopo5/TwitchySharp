7f1638de968a9f0cb04ffb9c8128eb96dcabcfc9
Yoyoyopo5 <Yoyoyopo5@DESKTOP-26KQ6JN>
2024-12-31 16:53:47 -0600

    Add Resolve Unban Requests API

---
+++ TwitchySharp.Api/Helix/Moderation/Enums/UnbanRequestResolutionStatus.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwitchySharp.Api.Helix.Moderation;
+/// <summary>
+/// Possible statuses for an unban request resolution.
+/// </summary>
+public enum UnbanRequestResolutionStatus
+{
+    Approved,
+    Denied
+}
