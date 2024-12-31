49aa1f4b1690a9af534d1339a82fd916fe3396b3
Yoyoyopo5 <Yoyoyopo5@DESKTOP-26KQ6JN>
2024-12-30 23:43:44 -0600

    Add Get Banned Users API

---
+++ TwitchySharp.Api/Helix/Moderation/Requests/GetBannedUsersRequest.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TwitchySharp.Helpers;
+using TwitchySharp.Api.Authorization;
+using TwitchySharp.Api.Models;
+
+namespace TwitchySharp.Api.Helix.Moderation;
+/// <summary>
+/// Gets all users that the broadcaster banned or put in a timeout.
+/// <br/>
+/// See <see href="https://dev.twitch.tv/docs/api/reference/#get-banned-users">get banned users</see> for more information.
+/// </summary>
+/// <remarks>
+/// Requires a user access token that includes <see cref="Scope.ModerationRead"/> or <see cref="Scope.ModeratorManageBannedUsers"/>.
+/// </remarks>
+/// <param name="clientId">The client id of the application.</param>
+/// <param name="accessToken">A user access token that includes <see cref="Scope.ModerationRead"/> or <see cref="Scope.ModeratorManageBannedUsers"/>.</param>
+/// <param name="broadcasterId">
+/// The user id of the broadcaster (channel) to get banned users for.
+/// This must be the same user that created the <paramref name="accessToken"/>.
+/// </param>
+/// <param name="userIds">
+/// A list of user ids used to filter the results. You may specify a maximum of 100 IDs. 
+/// The returned list includes only those users that were banned or put in a timeout.
+/// The list is returned in the same order that you specified the ids.
+/// </param>
+/// <param name="first">
+/// The maximum number of items to return per page in the response. 
+/// The minimum page size is 1 item per page and the maximum is 100 items per page. 
+/// The default is 20.
+/// </param>
+/// <param name="after">
+/// The cursor used to get the next page of results. 
+/// The <see cref="Pagination"/> property in the response contains the cursor’s value.
+/// </param>
+/// <param name="before">
+/// The cursor used to get the previous page of results. 
+/// The <see cref="Pagination"/> property in the response contains the cursor’s value.
+/// </param>
+public class GetBannedUsersRequest(
+    string clientId,
+    string accessToken,
+    string broadcasterId,
+    IEnumerable<string>? userIds = null,
+    int? first = null,
+    string? after = null,
+    string? before = null
+    )
+    : HelixApiRequest<GetBannedUsersResponse>(
+        "/moderation/banned" +
+        new HttpQueryParameters()
+            .Add("broadcaster_id", broadcasterId)
+            .Add("user_id", userIds)
+            .Add("first", first?.ToString())
+            .Add("after", after)
+            .Add("before", before),
+        clientId,
+        accessToken
+        );
---
+++ TwitchySharp.Api/Helix/Moderation/Responses/GetBannedUsersResponse.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using TwitchySharp.Api.Models;
+using TwitchySharp.Helpers;
+
+namespace TwitchySharp.Api.Helix.Moderation;
+/// <summary>
+/// Contains a list of a channel's banned or timed-out users.
+/// </summary>
+public record GetBannedUsersResponse
+{
+    /// <summary>
+    /// A list of the channel's banned and timed-out users.
+    /// </summary>
+    public required UserBan[] Data { get; init; }
+    /// <summary>
+    /// <inheritdoc cref="Models.Pagination"/>
+    /// </summary>
+    public required Pagination Pagination { get; init; }
+}
+
+/// <summary>
+/// Contains information about an individual user's channel ban.
+/// </summary>
+public record UserBan
+{
+    /// <summary>
+    /// The id of the banned or timed-out user.
+    /// </summary>
+    public required string UserId { get; init; }
+    /// <summary>
+    /// The login (username) of the banned or timed-out user.
+    /// </summary>
+    public required string UserLogin { get; init; }
+    /// <summary>
+    /// The display name of the banned or timed-out user.
+    /// </summary>
+    public required string UserName { get; init; }
+    /// <summary>
+    /// If the user was timed out, the date and time when the timeout expires.
+    /// If the user was banned, this is <see langword="null"/>.
+    /// </summary>
+    [JsonConverter(typeof(EmptyDateTimeOffsetConverter))]
+    public DateTimeOffset? ExpiresAt { get; init; }
+    /// <summary>
+    /// The date and time when the user was banned or timed out.
+    /// </summary>
+    public required DateTimeOffset CreatedAt { get; init; }
+    /// <summary>
+    /// The reason the user was banned or timed out if the moderator provided one.
+    /// Otherwise, an empty string.
+    /// </summary>
+    public required string Reason { get; init; }
+    /// <summary>
+    /// The user id of the moderator who banned or timed out the user.
+    /// </summary>
+    public required string ModeratorId { get; init; }
+    /// <summary>
+    /// The login (username) of the moderator who banned or timed out the user.
+    /// </summary>
+    public required string ModeratorLogin { get; init; }
+    /// <summary>
+    /// The display name of the moderator who banned or timed out the user.
+    /// </summary>
+    public required string ModeratorName { get; init; }
+}
