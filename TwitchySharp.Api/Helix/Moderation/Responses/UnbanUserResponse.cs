d266e55b63ad813f7715bc78bd1b57b1d6f03439
Yoyoyopo5 <Yoyoyopo5@DESKTOP-26KQ6JN>
2024-12-31 04:44:42 -0600

    Add Unban User API

---
+++ TwitchySharp.Api/Helix/Moderation/Responses/UnbanUserResponse.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwitchySharp.Api.Helix.Moderation;
+/// <summary>
+/// Empty response.
+/// </summary>
+public record UnbanUserResponse { }
