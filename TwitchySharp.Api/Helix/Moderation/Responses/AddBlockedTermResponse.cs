88f942f0cb788f810ae6c84c2992c8276bea72de
Yoyoyopo5 <Yoyoyopo5@DESKTOP-26KQ6JN>
2024-12-31 21:03:02 -0600

    Add Add Blocked Term API

---
+++ TwitchySharp.Api/Helix/Moderation/Responses/AddBlockedTermResponse.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwitchySharp.Api.Helix.Moderation;
+/// <summary>
+/// Contains a list of newly blocked terms.
+/// </summary>
+public record AddBlockedTermResponse
+{
+    /// <summary>
+    /// A list containing the single new term that was blocked.
+    /// </summary>
+    public required BlockedTerm[] Data { get; init; }
+}
