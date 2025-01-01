91b6463878315ccf737c05468a4a073848e3c125
Yoyoyopo5 <Yoyoyopo5@DESKTOP-26KQ6JN>
2025-01-01 02:23:07 -0600

    Add Update Shield Mode Status API

---
+++ TwitchySharp.Api/Helix/Moderation/Requests/UpdateShieldModeStatusRequest.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using TwitchySharp.Helpers;
+using TwitchySharp.Api.Authorization;
+
+namespace TwitchySharp.Api.Helix.Moderation;
+/// <summary>
+/// Activates or deactivates the broadcaster’s Shield Mode.
+/// <br/>
+/// See <see href="https://dev.twitch.tv/docs/api/reference/#update-shield-mode-status">update Shield Mode status</see> for more information.
+/// </summary>
+/// <remarks>
+/// Requires a user access token that includes <see cref="Scope.ModeratorManageShieldMode"/>.
+/// </remarks>
+/// <param name="clientId">The client id of the application.</param>
+/// <param name="accessToken">A user access token that includes <see cref="Scope.ModeratorManageShieldMode"/>.</param>
+/// <param name="broadcasterId">The user id of the broadcaster (channel) to update Shield Mode status for.</param>
+/// <param name="moderatorId">
+/// The user id of the broadcaster or a moderator of the broadcaster's channel.
+/// This must be the same user that created the <paramref name="accessToken"/>.
+/// </param>
+/// <param name="shieldModeStatus">The Shield Mode status to update to.</param>
+public class UpdateShieldModeStatusRequest(
+    string clientId,
+    string accessToken,
+    string broadcasterId,
+    string moderatorId,
+    UpdateShieldModeStatusRequestData shieldModeStatus
+    )
+    : HelixApiRequest<UpdateShieldModeStatusResponse, UpdateShieldModeStatusRequestData>(
+        "/moderation/shield_mode" +
+        new HttpQueryParameters()
+            .Add("broadcaster_id", broadcasterId)
+            .Add("moderator_id", moderatorId),
+        clientId,
+        accessToken,
+        shieldModeStatus
+        )
+{
+    public override HttpMethod Method => HttpMethod.Put;
+}
+
+/// <summary>
+/// Data used to set the status of Shield Mode on a channel.
+/// </summary>
+public record UpdateShieldModeStatusRequestData
+{
+    /// <summary>
+    /// Determines whether to activate or deactivate Shield Mode. 
+    /// Set to <see langword="true"/> to activate Shield Mode; otherwise, <see langword="false"/> to deactivate Shield Mode.
+    /// </summary>
+    public required bool IsActive { get; set; }
+}
---
+++ TwitchySharp.Api/Helix/Moderation/Responses/UpdateShieldModeStatusResponse.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwitchySharp.Api.Helix.Moderation;
+/// <summary>
+/// Contains information about a channel's Shield Mode status.
+/// </summary>
+public record UpdateShieldModeStatusResponse
+{
+    /// <summary>
+    /// A list containing a single object of the channel's Shield Mode status.
+    /// </summary>
+    public required ShieldModeStatus[] Data { get; init; }
+}
+
+/// <summary>
+/// Contains information about a channel's Shield Mode status.
+/// </summary>
+public record ShieldModeStatus
+{
+    /// <summary>
+    /// Determines whether Shield Mode is active. 
+    /// Is <see langword="true"/> if Shield Mode is active; otherwise, <see langword="false"/>.
+    /// </summary>
+    public required bool IsActive { get; init; }
+    /// <summary>
+    /// The user id of the moderator that last activated Shield Mode.
+    /// </summary>
+    public required string ModeratorId { get; init; }
+    /// <summary>
+    /// The login (username) of the moderator that last activated Shield Mode.
+    /// </summary>
+    public required string ModeratorLogin { get; init; }
+    /// <summary>
+    /// The display name of the moderator that last activated Shield Mode.
+    /// </summary>
+    public required string ModeratorName { get; init; }
+    /// <summary>
+    /// The date and time when Shield Mode was last activated.
+    /// </summary>
+    public required DateTimeOffset LastActivatedAt { get; init; }
+}
