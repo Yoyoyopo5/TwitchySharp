43e98b0c77cb5a6dbd1b6d4d4a8ea9cabf91f82b
Yoyoyopo5 <Yoyoyopo5@DESKTOP-26KQ6JN>
2025-01-02 01:41:42 -0600

    Move models

---
+++ TwitchySharp.Api/Helix/Polls/Models/ChatPollChoice.cs
@@ -0,0 +1,29 @@
+﻿namespace TwitchySharp.Api.Helix.Polls;
+
+/// <summary>
+/// Contains information about a specific choice in a chat poll.
+/// </summary>
+public record ChatPollChoice
+{
+    /// <summary>
+    /// The id of the choice.
+    /// </summary>
+    public required string Id { get; init; }
+    /// <summary>
+    /// The title of the choice.
+    /// This may be up to 25 characters.
+    /// </summary>
+    public required string Title { get; init; }
+    /// <summary>
+    /// The total number of votes cast for this choice.
+    /// </summary>
+    public required int Votes { get; init; }
+    /// <summary>
+    /// The number of votes that were cast using Channel Points.
+    /// </summary>
+    public required int ChannelPointsVotes { get; init; }
+    /// <summary>
+    /// Unused and always set to <c>0</c>.
+    /// </summary>
+    public required int BitsVotes { get; init; }
+}
