d8abb7b206bdc27bf345328814ef40170f3048e5
Yoyoyopo5 <Yoyoyopo5@DESKTOP-26KQ6JN>
2025-01-02 03:11:41 -0600

    Add End Poll API

---
+++ TwitchySharp.Api/Helix/Polls/Responses/EndPollResponse.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwitchySharp.Api.Helix.Polls;
+/// <summary>
+/// Contains information about the ended poll.
+/// </summary>
+public record EndPollResponse
+{
+    /// <summary>
+    /// A list containing the poll that was ended.
+    /// </summary>
+    public required ChatPoll[] Data { get; init; }
+}
