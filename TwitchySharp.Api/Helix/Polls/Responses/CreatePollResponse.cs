72f40f92ce07d602077153a23a0f9970e1e33c7b
Yoyoyopo5 <Yoyoyopo5@DESKTOP-26KQ6JN>
2025-01-02 03:00:49 -0600

    Add Create Poll API

---
+++ TwitchySharp.Api/Helix/Polls/Responses/CreatePollResponse.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwitchySharp.Api.Helix.Polls;
+/// <summary>
+/// Contains a list of created polls.
+/// </summary>
+public record CreatePollResponse
+{
+    /// <summary>
+    /// A list containing a single object for the poll that was created.
+    /// </summary>
+    public required ChatPoll[] Data { get; init; }
+}
