9bf026c484623e30d38e675f7e0ac2679d8b013d
Yoyoyopo5 <Yoyoyopo5@DESKTOP-26KQ6JN>
2025-01-02 00:31:55 -0600

    Add Get Shield Mode Status API

---
+++ TwitchySharp.Api/Helix/Moderation/Requests/GetShieldModeStatusRequest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TwitchySharp.Helpers;
+using TwitchySharp.Api.Authorization;
+
+namespace TwitchySharp.Api.Helix.Moderation;
+/// <summary>
+/// Gets the broadcaster’s Shield Mode activation status.
+/// <br/>
+/// See <see href="https://dev.twitch.tv/docs/api/reference/#get-shield-mode-status">get Shield Mode status</see> for more information.
+/// </summary>
+/// <remarks>
+/// Requires a user access token that includes <see cref="Scope.ModeratorReadShieldMode"/> or <see cref="Scope.ModeratorManageShieldMode"/>.
+/// </remarks>
+/// <param name="clientId">The client id of the application.</param>
+/// <param name="accessToken">A user access token that includes <see cref="Scope.ModeratorReadShieldMode"/> or <see cref="Scope.ModeratorManageShieldMode"/>.</param>
+/// <param name="broadcasterId">The user id of the broadcaster (channel) to get Shield Mode status for.</param>
+/// <param name="moderatorId">
+/// The user id of the broadcaster or a moderator of the broadcaster's channel.
+/// This must be the same user that created the <paramref name="accessToken"/>.
+/// </param>
+public class GetShieldModeStatusRequest(
+    string clientId,
+    string accessToken,
+    string broadcasterId,
+    string moderatorId
+    )
+    : HelixApiRequest<GetShieldModeStatusResponse>(
+        "/moderation/shield_mode" +
+        new HttpQueryParameters()
+            .Add("broadcaster_id", broadcasterId)
+            .Add("moderator_id", moderatorId),
+        clientId,
+        accessToken
+        );
---
+++ TwitchySharp.Api/Helix/Moderation/Responses/GetShieldModeStatusResponse.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwitchySharp.Api.Helix.Moderation;
+/// <summary>
+/// Contains information about a channel's current Shield Mode status.
+/// </summary>
+public record GetShieldModeStatusResponse
+{
+    /// <summary>
+    /// A list containing a single entry of a channel's Shield Mode status.
+    /// </summary>
+    public required ShieldModeStatus[] Data { get; init; }
+}
