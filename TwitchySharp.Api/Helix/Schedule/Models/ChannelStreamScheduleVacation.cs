545cfbe08d3fd0d10af891558802e1a61f8c08bf
Yoyoyopo5 <118655115+Yoyoyopo5@users.noreply.github.com>
2025-01-05 07:34:57 -0600

    Move models

---
+++ TwitchySharp.Api/Helix/Schedule/Models/ChannelStreamScheduleVacation.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace TwitchySharp.Api.Helix.Schedule;
+
+/// <summary>
+/// Contains information about a broadcaster's vacation schedule.
+/// </summary>
+public readonly record struct ChannelStreamScheduleVacation
+{
+    /// <summary>
+    /// The date and time of when the broadcaster’s vacation starts.
+    /// </summary>
+    public required DateTimeOffset StartTime { get; init; }
+    /// <summary>
+    /// The date and time of when the broadcaster’s vacation ends.
+    /// </summary>
+    public required DateTimeOffset EndTime { get; init; }
+}
