b0e5df285d9625d732a3378131589d83c888960b
Yoyoyopo5 <118655115+Yoyoyopo5@users.noreply.github.com>
2025-01-05 05:07:05 -0600

    Add End Prediction API

---
+++ TwitchySharp.Api/Helix/Predictions/Responses/EndPredictionResponse.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwitchySharp.Api.Helix.Predictions;
+/// <summary>
+/// Contains information about the ended prediction.
+/// </summary>
+public record EndPredictionResponse
+{
+    /// <summary>
+    /// A list containing the single prediction that was ended.
+    /// </summary>
+    public required ChatPrediction[] Data { get; init; }
+}
