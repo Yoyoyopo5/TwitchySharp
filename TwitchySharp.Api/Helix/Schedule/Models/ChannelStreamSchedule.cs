adab492e51a5261d3156a16ba71bbfc84524b2cb
Yoyoyopo5 <yoyoyopo5@DESKTOP-IT4LOU9>
2025-01-05 07:34:57 -0600

    Move models

---
+++ TwitchySharp.Api/Helix/Schedule/Models/ChannelStreamSchedule.cs
@@ -0,0 +1,30 @@
+﻿namespace TwitchySharp.Api.Helix.Schedule;
+
+/// <summary>
+/// Contains information about a specific broadcaster's stream schedule.
+/// </summary>
+public record ChannelStreamSchedule
+{
+    /// <summary>
+    /// The list of broadcasts in the broadcaster’s streaming schedule.
+    /// </summary>
+    public required ChannelStreamScheduleSegment[] Segments { get; init; }
+    /// <summary>
+    /// The user id of the broadcaster that owns the broadcast schedule.
+    /// </summary>
+    public required string BroadcasterId { get; init; }
+    /// <summary>
+    /// The display name of the broadcaster that owns the broadcast schedule.
+    /// </summary>
+    public required string BroadcasterName { get; init; }
+    /// <summary>
+    /// The login (username) of the broadcaster that owns the broadcast schedule.
+    /// </summary>
+    public required string BroadcasterLogin { get; init; }
+    /// <summary>
+    /// The dates when the broadcaster is on vacation and not streaming. 
+    /// Is set to <see langword="null"/> if vacation mode is not enabled.
+    /// </summary>
+    public ChannelStreamScheduleVacation? Vacation { get; init; }
+
+}
---
+++ TwitchySharp.Api/Helix/Schedule/Models/ChannelStreamScheduleSegment.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace TwitchySharp.Api.Helix.Schedule;
+
+/// <summary>
+/// Contains information about a specific stream schedule segment.
+/// </summary>
+public record ChannelStreamScheduleSegment
+{
+    /// <summary>
+    /// The id of this broadcast segment.
+    /// </summary>
+    public required string Id { get; init; }
+    /// <summary>
+    /// The date and time when the broadcast starts.
+    /// </summary>
+    public required DateTimeOffset StartTime { get; init; }
+    /// <summary>
+    /// The date and time when the broadcast ends.
+    /// </summary>
+    public required DateTimeOffset EndTime { get; init; }
+    /// <summary>
+    /// The broadcast segment’s title.
+    /// </summary>
+    public required string Title { get; init; }
+    /// <summary>
+    /// Indicates whether the broadcaster canceled this segment of a recurring broadcast. 
+    /// If the broadcaster canceled this segment, this field is set to the same value as <see cref="EndTime"/>; otherwise, it’s set to <see langword="null"/>.
+    /// </summary>
+    public DateTimeOffset? CanceledUntil { get; init; }
+    /// <summary>
+    /// The category that the broadcaster plans to stream or <see langword="null"/> if not specified.
+    /// </summary>
+    public ChannelStreamScheduleCategory? Category { get; init; }
+    /// <summary>
+    /// Indicates whether the broadcast is part of a recurring series that streams at the same time each week or is a one-time broadcast. 
+    /// Is <see langword="true"/> if the broadcast is part of a recurring series.
+    /// </summary>
+    public required bool IsRecurring { get; init; }
+
+}
