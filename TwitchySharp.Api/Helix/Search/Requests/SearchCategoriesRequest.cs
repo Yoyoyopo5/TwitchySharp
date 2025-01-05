f9c9055c01004b20d6f0c5f987f571ffdb4d8ead
Yoyoyopo5 <118655115+Yoyoyopo5@users.noreply.github.com>
2025-01-05 10:44:56 -0600

    Add Search Categories API

---
+++ TwitchySharp.Api/Helix/Search/Requests/SearchCategoriesRequest.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TwitchySharp.Helpers;
+using TwitchySharp.Api.Models;
+using System.Text.Encodings.Web;
+using System.Web;
+
+namespace TwitchySharp.Api.Helix.Search;
+/// <summary>
+/// Gets the games or categories that match the specified query.
+/// <br/>
+/// See <see href="https://dev.twitch.tv/docs/api/reference/#search-categories">search categories</see> for more information.
+/// </summary>
+/// <remarks>
+/// Requires an app or user access token.
+/// <para>
+/// To match, the category’s name must contain all parts of the query string. 
+/// For example, if the query string is 42, the response includes any category name that contains 42 in the title. 
+/// If the query string is a phrase like love computer, the response includes any category name that contains the words love and computer anywhere in the name. 
+/// The comparison is case insensitive.
+/// </para>
+/// </remarks>
+/// <param name="clientId">The client id of the application.</param>
+/// <param name="accessToken">An app or user access token.</param>
+/// <param name="query">The search string.</param>
+/// <param name="first">
+/// The maximum number of items to return per page in the response. 
+/// The minimum page size is 1 item per page and the maximum is 100 items per page. 
+/// The default is 20.
+/// </param>
+/// <param name="after">
+/// The cursor used to get the next page of results. 
+/// The <see cref="Pagination"/> object in the response contains the cursor’s value. 
+/// </param>
+public class SearchCategoriesRequest(
+    string clientId,
+    string accessToken,
+    string query,
+    int? first = null,
+    string? after = null
+    )
+    : HelixApiRequest<SearchCategoriesResponse>(
+        "/search/categories" +
+        new HttpQueryParameters()
+            .Add("query", HttpUtility.UrlEncode(query))
+            .Add("first", first?.ToString())
+            .Add("after", after),
+        clientId,
+        accessToken
+        );
---
+++ TwitchySharp.Api/Helix/Search/Responses/SearchCategoriesResponse.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TwitchySharp.Api.Models;
+
+namespace TwitchySharp.Api.Helix.Search;
+/// <summary>
+/// Contains a list of found categories.
+/// </summary>
+public record SearchCategoriesResponse
+{
+    /// <summary>
+    /// The list of categories.
+    /// </summary>
+    public required TwitchCategory[] Data { get; init; }
+    /// <inheritdoc cref="Models.Pagination"/>
+    public required Pagination Pagination { get; init; }
+}
+
+/// <summary>
+/// Contains information about a specific Twitch category (game).
+/// </summary>
+public record TwitchCategory
+{
+    /// <summary>
+    /// A URL to an image of the game’s box art or streaming category.
+    /// </summary>
+    public required string BoxArtUrl { get; init; }
+    /// <summary>
+    /// The name of the game or category.
+    /// </summary>
+    public required string Name { get; init; }
+    /// <summary>
+    /// The id of the category.
+    /// </summary>
+    public required string Id { get; init; }
+}
