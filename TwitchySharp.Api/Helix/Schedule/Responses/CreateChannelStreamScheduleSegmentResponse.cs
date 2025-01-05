d37146518443b6a0cd9a72a98efc7dc62365d500
Yoyoyopo5 <118655115+Yoyoyopo5@users.noreply.github.com>
2025-01-05 09:14:12 -0600

    Add Create Channel Stream Schedule Segment API

---
+++ TwitchySharp.Api/Helix/Schedule/Responses/CreateChannelStreamScheduleSegmentResponse.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwitchySharp.Api.Helix.Schedule;
+/// <inheritdoc cref="ChannelStreamSchedule"/>
+public record CreateChannelStreamScheduleSegmentResponse
+{
+    /// <summary>
+    /// The broadcaster's updated streaming schedule.
+    /// </summary>
+    public required ChannelStreamSchedule Data { get; init; }
+}
