e0023e7d6828798e2057f0ca0339255f898b6b83
Yoyoyopo5 <yoyoyopo5@DESKTOP-IT4LOU9>
2025-01-05 05:30:19 -0600

    Add Start Raid API

---
+++ TwitchySharp.Api/Helix/Raids/Requests/StartRaidRequest.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using TwitchySharp.Helpers;
+using TwitchySharp.Api.Authorization;
+
+namespace TwitchySharp.Api.Helix.Raids;
+/// <summary>
+/// Raid another channel by sending the broadcaster’s viewers to the targeted channel.
+/// <br/>
+/// See <see href="https://dev.twitch.tv/docs/api/reference/#start-a-raid">start a raid</see> for more information.
+/// </summary>
+/// <remarks>
+/// Requires a user access token that includes <see cref="Scope.ChannelManageRaids"/>. 
+/// </remarks>
+/// <param name="clientId">The client id of the application.</param>
+/// <param name="accessToken">A user access token that includes <see cref="Scope.ChannelManageRaids"/>.</param>
+/// <param name="fromBroadcasterId">
+/// The user id of the broadcaster (channel) that is sending the raid.
+/// This must be the same user that created the <paramref name="accessToken"/>.
+/// </param>
+/// <param name="toBroadcasterId">The user id of the broadcaster (channel) to send the raid to.</param>
+public class StartRaidRequest(
+    string clientId,
+    string accessToken,
+    string fromBroadcasterId,
+    string toBroadcasterId
+    )
+    : HelixApiRequest<StartRaidResponse>(
+        "raids" +
+        new HttpQueryParameters()
+            .Add("from_broadcaster_id", fromBroadcasterId)
+            .Add("to_broadcaster_id", toBroadcasterId),
+        clientId,
+        accessToken
+        )
+{
+    public override HttpMethod Method => HttpMethod.Post;
+}
---
+++ TwitchySharp.Api/Helix/Raids/Responses/StartRaidResponse.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwitchySharp.Api.Helix.Raids;
+/// <inheritdoc cref="StartedRaid"/>.
+public record StartRaidResponse
+{
+    /// <summary>
+    /// A list that contains a single object with information about the pending raid.
+    /// </summary>
+    public required StartedRaid[] Data { get; init; }
+}
+
+/// <summary>
+/// Contains information about the started raid.
+/// </summary>
+public record StartedRaid
+{
+    /// <summary>
+    /// The date and time of when the raid was requested.
+    /// </summary>
+    public required DateTimeOffset CreatedAt { get; init; }
+    /// <summary>
+    /// Indicates whether the channel being raided contains mature content.
+    /// </summary>
+    public required bool IsMature { get; init; }
+}
