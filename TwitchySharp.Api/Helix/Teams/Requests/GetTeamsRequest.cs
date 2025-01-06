8dd1fdee73d81071b0dc19f194056f728fd4f6f7
Yoyoyopo5 <118655115+Yoyoyopo5@users.noreply.github.com>
2025-01-06 12:06:56 -0600

    Add Get Teams API

---
+++ TwitchySharp.Api/Helix/Teams/Requests/GetTeamsRequest.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TwitchySharp.Helpers;
+
+namespace TwitchySharp.Api.Helix.Teams;
+/// <summary>
+/// Gets information about the specified Twitch team.
+/// <br/>
+/// See <see href="https://dev.twitch.tv/docs/api/reference/#get-teams">get teams</see> for more information.
+/// </summary>
+/// <remarks>
+/// Requires an app or user access token.
+/// </remarks>
+/// <param name="clientId">The client id of the application.</param>
+/// <param name="accessToken">An app or user access token.</param>
+/// <param name="query">
+/// The query to search for teams.
+/// Use an instance of <see cref="TeamsQueryById"/> or <see cref="TeamsQueryByName"/> depending on how you want to find the team.
+/// </param>
+public class GetTeamsRequest(
+    string clientId,
+    string accessToken,
+    TeamsQuery query
+    )
+    : HelixApiRequest<GetTeamsResponse>(
+        "/teams" +
+        new HttpQueryParameters()
+            .Add("name", query.Name)
+            .Add("id", query.Name),
+        clientId,
+        accessToken
+        );
+
+/// <summary>
+/// Use derived classes <see cref="TeamsQueryById"/> and <see cref="TeamsQueryByName"/> to create a teams query.
+/// </summary>
+public record TeamsQuery
+{
+    /// <summary>
+    /// The name of the team to get. 
+    /// </summary>
+    public string? Name { get; protected set; }
+    /// <summary>
+    /// The id of the team to get.
+    /// </summary>
+    public string? Id { get; protected set; }
+    protected TeamsQuery() { }
+}
+
+/// <summary>
+/// Query for a team by team name.
+/// </summary>
+public record TeamsQueryByName
+    : TeamsQuery
+{
+    /// <summary>
+    /// <inheritdoc cref="TeamsQueryByName"/>
+    /// </summary>
+    /// <param name="name">
+    /// <inheritdoc cref="TeamsQuery" path="/summary"/>
+    /// </param>
+    public TeamsQueryByName(string name)
+        => Name = name;
+}
+
+/// <summary>
+/// Query for a team by team id.
+/// </summary>
+public record TeamsQueryById
+    : TeamsQuery
+{
+    /// <summary>
+    /// <inheritdoc cref="TeamsQueryById"/>
+    /// </summary>
+    /// <param name="teamId">
+    /// <inheritdoc cref="TeamsQuery.Id" path="/summary"/>
+    /// </param>
+    public TeamsQueryById(string teamId)
+        => Id = teamId;
+}
---
+++ TwitchySharp.Api/Helix/Teams/Responses/GetTeamsResponse.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwitchySharp.Api.Helix.Teams;
+/// <inheritdoc cref="TwitchTeam"/>
+public record GetTeamsResponse
+{
+    /// <summary>
+    /// A list that contains a single team.
+    /// </summary>
+    public required TwitchTeam[] Data { get; init; }
+}
+
+/// <summary>
+/// Contains information about a specific team.
+/// </summary>
+public record TwitchTeam
+{
+    /// <summary>
+    /// The members of the team.
+    /// </summary>
+    public required TeamMember[] Users { get; init; }
+    /// <summary>
+    /// A URL to the team’s background image.
+    /// </summary>
+    public required string BackgroundImageUrl { get; init; }
+    /// <summary>
+    /// A URL to the team’s banner.
+    /// </summary>
+    public required string Banner { get; init; }
+    /// <summary>
+    /// The date and time when the team was created.
+    /// </summary>
+    public required DateTimeOffset CreatedAt { get; init; }
+    /// <summary>
+    /// The date and time of the last update to the team.
+    /// </summary>
+    public required DateTimeOffset UpdatedAt { get; init; }
+    /// <summary>
+    /// The team’s description. 
+    /// The description may contain formatting such as Markdown, HTML, newline (\n) characters, etc.
+    /// </summary>
+    public required string Info { get; init; }
+    /// <summary>
+    /// A URL to a thumbnail image of the team’s logo.
+    /// </summary>
+    public required string ThumbnailUrl { get; init; }
+    /// <summary>
+    /// The team’s name.
+    /// </summary>
+    public required string TeamName { get; init; }
+    /// <summary>
+    /// The team’s display name.
+    /// </summary>
+    public required string TeamDisplayName { get; init; }
+    /// <summary>
+    /// The id of the team.
+    /// </summary>
+    public required string Id { get; init; }
+}
+
+public record TeamMember
+{
+    public required string UserId { get; init; }
+    public required string UserLogin { get; init; }
+    public required string UserName { get; init; }
+}
