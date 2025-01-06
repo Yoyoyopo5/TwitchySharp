fea9b192d54a09e0c06e058ddfab53d71ea6193a
Yoyoyopo5 <yoyoyopo5@DESKTOP-IT4LOU9>
2025-01-06 05:37:43 -0600

    Add Create Stream Marker API

---
+++ TwitchySharp.Api/Helix/Streams/Responses/CreateStreamMarkerResponse.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwitchySharp.Api.Helix.Streams;
+/// <summary>
+/// Contains information about a newly created stream marker.
+/// </summary>
+public record CreateStreamMarkerResponse
+{
+    /// <summary>
+    /// A list containing the single stream marker that was created.
+    /// </summary>
+    public required StreamMarker[] Data { get; init; }
+}
+
+/// <summary>
+/// Contains information about a specific stream marker.
+/// </summary>
+public record StreamMarker
+{
+    /// <summary>
+    /// The id of the marker.
+    /// </summary>
+    public required string Id { get; init; }
+    /// <summary>
+    /// The date and time when the marker was created.
+    /// </summary>
+    public required DateTimeOffset CreatedAt { get; init; }
+    /// <summary>
+    /// The relative offset in seconds of the marker from the beginning of the stream.
+    /// </summary>
+    public required int PositionSeconds { get; init; }
+    /// <summary>
+    /// A description that the user gave the marker to help them remember why they marked the location.
+    /// </summary>
+    public required string Description { get; init; }
+}
