67e5213f4c04903ee6bd09f375395d22616cdab0
Yoyoyopo5 <118655115+Yoyoyopo5@users.noreply.github.com>
2025-01-06 03:47:00 -0600

    Add Get Stream Key API

---
+++ TwitchySharp.Api/Helix/Streams/Requests/GetStreamKeyRequest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TwitchySharp.Helpers;
+using TwitchySharp.Api.Authorization;
+
+namespace TwitchySharp.Api.Helix.Streams;
+/// <summary>
+/// Gets the channel’s stream key.
+/// <br/>
+/// See <see href="https://dev.twitch.tv/docs/api/reference/#get-stream-key"/> for more information.
+/// </summary>
+/// <remarks>
+/// Requires a user access token that includes <see cref="Scope.ChannelReadStreamKey"/>.
+/// </remarks>
+/// <param name="clientId">The client id of the application.</param>
+/// <param name="accessToken">A user access token that includes <see cref="Scope.ChannelReadStreamKey"/>.</param>
+/// <param name="broadcasterId">
+/// The user id of the broadcaster (channel) to get the stream key for.
+/// This must be the same user that created the <paramref name="accessToken"/>.
+/// </param>
+public class GetStreamKeyRequest(
+    string clientId,
+    string accessToken,
+    string broadcasterId
+    )
+    : HelixApiRequest<GetStreamKeyResponse>(
+        "/streams/key" +
+        new HttpQueryParameters()
+            .Add("broadcaster_id", broadcasterId),
+        clientId,
+        accessToken
+        );
---
+++ TwitchySharp.Api/Helix/Streams/Responses/GetStreamKeyResponse.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwitchySharp.Api.Helix.Streams;
+/// <inheritdoc cref="ChannelStreamKey"/>
+public record GetStreamKeyResponse
+{
+    /// <summary>
+    /// A list containing a single object with the stream key.
+    /// </summary>
+    public required ChannelStreamKey[] Data { get; init; }
+}
+
+/// <summary>
+/// Contains a broadcaster's stream key.
+/// </summary>
+public record ChannelStreamKey
+{
+    /// <summary>
+    /// The broadcaster's stream key.
+    /// </summary>
+    public required string StreamKey { get; init; }
+}
