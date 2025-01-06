a1621bed7ae55bbe05d098b523d784563285b85b
Yoyoyopo5 <yoyoyopo5@DESKTOP-IT4LOU9>
2025-01-06 05:15:28 -0600

    Add Get Followed Streams API

---
+++ TwitchySharp.Api/Helix/Streams/Responses/GetFollowedStreamsResponse.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TwitchySharp.Api.Models;
+
+namespace TwitchySharp.Api.Helix.Streams;
+/// <summary>
+/// Contains a list of active streams that a specific user follows.
+/// </summary>
+public record GetFollowedStreamsResponse
+{
+    /// <summary>
+    /// The list of active followed streams.
+    /// </summary>
+    public required TwitchStream[] Data { get; init; }
+    /// <inheritdoc cref="Models.Pagination"/>
+    public required Pagination Pagination { get; init; }
+}
