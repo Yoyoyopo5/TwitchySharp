0adb94ec1163f2d30590e3475bf684e99a98bb20
Yoyoyopo5 <118655115+Yoyoyopo5@users.noreply.github.com>
2025-01-06 07:24:16 -0600

    Move enum

---
+++ TwitchySharp.Api/Helix/Subscriptions/Enums/SubscriptionTier.cs
@@ -0,0 +1,40 @@
+﻿using System.Text.Json.Serialization;
+using System.Text.Json;
+using System;
+
+namespace TwitchySharp.Api.Helix.Subscriptions;
+
+/// <summary>
+/// Available subscription tiers.
+/// </summary>
+public enum SubscriptionTier
+{
+    Tier1,
+    Tier2,
+    Tier3
+}
+
+public class SubscriptionTierJsonConverter : JsonConverter<SubscriptionTier>
+{
+    private const string TIER_1 = "1000";
+    private const string TIER_2 = "2000";
+    private const string TIER_3 = "3000";
+
+    public override SubscriptionTier Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        => reader.GetString() switch
+        {
+            TIER_1 => SubscriptionTier.Tier1,
+            TIER_2 => SubscriptionTier.Tier2,
+            TIER_3 => SubscriptionTier.Tier3,
+            _ => throw new NotSupportedException()
+        };
+
+    public override void Write(Utf8JsonWriter writer, SubscriptionTier value, JsonSerializerOptions options)
+        => writer.WriteStringValue(value switch
+        {
+            SubscriptionTier.Tier1 => TIER_1,
+            SubscriptionTier.Tier2 => TIER_2,
+            SubscriptionTier.Tier3 => TIER_3,
+            _ => throw new NotSupportedException()
+        });
+}
