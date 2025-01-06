99107e1c4f21647ef76e77f6152b4704c0e9293c
Yoyoyopo5 <yoyoyopo5@DESKTOP-IT4LOU9>
2025-01-06 07:38:16 -0600

    Add Check User Subscription API

---
+++ TwitchySharp.Api/Helix/Subscriptions/Requests/CheckUserSubscriptionRequest.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TwitchySharp.Helpers;
+using TwitchySharp.Api.Authorization;
+
+namespace TwitchySharp.Api.Helix.Subscriptions;
+/// <summary>
+/// Checks whether the user subscribes to the broadcaster’s channel.
+/// <br/>
+/// See <see href="https://dev.twitch.tv/docs/api/reference/#check-user-subscription">check user subscription</see> for more information.
+/// </summary>
+/// <remarks>
+/// Requires a user access token that includes <see cref="Scope.UserReadSubscriptions"/>.
+/// <para>
+/// A Twitch extension may use an app access token if the broadcaster has granted <see cref="Scope.UserReadSubscriptions"/> from within the Twitch Extensions manager.
+/// </para>
+/// </remarks>
+/// <param name="clientId">The client id of the application.</param>
+/// <param name="accessToken">A user access token that includes <see cref="Scope.UserReadSubscriptions"/>, or an app access token if the application is an extension and the broadcaster has granted <see cref="Scope.UserReadSubscriptions"/> from within the Twitch Extensions manager.</param>
+/// <param name="broadcasterId">The user id of the broadcaster that the subscription is to.</param>
+/// <param name="userId">
+/// The id of the user to get the subscription for.
+/// This must be the same user that created the <paramref name="accessToken"/>.
+/// </param>
+public class CheckUserSubscriptionRequest(
+    string clientId,
+    string accessToken,
+    string broadcasterId,
+    string userId
+    )
+    : HelixApiRequest<CheckUserSubscriptionResponse>(
+        "/subscriptions/user" +
+        new HttpQueryParameters()
+            .Add("broadcaster_id", broadcasterId)
+            .Add("user_id", userId),
+        clientId,
+        accessToken
+        );
---
+++ TwitchySharp.Api/Helix/Subscriptions/Responses/CheckUserSubscriptionResponse.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace TwitchySharp.Api.Helix.Subscriptions;
+/// <inheritdoc cref="UserSubscription"/>
+public record CheckUserSubscriptionResponse
+{
+    /// <summary>
+    /// A list containing a single object with information about the user's subscription.
+    /// </summary>
+    public required UserSubscription[] Data { get; init; }
+}
+
+/// <summary>
+/// Contains information about a specific user's subscription.
+/// </summary>
+public record UserSubscription
+{
+    /// <inheritdoc cref="BroadcasterSubscriber.BroadcasterId"/>
+    public required string BroadcasterId { get; init; }
+    /// <inheritdoc cref="BroadcasterSubscriber.BroadcasterLogin"/>
+    public required string BroadcasterLogin { get; init; }
+    /// <inheritdoc cref="BroadcasterSubscriber.BroadcasterName"/>
+    public required string BroadcasterName { get; init; }
+    /// <inheritdoc cref="BroadcasterSubscriber.GifterId"/>
+    public required string GifterId { get; init; }
+    /// <inheritdoc cref="BroadcasterSubscriber.GifterLogin"/>
+    public required string GifterLogin { get; init; }
+    /// <inheritdoc cref="BroadcasterSubscriber.GifterName"/>
+    public required string GifterName { get; init; }
+    /// <inheritdoc cref="BroadcasterSubscriber.IsGift"/>
+    public required bool IsGift { get; init; }
+    /// <inheritdoc cref="BroadcasterSubscriber.Tier"/>
+    [JsonConverter(typeof(SubscriptionTierJsonConverter))]
+    public required SubscriptionTier Tier { get; init; }
+}
