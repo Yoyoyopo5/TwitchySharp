f0f66bd55d11b88d5cade53d782f64734746646a
Yoyoyopo5 <yoyoyopo5@DESKTOP-IT4LOU9>
2025-01-07 08:51:09 -0600

    Add Get User Extensions API

---
+++ TwitchySharp.Api/Helix/Users/Enums/UserExtensionType.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using TwitchySharp.Helpers;
+
+namespace TwitchySharp.Api.Helix.Users;
+/// <summary>
+/// Contains static definitions for possible values for extension types.
+/// See <see href="https://dev.twitch.tv/docs/extensions/#extension-types">Extension Types</see>
+/// </summary>
+/// <param name="Value">
+/// A custom value for extension type. 
+/// Use this if a static definition is not available for the type you want.
+/// </param>
+[JsonConverter(typeof(ValueBackedEnumJsonConverter<UserExtensionType, string>))] // Using this here so we can deserialize an array of them.
+public record UserExtensionType(string Value)
+    : ValueBackedEnum<string>(Value)
+{
+    /// <summary>
+    /// Displays as part of the video, taking up part of the screen. Component Extensions can be hidden by viewers.
+    /// </summary>
+    public static UserExtensionType Component { get; } = new("component");
+    /// <summary>
+    /// Displayed similarly to a panel extension on a mobile device.
+    /// </summary>
+    public static UserExtensionType Mobile { get; } = new("mobile");
+    /// <summary>
+    /// Displays on top of the whole video as a transparent overlay.
+    /// </summary>
+    public static UserExtensionType Overlay { get; } = new("overlay");
+    /// <summary>
+    /// Displays in a box under the video.
+    /// </summary>
+    public static UserExtensionType Panel { get; } = new("panel");
+}
---
+++ TwitchySharp.Api/Helix/Users/Requests/GetUserExtensionsRequest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TwitchySharp.Api.Authorization;
+
+namespace TwitchySharp.Api.Helix.Users;
+/// <summary>
+/// Gets a list of all extensions (both active and inactive) that a broadcaster has installed.
+/// <br/>
+/// See <see href="https://dev.twitch.tv/docs/api/reference/#get-user-extensions">get user extensions</see> for more information.
+/// </summary>
+/// <remarks>
+/// Requires a user access token that includes <see cref="Scope.UserReadBroadcast"/> or <see cref="Scope.UserEditBroadcast"/>. 
+/// <see cref="Scope.UserEditBroadcast"/> is required to get inactive extensions.
+/// The user who created the token is the broadcaster to get extensions for.
+/// </remarks>
+/// <param name="clientId">The client id of the application.</param>
+/// <param name="accessToken">
+/// A user access token that includes <see cref="Scope.UserReadBroadcast"/> or <see cref="Scope.UserEditBroadcast"/>.
+/// The user who created the token is the broadcaster to get extensions for.
+/// </param>
+public class GetUserExtensionsRequest(
+    string clientId,
+    string accessToken
+    )
+    : HelixApiRequest<GetUserExtensionsResponse>(
+        "/users/extensions/list",
+        clientId,
+        accessToken
+        );
