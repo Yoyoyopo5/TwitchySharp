2e58ca42b11b84896d048aa334488e4eaeecf9e9
Yoyoyopo5 <yoyoyopo5@DESKTOP-IT4LOU9>
2025-01-07 09:30:12 -0600

    Add Get User Active Extensions API

---
+++ TwitchySharp.Api/Helix/Users/Requests/GetUserActiveExtensionsRequest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TwitchySharp.Helpers;
+using TwitchySharp.Api.Authorization;
+
+namespace TwitchySharp.Api.Helix.Users;
+/// <summary>
+/// Gets the active extensions that the broadcaster has installed for each configuration.
+/// <br/>
+/// See <see href="https://dev.twitch.tv/docs/api/reference/#get-user-active-extensions">get user active extensions</see> for more information.
+/// </summary>
+/// <remarks>
+/// Requires an app or user access token.
+/// To include extensions that are under development, you must use a user access token that includes <see cref="Scope.UserReadBroadcast"/> or <see cref="Scope.UserEditBroadcast"/> and was created by the <paramref name="userId"/>.
+/// </remarks>
+/// <param name="clientId">The client id of the application.</param>
+/// <param name="accessToken">An app or user access token.</param>
+/// <param name="userId">
+/// The user id of the broadcaster to get active extensions for.
+/// <para>
+/// Note: Optional only if using a user access token for <paramref name="accessToken"/>. In that case, the user that created the token is the one to get extensions for.
+/// </para>
+/// </param>
+public class GetUserActiveExtensionsRequest(
+    string clientId,
+    string accessToken,
+    string? userId
+    )
+    : HelixApiRequest<GetUserActiveExtensionsResponse>(
+        "/users/extensions" +
+        new HttpQueryParameters()
+            .Add("user_id", userId),
+        clientId,
+        accessToken
+        );
---
+++ TwitchySharp.Api/Helix/Users/Responses/GetUserActiveExtensionsResponse.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace TwitchySharp.Api.Helix.Users;
+/// <inheritdoc cref="UserActiveExtensions"/>
+public record GetUserActiveExtensionsResponse
+{
+    /// <summary>
+    /// The active extensions that the broadcaster has installed.
+    /// </summary>
+    public required UserActiveExtensions Data { get; init; }
+}
+
+/// <summary>
+/// Contains information about a broadcaster's active extensions, grouped by extension type.
+/// </summary>
+public record UserActiveExtensions
+{
+    /// <summary>
+    /// A dictionary that contains active panel extensions. 
+    /// The dictionary keys are sequential numbers beginning with 1.
+    /// </summary>
+    public required ImmutableDictionary<string, UserActiveExtension> Panel { get; init; }
+    /// <summary>
+    /// A dictionary that contains active overlay extension.
+    /// The dictionary keys are sequential numbers beginning with 1.
+    /// </summary>
+    public required ImmutableDictionary<string, UserActiveExtension> Overlay { get; init; }
+    /// <summary>
+    /// A dictionary that contains active component extensions.
+    /// The dictionary keys are sequential numbers beginning with 1.
+    /// </summary>
+    public required ImmutableDictionary<string, UserActiveComponentExtension> Component { get; init; }
+}
+
+/// <summary>
+/// Contains information about a specific active extension.
+/// </summary>
+public record UserActiveExtension
+{
+    /// <summary>
+    /// Indicates the extension’s activation state. 
+    /// If <see langword="false"/>, the user has not configured this extension.
+    /// </summary>
+    public required bool Active { get; init; }
+    /// <summary>
+    /// The id of the extension.
+    /// </summary>
+    public required string Id { get; init; }
+    /// <summary>
+    /// The version of the extension.
+    /// </summary>
+    public required string Version { get; init; }
+    /// <summary>
+    /// The name of the extension.
+    /// </summary>
+    public required string Name { get; init; }
+}
+
+/// <summary>
+/// Contains information about a specific active component extension.
+/// </summary>
+public record UserActiveComponentExtension
+    : UserActiveExtension
+{
+    /// <summary>
+    /// The x-coordinate where the extension is placed.
+    /// </summary>
+    public required int X { get; init; }
+    /// <summary>
+    /// The y-coordinate where the extension is placed.
+    /// </summary>
+    public required int Y { get; init; }
+}
