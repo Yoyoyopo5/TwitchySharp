0f43c753499f68301496194c96a613f1382e7ce8
Yoyoyopo5 <yoyoyopo5@DESKTOP-IT4LOU9>
2025-01-09 17:14:32 -0600

    Move model

---
+++ TwitchySharp.Api/Helix/Users/Models/InstalledExtension.cs
@@ -0,0 +1,28 @@
+﻿namespace TwitchySharp.Api.Helix.Users;
+
+/// <summary>
+/// Contains information about a specific installed extension.
+/// </summary>
+public record InstalledExtension
+{
+    /// <summary>
+    /// The id of the extension.
+    /// </summary>
+    public required string Id { get; init; }
+    /// <summary>
+    /// The version of the extension.
+    /// </summary>
+    public required string Version { get; init; }
+    /// <summary>
+    /// The name of the extension.
+    /// </summary>
+    public required string Name { get; init; }
+    /// <summary>
+    /// Indicates whether the extension is configured and can be activated.
+    /// </summary>
+    public required bool CanActivate { get; init; }
+    /// <summary>
+    /// The extension types that you can activate for this extension.
+    /// </summary>
+    public required UserExtensionType[] Type { get; init; }
+}
--- TwitchySharp.Api/Helix/Users/Responses/GetUserExtensionsResponse.cs
+++ TwitchySharp.Api/Helix/Users/Responses/GetUserExtensionsResponse.cs
@@ -17,30 +17,3 @@ public record GetUserExtensionsResponse
     /// </summary>
     public required InstalledExtension[] Data { get; init; }
 }
-
-/// <summary>
-/// Contains information about a specific installed extension.
-/// </summary>
-public record InstalledExtension
-{
-    /// <summary>
-    /// The id of the extension.
-    /// </summary>
-    public required string Id { get; init; }
-    /// <summary>
-    /// The version of the extension.
-    /// </summary>
-    public required string Version { get; init; }
-    /// <summary>
-    /// The name of the extension.
-    /// </summary>
-    public required string Name { get; init; }
-    /// <summary>
-    /// Indicates whether the extension is configured and can be activated.
-    /// </summary>
-    public required bool CanActivate { get; init; }
-    /// <summary>
-    /// The extension types that you can activate for this extension.
-    /// </summary>
-    public required UserExtensionType[] Type { get; init; }
-}
