2e4313a05461a2279a5d46138c3f46d4435c0fac
Yoyoyopo5 <yoyoyopo5@DESKTOP-IT4LOU9>
2025-01-12 20:27:50 -0600

    Add TimeSpan json converters

---
+++ TwitchySharp.Helpers/JsonConverters/TimeSpan/MinutesTimeSpanJsonConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace TwitchySharp.Helpers.JsonConverters;
+public class MinutesTimeSpanJsonConverter : JsonConverter<TimeSpan>
+{
+    public override TimeSpan Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        => reader.TokenType switch
+        {
+            JsonTokenType.Number => TimeSpan.FromMinutes(reader.GetDouble()),
+            JsonTokenType.String => reader.GetString() switch
+            {
+                string value => TimeSpan.FromMinutes(double.Parse(value)),
+                _ => default
+            },
+            _ => default
+        };
+
+    public override void Write(Utf8JsonWriter writer, TimeSpan value, JsonSerializerOptions options)
+        => writer.WriteNumberValue(value.TotalSeconds);
+}
---
+++ TwitchySharp.Helpers/JsonConverters/TimeSpan/SecondsTimeSpanJsonConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace TwitchySharp.Helpers.JsonConverters;
+public class SecondsTimeSpanJsonConverter : JsonConverter<TimeSpan>
+{
+    public override TimeSpan Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        => reader.TokenType switch
+        {
+            JsonTokenType.Number => TimeSpan.FromSeconds(reader.GetDouble()),
+            JsonTokenType.String => reader.GetString() switch
+            {
+                string value => TimeSpan.FromSeconds(double.Parse(value)),
+                _ => default
+            },
+            _ => default
+        };
+
+    public override void Write(Utf8JsonWriter writer, TimeSpan value, JsonSerializerOptions options)
+        => writer.WriteNumberValue(value.TotalSeconds);
+}
