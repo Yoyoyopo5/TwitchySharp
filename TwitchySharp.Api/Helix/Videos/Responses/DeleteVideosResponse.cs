65aaba4d5322e81623a46d44e756110923fb3393
Yoyoyopo5 <118655115+Yoyoyopo5@users.noreply.github.com>
2025-01-13 01:22:09 -0600

    Add Delete Videos API

---
+++ TwitchySharp.Api/Helix/Videos/Responses/DeleteVideosResponse.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwitchySharp.Api.Helix.Videos;
+/// <summary>
+/// Contains a list of deleted videos.
+/// </summary>
+public record DeleteVideosResponse
+{
+    /// <summary>
+    /// The list of videos ids that were deleted.
+    /// </summary>
+    public required string[] Data { get; init; }
+}
