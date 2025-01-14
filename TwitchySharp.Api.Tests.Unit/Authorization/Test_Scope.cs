77c35f6579108d7ac1ca136ceb51bac87c96dafc
Yoyoyopo5 <118655115+Yoyoyopo5@users.noreply.github.com>
2025-01-13 20:39:02 -0600

    Add FormatScope test

---
+++ TwitchySharp.Api.Tests.Unit/Authorization/Test_Scope.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TwitchySharp.Api.Authorization;
+
+namespace TwitchySharp.Api.Tests.Unit.Authorization;
+public class Test_Scope
+{
+    [Fact]
+    public void FormatScopes_SingleScope_ReturnsScopeString()
+    {
+        const string MOCK_SCOPE_STRING = "analytics:read:extensions";
+
+        IEnumerable<Scope> stubScopes = [Scope.AnalyticsReadExtensions];
+
+        string result = stubScopes.FormatScopes();
+
+        Assert.Equal(MOCK_SCOPE_STRING, result);
+    }
+}
