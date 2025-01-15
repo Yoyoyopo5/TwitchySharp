c7bb6fe2eced90495c6e8a7955382b22011843d7
Yoyoyopo5 <yoyoyopo5@DESKTOP-IT4LOU9>
2025-01-14 21:58:20 -0600

    Fix AuthorizationCodeRequest type param and add documentation

--- TwitchySharp.Api/Authorization/Requests/AuthorizationCodeRequest.cs
+++ TwitchySharp.Api/Authorization/Requests/AuthorizationCodeRequest.cs
@@ -2,18 +2,27 @@
 using System.Collections.Generic;
 using System.Net.Http;
 using System.Text;
+using TwitchySharp.Api.Authorization.ClientUrls;
 
 namespace TwitchySharp.Api.Authorization;
 /// <summary>
-/// 
+/// Used to get a user access token and refresh token.
+/// <br/>
+/// See <see href="https://dev.twitch.tv/docs/authentication/getting-tokens-oauth/#authorization-code-grant-flow">Authorization Code Grant Flow</see> for more information.
 /// </summary>
-/// <typeparam name="TResponse"></typeparam>
-/// <param name="clientId"></param>
-/// <param name="clientSecret"></param>
-/// <param name="code"></param>
-/// <param name="redirectUri"></param>
-public class AuthorizationCodeRequest<TResponse>(string clientId, string clientSecret, string code, string redirectUri) // Think of a better name?
-    : AuthorizationApiRequest<TResponse>("/token")
+/// <remarks>
+/// This flow is meant for apps that use a server, can securely store a client secret, and can make server-to-server requests to the Twitch API.
+/// Requires a code obtained when a user authorizes your application via an <see cref="AuthorizationCodeGrantUrl"/>.
+/// </remarks>
+/// <param name="clientId">The client id of the application.</param>
+/// <param name="clientSecret">The client secret of the application.</param>
+/// <param name="code">The code query parameter obtained from the redirect of an <see cref="AuthorizationCodeGrantUrl"/>.</param>
+/// <param name="redirectUri">
+/// The redirect URI of the application (this is registered via the Twitch developer console).
+/// This should be the URI that the <see cref="AuthorizationCodeGrantUrl"/> redirected to.
+/// </param>
+public class AuthorizationCodeRequest(string clientId, string clientSecret, string code, string redirectUri) // Think of a better name?
+    : AuthorizationApiRequest<AuthorizationCodeResponse>("/token")
 {
     /// <summary>
     /// The client id of the application that will be used in the request.
