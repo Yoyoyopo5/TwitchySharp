bfde01b37eeaa25a3dc5e15f145aa604c9ff39d5
Yoyoyopo5 <yoyoyopo5@DESKTOP-IT4LOU9>
2025-01-15 16:20:27 -0600

    Add ClientCredentialsRequest test

---
+++ TwitchySharp.Api.Tests.Integration/Authorization/Test_ClientCredentialsRequest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TwitchySharp.Api.Authorization;
+
+namespace TwitchySharp.Api.Tests.Integration.Authorization;
+[Collection("authorization")]
+public class Test_ClientCredentialsRequest
+{
+    private AuthorizationFixture _fixture;
+    public Test_ClientCredentialsRequest(AuthorizationFixture fixture)
+        => _fixture = fixture;
+
+    [Fact]
+    public async void Send_ClientCredentialsRequest_ReturnSuccessResponse()
+    {
+        ClientCredentialsRequest stubRequest = new(_fixture.ClientId, _fixture.ClientSecret);
+
+        ClientCredentialsResponse actualResponse = await _fixture.Api.SendRequestAsync(stubRequest);
+    }
+}
