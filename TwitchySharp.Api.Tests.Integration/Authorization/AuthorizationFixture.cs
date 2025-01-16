8a7eda6cdd8a95e995ab49c1952f8aab239fef4a
Yoyoyopo5 <118655115+Yoyoyopo5@users.noreply.github.com>
2025-01-15 19:09:47 -0600

    Refactor tests to use secrets config

--- TwitchySharp.Api.Tests.Integration/Authorization/AuthorizationFixture.cs
+++ TwitchySharp.Api.Tests.Integration/Authorization/AuthorizationFixture.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.RateLimiting;
 using System.Threading.Tasks;
@@ -9,15 +11,16 @@ namespace TwitchySharp.Api.Tests.Integration.Authorization;
 public class AuthorizationFixture : IDisposable
 {
     public TwitchApi Api { get; }
-    public string ClientId { get; }
-    public string ClientSecret { get; }
-    public string RedirectUrl => "http://localhost:5000";
+    public AuthorizationSecrets Secrets { get; }
     private readonly TwitchHttpClient _twitchHttpClient;
     private readonly HttpClient _httpClient;
     private readonly RateLimiter _rateLimiter;
+    
 
     public AuthorizationFixture()
     {
+        Secrets = new ConfigurationBuilder().AddUserSecrets(Assembly.GetExecutingAssembly()).Build().GetRequiredSection("Authorization").Get<AuthorizationSecrets>()!;
+        
         _httpClient = new HttpClient();
         _rateLimiter = new TokenBucketRateLimiter(new TokenBucketRateLimiterOptions()
         {
@@ -28,9 +31,6 @@ public class AuthorizationFixture : IDisposable
         });
         _twitchHttpClient = new TwitchHttpClient(_httpClient, _rateLimiter);
         Api = new TwitchApi(_twitchHttpClient);
-
-        ClientId = System.Environment.GetEnvironmentVariable("TWITCHYSHARP_TEST_CLIENT_ID", EnvironmentVariableTarget.User)!;
-        ClientSecret = System.Environment.GetEnvironmentVariable("TWITCHYSHARP_TEST_CLIENT_SECRET", EnvironmentVariableTarget.User)!;
     }
 
     public void Dispose()
@@ -42,3 +42,14 @@ public class AuthorizationFixture : IDisposable
 
 [CollectionDefinition("authorization")]
 public class AuthorizationCollection : ICollectionFixture<AuthorizationFixture> { }
+
+public record AuthorizationSecrets
+{
+    public required string ClientId { get; init; }
+    public required string ClientSecret { get; init; }
+    public required string RedirectUri { get; init; }
+    public required string UserAccessToken { get; init; }
+    public required string UserRefreshToken { get; init; }
+    public required string UserAuthorizationCode { get; init; }
+    public required string DeviceAuthorizationCode { get; init; }
+}
