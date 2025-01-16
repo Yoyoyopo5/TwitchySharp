c9aa5f45d19a9cf82784ffa67d5a67089d9d8ad8
Yoyoyopo5 <118655115+Yoyoyopo5@users.noreply.github.com>
2025-01-16 12:59:06 -0600

    Add ValidateAccessTokenRequest test

---
+++ TwitchySharp.Api.Tests.Integration/Authorization/Test_ValidateAccessTokenRequest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TwitchySharp.Api.Authorization;
+
+namespace TwitchySharp.Api.Tests.Integration.Authorization;
+[Collection("authorization")]
+public class Test_ValidateAccessTokenRequest(AuthorizationFixture fixture)
+{
+    private readonly AuthorizationFixture _fixture = fixture;
+
+    [Fact]
+    public async void Send_ValidateAccessTokenRequest_ReturnSuccessResponse()
+    {
+        ValidateAccessTokenRequest stubRequest = new(_fixture.Secrets.UserAccessToken);
+
+        ValidateAccessTokenResponse actualResponse = await _fixture.Api.SendRequestAsync(stubRequest);
+    }
+}
