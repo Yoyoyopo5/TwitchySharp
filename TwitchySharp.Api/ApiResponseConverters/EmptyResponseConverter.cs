0fdccd19e6b73978eee0d4b5afa20753b55afbec
Yoyoyopo5 <118655115+Yoyoyopo5@users.noreply.github.com>
2025-01-16 08:19:55 -0600

    Add converter for empty responses

---
+++ TwitchySharp.Api/ApiResponseConverters/EmptyResponseConverter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TwitchySharp.Api.ApiResponseConverters;
+internal class EmptyResponseConverter : IConvertApiResponse
+{
+    public ValueTask<TResponse> Convert<TResponse>(HttpResponseMessage httpResponse, CancellationToken ct = default)
+        => ValueTask.FromResult(Activator.CreateInstance<TResponse>());
+}
