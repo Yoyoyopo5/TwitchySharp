71d5d6172738ec3051afc3a55e90f2a60d7bf05c
Yoyoyopo5 <yoyoyopo5@DESKTOP-IT4LOU9>
2025-01-17 06:50:52 -0600

    Add fixture for Helix API testing

---
+++ TwitchySharp.Api.Tests.Integration/Helix/HelixFixture.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwitchySharp.Api.Tests.Integration.Helix;
+public class HelixFixture() : ApiFixture<HelixSecrets>("Helix");
+
+[CollectionDefinition("helix")]
+public class HelixCollection : ICollectionFixture<HelixFixture> { }
+
+public record HelixSecrets
+{
+    public required string ClientId { get; init; }
+    public required string ClientSecret { get; init; }
+    public required string UserAccessToken { get; init; }
+    public required string AppAccessToken { get; init; }
+}
