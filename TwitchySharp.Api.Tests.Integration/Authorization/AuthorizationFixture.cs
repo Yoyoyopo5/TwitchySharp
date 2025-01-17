6f770024cf5e50cc8570d523beccaacd3ed74c35
Yoyoyopo5 <yoyoyopo5@DESKTOP-IT4LOU9>
2025-01-17 06:50:19 -0600

    Generalize fixture

--- TwitchySharp.Api.Tests.Integration/Authorization/AuthorizationFixture.cs
+++ TwitchySharp.Api.Tests.Integration/Authorization/AuthorizationFixture.cs
@@ -8,37 +8,7 @@ using System.Threading.RateLimiting;
 using System.Threading.Tasks;
 
 namespace TwitchySharp.Api.Tests.Integration.Authorization;
-public class AuthorizationFixture : IDisposable
-{
-    public TwitchApi Api { get; }
-    public AuthorizationSecrets Secrets { get; }
-    private readonly TwitchHttpClient _twitchHttpClient;
-    private readonly HttpClient _httpClient;
-    private readonly RateLimiter _rateLimiter;
-
-
-    public AuthorizationFixture()
-    {
-        Secrets = new ConfigurationBuilder().AddUserSecrets(Assembly.GetExecutingAssembly()).Build().GetRequiredSection("Authorization").Get<AuthorizationSecrets>()!;
-        
-        _httpClient = new HttpClient();
-        _rateLimiter = new TokenBucketRateLimiter(new TokenBucketRateLimiterOptions()
-        {
-            QueueLimit = 100,
-            TokenLimit = 800,
-            TokensPerPeriod = 800,
-            ReplenishmentPeriod = TimeSpan.FromHours(6) // I believe this is standard.
-        });
-        _twitchHttpClient = new TwitchHttpClient(_httpClient, _rateLimiter);
-        Api = new TwitchApi(_twitchHttpClient);
-    }
-
-    public void Dispose()
-    {
-        _httpClient.Dispose();
-        _rateLimiter.Dispose();
-    }
-}
+public class AuthorizationFixture() : ApiFixture<AuthorizationSecrets>("Authorization");
 
 [CollectionDefinition("authorization")]
 public class AuthorizationCollection : ICollectionFixture<AuthorizationFixture> { }
