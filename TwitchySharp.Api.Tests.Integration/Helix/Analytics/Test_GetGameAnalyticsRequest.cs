c8e629dc2c5517b532ea250e430d9db9214f6eab
Yoyoyopo5 <118655115+Yoyoyopo5@users.noreply.github.com>
2025-01-17 12:27:50 -0600

    Add GetGameAnalyticsRequest test

---
+++ TwitchySharp.Api.Tests.Integration/Helix/Analytics/Test_GetGameAnalyticsRequest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TwitchySharp.Api.Helix.Analytics;
+
+namespace TwitchySharp.Api.Tests.Integration.Helix.Analytics;
+[Collection("helix")]
+public class Test_GetGameAnalyticsRequest(HelixFixture fixture)
+{
+    private readonly HelixFixture _fixture = fixture;
+
+    [Fact]
+    public async void Send_GetGameAnalyticsRequest_ReturnSuccessResponse()
+    {
+        GetGameAnalyticsRequest stubRequest = new(_fixture.Secrets.ClientId, _fixture.Secrets.UserAccessToken);
+
+        GetGameAnalyticsResponse actualResponse = await _fixture.Api.SendRequestAsync(stubRequest);
+    }
+}
