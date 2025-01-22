446243c352566c2a7bfe15aef8c1ea5c269bf050
Yoyoyopo5 <118655115+Yoyoyopo5@users.noreply.github.com>
2025-01-22 15:18:23 -0600

    Add Get Charity Campaign test

---
+++ TwitchySharp.Api.Tests.Integration/Helix/Charity/Test_GetCharityCampaign.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TwitchySharp.Api.Helix.Charity;
+
+namespace TwitchySharp.Api.Tests.Integration.Helix.Charity;
+[Collection("helix")]
+public class Test_GetCharityCampaign(HelixFixture fixture)
+{
+    private readonly HelixFixture _fixture = fixture;
+
+    [Fact]
+    public async void Send_GetCharityCampaignRequest_ReturnSuccessResponse()
+    {
+        string broadcasterId = await _fixture.GetUserIdFromAccessTokenAsync();
+
+        await _fixture.Api.SendRequestAsync(new GetCharityCampaignRequest(
+            _fixture.Secrets.ClientId,
+            _fixture.Secrets.UserAccessToken,
+            broadcasterId
+            ));
+    }
+}
