83471f9dbc67190acda6953768e406509fe179e0
Yoyoyopo5 <yoyoyopo5@DESKTOP-IT4LOU9>
2025-01-22 14:29:10 -0600

    Add Modify Channel Imformation test

---
+++ TwitchySharp.Api.Tests.Integration/Helix/Channels/Test_ModifyChannelInformation.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TwitchySharp.Api.Helix.Channels;
+using TwitchySharp.Api.Models;
+
+namespace TwitchySharp.Api.Tests.Integration.Helix.Channels;
+[Collection("helix")]
+public class Test_ModifyChannelInformation(HelixFixture fixture)
+{
+    private readonly HelixFixture _fixture = fixture;
+
+    [Fact]
+    public async void Send_ModifyChannelInformationRequest_ReturnSuccessResponse()
+    {
+        string broadcasterId = await _fixture.GetUserIdFromAccessTokenAsync();
+
+        await _fixture.Api.SendRequestAsync(new ModifyChannelInformationRequest(
+            _fixture.Secrets.ClientId,
+            _fixture.Secrets.UserAccessToken,
+            broadcasterId,
+            new ModifyChannelInformationRequestData()
+            {
+                GameId = "1",
+                BroadcasterLanguage = "en",
+                Title = "test title pls ignore",
+                Tags = ["testtag"],
+                ContentClassificationLabels = [new ContentClassificationLabel(ContentClassificationLabelType.DrugsIntoxication, true)],
+                IsBrandedContent = false
+            }
+            ));
+    }
+}
