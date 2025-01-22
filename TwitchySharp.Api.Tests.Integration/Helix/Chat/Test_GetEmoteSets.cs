7c20e357bfc2cf8586d4af21d8f8e1c140437f67
Yoyoyopo5 <yoyoyopo5@DESKTOP-IT4LOU9>
2025-01-22 16:34:50 -0600

    Add Get Emote Sets test

---
+++ TwitchySharp.Api.Tests.Integration/Helix/Chat/Test_GetEmoteSets.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TwitchySharp.Api.Helix.Chat;
+using TwitchySharp.Api.Helix.Chat.Requests;
+
+namespace TwitchySharp.Api.Tests.Integration.Helix.Chat;
+[Collection("helix")]
+public class Test_GetEmoteSets(HelixFixture fixture)
+{
+    private readonly HelixFixture _fixture = fixture;
+
+    [Fact]
+    public async void Send_GetEmoteSetsRequest_ReturnSuccessResponse()
+    {
+        string broadcasterId = await _fixture.GetUserIdFromAccessTokenAsync();
+
+        string emoteSetId = await GetEmoteSetId(broadcasterId);
+
+        await _fixture.Api.SendRequestAsync(new GetEmoteSetsRequest(
+            _fixture.Secrets.ClientId,
+            _fixture.Secrets.UserAccessToken,
+            [emoteSetId]
+            ));
+    }
+
+    private async ValueTask<string> GetEmoteSetId(string broadcasterId)
+        => (await _fixture.Api.SendRequestAsync(new GetChannelEmotesRequest(
+                _fixture.Secrets.ClientId,
+                _fixture.Secrets.UserAccessToken,
+                broadcasterId
+            ))).Data.First().EmoteSetId;
+}
