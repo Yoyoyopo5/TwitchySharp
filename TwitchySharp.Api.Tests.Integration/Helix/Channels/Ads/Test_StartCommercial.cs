036c8296ba55f0655a5c6ddf5b4a117d39114872
Yoyoyopo5 <yoyoyopo5@DESKTOP-IT4LOU9>
2025-01-22 15:10:03 -0600

    Add Start Commerical test

---
+++ TwitchySharp.Api.Tests.Integration/Helix/Channels/Ads/Test_StartCommercial.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TwitchySharp.Api.Helix.Channels.Ads;
+
+namespace TwitchySharp.Api.Tests.Integration.Helix.Channels.Ads;
+[Collection("helix")]
+public class Test_StartCommercial(HelixFixture fixture)
+{
+    private readonly HelixFixture _fixture = fixture;
+
+    [Fact]
+    public async void Send_StartCommercialRequest_ReturnSuccessResponse()
+    {
+        string broadcasterId = await _fixture.GetUserIdFromAccessTokenAsync();
+
+        await _fixture.Api.SendRequestAsync(new StartCommericalRequest(
+            _fixture.Secrets.ClientId,
+            _fixture.Secrets.UserAccessToken,
+            broadcasterId,
+            30
+            ));
+    }
+}
