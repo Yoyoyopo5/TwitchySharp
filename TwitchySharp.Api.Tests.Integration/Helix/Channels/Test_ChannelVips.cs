8839e7ecd01f72733e9484c383c4f2c13a698834
Yoyoyopo5 <118655115+Yoyoyopo5@users.noreply.github.com>
2025-01-21 20:29:22 -0600

    Add Channel VIP tests

---
+++ TwitchySharp.Api.Tests.Integration/Helix/Channels/Test_ChannelVips.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TwitchySharp.Api.Helix.Channels;
+
+namespace TwitchySharp.Api.Tests.Integration.Helix.Channels;
+[Collection("helix")]
+public class Test_ChannelVips(HelixFixture fixture)
+{
+    private readonly HelixFixture _fixture = fixture;
+
+    [Fact]
+    public async void Send_VipRequests_ReturnSuccessResponse()
+    {
+        const string TEST_USER_ID = "52137752";
+
+        string broadcasterId = await _fixture.GetUserIdFromAccessTokenAsync();
+
+        await AddVip(TEST_USER_ID, broadcasterId);
+        ChannelVip addedVip = await GetVip(TEST_USER_ID, broadcasterId);
+        await RemoveVip(addedVip, broadcasterId);
+    }
+
+    private async ValueTask AddVip(string userId, string broadcasterId)
+        => await _fixture.Api.SendRequestAsync(
+                new AddChannelVipRequest(
+                    _fixture.Secrets.ClientId,
+                    _fixture.Secrets.UserAccessToken,
+                    broadcasterId,
+                    userId
+                    )
+                );
+
+    private async ValueTask<ChannelVip> GetVip(string userId, string broadcasterId)
+        => (await _fixture.Api.SendRequestAsync(
+                new GetVipsRequest(
+                    _fixture.Secrets.ClientId,
+                    _fixture.Secrets.UserAccessToken,
+                    broadcasterId,
+                    [userId]
+                    )
+            )).Data.First();
+
+    private async ValueTask RemoveVip(ChannelVip vip, string broadcasterId)
+        => await _fixture.Api.SendRequestAsync(
+                new RemoveChannelVipRequest(
+                    _fixture.Secrets.ClientId,
+                    _fixture.Secrets.UserAccessToken,
+                    broadcasterId,
+                    vip.UserId
+                    )
+            );
+}
