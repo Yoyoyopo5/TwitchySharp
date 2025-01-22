e13ffa6ff4473db830cc804cf5fa07acd6b8a60f
Yoyoyopo5 <118655115+Yoyoyopo5@users.noreply.github.com>
2025-01-22 14:29:22 -0600

    Fix api converter

--- TwitchySharp.Api/Helix/Channels/Responses/ModifyChannelInformationResponse.cs
+++ TwitchySharp.Api/Helix/Channels/Responses/ModifyChannelInformationResponse.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using TwitchySharp.Api.ApiResponseConverters;
 
 namespace TwitchySharp.Api.Helix.Channels;
 /// <summary>
 /// Empty response. Contains no data.
 /// </summary>
+[ApiConverter(typeof(EmptyResponseConverter))]
 public record ModifyChannelInformationResponse { }
