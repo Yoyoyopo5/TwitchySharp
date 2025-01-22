41c0f7f18a36f2fc157d307252a85991ad7b3d8f
Yoyoyopo5 <yoyoyopo5@DESKTOP-IT4LOU9>
2025-01-22 15:36:48 -0600

    Reformat properties

--- TwitchySharp.Api/Helix/Chat/Models/EmoteImage.cs
+++ TwitchySharp.Api/Helix/Chat/Models/EmoteImage.cs
@@ -1,4 +1,6 @@
-﻿namespace TwitchySharp.Api.Helix.Chat;
+﻿using System.Text.Json.Serialization;
+
+namespace TwitchySharp.Api.Helix.Chat;
 
 /// <summary>
 /// Contains URLs for an emote's images.
@@ -9,13 +11,16 @@ public record EmoteImage
     /// <summary>
     /// A URL to the small version (28px x 28px) of the emote.
     /// </summary>
-    public required string Url_1x { get; init; }
+    [JsonPropertyName("url_1x")]
+    public required string Url1x { get; init; }
     /// <summary>
     /// A URL to the medium version (56px x 56px) of the emote.
     /// </summary>
-    public required string Url_2x { get; init; }
+    [JsonPropertyName("url_2x")]
+    public required string Url2x { get; init; }
     /// <summary>
     /// A URL to the large version (112px x 112px) of the emote.
     /// </summary>
-    public required string Url_4x { get; init; }
+    [JsonPropertyName("url_4x")]
+    public required string Url4x { get; init; }
 }
