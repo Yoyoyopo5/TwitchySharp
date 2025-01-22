a498cab3677509333bfea578d5f4bf566a3553a5
Yoyoyopo5 <yoyoyopo5@DESKTOP-IT4LOU9>
2025-01-22 14:03:20 -0600

    Add Get Channel Information test

---
+++ TwitchySharp.Api.Tests.Integration/Helix/Channels/Test_GetChannelInformation.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TwitchySharp.Api.Helix.Channels;
+
+namespace TwitchySharp.Api.Tests.Integration.Helix.Channels;
+[Collection("helix")]
+public class Test_GetChannelInformation(HelixFixture fixture)
+{
+    private readonly HelixFixture _fixture = fixture;
+
+    [Fact]
+    public async void Send_GetChannelInformationRequest_ReturnSuccessResponse()
+    {
+        string broadcasterId = await _fixture.GetUserIdFromAccessTokenAsync();
+
+        await _fixture.Api.SendRequestAsync(new GetChannelInformationRequest(_fixture.Secrets.ClientId, _fixture.Secrets.UserAccessToken, [broadcasterId]));
+    }
+}
