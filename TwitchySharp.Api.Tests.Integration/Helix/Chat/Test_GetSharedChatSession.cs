ca1c5b76e441fa81a2f2330b0f23bcbd91009916
Yoyoyopo5 <118655115+Yoyoyopo5@users.noreply.github.com>
2025-01-22 19:53:53 -0600

    Add Get Shared Chat Session test

---
+++ TwitchySharp.Api.Tests.Integration/Helix/Chat/Test_GetSharedChatSession.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TwitchySharp.Api.Helix.Chat;
+
+namespace TwitchySharp.Api.Tests.Integration.Helix.Chat;
+[Collection("helix")]
+public class Test_GetSharedChatSession(HelixFixture fixture)
+{
+    private readonly HelixFixture _fixture = fixture;
+
+    [Fact]
+    public async void Send_GetSharedChatSessionRequest_ReturnSuccessResponse()
+    {
+        string broadcasterId = await _fixture.GetUserIdFromAccessTokenAsync();
+
+        await _fixture.Api.SendRequestAsync(new GetSharedChatSessionRequest(
+            _fixture.Secrets.ClientId,
+            _fixture.Secrets.UserAccessToken,
+            broadcasterId
+            ));
+    }
+}
