de8244246b4fb382f3f0c6fd32efe47a429d05d6
Yoyoyopo5 <118655115+Yoyoyopo5@users.noreply.github.com>
2025-01-23 19:23:58 -0600

    Add Create Clip test

---
+++ TwitchySharp.Api.Tests.Integration/Helix/Clips/Test_CreateClip.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TwitchySharp.Api.Helix.Clips;
+
+namespace TwitchySharp.Api.Tests.Integration.Helix.Clips;
+[Collection("helix")]
+public class Test_CreateClip(HelixFixture fixture)
+{
+    private readonly HelixFixture _fixture = fixture;
+
+    [Fact]
+    public async void Send_CreateClipRequest_ReturnSuccessResponse()
+    {
+        string broadcasterId = await _fixture.GetUserIdFromAccessTokenAsync();
+
+        await _fixture.Api.SendRequestAsync(new CreateClipRequest(
+            _fixture.Secrets.ClientId,
+            _fixture.Secrets.UserAccessToken,
+            broadcasterId
+            ));
+    }
+}
