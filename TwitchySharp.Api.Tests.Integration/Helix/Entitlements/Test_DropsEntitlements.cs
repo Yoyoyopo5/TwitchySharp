5dd92072b41aae73ce9fe823feab6bf62758f7a9
Yoyoyopo5 <118655115+Yoyoyopo5@users.noreply.github.com>
2025-01-23 23:46:04 -0600

    Add Drops Entitlements API test

---
+++ TwitchySharp.Api.Tests.Integration/Helix/Entitlements/Test_DropsEntitlements.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TwitchySharp.Api.Helix.Entitlements;
+
+namespace TwitchySharp.Api.Tests.Integration.Helix.Entitlements;
+[Collection("helix")]
+public class Test_DropsEntitlements(HelixFixture fixture)
+{
+    private readonly HelixFixture _fixture = fixture;
+
+    [Fact]
+    public async void Send_DropsEntitlementRequests_ReturnSuccessResponses()
+    {
+        string? entitlementId = (await GetDropsEntitlements()).Data.FirstOrDefault()?.Id;
+        // Can't really test the update API unless we can actually get a drops entitlement on an account...
+        if (entitlementId is null) return;
+        await UpdateDropsEntitlements(entitlementId);
+    }
+
+    private ValueTask<GetDropsEntitlementsResponse> GetDropsEntitlements()
+        => _fixture.Api.SendRequestAsync(new GetDropsEntitlementsRequest(
+            _fixture.Secrets.ClientId,
+            _fixture.Secrets.UserAccessToken
+            ));
+
+    private ValueTask<UpdateDropsEntitlementsResponse> UpdateDropsEntitlements(string entitlementId)
+        => _fixture.Api.SendRequestAsync(new UpdateDropsEntitlementsRequest(
+            _fixture.Secrets.ClientId,
+            _fixture.Secrets.UserAccessToken,
+            new UpdateDropsEntitlementsRequestData()
+            {
+                EntitlementIds = [entitlementId],
+                FulfillmentStatus = DropsEntitlementStatus.Fulfilled
+            }
+            ));
+}
