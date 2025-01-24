5d9d1eb5182a8e7eb722630e48bddd1dde35fe87
Yoyoyopo5 <yoyoyopo5@DESKTOP-IT4LOU9>
2025-01-23 19:33:51 -0600

    Add tests for Get Clips

---
+++ TwitchySharp.Api.Tests.Integration/Helix/Clips/Test_GetClips.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TwitchySharp.Api.Helix.Clips;
+
+namespace TwitchySharp.Api.Tests.Integration.Helix.Clips;
+[Collection("helix")]
+public class Test_GetClips(HelixFixture fixture)
+{
+    private readonly HelixFixture _fixture = fixture;
+
+    [Fact]
+    public async void Send_GetClipsRequestBroadcasterId_ReturnSuccessResponse()
+    {
+        string broadcasterId = await _fixture.GetUserIdFromAccessTokenAsync();
+
+        await _fixture.Api.SendRequestAsync(new GetClipsRequest(
+            _fixture.Secrets.ClientId,
+            _fixture.Secrets.UserAccessToken,
+            broadcasterId
+            ));
+    }
+
+    [Fact]
+    public async void Send_GetClipsRequestGameId_ReturnSuccessResponse()
+    {
+        await _fixture.Api.SendRequestAsync(new GetClipsRequest(
+            _fixture.Secrets.ClientId,
+            _fixture.Secrets.UserAccessToken,
+            null,
+            "1"
+            ));
+    }
+
+    [Fact]
+    public async void Send_GetClipsRequestClipId_ReturnSuccessResponse()
+    {
+        await _fixture.Api.SendRequestAsync(new GetClipsRequest(
+            _fixture.Secrets.ClientId,
+            _fixture.Secrets.UserAccessToken,
+            null,
+            null,
+            "ObservantVictoriousButterOSfrog-Uur1-ZdwTmNRzuQ7"
+            ));
+    }
+}
