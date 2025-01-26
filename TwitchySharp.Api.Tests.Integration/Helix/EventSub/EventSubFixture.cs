ba9001bf4494f745d1d0a6a902ebd37dd597a3ad
Yoyoyopo5 <yoyoyopo5@DESKTOP-IT4LOU9>
2025-01-25 23:41:47 -0600

    Refactor EventSub tests

---
+++ TwitchySharp.Api.Tests.Integration/Helix/EventSub/EventSubFixture.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TwitchySharp.Api.Helix.EventSub;
+using TwitchySharp.Api.Helix.EventSub.Models.Types.Automod.Message;
+
+namespace TwitchySharp.Api.Tests.Integration.Helix.EventSub;
+/// <summary>
+/// Handles one-time setup of subscription types.
+/// </summary>
+public class EventSubFixture : HelixFixture, IAsyncLifetime
+{
+    private IReadOnlyDictionary<string, IEventSubSubscriptionType> _subscriptionTypes = ImmutableDictionary<string, IEventSubSubscriptionType>.Empty;
+
+    public async Task InitializeAsync()
+        => _subscriptionTypes = await GenerateTypeMapAsync();
+
+    public Task DisposeAsync()
+        => Task.CompletedTask;
+
+    private async ValueTask<IReadOnlyDictionary<string, IEventSubSubscriptionType>> GenerateTypeMapAsync()
+    {
+        string broadcasterId = await GetUserIdFromAccessTokenAsync();
+
+        return new Dictionary<string, IEventSubSubscriptionType>
+        {
+            { nameof(AutomodMessageHoldV2), new AutomodMessageHoldV2(broadcasterId, broadcasterId) }
+        };
+    }
+
+    public IEventSubSubscriptionType GetSubscriptionType(string subscriptionTypeName)
+        => _subscriptionTypes[subscriptionTypeName];
+}
