2e4e5abae24b117da3c32143f381c101cb51f2d1
Yoyoyopo5 <yoyoyopo5@DESKTOP-IT4LOU9>
2025-01-31 08:25:19 -0600

    Add Send Extension Chat Message test

---
+++ TwitchySharp.Api.Tests.Integration/Helix/Extensions/Test_SendExtensionChatMessage.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TwitchySharp.Api.Authorization.Extensions;
+using TwitchySharp.Api.Helix.Extensions;
+
+namespace TwitchySharp.Api.Tests.Integration.Helix.Extensions;
+[Collection("helix")]
+public class Test_SendExtensionChatMessage(HelixFixture fixture)
+{
+    private readonly HelixFixture _fixture = fixture;
+
+    [Fact]
+    public async void Send_SendExtensionChatMessageRequest_ReturnSuccessResponse()
+    {
+        const string EXTENSION_VERSION = "0.0.1";
+        string broadcasterId = await _fixture.GetUserIdFromAccessTokenAsync();
+
+        string jwt = new ExtensionJwtPayload(broadcasterId)
+            .Sign(_fixture.Secrets.ExtensionSecret);
+
+        await _fixture.Api.SendRequestAsync(new SendExtensionChatMessageRequest(
+            _fixture.Secrets.ExtensionClientId,
+            jwt,
+            broadcasterId,
+            new SendExtensionChatMessageRequestData()
+            {
+                ExtensionId = _fixture.Secrets.ExtensionClientId,
+                ExtensionVersion = EXTENSION_VERSION,
+                Text = "test message"
+            }
+            ));
+    }
+}
