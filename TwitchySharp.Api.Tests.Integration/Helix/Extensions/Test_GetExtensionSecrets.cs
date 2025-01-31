ec146590e58046521d0bc8f499ba2a14661294a1
Yoyoyopo5 <yoyoyopo5@DESKTOP-IT4LOU9>
2025-01-31 04:30:35 -0600

    Add Get Extension Secrets test

---
+++ TwitchySharp.Api.Tests.Integration/Helix/Extensions/Test_GetExtensionSecrets.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TwitchySharp.Api.Authorization.Extensions;
+using TwitchySharp.Api.Helix.Extensions;
+
+namespace TwitchySharp.Api.Tests.Integration.Helix.Extensions;
+[Collection("helix")]
+public class Test_GetExtensionSecrets(HelixFixture fixture)
+{
+    private readonly HelixFixture _fixture = fixture;
+
+    [Fact]
+    public async void Send_GetExtensionSecretsRequest_ReturnSuccessResponse()
+    {
+        string jwt = new ExtensionJwtPayload(await _fixture.GetUserIdFromAccessTokenAsync())
+            .Sign(_fixture.Secrets.ExtensionSecret);
+
+        await _fixture.Api.SendRequestAsync(new GetExtensionSecretsRequest(
+            _fixture.Secrets.ExtensionClientId,
+            jwt,
+            _fixture.Secrets.ExtensionClientId
+            ));
+    }
+}
