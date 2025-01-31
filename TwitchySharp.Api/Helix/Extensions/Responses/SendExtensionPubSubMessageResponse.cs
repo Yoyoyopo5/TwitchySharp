f654c9bacef07344004eeaf09718d13ff750823f
Yoyoyopo5 <118655115+Yoyoyopo5@users.noreply.github.com>
2025-01-31 09:44:48 -0600

    Add empty api converter attribute

--- TwitchySharp.Api/Helix/Extensions/Responses/SendExtensionPubSubMessageResponse.cs
+++ TwitchySharp.Api/Helix/Extensions/Responses/SendExtensionPubSubMessageResponse.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TwitchySharp.Api.ApiResponseConverters;
 
 namespace TwitchySharp.Api.Helix.Extensions;
 /// <summary>
 /// Empty response.
 /// </summary>
+[ApiConverter(typeof(EmptyResponseConverter))]
 public class SendExtensionPubSubMessageResponse { }
