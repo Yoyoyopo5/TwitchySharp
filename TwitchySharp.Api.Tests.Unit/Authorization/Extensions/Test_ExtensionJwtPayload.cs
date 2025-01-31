17968aaebfaa669a49d4b25ee755c2e343a3c42c
Yoyoyopo5 <118655115+Yoyoyopo5@users.noreply.github.com>
2025-01-31 02:55:02 -0600

    Add Extension JWT test

---
+++ TwitchySharp.Api.Tests.Unit/Authorization/Extensions/Test_ExtensionJwtPayload.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TwitchySharp.Api.Authorization.Extensions;
+
+namespace TwitchySharp.Api.Tests.Unit.Authorization.Extensions;
+public class Test_ExtensionJwtPayload
+{
+    [Fact]
+    public void Sign_ExtensionJwtPayload_ReturnSignedJwt()
+    {
+        const string FAKE_USER_ID = "123";
+        const string FAKE_EXTENSION_SECRET = "iWLtPMsRJPfdZNdC/4Ug9gtZNdWlfFDvGweKuW4EVjk=";
+        DateTimeOffset fakeExpiry = DateTimeOffset.FromUnixTimeSeconds(9001);
+        ExtensionJwtPayload stubPayload = new(FAKE_USER_ID) { ExpiresAt = fakeExpiry };
+        const string MOCK_SIGNED_JWT = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHQiOjkwMDEsInVzZXJfaWQiOiIxMjMiLCJyb2xlIjoiZXh0ZXJuYWwifQ.82xiab2B8PIYDs65fUVgYDxApDooCtjUfkzDtDqj94E";
+
+        string actual = stubPayload.Sign(FAKE_EXTENSION_SECRET);
+
+        Assert.Equal(MOCK_SIGNED_JWT, actual);
+    }
+}
