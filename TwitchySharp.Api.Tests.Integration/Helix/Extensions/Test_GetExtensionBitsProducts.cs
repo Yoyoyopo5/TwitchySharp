503991ef4a2101f7bb6f050195cbd9c8501084dc
Yoyoyopo5 <yoyoyopo5@DESKTOP-IT4LOU9>
2025-01-31 05:17:35 -0600

    Add Get Extension Bits Products test

---
+++ TwitchySharp.Api.Tests.Integration/Helix/Extensions/Test_GetExtensionBitsProducts.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TwitchySharp.Api.Authorization;
+using TwitchySharp.Api.Helix.Extensions;
+
+namespace TwitchySharp.Api.Tests.Integration.Helix.Extensions;
+[Collection("helix")]
+public class Test_GetExtensionBitsProducts(HelixFixture fixture)
+{
+    private readonly HelixFixture _fixture = fixture;
+
+    [Fact]
+    public async void Send_GetExtensionBitsProductsRequest_ReturnSuccessResponse()
+    {
+        string appToken = (await _fixture.Api.SendRequestAsync(new ClientCredentialsRequest(
+            _fixture.Secrets.ExtensionClientId,
+            _fixture.Secrets.ExtensionClientSecret
+            ))).AccessToken;
+
+        await _fixture.Api.SendRequestAsync(new GetExtensionBitsProductsRequest(
+            _fixture.Secrets.ExtensionClientId,
+            appToken
+            ));
+    }
+}
