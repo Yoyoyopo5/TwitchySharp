5b293b035b4862e7aa6bd3f0dab4ea167f640f39
Yoyoyopo5 <118655115+Yoyoyopo5@users.noreply.github.com>
2025-02-05 13:30:55 -0600

    Add empty response converter attributes

--- TwitchySharp.Api/Helix/GuestStar/Responses/DeleteGuestStarSlotResponse.cs
+++ TwitchySharp.Api/Helix/GuestStar/Responses/DeleteGuestStarSlotResponse.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TwitchySharp.Api.ApiResponseConverters;
 
 namespace TwitchySharp.Api.Helix.GuestStar;
 /// <summary>
 /// Empty response.
 /// </summary>
+[ApiConverter(typeof(EmptyResponseConverter))]
 public record DeleteGuestStarSlotResponse { }
