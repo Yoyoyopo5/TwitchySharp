a1c8d6077e3aa78433cedd36b305216e10cbfc3f
Yoyoyopo5 <118655115+Yoyoyopo5@users.noreply.github.com>
2025-02-06 13:16:43 -0600

    Add preliminary test for Manage Held AutoMod Messages test

---
+++ TwitchySharp.Api.Tests.Integration/Helix/Moderation/Test_ManageHeldAutoModMessages.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TwitchySharp.Api.Helix.Moderation;
+
+namespace TwitchySharp.Api.Tests.Integration.Helix.Moderation;
+[Collection("helix")]
+public class Test_ManageHeldAutoModMessages(HelixFixture fixture)
+{
+    private readonly HelixFixture _fixture = fixture;
+
+    [Fact]
+    public async void Send_ManageHeldAutoModMessages_ReturnSuccessResponse()
+    {
+        const string heldMessageId = "1234"; // Need to use EventSub to test this endpoint.
+        string broadcasterId = await _fixture.GetUserIdFromAccessTokenAsync();
+
+        await _fixture.Api.SendRequestAsync(new ManageHeldAutoModMessagesRequest(
+            _fixture.Secrets.Client.Id,
+            _fixture.Secrets.User.AccessToken,
+            new ManageHeldAutoModMessagesRequestData()
+            {
+                MessageId = heldMessageId,
+                UserId = broadcasterId,
+                Action = AutoModAction.Allow
+            }
+            ));
+    }
+}
