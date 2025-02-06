5ec2bdba20b7b82bfa7fff7d402a3d66060bf4e5
Yoyoyopo5 <118655115+Yoyoyopo5@users.noreply.github.com>
2025-02-06 13:34:14 -0600

    Add Resolve Unban Requests test

---
+++ TwitchySharp.Api.Tests.Integration/Helix/Moderation/Test_ResolveUnbanRequests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TwitchySharp.Api.Helix.Moderation;
+
+namespace TwitchySharp.Api.Tests.Integration.Helix.Moderation;
+[Collection("helix")]
+public class Test_ResolveUnbanRequests(HelixFixture fixture)
+{
+    private readonly HelixFixture _fixture = fixture;
+
+    [Fact]
+    public async void Send_ResolveUnbanRequestsRequest_ReturnSuccessResponse()
+    {
+        string broadcasterId = await _fixture.GetUserIdFromAccessTokenAsync();
+
+        string unbanRequestId = (await GetUnbanRequests(broadcasterId)).Data.First().Id;
+        await _fixture.Api.SendRequestAsync(new ResolveUnbanRequestsRequest(
+            _fixture.Secrets.Client.Id,
+            _fixture.Secrets.User.AccessToken,
+            broadcasterId,
+            broadcasterId,
+            unbanRequestId,
+            UnbanRequestResolutionStatus.Approved,
+            "Lucky you :)"
+            ));
+    }
+
+    private ValueTask<GetUnbanRequestsResponse> GetUnbanRequests(string broadcasterId)
+        => _fixture.Api.SendRequestAsync(new GetUnbanRequestsRequest(
+            _fixture.Secrets.Client.Id,
+            _fixture.Secrets.User.AccessToken,
+             broadcasterId,
+             broadcasterId,
+             UnbanRequestStatus.Pending
+            ));
+};
