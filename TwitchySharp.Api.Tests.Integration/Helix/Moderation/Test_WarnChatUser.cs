c6de83a1e6b1f9d839d20c8fd3c00e0e68619724
Yoyoyopo5 <118655115+Yoyoyopo5@users.noreply.github.com>
2025-02-06 13:48:57 -0600

    Add Warn Chat User test

---
+++ TwitchySharp.Api.Tests.Integration/Helix/Moderation/Test_WarnChatUser.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TwitchySharp.Api.Helix.Moderation;
+
+namespace TwitchySharp.Api.Tests.Integration.Helix.Moderation;
+[Collection("helix")]
+public class Test_WarnChatUser(HelixFixture fixture)
+{
+    private readonly HelixFixture _fixture = fixture;
+
+    [Fact]
+    public async void Send_WarnChatUserRequest_ReturnSuccessResponse()
+    {
+        string userToWarn = "52137752";
+        string broadcasterId = await _fixture.GetUserIdFromAccessTokenAsync();
+
+        await _fixture.Api.SendRequestAsync(new WarnChatUserRequest(
+            _fixture.Secrets.Client.Id,
+            _fixture.Secrets.User.AccessToken,
+            broadcasterId,
+            broadcasterId,
+            new WarnChatUserRequestData()
+            {
+                Data = new()
+                {
+                    UserId = userToWarn,
+                    Reason = "You have been selected for a random test of the warning system."
+                }
+            }
+            ));
+    }
+}
