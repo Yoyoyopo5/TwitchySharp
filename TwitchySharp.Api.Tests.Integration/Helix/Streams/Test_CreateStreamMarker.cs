59b8e4356295f05901f84525cf35d87e2bda9ff1
Yoyoyopo5 <118655115+Yoyoyopo5@users.noreply.github.com>
2025-02-08 12:58:42 -0600

    Add Create Stream Marker test

---
+++ TwitchySharp.Api.Tests.Integration/Helix/Streams/Test_CreateStreamMarker.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TwitchySharp.Api.Helix.Streams.Requests;
+
+namespace TwitchySharp.Api.Tests.Integration.Helix.Streams;
+[Collection("helix")]
+public class Test_CreateStreamMarker(HelixFixture fixture)
+{
+    private readonly HelixFixture _fixture = fixture;
+
+    [Fact]
+    public async void Send_CreateStreamMarkerRequest_ReturnSuccessResponse()
+    {
+        string broadcasterId = await _fixture.GetUserIdFromAccessTokenAsync();
+
+        await _fixture.Api.SendRequestAsync(new CreateStreamMarkerRequest(
+            _fixture.Secrets.Client.Id,
+            _fixture.Secrets.User.AccessToken,
+            new CreateStreamMarkerRequestData()
+            {
+                UserId = broadcasterId,
+                Description = "test-marker"
+            }
+            ));
+    }
+}
