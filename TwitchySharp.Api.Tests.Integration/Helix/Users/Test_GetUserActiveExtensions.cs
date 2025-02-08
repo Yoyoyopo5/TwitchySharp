ca4193c54c2f63af8d2113a0e5f5f3864b9e9d7e
Yoyoyopo5 <yoyoyopo5@DESKTOP-IT4LOU9>
2025-02-08 16:51:30 -0600

    Add Get User Active Extensions test

---
+++ TwitchySharp.Api.Tests.Integration/Helix/Users/Test_GetUserActiveExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TwitchySharp.Api.Helix.Users;
+
+namespace TwitchySharp.Api.Tests.Integration.Helix.Users;
+[Collection("helix")]
+public class Test_GetUserActiveExtensions(HelixFixture fixture)
+{
+    private readonly HelixFixture _fixture = fixture;
+
+    [Fact]
+    public async void Send_GetUserActiveExtensionsRequest_ReturnSuccessResponse()
+    {
+        string broadcasterId = await _fixture.GetUserIdFromAccessTokenAsync();
+
+        await _fixture.Api.SendRequestAsync(new GetUserActiveExtensionsRequest(
+            _fixture.Secrets.Client.Id,
+            _fixture.Secrets.User.AccessToken,
+            broadcasterId
+            ));
+    }
+}
