b71936974907b23dcd43e1b3ea61fd551ac71a1e
Yoyoyopo5 <yoyoyopo5@DESKTOP-IT4LOU9>
2025-02-15 05:17:18 -0600

    Add preliminary EventSub models and methods

---
+++ TwitchySharp.EventSub/NotificationConverters/NotificationConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using TwitchySharp.EventSub.Notifications;
+using TwitchySharp.EventSub.Notifications.EventModels;
+using TwitchySharp.Shared;
+using TwitchySharp.Shared.EventSub.Models;
+
+namespace TwitchySharp.EventSub.NotificationConverters;
+
+public interface INotificationConverter
+{
+    IEventSubNotification Convert(JsonDocument json, EventSubSubscriptionType subscriptionType);
+}
+
+public class NotificationConverter(IReadOnlyDictionary<EventSubSubscriptionType, Type>? notificationTypes = null)
+    : INotificationConverter
+{
+    public static readonly IReadOnlyDictionary<EventSubSubscriptionType, Type> DefaultNotificationTypes = new Dictionary<EventSubSubscriptionType, Type>()
+    {
+        { EventSubSubscriptionType.AutomodMessageHold, typeof(AutomodMessageHoldNotification) }
+    };
+
+    private JsonSerializerOptions _serializerOptions = JsonConfig.ApiOptions;
+    private IReadOnlyDictionary<EventSubSubscriptionType, Type> _notificationTypes = notificationTypes ?? DefaultNotificationTypes;
+    public IEventSubNotification Convert(JsonDocument json, EventSubSubscriptionType subscriptionType)
+        => (IEventSubNotification?)json.Deserialize(_notificationTypes[subscriptionType], _serializerOptions) ?? throw new ArgumentException("JSON cannot be null literal.");
+}
---
+++ TwitchySharp.EventSub/Notifications/EventModels/Automod/Message/AutomodMessageHold.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using TwitchySharp.Helpers;
+using TwitchySharp.Helpers.JsonConverters.DateTime;
+
+namespace TwitchySharp.EventSub.Notifications.EventModels;
+public record AutomodMessageHoldNotification : EventSubNotification<AutoModMessageHoldEvent, AutomodMessageHoldCondition>;
+public record AutomodMessageHoldCondition
+{
+    public required string BroadcasterUserId { get; init; }
+    public required string ModeratorUserId { get; init; }
+}
+public record AutoModMessageHoldEvent
+{
+    public required string BroadcasterUserId { get; init; }
+    public required string BroadcasterUserLogin { get; init; }
+    public required string BroadcasterUserName { get; init; }
+    public required string UserId { get; init; }
+    public required string UserLogin { get; init; }
+    public required string UserName { get; init; }
+    public required string MessageId { get; init; }
+    public required AutomodCaughtChatMessage Message { get; init; }
+    public required string Category { get; init; }
+    public required int Level { get; init; }
+    public required DateTimeOffset HeldAt { get; init; }
+}
+
+public record AutomodCaughtChatMessage
+{
+    public required string Text { get; init; }
+    public required AutomodCaughtMessageFragment[] Fragments { get; init; }
+}
+
+public record AutomodCaughtMessageFragment
+{
+    public required AutomodCaughtMessageFragmentType Type { get; init; }
+    public required string Text { get; init; }
+    public ChatEmote? Emote { get; init; }
+    public Cheermote? Cheermote { get; init; }
+}
+
+[JsonConverter(typeof(ValueBackedEnumJsonConverter<AutomodCaughtMessageFragmentType, string>))]
+public record AutomodCaughtMessageFragmentType(string Value)
+    : ValueBackedEnum<string>(Value)
+{
+    public static AutomodCaughtMessageFragmentType Text { get; } = new("text");
+    public static AutomodCaughtMessageFragmentType Emote { get; } = new("emote");
+    public static AutomodCaughtMessageFragmentType Cheermote { get; } = new("cheermote");
+}
+
+public record ChatEmote
+{
+    public required string Id { get; init; }
+    public required string EmoteSetId { get; init; }
+}
+
+public record Cheermote
+{
+    public required string Prefix { get; init; }
+    public required int Bits { get; init; }
+    public required int Tier { get; init; }
+}
