ccf3946ec5951feb7f673fc3f999f4c33f89fdc2
Yoyoyopo5 <yoyoyopo5@DESKTOP-IT4LOU9>
2025-02-15 05:16:53 -0600

    Add items to shared project from Api

---
+++ TwitchySharp.Shared/EventSub/Enums/EventSubTransportMethod.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.Json.Serialization;
+using TwitchySharp.Helpers;
+
+namespace TwitchySharp.Shared.EventSub.Enums;
+
+/// <summary>
+/// Possible EventSub transport methods.
+/// </summary>
+[JsonConverter(typeof(ValueBackedEnumJsonConverter<EventSubTransportMethod, string>))]
+public record EventSubTransportMethod(string Value)
+    : ValueBackedEnum<string>(Value)
+{
+    public static EventSubTransportMethod Webhook { get; } = new("webhook");
+    public static EventSubTransportMethod Websocket { get; } = new("websocket");
+    public static EventSubTransportMethod Conduit { get; } = new("conduit");
+}
