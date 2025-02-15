e6e23928ef7ca858b214cbd211a45e647e72cbb1
Yoyoyopo5 <118655115+Yoyoyopo5@users.noreply.github.com>
2025-02-15 05:16:53 -0600

    Add items to shared project from Api

---
+++ TwitchySharp.Shared/EventSub/Constants/EventSubSubscriptionTypeVersions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwitchySharp.Shared.EventSub.Constants;
+/// <summary>
+/// Contains constant values for EventSub subscription type versions.
+/// </summary>
+public static class EventSubSubscriptionTypeVersions
+{
+    public const string V1 = "1";
+    public const string V2 = "2";
+    public const string BETA = "beta";
+}
---
+++ TwitchySharp.Shared/JsonConfig.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json;
+
+namespace TwitchySharp.Shared;
+public static class JsonConfig
+{
+    /// <summary>
+    /// The default options used when serializing and deserializing API requests and responses.
+    /// </summary>
+    public readonly static JsonSerializerOptions ApiOptions = new()
+    {
+        PropertyNamingPolicy = JsonNamingPolicy.SnakeCaseLower, 
+        DefaultIgnoreCondition = System.Text.Json.Serialization.JsonIgnoreCondition.WhenWritingNull // This should be okay for writing optional params in requests, if something gets screwed up we can probably use an attribute to fix it for that case.
+    };
+}
