9d93d17f17293b8d41a0ace7a317a6a66848f689
Yoyoyopo5 <118655115+Yoyoyopo5@users.noreply.github.com>
2025-02-18 06:20:34 -0600

    Add Websockets test proj

---
+++ TwitchySharp.EventSub.Websocket.Tests.Integration/WebsocketFixture.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using TwitchySharp.Api;
+using TwitchySharp.EventSub.Notifications;
+using TwitchySharp.EventSub.Websocket.Messages.Payloads;
+
+namespace TwitchySharp.EventSub.Websocket.Tests.Integration;
+public class WebsocketFixture : IDisposable
+{
+    public TestHandler Handler { get; }
+    public TwitchEventSubWebsocketClient Client { get; }
+    public TwitchApi Api { get; }
+    public WebsocketSecrets Secrets { get; }
+
+    public WebsocketFixture()
+    {
+        Handler = new TestHandler();
+        Client = new TwitchEventSubWebsocketClient(Handler);
+        Api = new TwitchApi(new());
+        Secrets = new ConfigurationBuilder().AddUserSecrets(Assembly.GetExecutingAssembly()).Build().GetRequiredSection("Secrets").Get<WebsocketSecrets>()!;
+
+        CancellationTokenSource cts = new(2000);
+        Client.StartAsync(cts.Token).GetAwaiter().GetResult();
+    }
+
+    public void Dispose()
+    {
+        CancellationTokenSource cts = new(1000);
+        Client.StopAsync(cts.Token).GetAwaiter().GetResult();
+        Client.Dispose();
+    }
+}
+
+public record WebsocketSecrets
+{
+    public required string ClientId { get; init; }
+    public required string UserAccessToken { get; init; }
+}
+
+public class TestHandler : IWebsocketEventSubHandler
+{
+    public EventSubWebsocketSession? ReceivedConnected { get; private set; }
+    public bool ReceivedKeepalive { get; private set; } = false;
+    public IEventSubNotification? ReceivedNotification { get; private set; }
+    public EventSubSubscription? ReceivedRevocation { get; private set; }
+    public Exception? ReceivedException { get; private set; }
+
+    public ValueTask OnConnected(EventSubWebsocketSession session, CancellationToken ct = default)
+    {
+        ReceivedConnected = session;
+        return ValueTask.CompletedTask;
+    }
+
+    public ValueTask OnException(Exception exception, CancellationToken ct = default)
+    {
+        ReceivedException = exception;
+        return ValueTask.CompletedTask;
+    }
+
+    public ValueTask OnKeepalive(CancellationToken ct = default)
+    {
+        ReceivedKeepalive = true;
+        return ValueTask.CompletedTask;
+    }
+
+    public ValueTask OnNotified(IEventSubNotification notification, CancellationToken ct = default)
+    {
+        ReceivedNotification = notification;
+        return ValueTask.CompletedTask;
+    }
+
+    public ValueTask OnSubscriptionRevoked(EventSubSubscription subscription, CancellationToken ct = default)
+    {
+        ReceivedRevocation = subscription;
+        return ValueTask.CompletedTask;
+    }
+}
