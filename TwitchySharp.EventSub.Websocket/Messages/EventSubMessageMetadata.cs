7aa02c12a78bf43fb88121b3355e5c123ad7a252
Yoyoyopo5 <118655115+Yoyoyopo5@users.noreply.github.com>
2025-02-18 05:25:55 -0600

    Fix typos in message type constants

--- TwitchySharp.EventSub.Websocket/Messages/EventSubMessageMetadata.cs
+++ TwitchySharp.EventSub.Websocket/Messages/EventSubMessageMetadata.cs
@@ -14,9 +14,9 @@ internal record EventSubMessageMetadata
 
 internal static class EventSubMessageTypes
 {
-    public const string WELCOME = "welcome";
-    public const string KEEPALIVE = "keepalive";
+    public const string WELCOME = "session_welcome";
+    public const string KEEPALIVE = "session_keepalive";
     public const string NOTIFICATION = "notification";
-    public const string RECONNECT = "reconnect";
+    public const string RECONNECT = "session_reconnect";
     public const string REVOCATION = "revocation";
 }
