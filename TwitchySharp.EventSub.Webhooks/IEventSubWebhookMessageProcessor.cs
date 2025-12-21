8bc8a0324bf83a6ab6ddc22f5687d374e7be73fb
Yoyoyopo5 <118655115+Yoyoyopo5@users.noreply.github.com>
2025-12-21 17:38:11 -0600

    Remove extraneous methods on webhook message processor interface.

--- TwitchySharp.EventSub.Webhooks/IEventSubWebhookMessageProcessor.cs
+++ TwitchySharp.EventSub.Webhooks/IEventSubWebhookMessageProcessor.cs
@@ -5,14 +5,11 @@ using System.Text;
 using System.Threading.Tasks;
 using TwitchySharp.EventSub.Notifications;
 using TwitchySharp.EventSub.Webhooks.Requests;
+using TwitchySharp.EventSub.Webhooks.Responses;
 
 namespace TwitchySharp.EventSub.Webhooks;
 public interface IEventSubWebhookMessageProcessor
 {
-    ValueTask HandleRequest(EventSubWebhookRequestHeader requestHeader, Stream bodyStream);
-    ValueTask HandleRequest(EventSubWebhookRequestHeader requestHeader, string body);
-
-    ValueTask Notification(IEventSubNotification notification);
-    ValueTask CallbackVerification(string challenge, EventSubSubscription subscription);
-    ValueTask Revocation(EventSubSubscription subscription);
+    ValueTask<WebhookResponseData> HandleRequest(EventSubWebhookRequestHeader requestHeader, Stream bodyStream, CancellationToken ct = default);
+    ValueTask<WebhookResponseData> HandleRequest(EventSubWebhookRequestHeader requestHeader, string body, CancellationToken ct = default);
 }
