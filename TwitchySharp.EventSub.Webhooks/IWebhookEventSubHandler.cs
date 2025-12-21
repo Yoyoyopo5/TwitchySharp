5fa943baaf6b9d09ca16b8733466a33cdec524d7
Yoyoyopo5 <118655115+Yoyoyopo5@users.noreply.github.com>
2025-12-21 17:26:00 -0600

    Remove OnException method from webhook handler interface (exceptions can be bubbled normally)

--- TwitchySharp.EventSub.Webhooks/IWebhookEventSubHandler.cs
+++ TwitchySharp.EventSub.Webhooks/IWebhookEventSubHandler.cs
@@ -4,11 +4,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using TwitchySharp.EventSub.Notifications;
+using TwitchySharp.EventSub.Webhooks.Responses;
 
 namespace TwitchySharp.EventSub.Webhooks;
 public interface IWebhookEventSubHandler
 {
-    ValueTask OnSubscriptionRevoked(EventSubSubscription revokedSubscription);
-    ValueTask OnNotified(IEventSubNotification notification);
-    ValueTask OnException(Exception ex);
+    ValueTask<RevocationResponseData> OnSubscriptionRevoked(EventSubSubscription revokedSubscription);
+    ValueTask<NotificationResponseData> OnNotified(IEventSubNotification notification);
 }
