5c3078978a1b2db2de3ec2a6c0148d0b202c9a49
Yoyoyopo5 <118655115+Yoyoyopo5@users.noreply.github.com>
2025-12-22 02:56:27 -0600

    Add options and default service collection extensions.

---
+++ TwitchySharp.EventSub.Webhooks.AspNetCore/TwitchEventSubWebhooksOptions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using TwitchySharp.Shared.EventSub.Enums;
+
+namespace TwitchySharp.EventSub.Webhooks.AspNetCore;
+
+public class TwitchEventSubWebhooksOptions
+{
+    public JsonSerializerOptions? JsonSerializerOptions { get; set; }
+    public IReadOnlyDictionary<EventSubSubscriptionType, Type>? NotificationTypes { get; set; }
+}
