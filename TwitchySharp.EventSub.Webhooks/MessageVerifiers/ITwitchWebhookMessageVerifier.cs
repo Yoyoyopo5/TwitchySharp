ca7378227514d54de5fa9f849150c9361290bb9d
Yoyoyopo5 <yoyoyopo5@DESKTOP-IT4LOU9>
2025-12-22 02:25:19 -0600

    Add secrets resolver interface and organize.

--- TwitchySharp.EventSub.Webhooks/MessageVerifiers/ITwitchWebhookMessageVerifier.cs
+++ TwitchySharp.EventSub.Webhooks/MessageVerifiers/ITwitchWebhookMessageVerifier.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace TwitchySharp.EventSub.Webhooks;
+namespace TwitchySharp.EventSub.Webhooks.MessageVerifiers;
 /// <summary>
 /// Determines if a Twitch EventSub webhook message is from Twitch by verifying its signature.
 /// See <see href="https://dev.twitch.tv/docs/eventsub/handling-webhook-events/#verifying-the-event-message">Verifying the Event Message</see> for more information.
---
+++ TwitchySharp.EventSub.Webhooks/SecretResolvers/FixedSecretTwitchWebhookSecretsResolver.cs
@@ -0,0 +1,9 @@
+﻿namespace TwitchySharp.EventSub.Webhooks.SecretResolvers;
+
+public class FixedSecretTwitchWebhookSecretsResolver(string secret) : ITwitchEventSubWebhookSecretsResolver
+{
+    public ValueTask<string> GetSecret(EventSubWebhookRequestHeader requestHeaders, string body, CancellationToken ct = default)
+        => ValueTask.FromResult(secret);
+    public ValueTask<string> GetSecret(EventSubWebhookRequestHeader requestHeaders, Stream body, CancellationToken ct = default)
+        => ValueTask.FromResult(secret);
+}
