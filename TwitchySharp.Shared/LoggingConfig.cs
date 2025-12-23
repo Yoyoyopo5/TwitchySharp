1e69b703df2596f09c206d0c1f551a3444762433
Yoyoyopo5 <118655115+Yoyoyopo5@users.noreply.github.com>
2025-12-23 02:36:02 -0600

    Add logging category as shared constant.

---
+++ TwitchySharp.Shared/LoggingConfig.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwitchySharp.Shared;
+
+public static class LoggingConfig
+{
+    public const string LOGGER_CATEGORY = "TwitchySharp";
+}
