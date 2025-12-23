c0b6f9afe6184f3b45bf24591152b20b230d017e
Yoyoyopo5 <yoyoyopo5@DESKTOP-IT4LOU9>
2025-12-22 19:44:31 -0600

    Add header conversion service for IHeaderDictionary.

---
+++ TwitchySharp.EventSub.Webhooks.AspNetCore/DefaultTwitchWebhooksHeaderConverter.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace TwitchySharp.EventSub.Webhooks.AspNetCore;
+
+internal class DefaultTwitchWebhooksHeaderConverter : ITwitchWebhooksHeaderConverter
+{
+    private static IEnumerable<string> RequiredHeaders { get; } = [
+        "Twitch-Eventsub-Message-Id",
+        "Twitch-Eventsub-Message-Type",
+        "Twitch-Eventsub-Message-Signature",
+        "Twitch-Eventsub-Message-Timestamp",
+        "Twitch-Eventsub-Subscription-Type",
+        "Twitch-Eventsub-Subscription-Version"
+        ];
+    public TwitchWebhooksRequestHeaderConversionResult Convert(IHeaderDictionary headers)
+        => new()
+            {
+                MissingHeaders = RequiredHeaders.Where(headerName => !headers.ContainsKey(headerName)),
+                ConvertedHeader = new()
+                {
+                    TwitchEventsubMessageId = headers["Twitch-Eventsub-Message-Id"].FirstOrDefault() ?? string.Empty,
+                    TwitchEventsubMessageRetry = headers["Twitch-Eventsub-Message-Retry"].FirstOrDefault(),
+                    TwitchEventsubMessageType = headers["Twitch-Eventsub-Message-Type"].FirstOrDefault() ?? string.Empty,
+                    TwitchEventsubMessageSignature = headers["Twitch-Eventsub-Message-Signature"].FirstOrDefault() ?? string.Empty,
+                    TwitchEventsubMessageTimestamp = headers["Twitch-Eventsub-Message-Timestamp"].FirstOrDefault() ?? string.Empty,
+                    TwitchEventsubSubscriptionType = headers["Twitch-Eventsub-Subscription-Type"].FirstOrDefault() ?? string.Empty,
+                    TwitchEventsubSubscriptionVersion = headers["Twitch-Eventsub-Subscription-Version"].FirstOrDefault() ?? string.Empty
+                }
+            };
+}
