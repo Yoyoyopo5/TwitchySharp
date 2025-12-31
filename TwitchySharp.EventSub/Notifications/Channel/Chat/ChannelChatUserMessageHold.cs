8ca7eff5e6547410272748fb8202c886c0d45793
Yoyoyopo5 <yoyoyopo5@DESKTOP-IT4LOU9>
2025-12-30 19:50:37 -0600

    Add ChannelChatUserMessageHold notification type.

---
+++ TwitchySharp.EventSub/Notifications/Channel/Chat/ChannelChatUserMessageHold.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TwitchySharp.Shared.EventSub.Enums;
+
+namespace TwitchySharp.EventSub.Notifications.Channel;
+
+/// <summary>
+/// <inheritdoc cref="EventSubSubscriptionType.ChannelChatUserMessageHold"/>
+/// </summary>
+/// <remarks>
+/// See <see href="https://dev.twitch.tv/docs/eventsub/eventsub-subscription-types/#channelchatuser_message_hold">Channel Chat User Message Hold</see> for more information.
+/// </remarks>
+public record ChannelChatUserMessageHoldNotification : EventSubNotification<ChannelChatUserMessageHoldEvent, ChannelChatUserMessageHoldCondition>;
+/// <summary>
+/// Contains subscription information specific to <see cref="EventSubSubscriptionType.ChannelChatUserMessageHold"/>.
+/// </summary>
+public record ChannelChatUserMessageHoldCondition
+{
+    /// <summary>
+    /// The user id of the broadcaster (channel) to get Channel Chat User Message Hold notifications for.
+    /// </summary>
+    public required string BroadcasterUserId { get; init; }
+    /// <summary>
+    /// The id of the user to read chat as.
+    /// </summary>
+    public required string UserId { get; init; }
+}
+/// <summary>
+/// Contains information about a specific <see cref="EventSubSubscriptionType.ChannelChatUserMessageHold"/> event.
+/// </summary>
+public record ChannelChatUserMessageHoldEvent
+{
+    /// <summary>
+    /// The user id of the broadcaster (channel) that the message was sent in.
+    /// </summary>
+    public required string BroadcasterUserId { get; init; }
+    /// <summary>
+    /// The login (username) of the broadcaster (channel) that the message was sent in.
+    /// </summary>
+    public required string BroadcasterUserLogin { get; init; }
+    /// <summary>
+    /// The display name of the broadcaster (channel) that the message was sent in.
+    /// </summary>
+    public required string BroadcasterUserName { get; init; }
+    /// <summary>
+    /// The id of the user that sent the held message.
+    /// </summary>
+    public required string UserId { get; init; }
+    /// <summary>
+    /// The login (username) of the user that sent the held message.
+    /// </summary>
+    public required string UserLogin { get; init; }
+    /// <summary>
+    /// The display name of the user that sent the held message.
+    /// </summary>
+    public required string UserName { get; init; }
+    /// <summary>
+    /// The id of the message that was held.
+    /// </summary>
+    public required string MessageId { get; init; }
+    /// <summary>
+    /// The message that was held.
+    /// </summary>
+    public required ChannelChatMessage Message { get; init; }
+}
