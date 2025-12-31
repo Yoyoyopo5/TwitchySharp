1379175e3fceefd366647deae77c64eea76a40e0
Yoyoyopo5 <118655115+Yoyoyopo5@users.noreply.github.com>
2025-12-31 16:16:02 -0600

    Add ChannelSubscriptionEnd notification type.

---
+++ TwitchySharp.EventSub/Notifications/Channel/Subscription/ChannelSubscriptionEnd.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TwitchySharp.Shared.Enums;
+using TwitchySharp.Shared.EventSub.Enums;
+
+namespace TwitchySharp.EventSub.Notifications.Channel;
+/// <summary>
+/// <inheritdoc cref="EventSubSubscriptionType.ChannelSubscriptionEnd"/>
+/// </summary>
+/// <remarks>
+/// See <see href="https://dev.twitch.tv/docs/eventsub/eventsub-subscription-types/#channelsubscriptionend">Channel Subscription End</see> for more information.
+/// </remarks>
+public record ChannelSubscriptionEndNotification : EventSubNotification<ChannelSubscriptionEndEvent, ChannelSubscriptionEndCondition>;
+/// <summary>
+/// Contains subscription information specific to <see cref="EventSubSubscriptionType.ChannelSubscriptionEnd"/>.
+/// </summary>
+public record ChannelSubscriptionEndCondition
+{
+    /// <summary>
+    /// The user id of the broadcaster (channel) to get Subscription End notifications for.
+    /// </summary>
+    public required string BroadcasterUserId { get; init; }
+}
+/// <summary>
+/// Contains information about a specific <see cref="EventSubSubscriptionType.ChannelSubscriptionEnd"/> event.
+/// </summary>
+public record ChannelSubscriptionEndEvent
+{
+    /// <summary>
+    /// The id of the user whose subscription ended.
+    /// </summary>
+    public required string UserId { get; init; }
+    /// <summary>
+    /// The login (username) of the user whose subscription ended.
+    /// </summary>
+    public required string UserLogin { get; init; }
+    /// <summary>
+    /// The display name of the user whose subscription ended.
+    /// </summary>
+    public required string UserName { get; init; }
+    /// <summary>
+    /// The user id of the broadcaster (channel) that the original subscription was made to.
+    /// </summary>
+    public required string BroadcasterUserId { get; init; }
+    /// <summary>
+    /// The login (username) of the broadcaster (channel) that the original subscription was made to.
+    /// </summary>
+    public required string BroadcasterUserLogin { get; init; }
+    /// <summary>
+    /// The display name of the broadcaster (channel) that the original subscription was made to.
+    /// </summary>
+    public required string BroadcasterUserName { get; init; }
+    /// <summary>
+    /// The tier of the subscription that ended.
+    /// </summary>
+    public required SubscriptionTier Tier { get; init; }
+    /// <summary>
+    /// Indicates whether the subscription was a gift.
+    /// </summary>
+    public required bool IsGift { get; init; }
+}
