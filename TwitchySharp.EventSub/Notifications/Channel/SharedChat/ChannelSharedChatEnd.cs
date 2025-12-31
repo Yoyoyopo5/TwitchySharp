9b98a4262ce713b988a7610548b5a0fbaf667f29
Yoyoyopo5 <yoyoyopo5@DESKTOP-IT4LOU9>
2025-12-30 21:42:03 -0600

    Add ChannelSharedChatEnd notification type.

---
+++ TwitchySharp.EventSub/Notifications/Channel/SharedChat/ChannelSharedChatEnd.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TwitchySharp.Shared.EventSub.Enums;
+
+namespace TwitchySharp.EventSub.Notifications.Channel;
+/// <summary>
+/// <inheritdoc cref="EventSubSubscriptionType.ChannelSharedChatSessionEnd"/>
+/// </summary>
+/// <remarks>
+/// See <see href="https://dev.twitch.tv/docs/eventsub/eventsub-subscription-types/#channelshared_chatend">Channel Shared Chat End</see> for more information.
+/// </remarks>
+public record ChannelSharedChatEndNotification : EventSubNotification<ChannelSharedChatEndEvent, ChannelSharedChatEndCondition>;
+
+/// <summary>
+/// Contains subscription information specific to <see cref="EventSubSubscriptionType.ChannelSharedChatSessionEnd"/>.
+/// </summary>
+public record ChannelSharedChatEndCondition
+{
+    /// <summary>
+    /// The user id of the broadcaster (channel) to get Shared Chat End notifications for.
+    /// </summary>
+    public required string BroadcasterUserId { get; init; }
+}
+/// <summary>
+/// Contains information about a specific <see cref="EventSubSubscriptionType.ChannelSharedChatSessionEnd"/> event.
+/// </summary>
+public record ChannelSharedChatEndEvent
+{
+    /// <summary>
+    /// The id of the shared chat session.
+    /// </summary>
+    public required string SessionId { get; init; }
+    /// <summary>
+    /// The user id of the broadcaster (channel) from the subscription condition that is no longer active in the shared chat session.
+    /// </summary>
+    public required string BroadcasterUserId { get; init; }
+    /// <summary>
+    /// The display name of the broadcaster (channel) from the subscription condition that is no longer active in the shared chat session.
+    /// </summary>
+    public required string BroadcasterUserName { get; init; }
+    /// <summary>
+    /// The login (username) of the broadcaster (channel) from the subscription condition that is no longer active in the shared chat session.
+    /// </summary>
+    public required string BroadcasterUserLogin { get; init; }
+    /// <summary>
+    /// The user id of the broadcaster (channel) that was hosting the shared chat session.
+    /// </summary>
+    public required string HostBroadcasterUserId { get; init; }
+    /// <summary>
+    /// The display name of the broadcaster (channel) that was hosting the shared chat session.
+    /// </summary>
+    public required string HostBroadcasterUserName { get; init; }
+    /// <summary>
+    /// The login (username) of the broadcaster (channel) that was hosting the shared chat session.
+    /// </summary>
+    public required string HostBroadcasterUserLogin { get; init; }
+}
