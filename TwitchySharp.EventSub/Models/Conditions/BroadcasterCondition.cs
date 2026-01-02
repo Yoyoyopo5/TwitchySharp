13dae77917e415d34515b6a418a060fff25233d4
Yoyoyopo5 <yoyoyopo5@DESKTOP-IT4LOU9>
2026-01-02 04:50:56 -0600

    Add general condition types to cut down on code duplication.

---
+++ TwitchySharp.EventSub/Models/Conditions/BroadcasterCondition.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwitchySharp.EventSub.Models.Conditions;
+
+/// <summary>
+/// An EventSub notification condition with only a broadcaster user id.
+/// </summary>
+public record BroadcasterCondition
+{
+    /// <summary>
+    /// The user id of the broadcaster (channel) that the notification is for.
+    /// </summary>
+    public required string BroadcasterUserId { get; init; }
+}
---
+++ TwitchySharp.EventSub/Models/Conditions/BroadcasterModeratorCondition.cs
@@ -0,0 +1,12 @@
+﻿namespace TwitchySharp.EventSub.Models.Conditions;
+
+/// <summary>
+/// An EventSub notification condition with a broadcaster and moderator user id.
+/// </summary>
+public record BroadcasterModeratorCondition : BroadcasterCondition
+{
+    /// <summary>
+    /// The user id of the moderator (or the broadcaster) the notification is for.
+    /// </summary>
+    public required string ModeratorUserId { get; init; }
+}
