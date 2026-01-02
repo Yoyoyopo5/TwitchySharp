4737de5fd96e5ee13f10532048edd46312008765
Yoyoyopo5 <118655115+Yoyoyopo5@users.noreply.github.com>
2026-01-02 04:50:56 -0600

    Add general condition types to cut down on code duplication.

---
+++ TwitchySharp.EventSub/Models/Conditions/UserCondition.cs
@@ -0,0 +1,12 @@
+﻿namespace TwitchySharp.EventSub.Models.Conditions;
+
+/// <summary>
+/// An EventSub notification condition with a user id.
+/// </summary>
+public record UserCondition
+{
+    /// <summary>
+    /// The id of the user the notification is for.
+    /// </summary>
+    public required string UserId { get; init; }
+}
