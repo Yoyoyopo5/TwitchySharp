f6dae21b1e44d75c8245cf8ab2c7a01f55ea1493
Yoyoyopo5 <118655115+Yoyoyopo5@users.noreply.github.com>
2026-01-02 04:50:56 -0600

    Add general condition types to cut down on code duplication.

---
+++ TwitchySharp.EventSub/Models/Conditions/BroadcasterRewardCondition.cs
@@ -0,0 +1,12 @@
+﻿namespace TwitchySharp.EventSub.Models.Conditions;
+
+/// <summary>
+/// An EventSub notification condition with a broadcaster user id and channel points reward id.
+/// </summary>
+public record BroadcasterRewardCondition : BroadcasterCondition
+{
+    /// <summary>
+    /// The id of the channel points reward the notification is for.
+    /// </summary>
+    public required string RewardId { get; init; }
+}
