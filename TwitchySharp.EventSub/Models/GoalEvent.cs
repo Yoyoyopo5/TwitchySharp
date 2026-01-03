02cce7162263cbce974ca0f2caaea585cdfdecf3
Yoyoyopo5 <118655115+Yoyoyopo5@users.noreply.github.com>
2026-01-03 06:48:03 -0600

    Add channel goal notification types.

---
+++ TwitchySharp.EventSub/Models/GoalEvent.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TwitchySharp.Helpers;
+using TwitchySharp.Shared.EventSub.Enums;
+
+namespace TwitchySharp.EventSub.Models;
+
+/// <summary>
+/// The base class for channel goal events.
+/// </summary>
+/// <remarks>
+/// <see cref="EventSubSubscriptionType.GoalBegin"/>,
+/// <see cref="EventSubSubscriptionType.GoalProgress"/>,
+/// <see cref="EventSubSubscriptionType.GoalEnd"/>.
+/// </remarks>
+public record GoalEvent
+{
+    /// <summary>
+    /// The id of the event.
+    /// </summary>
+    public required string Id { get; init; }
+    /// <summary>
+    /// The user id of the broadcaster (channel) hosting the goal.
+    /// </summary>
+    public required string BroadcasterUserId { get; init; }
+    /// <summary>
+    /// The display name of the broadcaster (channel) hosting the goal.
+    /// </summary>
+    public required string BroadcasterUserName { get; init; }
+    /// <summary>
+    /// The login (username) of the broadcaster (channel) hosting the goal.
+    /// </summary>
+    public required string BroadcasterUserLogin { get; init; }
+    /// <summary>
+    /// The type of goal.
+    /// </summary>
+    public required ChannelGoalType Type { get; init; }
+    /// <summary>
+    /// The description of the goal, if specified by the broadcaster.
+    /// This can be a maximum of 40 characters.
+    /// </summary>
+    public required string Description { get; init; } // May be nullable.
+    /// <summary>
+    /// The current value of the goal.
+    /// The exact meaning of this integer is determined by <see cref="Type"/>.
+    /// </summary>
+    public required int CurrentAmount { get; init; }
+    /// <summary>
+    /// The target value of the goal.
+    /// </summary>
+    public required int TargetAmount { get; init; }
+    /// <summary>
+    /// The date and time when the goal was created.
+    /// </summary>
+    public required DateTimeOffset StartedAt { get; init; }
+}
+
+/// <summary>
+/// Contains static definitions for channel goal types.
+/// </summary>
+/// <param name="Value">The string value of the goal type.</param>
+public record ChannelGoalType(string Value) : ValueBackedEnum<string>(Value) // Not sure if we should merge this with the one from Api lib, there seems to be subtle differences in the docs spec.
+{
+    /// <summary>
+    /// The goal is to increase followers.
+    /// </summary>
+    public static ChannelGoalType Followers { get; } = new("follow"); // Api endpoint spec lists as "follower", need to investigate with live API.
+    /// <summary>
+    /// The goal is to increase subscriptions. 
+    /// This type shows the net increase or decrease in tier points associated with the subscriptions.
+    /// </summary>
+    public static ChannelGoalType Subscriptions { get; } = new("subscription");
+    /// <summary>
+    /// The goal is to increase subscriptions. 
+    /// This type shows the net increase or decrease in the number of subscriptions.
+    /// </summary>
+    public static ChannelGoalType SubscriptionCount { get; } = new("subscription_count");
+    /// <summary>
+    /// The goal is to increase subscriptions. 
+    /// This type shows only the net increase in tier points associated with the subscriptions (it does not account for users that unsubscribed since the goal started).
+    /// </summary>
+    public static ChannelGoalType NewSubscription { get; } = new("new_subscription");
+    /// <summary>
+    /// The goal is to increase subscriptions. 
+    /// This type shows only the net increase in the number of subscriptions (it does not account for users that unsubscribed since the goal started).
+    /// </summary>
+    public static ChannelGoalType NewSubscriptionCount { get; } = new("new_subscription_count");
+    /// <summary>
+    /// The goal is to increase the amount of Bits used on the channel.
+    /// </summary>
+    public static ChannelGoalType NewBits { get; } = new("new_bit");
+    /// <summary>
+    /// The goal is to increase the number of unique Cheerers to Cheer on the channel.
+    /// </summary>
+    public static ChannelGoalType NewCheerers { get; } = new("new_cheerer");
+}
---
+++ TwitchySharp.EventSub/Notifications/Channel/Goals/GoalEnd.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TwitchySharp.Shared.EventSub.Enums;
+using TwitchySharp.EventSub.Models.Conditions;
+using TwitchySharp.EventSub.Models;
+
+namespace TwitchySharp.EventSub.Notifications.Channel.Goals;
+/// <summary>
+/// <inheritdoc cref="EventSubSubscriptionType.GoalEnd"/>
+/// </summary>
+/// <remarks>
+/// See <see href="https://dev.twitch.tv/docs/eventsub/eventsub-subscription-types/#channelgoalend">Goal End</see> for more information.
+/// </remarks>
+public record GoalEndNotification : EventSubNotification<GoalEndEvent, GoalEndCondition>;
+/// <summary>
+/// Contains subscription information specific to <see cref="EventSubSubscriptionType.GoalEnd"/>.
+/// </summary>
+public record GoalEndCondition : BroadcasterCondition;
+/// <summary>
+/// Contains information about a specific <see cref="EventSubSubscriptionType.GoalEnd"/> event.
+/// </summary>
+public record GoalEndEvent : GoalEvent
+{
+    /// <summary>
+    /// Indicates whether the goal was achieved (i.e., the target amount was reached when the goal ended).
+    /// </summary>
+    public required bool IsAchieved { get; init; }
+    /// <summary>
+    /// The date and time when the goal was ended by the broadcaster.
+    /// </summary>
+    public required DateTimeOffset EndedAt { get; init; }
+}
