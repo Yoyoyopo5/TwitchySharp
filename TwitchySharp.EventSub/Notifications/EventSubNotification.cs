753f21e79f18d15933b8771439b011f137aae9b6
Yoyoyopo5 <yoyoyopo5@DESKTOP-IT4LOU9>
2026-01-03 04:55:51 -0600

    Add support for EventSub notifications with multiple events (DropEntitlementGrant).

--- TwitchySharp.EventSub/Notifications/EventSubNotification.cs
+++ TwitchySharp.EventSub/Notifications/EventSubNotification.cs
@@ -31,6 +31,33 @@ public record EventSubNotification<TEvent, TCondition> : IEventSubNotification
 }
 
 /// <summary>
+/// Base class for EventSub notifications that have multiple events.
+/// </summary>
+/// <remarks>
+/// Right now, only <see cref="EventSubSubscriptionType.DropEntitlementGrant"/> has multiple events in its notification.
+/// Annoying, but we need this type to support it.
+/// </remarks>
+/// <typeparam name="TEvent">Type of the event property of the notification.</typeparam>
+/// <typeparam name="TCondition">Type of the condition property of the subscription property of the notification.</typeparam>
+public record EventSubNotificationWithMultipleEvents<TEvent, TCondition> : IEventSubNotification
+    where TEvent : class
+    where TCondition : class
+{
+    /// <summary>
+    /// Contains information about the subscription that the notification is for.
+    /// </summary>
+    public required EventSubSubscription<TCondition> Subscription { get; init; }
+    /// <summary>
+    /// <inheritdoc cref="IEventSubNotification.Subscription"/>
+    /// </summary>
+    EventSubSubscription IEventSubNotification.Subscription => Subscription;
+    /// <summary>
+    /// Contains information about the event that triggered the notification.
+    /// </summary>
+    public required TEvent[] Events { get; init; }
+}
+
+/// <summary>
 /// Contains basic functionality for untyped notifications.
 /// Use reflection, a switch expression (preferred), or properties of the <see cref="Subscription"/> to determine the underlying type of the notification.
 /// </summary>
