69e00995f048f5ae502acdce63b1434c16024166
Yoyoyopo5 <yoyoyopo5@DESKTOP-IT4LOU9>
2026-01-02 19:10:28 -0600

    Add ChannelPointsCustomRewardUpdate notification type.

---
+++ TwitchySharp.EventSub/Notifications/Channel/ChannelPoints/ChannelPointsCustomRewardUpdate.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TwitchySharp.EventSub.Models;
+using TwitchySharp.EventSub.Models.Conditions;
+using TwitchySharp.Shared.EventSub.Enums;
+
+namespace TwitchySharp.EventSub.Notifications.Channel.ChannelPoints;
+/// <summary>
+/// <inheritdoc cref="EventSubSubscriptionType.ChannelPointsCustomRewardUpdate"/>
+/// </summary>
+/// <remarks>
+/// See <see href="https://dev.twitch.tv/docs/eventsub/eventsub-subscription-types/#channelchannel_points_custom_rewardupdate">Channel Points Custom Reward Update</see> for more information.
+/// </remarks>
+public record ChannelPointsCustomRewardUpdateNotification : EventSubNotification<ChannelPointsCustomRewardUpdateEvent, ChannelPointsCustomRewardUpdateCondition>;
+/// <summary>
+/// Contains subscription information specific to <see cref="EventSubSubscriptionType.ChannelPointsCustomRewardUpdate"/>.
+/// </summary>
+public record ChannelPointsCustomRewardUpdateCondition : BroadcasterRewardCondition;
+/// <summary>
+/// Contains information about a specific <see cref="EventSubSubscriptionType.ChannelPointsCustomRewardUpdate"/> event.
+/// </summary>
+public record ChannelPointsCustomRewardUpdateEvent : ChannelPointsCustomRewardEvent;
