0201179afd341b41dc5c869260ebea89f6667b56
Yoyoyopo5 <118655115+Yoyoyopo5@users.noreply.github.com>
2026-01-03 04:34:01 -0600

    Move all EventSub types in Api library to use constant values defined in Shared lib.

--- TwitchySharp.Api/Helix/EventSub/Requests/CreateEventSubSubscriptionRequest.cs
+++ TwitchySharp.Api/Helix/EventSub/Requests/CreateEventSubSubscriptionRequest.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 using TwitchySharp.Api.Authorization;
+using TwitchySharp.Shared.EventSub.Constants;
 
 namespace TwitchySharp.Api.Helix.EventSub;
 /// <summary>
