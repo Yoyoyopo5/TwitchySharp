7b8da52f3498731c869fe1f9fb027a44ecd20206
Yoyoyopo5 <118655115+Yoyoyopo5@users.noreply.github.com>
2026-01-03 00:38:45 -0600

    Add Channel VIP notification types.

---
+++ TwitchySharp.EventSub/Notifications/Channel/Vip/ChannelVipAdd.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TwitchySharp.EventSub.Models;
+using TwitchySharp.EventSub.Models.Conditions;
+using TwitchySharp.Shared.EventSub.Enums;
+
+namespace TwitchySharp.EventSub.Notifications.Channel.Vip;
+/// <summary>
+/// <inheritdoc cref="EventSubSubscriptionType.ChannelVipAdd"/>
+/// </summary>
+/// <remarks>
+/// See <see href="https://dev.twitch.tv/docs/eventsub/eventsub-subscription-types/#channelvipadd">Channel VIP Add</see> for more information.
+/// </remarks>
+public record ChannelVipAddNotification : EventSubNotification<ChannelVipAddEvent, ChannelVipAddCondition>;
+/// <summary>
+/// Contains subscription information specific to <see cref="EventSubSubscriptionType.ChannelVIPAdd"/>.
+/// </summary>
+public record ChannelVipAddCondition : BroadcasterCondition;
+/// <summary>
+/// Contains information about a specific <see cref="EventSubSubscriptionType.ChannelVIPAdd"/> event.
+/// </summary>
+public record ChannelVipAddEvent : ChannelVipEvent;
---
+++ TwitchySharp.EventSub/Notifications/Channel/Vip/ChannelVipRemove.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TwitchySharp.EventSub.Models;
+using TwitchySharp.EventSub.Models.Conditions;
+using TwitchySharp.Shared.EventSub.Enums;
+
+namespace TwitchySharp.EventSub.Notifications.Channel.Vip;
+/// <summary>
+/// <inheritdoc cref="EventSubSubscriptionType.ChannelVIPRemove"/>
+/// </summary>
+/// <remarks>
+/// See <see href="https://dev.twitch.tv/docs/eventsub/eventsub-subscription-types/#channelvipremove">Channel VIP Remove</see> for more information.
+/// </remarks>
+public record ChannelVipRemoveNotification : EventSubNotification<ChannelVipRemoveEvent, ChannelVipRemoveCondition>;
+/// <summary>
+/// Contains subscription information specific to <see cref="EventSubSubscriptionType.ChannelVIPRemove"/>.
+/// </summary>
+public record ChannelVipRemoveCondition : BroadcasterCondition;
+/// <summary>
+/// Contains information about a specific <see cref="EventSubSubscriptionType.ChannelVIPRemove"/> event.
+/// </summary>
+public record ChannelVipRemoveEvent : ChannelVipEvent;
