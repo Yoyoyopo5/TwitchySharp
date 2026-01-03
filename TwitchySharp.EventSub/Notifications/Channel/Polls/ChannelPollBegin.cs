d9c56aaf2b96f9ad316a362deb94c5026b34da25
Yoyoyopo5 <yoyoyopo5@DESKTOP-IT4LOU9>
2026-01-02 20:55:13 -0600

    Add Channel Poll notification types.

---
+++ TwitchySharp.EventSub/Notifications/Channel/Polls/ChannelPollBegin.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TwitchySharp.EventSub.Models;
+using TwitchySharp.EventSub.Models.Conditions;
+using TwitchySharp.Shared.EventSub.Enums;
+
+namespace TwitchySharp.EventSub.Notifications.Channel.Polls;
+/// <summary>
+/// <inheritdoc cref="EventSubSubscriptionType.ChannelPollBegin"/>
+/// </summary>
+/// <remarks>
+/// See <see href="https://dev.twitch.tv/docs/eventsub/eventsub-subscription-types/#channelpollbegin">Channel Poll Begin</see> for more information.
+/// </remarks>
+public record ChannelPollBeginNotification : EventSubNotification<ChannelPollBeginEvent, ChannelPollBeginCondition>;
+/// <summary>
+/// Contains subscription information specific to <see cref="EventSubSubscriptionType.ChannelPollBegin"/>.
+/// </summary>
+public record ChannelPollBeginCondition : BroadcasterCondition;
+/// <summary>
+/// Contains information about a specific <see cref="EventSubSubscriptionType.ChannelPollBegin"/> event.
+/// </summary>
+public record ChannelPollBeginEvent : ChannelPollEvent
+{
+    /// <summary>
+    /// The date and time when the poll will end.
+    /// </summary>
+    public required DateTimeOffset EndsAt { get; init; }
+}
---
+++ TwitchySharp.EventSub/Notifications/Channel/Polls/ChannelPollEnd.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TwitchySharp.EventSub.Models;
+using TwitchySharp.EventSub.Models.Conditions;
+using TwitchySharp.Shared.EventSub.Enums;
+
+namespace TwitchySharp.EventSub.Notifications.Channel.Polls;
+/// <summary>
+/// <inheritdoc cref="EventSubSubscriptionType.ChannelPollEnd"/>
+/// </summary>
+/// <remarks>
+/// See <see href="https://dev.twitch.tv/docs/eventsub/eventsub-subscription-types/#channelpollend">Channel Poll End</see> for more information.
+/// </remarks>
+public record ChannelPollEndNotification : EventSubNotification<ChannelPollEndEvent, ChannelPollEndCondition>;
+/// <summary>
+/// Contains subscription information specific to <see cref="EventSubSubscriptionType.ChannelPollEnd"/>.
+/// </summary>
+public record ChannelPollEndCondition : BroadcasterCondition;
+/// <summary>
+/// Contains information about a specific <see cref="EventSubSubscriptionType.ChannelPollEnd"/> event.
+/// </summary>
+public record ChannelPollEndEvent : ChannelPollEvent
+{
+    /// <summary>
+    /// The date and time when the poll ended.
+    /// </summary>
+    public required DateTimeOffset EndedAt { get; init; }
+}
