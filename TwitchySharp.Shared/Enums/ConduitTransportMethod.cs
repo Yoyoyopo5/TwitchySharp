dc6ed56011d7847434dcd3c82f53a83681e9faf3
Yoyoyopo5 <yoyoyopo5@DESKTOP-IT4LOU9>
2026-01-03 04:07:53 -0600

    Add ConduitTransportMethod ValueBackedEnum in Shared lib as replacement for the enum.

---
+++ TwitchySharp.Shared/Enums/ConduitTransportMethod.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using TwitchySharp.Helpers;
+
+namespace TwitchySharp.Shared.Enums;
+
+/// <summary>
+/// Contains static definitions for possible conduit transport methods.
+/// </summary>
+/// <param name="Value">The string value of the transport method.</param>
+[JsonConverter(typeof(ValueBackedEnumJsonConverter<ConduitTransportMethod, string>))]
+public record ConduitTransportMethod(string Value) : ValueBackedEnum<string>(Value)
+{
+    public static ConduitTransportMethod Websocket { get; } = new("websocket");
+    public static ConduitTransportMethod Webhook { get; } = new("webhook");
+}
