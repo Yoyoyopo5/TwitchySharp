c249df248220357a044bff9476893d10755c9f18
Yoyoyopo5 <118655115+Yoyoyopo5@users.noreply.github.com>
2026-01-04 11:30:50 -0600

    Add basic interfaces for event types.

---
+++ TwitchySharp.EventSub/Interfaces/IHaveBroadcaster.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwitchySharp.EventSub.Interfaces;
+/// <summary>
+/// An event associated with a specific broadcaster.
+/// </summary>
+public interface IHaveBroadcaster
+{
+    /// <summary>
+    /// The user id of the broadcaster (channel) that the event is for.
+    /// </summary>
+    string BroadcasterUserId { get; init; }
+    /// <summary>
+    /// The login (username) of the broadcaster (channel) that the event is for.
+    /// </summary>
+    string BroadcasterUserLogin { get; init; }
+    /// <summary>
+    /// The display name of the broadcaster (channel) that the event is for.
+    /// </summary>
+    string BroadcasterUserName { get; init; }
+}
