9ab12997e4eadce03165d9adc4c958cf60171e95
Yoyoyopo5 <118655115+Yoyoyopo5@users.noreply.github.com>
2026-01-04 06:44:47 -0600

    Add User Authorization notification types.

---
+++ TwitchySharp.EventSub/Models/UserAuthorizationEvent.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TwitchySharp.Shared.EventSub.Enums;
+
+namespace TwitchySharp.EventSub.Models;
+
+/// <summary>
+/// The base class for user authorization events.
+/// </summary>
+/// <remarks>
+/// <see cref="EventSubSubscriptionType.UserAuthorizationGrant"/>,
+/// <see cref="EventSubSubscriptionType.UserAuthorizationRevoke"/>.
+/// </remarks>
+public record UserAuthorizationEvent
+{
+    /// <summary>
+    /// The client id of the application the authorization is associated with.
+    /// </summary>
+    public required string ClientId { get; init; }
+    /// <summary>
+    /// The id of the user whose authorization status changed.
+    /// </summary>
+    public required string UserId { get; init; }
+    /// <summary>
+    /// The login (username) of the user whose authorization status changed.
+    /// </summary>
+    /// <remarks>
+    /// This is <see langword="null"/> if the user no longer exists.
+    /// </remarks>
+    public string? UserLogin { get; init; }
+    /// <summary>
+    /// The display name of the user whose authorization status changed.
+    /// </summary>
+    /// <remarks>
+    /// This is <see langword="null"/> if the user no longer exists.
+    /// </remarks>
+    public string? UserName { get; init; }
+}
---
+++ TwitchySharp.EventSub/Notifications/User/Authorization/UserAuthorizationRevoke.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TwitchySharp.Shared.EventSub.Enums;
+using TwitchySharp.EventSub.Models;
+using TwitchySharp.EventSub.Models.Conditions;
+
+namespace TwitchySharp.EventSub.Notifications.User.Authorization;
+/// <summary>
+/// <inheritdoc cref="EventSubSubscriptionType.UserAuthorizationRevoke"/>
+/// </summary>
+/// <remarks>
+/// See <see href="https://dev.twitch.tv/docs/eventsub/eventsub-subscription-types/#userauthorizationrevoke">User Authorization Revoke</see> for more information.
+/// </remarks>
+public record UserAuthorizationRevokeNotification : EventSubNotification<UserAuthorizationRevokeEvent, UserAuthorizationRevokeCondition>;
+/// <summary>
+/// Contains subscription information specific to <see cref="EventSubSubscriptionType.UserAuthorizationRevoke"/>.
+/// </summary>
+public record UserAuthorizationRevokeCondition : ClientCondition;
+/// <summary>
+/// Contains information about a specific <see cref="EventSubSubscriptionType.UserAuthorizationRevoke"/> event.
+/// </summary>
+public record UserAuthorizationRevokeEvent : UserAuthorizationEvent;
