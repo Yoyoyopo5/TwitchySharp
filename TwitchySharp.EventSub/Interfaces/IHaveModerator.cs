75105dd8d12f17d105bc5cef592425ff0a8c9da5
Yoyoyopo5 <118655115+Yoyoyopo5@users.noreply.github.com>
2026-01-04 11:30:50 -0600

    Add basic interfaces for event types.

---
+++ TwitchySharp.EventSub/Interfaces/IHaveModerator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwitchySharp.EventSub.Interfaces;
+
+/// <summary>
+/// An event associated with a specific moderator.
+/// </summary>
+public interface IHaveModerator
+{
+    /// <summary>
+    /// The user id of the moderator associated with the event.
+    /// </summary>
+    string ModeratorUserId { get; init; }
+    /// <summary>
+    /// The display name of the moderator associated with the event.
+    /// </summary>
+    string ModeratorUserName { get; init; }
+    /// <summary>
+    /// The login (username) of the moderator associated with the event.
+    /// </summary>
+    string ModeratorUserLogin { get; init; }
+}
