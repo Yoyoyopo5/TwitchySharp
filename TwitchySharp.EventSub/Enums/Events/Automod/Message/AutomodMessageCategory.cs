2d1785ee24b539aa93c3ee2efd2930363b36dc29
Yoyoyopo5 <118655115+Yoyoyopo5@users.noreply.github.com>
2026-01-04 11:50:51 -0600

    Organize folder and namespace structure.

---
+++ TwitchySharp.EventSub/Enums/Events/Automod/Message/AutomodMessageCategory.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.Json.Serialization;
+using TwitchySharp.Helpers;
+
+namespace TwitchySharp.EventSub.Enums.Events.Automod.Message;
+
+/// <summary>
+/// Contains static definitions for possible Automod categories for held messages.
+/// </summary>
+/// <param name="Value">The string value of the Automod message category.</param>
+[JsonConverter(typeof(ValueBackedEnumJsonConverter<AutomodMessageCategory, string>))]
+public record AutomodMessageCategory(string Value) : ValueBackedEnum<string>(Value)
+{
+    // TODO: figure out what these categories can be, docs did not list.
+}
---
+++ TwitchySharp.EventSub/Enums/Events/Channel/Bits/ChannelBitsUseType.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.Json.Serialization;
+using TwitchySharp.Helpers;
+
+namespace TwitchySharp.EventSub.Enums.Events.Channel.Bits;
+
+/// <summary>
+/// Contains static definitions for possible channel bits use types.
+/// </summary>
+/// <param name="Value"></param>
+[JsonConverter(typeof(ValueBackedEnumJsonConverter<ChannelBitsUseType, string>))]
+public record ChannelBitsUseType(string Value) : ValueBackedEnum<string>(Value)
+{
+    public static ChannelBitsUseType Cheer { get; } = new("cheer");
+    public static ChannelBitsUseType PowerUp { get; } = new("power_up");
+}
