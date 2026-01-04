471ed419924254582be30a823f5df7158c5f2c24
Yoyoyopo5 <118655115+Yoyoyopo5@users.noreply.github.com>
2026-01-04 03:04:01 -0600

    Add Hype Train notification types.

---
+++ TwitchySharp.EventSub/Notifications/Channel/HypeTrain/HypeTrainBeginV2.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TwitchySharp.Shared.EventSub.Enums;
+using TwitchySharp.EventSub.Models;
+using TwitchySharp.EventSub.Models.Conditions;
+
+namespace TwitchySharp.EventSub.Notifications.Channel.HypeTrain;
+/// <summary>
+/// <inheritdoc cref="EventSubSubscriptionType.HypeTrainBeginV2"/>
+/// </summary>
+/// <remarks>
+/// See <see href="https://dev.twitch.tv/docs/eventsub/eventsub-subscription-types/#channelhype_trainbegin-v2">Hype Train Begin V2</see> for more information.
+/// </remarks>
+public record HypeTrainBeginV2Notification : EventSubNotification<HypeTrainBeginV2Event, HypeTrainBeginV2Condition>;
+/// <summary>
+/// Contains subscription information specific to <see cref="EventSubSubscriptionType.HypeTrainBeginV2"/>.
+/// </summary>
+public record HypeTrainBeginV2Condition : BroadcasterCondition;
+/// <summary>
+/// Contains information about a specific <see cref="EventSubSubscriptionType.HypeTrainBeginV2"/> event.
+/// </summary>
+public record HypeTrainBeginV2Event : HypeTrainActiveEvent
+{
+    /// <summary>
+    /// The highest level this type of Hype Train has ever reached for the broadcaster.
+    /// </summary>
+    public required int AllTimeHighLevel { get; init; }
+    /// <summary>
+    /// The highest total points this type of Hype Train has ever reached for the broadcaster.
+    /// </summary>
+    public required int AllTimeHighTotal { get; init; }
+}
---
+++ TwitchySharp.EventSub/Notifications/Channel/HypeTrain/HypeTrainProgressV2.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TwitchySharp.Shared.EventSub.Enums;
+using TwitchySharp.EventSub.Models;
+using TwitchySharp.EventSub.Models.Conditions;
+
+namespace TwitchySharp.EventSub.Notifications.Channel.HypeTrain;
+/// <summary>
+/// <inheritdoc cref="EventSubSubscriptionType.HypeTrainProgressV2"/>
+/// </summary>
+/// <remarks>
+/// See <see href="https://dev.twitch.tv/docs/eventsub/eventsub-subscription-types/#channelhype_trainprogress-v2">Hype Train Progress V2</see> for more information.
+/// </remarks>
+public record HypeTrainProgressV2Notification : EventSubNotification<HypeTrainProgressV2Event, HypeTrainProgressV2Condition>;
+/// <summary>
+/// Contains subscription information specific to <see cref="EventSubSubscriptionType.HypeTrainProgressV2"/>.
+/// </summary>
+public record HypeTrainProgressV2Condition : BroadcasterCondition;
+/// <summary>
+/// Contains information about a specific <see cref="EventSubSubscriptionType.HypeTrainProgressV2"/> event.
+/// </summary>
+public record HypeTrainProgressV2Event : HypeTrainActiveEvent;
