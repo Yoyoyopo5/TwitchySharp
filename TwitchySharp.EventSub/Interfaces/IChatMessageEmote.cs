7e9a2a082b8301cc5971fa91128a5d16b18151ac
Yoyoyopo5 <118655115+Yoyoyopo5@users.noreply.github.com>
2026-01-04 12:29:55 -0600

    Decouple Bits notification type from ChannelChatMessage types and add interfaces.

---
+++ TwitchySharp.EventSub/Interfaces/IChatMessageEmote.cs
@@ -0,0 +1,26 @@
+﻿using TwitchySharp.EventSub.Enums;
+
+namespace TwitchySharp.EventSub.Interfaces;
+
+/// <summary>
+/// An emote appearing in a Twitch chat message.
+/// </summary>
+public interface IChatMessageEmote
+{
+    /// <summary>
+    /// The id of the emote.
+    /// </summary>
+    string Id { get; }
+    /// <summary>
+    /// The id of the emote set that the emote belongs to.
+    /// </summary>
+    string EmoteSetId { get; }
+    /// <summary>
+    /// The user id of the broadcaster (channel) who owns the emote.
+    /// </summary>
+    string OwnerId { get; }
+    /// <summary>
+    /// The formats that the emote is available in.
+    /// </summary>
+    ChatMessageEmoteFormat[] Format { get; }
+}
