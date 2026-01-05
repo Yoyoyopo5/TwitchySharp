219181db8eca9f63be52cff3855c426569309d97
Yoyoyopo5 <yoyoyopo5@DESKTOP-IT4LOU9>
2026-01-05 05:25:01 -0600

    Organize and add interfaces for Channel Poll event types.

---
+++ TwitchySharp.EventSub/Interfaces/Events/Channel/Polls/IHavePoll.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TwitchySharp.EventSub.Models.Events.Channel.Polls;
+
+namespace TwitchySharp.EventSub.Interfaces.Events.Channel.Polls;
+
+/// <summary>
+/// A chat poll.
+/// </summary>
+public interface IHavePoll
+{
+    /// <summary>
+    /// The id of the poll.
+    /// </summary>
+    string Id { get; }
+    /// <summary>
+    /// The title of the poll.
+    /// </summary>
+    string Title { get; }
+    /// <summary>
+    /// The choices for the poll, including their vote count.
+    /// </summary>
+    ChannelPollChoice[] Choices { get; }
+    /// <summary>
+    /// The setting for Channel Points voting.
+    /// </summary>
+    ChannelPollChannelPointsVotingSetting ChannelPointsVoting { get; }
+    /// <summary>
+    /// The date and time the poll began.
+    /// </summary>
+    DateTimeOffset StartedAt { get; }
+}
--- TwitchySharp.EventSub/Notifications/Channel/Polls/ChannelPollEnd.cs
+++ TwitchySharp.EventSub/Notifications/Channel/Polls/ChannelPollEnd.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using TwitchySharp.EventSub.Models;
+using TwitchySharp.EventSub.Interfaces.Events;
+using TwitchySharp.EventSub.Interfaces.Events.Channel.Polls;
 using TwitchySharp.EventSub.Models.Conditions;
+using TwitchySharp.EventSub.Models.Events.Channel.Polls;
 using TwitchySharp.Shared.EventSub.Enums;
 
 namespace TwitchySharp.EventSub.Notifications.Channel.Polls;
@@ -22,8 +24,25 @@ public record ChannelPollEndCondition : BroadcasterCondition;
 /// <summary>
 /// Contains information about a specific <see cref="EventSubSubscriptionType.ChannelPollEnd"/> event.
 /// </summary>
-public record ChannelPollEndEvent : ChannelPollEvent
+public record ChannelPollEndEvent : IHavePoll, IHaveBroadcaster
 {
+    public required string Id { get; init; }
+    /// <summary>
+    /// The user id of the broadcaster (channel) hosting the poll.
+    /// </summary>
+    public required string BroadcasterUserId { get; init; }
+    /// <summary>
+    /// The login (username) of the broadcaster (channel) hosting the poll.
+    /// </summary>
+    public required string BroadcasterUserLogin { get; init; }
+    /// <summary>
+    /// The display name of the broadcaster (channel) hosting the poll.
+    /// </summary>
+    public required string BroadcasterUserName { get; init; }
+    public required string Title { get; init; }
+    public required ChannelPollChoice[] Choices { get; init; }
+    public required ChannelPollChannelPointsVotingSetting ChannelPointsVoting { get; init; }
+    public required DateTimeOffset StartedAt { get; init; }
     /// <summary>
     /// The date and time when the poll ended.
     /// </summary>
