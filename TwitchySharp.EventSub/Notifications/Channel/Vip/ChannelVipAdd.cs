54447c1f241ca830c2c3378da0b108f58e882f20
Yoyoyopo5 <yoyoyopo5@DESKTOP-IT4LOU9>
2026-01-07 08:06:44 -0600

    Add interfaces for Channel VIP event types.

--- TwitchySharp.EventSub/Notifications/Channel/Vip/ChannelVipAdd.cs
+++ TwitchySharp.EventSub/Notifications/Channel/Vip/ChannelVipAdd.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TwitchySharp.EventSub.Interfaces.Events;
 using TwitchySharp.EventSub.Models;
 using TwitchySharp.EventSub.Models.Conditions;
 using TwitchySharp.Shared.EventSub.Enums;
@@ -22,4 +23,30 @@ public record ChannelVipAddCondition : BroadcasterCondition;
 /// <summary>
 /// Contains information about a specific <see cref="EventSubSubscriptionType.ChannelVIPAdd"/> event.
 /// </summary>
-public record ChannelVipAddEvent : ChannelVipEvent;
+public record ChannelVipAddEvent : IHaveBroadcaster, IHaveUser
+{
+    /// <summary>
+    /// The id of the user added as a VIP.
+    /// </summary>
+    public required string UserId { get; init; }
+    /// <summary>
+    /// The login (username) of the user added as a VIP.
+    /// </summary>
+    public required string UserLogin { get; init; }
+    /// <summary>
+    /// The display name of the user added as a VIP.
+    /// </summary>
+    public required string UserName { get; init; }
+    /// <summary>
+    /// The user id of the broadcaster (channel) in whose chat the VIP was added.
+    /// </summary>
+    public required string BroadcasterUserId { get; init; }
+    /// <summary>
+    /// The login (username) of the broadcaster (channel) in whose chat the VIP was added.
+    /// </summary>
+    public required string BroadcasterUserLogin { get; init; }
+    /// <summary>
+    /// The display name of the broadcaster (channel) in whose chat the VIP was added.
+    /// </summary>
+    public required string BroadcasterUserName { get; init; }
+}
