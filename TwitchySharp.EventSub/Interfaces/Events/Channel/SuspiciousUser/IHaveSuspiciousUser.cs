a006fe04c468cec65ca868049084988978706c4e
Yoyoyopo5 <118655115+Yoyoyopo5@users.noreply.github.com>
2026-01-07 06:37:27 -0600

    Organize and add interfaces to Suspicious User event types.

---
+++ TwitchySharp.EventSub/Interfaces/Events/Channel/SuspiciousUser/IHaveSuspiciousUser.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TwitchySharp.EventSub.Enums.Events.Channel.SuspiciousUser;
+
+namespace TwitchySharp.EventSub.Interfaces.Events.Channel.SuspiciousUser;
+
+/// <summary>
+/// A suspicious chat user.
+/// </summary>
+public interface IHaveSuspiciousUser : IHaveUser
+{
+    /// <summary>
+    /// The current status of the suspicious user as set by a moderator.
+    /// </summary>
+    SuspiciousUserStatus LowTrustStatus { get; }
+}
---
+++ TwitchySharp.EventSub/Models/Events/Channel/SuspiciousUser/SuspiciousUserChatMessageCheermote.cs
@@ -0,0 +1,13 @@
+﻿using TwitchySharp.EventSub.Interfaces;
+
+namespace TwitchySharp.EventSub.Models.Events.Channel.SuspiciousUser;
+
+/// <summary>
+/// Contains information about a cheermote used in a suspicious user chat message.
+/// </summary>
+public record SuspiciousUserChatMessageCheermote : IChatMessageCheermote
+{
+    public required string Prefix { get; init; }
+    public required int Bits { get; init; } // Docs have typo
+    public required int Tier { get; init; }
+}
