0825f96d9e95df950f3fa7c80f4a33609e35f7e1
Yoyoyopo5 <yoyoyopo5@DESKTOP-IT4LOU9>
2026-01-07 13:07:48 -0600

    Add interfaces for Channel Warning event types.

--- TwitchySharp.EventSub/Notifications/Channel/Warning/ChannelWarningAcknowledgement.cs
+++ TwitchySharp.EventSub/Notifications/Channel/Warning/ChannelWarningAcknowledgement.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TwitchySharp.EventSub.Interfaces.Events;
 using TwitchySharp.EventSub.Models.Conditions;
 using TwitchySharp.Shared.EventSub.Enums;
 
@@ -21,7 +22,7 @@ public record ChannelWarningAcknowledgementCondition : BroadcasterModeratorCondi
 /// <summary>
 /// Contains information about a specific <see cref="EventSubSubscriptionType.ChannelWarningAcknowledgement"/> event.
 /// </summary>
-public record ChannelWarningAcknowledgementEvent
+public record ChannelWarningAcknowledgementEvent : IHaveBroadcaster, IHaveUser
 {
     /// <summary>
     /// The user id of the broadcaster (channel) where the warning was issued.
