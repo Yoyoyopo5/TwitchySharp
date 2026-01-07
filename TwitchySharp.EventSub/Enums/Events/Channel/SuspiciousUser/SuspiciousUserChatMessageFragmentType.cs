b253a27a7d3273cec642b766853fea843c7178d0
Yoyoyopo5 <118655115+Yoyoyopo5@users.noreply.github.com>
2026-01-07 06:37:27 -0600

    Organize and add interfaces to Suspicious User event types.

---
+++ TwitchySharp.EventSub/Enums/Events/Channel/SuspiciousUser/SuspiciousUserChatMessageFragmentType.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.Json.Serialization;
+using TwitchySharp.Helpers;
+
+namespace TwitchySharp.EventSub.Enums.Events.Channel.SuspiciousUser;
+
+/// <summary>
+/// Contains static definitions of possible chat message fragment types for suspicious user messages.
+/// </summary>
+/// <param name="Value">The string value of the fragment type.</param>
+[JsonConverter(typeof(ValueBackedEnumJsonConverter<SuspiciousUserChatMessageFragmentType, string>))]
+public record SuspiciousUserChatMessageFragmentType(string Value) : ValueBackedEnum<string>(Value)
+{
+    public static SuspiciousUserChatMessageFragmentType Text { get; } = new("text");
+    public static SuspiciousUserChatMessageFragmentType Cheermote { get; } = new("cheermote");
+    public static SuspiciousUserChatMessageFragmentType Emote { get; } = new("emote");
+}
---
+++ TwitchySharp.EventSub/Models/Events/Channel/SuspiciousUser/SuspiciousUserChatMessage.cs
@@ -0,0 +1,17 @@
+﻿using TwitchySharp.EventSub.Interfaces;
+
+namespace TwitchySharp.EventSub.Models.Events.Channel.SuspiciousUser;
+
+/// <summary>
+/// Contains information about a specific chat message from a suspicious user.
+/// </summary>
+public record SuspiciousUserChatMessage : IChatMessage
+{
+    /// <summary>
+    /// The id of the message.
+    /// </summary>
+    public required string MessageId { get; init; }
+    public required string Text { get; init; }
+    public required SuspiciousUserChatMessageFragment[] Fragments { get; init; }
+    IEnumerable<IChatMessageFragment> IChatMessage.Fragments => Fragments;
+}
