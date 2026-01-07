10d0bba0f7cd4d22cc9c999b50002f17032c2ee8
Yoyoyopo5 <118655115+Yoyoyopo5@users.noreply.github.com>
2026-01-07 14:04:23 -0600

    Add interfaces for User Authorization event types.

--- TwitchySharp.EventSub/Notifications/User/Authorization/UserAuthorizationRevoke.cs
+++ TwitchySharp.EventSub/Notifications/User/Authorization/UserAuthorizationRevoke.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using TwitchySharp.Shared.EventSub.Enums;
 using TwitchySharp.EventSub.Models;
 using TwitchySharp.EventSub.Models.Conditions;
+using TwitchySharp.EventSub.Interfaces.Events;
 
 namespace TwitchySharp.EventSub.Notifications.User.Authorization;
 /// <summary>
@@ -22,4 +23,28 @@ public record UserAuthorizationRevokeCondition : ClientCondition;
 /// <summary>
 /// Contains information about a specific <see cref="EventSubSubscriptionType.UserAuthorizationRevoke"/> event.
 /// </summary>
-public record UserAuthorizationRevokeEvent : UserAuthorizationEvent;
+public record UserAuthorizationRevokeEvent : IHaveClient
+{
+    /// <summary>
+    /// The client id of the application the authorization is associated with.
+    /// </summary>
+    public required string ClientId { get; init; }
+    /// <summary>
+    /// The id of the user whose authorization status was revoked.
+    /// </summary>
+    public required string UserId { get; init; }
+    /// <summary>
+    /// The login (username) of the user whose authorization status was revoked.
+    /// </summary>
+    /// <remarks>
+    /// This is <see langword="null"/> if the user no longer exists.
+    /// </remarks>
+    public string? UserLogin { get; init; }
+    /// <summary>
+    /// The display name of the user whose authorization status was revoked.
+    /// </summary>
+    /// <remarks>
+    /// This is <see langword="null"/> if the user no longer exists.
+    /// </remarks>
+    public string? UserName { get; init; }
+}
