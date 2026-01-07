d576e188f0b045fa0d4467d88a0ac8264aef535f
Yoyoyopo5 <yoyoyopo5@DESKTOP-IT4LOU9>
2026-01-07 13:51:14 -0600

    Organize and add interface to Drop Entitlement Grant event type.

--- TwitchySharp.EventSub/Notifications/Drops/DropEntitlementGrant.cs
+++ TwitchySharp.EventSub/Notifications/Drops/DropEntitlementGrant.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TwitchySharp.EventSub.Models.Events.Drops;
 using TwitchySharp.Shared.EventSub.Enums;
 
 namespace TwitchySharp.EventSub.Notifications.Drops;
@@ -44,51 +45,4 @@ public record DropEntitlementGrantEvent
     /// Contains information about the specific drop entitlement grant. 
     /// </summary>
     public required DropEntitlementGrantEventData Data { get; init; }
-}
-
-/// <summary>
-/// Contains information about a specific Twitch drops entitlement grant event.
-/// </summary>
-public record DropEntitlementGrantEventData
-{
-    /// <summary>
-    /// The id of the organization that owns the category (game) that the drop is for.
-    /// </summary>
-    public required string OrganizationId { get; init; }
-    /// <summary>
-    /// The id of the category (game) that the drop is for.
-    /// </summary>
-    public required string CategoryId { get; init; }
-    /// <summary>
-    /// The name of the category (name).
-    /// </summary>
-    public required string CategoryName { get; init; }
-    /// <summary>
-    /// The Drops campaign the entitlement is associated with.
-    /// </summary>
-    public required string CampaignId { get; init; }
-    /// <summary>
-    /// The id of the user who was granted the drop entitlement.
-    /// </summary>
-    public required string UserId { get; init; }
-    /// <summary>
-    /// The display name of the user who was granted the drop entitlement.
-    /// </summary>
-    public required string UserName { get; init; }
-    /// <summary>
-    /// The login (username) of the user who was granted the drop entitlement.
-    /// </summary>
-    public required string UserLogin { get; init; }
-    /// <summary>
-    /// The id of the drop entitlement.
-    /// </summary>
-    public required string EntitlementId { get; init; }
-    /// <summary>
-    /// The id of the benefit.
-    /// </summary>
-    public required string BenefitId { get; init; }
-    /// <summary>
-    /// The date and time when this drop entitlement was granted on Twitch.
-    /// </summary>
-    public required DateTimeOffset CreatedAt { get; init; }
 }
