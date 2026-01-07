bb119bf1b7b4c88818a5a7adcec71d45148b7089
Yoyoyopo5 <118655115+Yoyoyopo5@users.noreply.github.com>
2026-01-07 14:06:12 -0600

    Organize UserWhisper event type.

---
+++ TwitchySharp.EventSub/Models/Events/User/Whisper/WhisperMessage.cs
@@ -0,0 +1,12 @@
+﻿namespace TwitchySharp.EventSub.Models.Events.User.Whisper;
+
+/// <summary>
+/// Contains information about a specific message sent using Twitch Whispers.
+/// </summary>
+public record WhisperMessage
+{
+    /// <summary>
+    /// The text of the message.
+    /// </summary>
+    public required string Text { get; init; }
+}
