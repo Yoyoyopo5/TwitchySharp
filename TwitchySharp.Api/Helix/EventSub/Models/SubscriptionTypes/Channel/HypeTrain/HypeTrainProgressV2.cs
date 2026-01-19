ad6f34249f5fba0d1551dd39f9b9d799be727adc
Yoyoyopo5 <yoyoyopo5@DESKTOP-IT4LOU9>
2026-01-19 03:56:47 -0600

    Add V2 event sub subscription types for HypeTrain V2 and mark V1 types as obsolete.

--- TwitchySharp.Api/Helix/EventSub/Models/SubscriptionTypes/Channel/HypeTrain/HypeTrainProgressV2.cs
+++ TwitchySharp.Api/Helix/EventSub/Models/SubscriptionTypes/Channel/HypeTrain/HypeTrainProgressV2.cs
@@ -1,25 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using TwitchySharp.Api.Authorization;
 using TwitchySharp.Shared.EventSub.Constants;
 
 namespace TwitchySharp.Api.Helix.EventSub.SubscriptionTypes;
+
 /// <summary>
-/// A Hype Train ends on the specified channel.
+/// A Hype Train makes progress on the specified channel.
 /// </summary>
 /// <remarks>
 /// Requires a user access token that includes <see cref="Scope.ChannelReadHypeTrain"/>.
 /// </remarks>
 /// <param name="BroadcasterUserId">
-/// The user id of the broadcaster (channel) that you want to get Hype Train end notifications for.
+/// The user id of the broadcaster (channel) that you want to get Hype Train progress notifications for.
 /// This user must have also created a user access token including <see cref="Scope.ChannelReadHypeTrain"/> for your application.
 /// </param>
-[Obsolete("Use the V2 subscription type")]
-public sealed record HypeTrainEnd(string BroadcasterUserId)
+public sealed record HypeTrainProgressV2(string BroadcasterUserId)
     : IEventSubSubscriptionType
 {
-    public string Type => EventSubSubscriptionTypeNames.HYPE_TRAIN_END;
-    public string Version => EventSubSubscriptionTypeVersions.V1;
+    public string Type => EventSubSubscriptionTypeNames.HYPE_TRAIN_PROGRESS;
+    public string Version => EventSubSubscriptionTypeVersions.V2;
 
     private readonly EventSubSubscriptionCondition _condition =
         new EventSubSubscriptionCondition()
