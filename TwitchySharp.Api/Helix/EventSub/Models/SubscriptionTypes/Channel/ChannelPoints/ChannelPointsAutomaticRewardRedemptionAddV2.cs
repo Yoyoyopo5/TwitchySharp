560e33ee4b3878993a236fb2ec18f8d61751779b
Yoyoyopo5 <yoyoyopo5@DESKTOP-IT4LOU9>
2026-01-19 03:30:52 -0600

    Add ChannelPointsAutomaticRewardRedemptionAddV2 subscription type and update documentation.

--- TwitchySharp.Api/Helix/EventSub/Models/SubscriptionTypes/Channel/ChannelPoints/ChannelPointsAutomaticRewardRedemptionAddV2.cs
+++ TwitchySharp.Api/Helix/EventSub/Models/SubscriptionTypes/Channel/ChannelPoints/ChannelPointsAutomaticRewardRedemptionAddV2.cs
@@ -1,19 +1,21 @@
 ﻿using System.Collections.Generic;
 using TwitchySharp.Shared.EventSub.Constants;
+using TwitchySharp.Api.Authorization;
 
-namespace TwitchySharp.Api.Helix.EventSub.SubscriptionTypes;
+namespace TwitchySharp.Api.Helix.EventSub.Models.SubscriptionTypes.Channel.ChannelPoints;
+
 /// <summary>
 /// A viewer has redeemed an automatic channel points reward on the specified channel.
 /// </summary>
 /// <remarks>
 /// Requires a user access token that includes <see cref="Scope.ChannelReadRedemptions"/> or <see cref="Scope.ChannelManageRedemptions"/>.
 /// </remarks>
-/// <param name="BroadcasterUserId">The broadcaster user ID for the channel you want to receive channel points reward add notifications for.</param>
-public sealed record ChannelPointsAutomaticRewardRedemptionAdd(string BroadcasterUserId)
+/// <param name="BroadcasterUserId">The broadcaster user ID for the channel you want to receive Channel Points Reward Add V2 notifications for.</param>
+public sealed record ChannelPointsAutomaticRewardRedemptionAddV2(string BroadcasterUserId)
     : IEventSubSubscriptionType
 {
     public string Type => EventSubSubscriptionTypeNames.CHANNEL_POINTS_AUTOMATIC_REWARD_REDEMPTION_ADD;
-    public string Version => EventSubSubscriptionTypeVersions.V1;
+    public string Version => EventSubSubscriptionTypeVersions.V2;
 
     private readonly EventSubSubscriptionCondition _condition =
         new EventSubSubscriptionCondition()
