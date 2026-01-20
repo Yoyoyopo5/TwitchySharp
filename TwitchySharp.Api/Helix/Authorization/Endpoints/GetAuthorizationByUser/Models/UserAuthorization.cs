353c5228b4fce4da9111d1cd37e6aecd45cf2ae1
Yoyoyopo5 <yoyoyopo5@DESKTOP-IT4LOU9>
2026-01-20 00:28:44 -0600

    Add the GetAuthorizationByUser api endpoint.

---
+++ TwitchySharp.Api/Helix/Authorization/Endpoints/GetAuthorizationByUser/Models/UserAuthorization.cs
@@ -0,0 +1,26 @@
+﻿using TwitchySharp.Api.Authorization;
+
+namespace TwitchySharp.Api.Helix.Authorization;
+
+/// <summary>
+/// Contains information about a specific user's authorization with a specific app.
+/// </summary>
+public record UserAuthorization
+{
+    /// <summary>
+    /// The user id of the authorized user.
+    /// </summary>
+    public required string UserId { get; init; }
+    /// <summary>
+    /// The login (username) of the authorized user.
+    /// </summary>
+    public required string UserLogin { get; init; }
+    /// <summary>
+    /// The display name of the authorized user.
+    /// </summary>
+    public required string UserName { get; init; }
+    /// <summary>
+    /// The scopes that the user has granted to the client (app).
+    /// </summary>
+    public required Scope[] Scopes { get; init; }
+}
