c4a9cb9bb64e159210b9b8f7c428ea2f8c84be48
Yoyoyopo5 <118655115+Yoyoyopo5@users.noreply.github.com>
2026-01-20 00:17:14 -0600

    Add GetClipsDownload api endpoint.

---
+++ TwitchySharp.Api/Helix/Clips/Endpoints/GetClipsDownload/GetClipsDownloadResponse.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwitchySharp.Api.Helix.Clips;
+
+/// <summary>
+/// Contains an array of download information for a set of Twitch clips.
+/// </summary>
+public record GetClipsDownloadResponse
+{
+    /// <summary>
+    /// An array of clips download information.
+    /// </summary>
+    public required TwitchClipDownload[] Data { get; init; }
+}
