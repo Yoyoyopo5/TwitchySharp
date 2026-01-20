36b073374e9754dca8b601c8d6774320c2fa2928
Yoyoyopo5 <118655115+Yoyoyopo5@users.noreply.github.com>
2026-01-19 23:57:22 -0600

    Remove GetHypeTrainEvents endpoint and add GetHypeTrainStatus request and response.

---
+++ TwitchySharp.Api/Helix/HypeTrain/Endpoints/GetHypeTrainStatus/Enums/HypeTrainType.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.Json.Serialization;
+using TwitchySharp.Helpers;
+
+namespace TwitchySharp.Api.Helix.HypeTrain;
+
+/// <summary>
+/// Contains static definitions for possible Hype Train types.
+/// </summary>
+/// <param name="Value">The string value of the Hype Train type.</param>
+[JsonConverter(typeof(ValueBackedEnumJsonConverter<HypeTrainType, string>))]
+public record HypeTrainType(string Value) : ValueBackedEnum<string>(Value)
+{
+    public static HypeTrainType Treasure { get; } = new("treasure");
+    public static HypeTrainType GoldenKappa { get; } = new("golden_kappa");
+    public static HypeTrainType Regular { get; } = new("regular");
+}
---
+++ TwitchySharp.Api/Helix/HypeTrain/Endpoints/GetHypeTrainStatus/Models/HypeTrainContribution.cs
@@ -0,0 +1,28 @@
+﻿namespace TwitchySharp.Api.Helix.HypeTrain;
+
+/// <summary>
+/// Contains information about a specific Hype Train contribution.
+/// </summary>
+public record HypeTrainContribution
+{
+    /// <summary>
+    /// The user id of the contributor.
+    /// </summary>
+    public required string UserId { get; init; }
+    /// <summary>
+    /// The login (username) of the contributor.
+    /// </summary>
+    public required string UserLogin { get; init; }
+    /// <summary>
+    /// The display name of the contributor.
+    /// </summary>
+    public required string UserName { get; init; }
+    /// <summary>
+    /// The type of contribution.
+    /// </summary>
+    public required HypeTrainContributionType Type { get; init; }
+    /// <summary>
+    /// The total number of points contributed.
+    /// </summary>
+    public required int Total { get; init; }
+}
