b60ce35313ad76a11e0939d669eff411bc6c24ba
Yoyoyopo5 <118655115+Yoyoyopo5@users.noreply.github.com>
2026-01-20 01:53:59 -0600

    Add beta Suspicious User moderation endpoints and missing scopes.

---
+++ TwitchySharp.Api/Helix/Moderation/Endpoints/AddSuspiciousStatusToChatUser/AddSuspiciousStatusToChatUserRequest.cs
@@ -0,0 +1,70 @@
+﻿using System.Net.Http;
+using TwitchySharp.Api.Authorization;
+using TwitchySharp.Helpers;
+
+namespace TwitchySharp.Api.Helix.Moderation;
+/// <summary>
+/// <b>BETA</b> Adds a suspicious user status to a chatter on the broadcaster’s channel.
+/// </summary>
+/// <remarks>
+/// Requires an app or user access token that includes <see cref="Scope.ModeratorManageSuspiciousUsers"/>.
+/// <br/>
+/// See <see href="https://dev.twitch.tv/docs/api/reference/#add-suspicious-status-to-chat-user">Add Suspicious Status to Chat User</see> for more information.
+/// </remarks>
+public record AddSuspiciousStatusToChatUserRequest
+    : TwitchHelixRequest<AddSuspiciousStatusToChatUserResponse>
+{
+    /// <param name="clientId">The client id of the application.</param>
+    /// <param name="accessToken">An app or user access token that includes <see cref="Scope.ModeratorManageSuspiciousUsers"/>.</param>
+    /// <param name="parameters">The request parameters.</param>
+    /// <param name="data">The request data.</param>
+    public AddSuspiciousStatusToChatUserRequest(
+        string clientId,
+        string accessToken,
+        AddSuspiciousStatusToChatUserRequestParameters parameters,
+        AddSuspiciousStatusToChatUserRequestData data
+        )
+        : base(
+            "/moderation/suspicious_users",
+            clientId,
+            accessToken,
+            new HttpQueryParameters()
+                .Add("broadcaster_id", parameters.BroadcasterId)
+                .Add("moderator_id", parameters.ModeratorId)
+            )
+    {
+        Method = HttpMethod.Post;
+        ContentObject = data;
+    }
+}
+
+/// <summary>
+/// Request parameters for <see cref="AddSuspiciousStatusToChatUserRequest"/>.
+/// </summary>
+public record AddSuspiciousStatusToChatUserRequestParameters
+{
+    /// <summary>
+    /// The user id of the broadcaster (channel) in whose chat the suspicious user status is being applied.
+    /// </summary>
+    public required string BroadcasterId { get; set; }
+    /// <summary>
+    /// The user id of the moderator (or the broadcaster) to update the suspicious user status on behalf of.
+    /// This should be the same user that created the user access token for the request.
+    /// </summary>
+    public required string ModeratorId { get; set; }
+}
+
+/// <summary>
+/// Contains data used with a <see cref="AddSuspiciousStatusToChatUserRequest"/>.
+/// </summary>
+public record AddSuspiciousStatusToChatUserRequestData
+{
+    /// <summary>
+    /// The id of the user to add suspicious user status to.
+    /// </summary>
+    public required string UserId { get; set; }
+    /// <summary>
+    /// The type of suspicious user status to add.
+    /// </summary>
+    public required SuspiciousUserStatus Status { get; set; }
+}
---
+++ TwitchySharp.Api/Helix/Moderation/Models/SuspiciousUser.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace TwitchySharp.Api.Helix.Moderation;
+
+/// <summary>
+/// Contains information about a specific suspicious chatter.
+/// </summary>
+public record SuspiciousUser
+{
+    /// <summary>
+    /// The id of the user.
+    /// </summary>
+    public required string UserId { get; init; }
+    /// <summary>
+    /// The user id of the broadcaster (channel) in whose chat the user has the suspicious status.
+    /// </summary>
+    public required string BroadcasterId { get; init; }
+    /// <summary>
+    /// The user id of the moderator who applied the last suspicious status.
+    /// </summary>
+    public required string ModeratorId { get; init; }
+    /// <summary>
+    /// The date and time when the status was last updated.
+    /// </summary>
+    public required DateTimeOffset UpdatedAt { get; init; }
+    /// <summary>
+    /// The type of suspicious user status.
+    /// </summary>
+    public required SuspiciousUserStatus Status { get; init; }
+    /// <summary>
+    /// An array of suspicious user types that this user represents.
+    /// </summary>
+    public required SuspiciousUserType[] Types { get; init; } 
+}
