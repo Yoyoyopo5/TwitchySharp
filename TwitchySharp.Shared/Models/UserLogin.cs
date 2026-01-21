e7224c27d2fbdfc0d91bd8eb7f5b80cc927e4b1c
Yoyoyopo5 <118655115+Yoyoyopo5@users.noreply.github.com>
2026-01-21 03:43:06 -0600

    Add UserLogin and UserName wrapper types to shared library.

---
+++ TwitchySharp.Shared/Models/UserLogin.cs
@@ -0,0 +1,26 @@
+﻿using System.Text.Json.Serialization;
+using TwitchySharp.Helpers;
+
+namespace TwitchySharp.Shared.Models;
+
+/// <summary>
+/// A Twitch user's login (username).
+/// </summary>
+/// <remarks>
+/// This is what users use to log in to Twitch with, and is always lower case.
+/// It is also what appears in the url of their Twitch channel page.
+/// Contrast this with <see cref="UserName"/>.
+/// </remarks>
+/// <param name="Value">
+/// The string value of the user login. 
+/// This will be made lower case.
+/// </param>
+[JsonConverter(typeof(WrapperJsonConverter<UserLogin, string>))]
+public readonly record struct UserLogin(string Value) : IWrapValue<string>
+{
+    public string Value { get; } = Value.ToLower();
+    public static implicit operator string(UserLogin login)
+        => login.Value;
+    public override string ToString()
+        => Value;
+}
