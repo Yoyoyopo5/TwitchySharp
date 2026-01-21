0da699c61a96d6f10aa02606da7ed8543063377e
Yoyoyopo5 <118655115+Yoyoyopo5@users.noreply.github.com>
2026-01-21 03:43:06 -0600

    Add UserLogin and UserName wrapper types to shared library.

---
+++ TwitchySharp.Shared/Models/UserName.cs
@@ -0,0 +1,22 @@
+﻿using System.Text.Json.Serialization;
+using TwitchySharp.Helpers;
+
+namespace TwitchySharp.Shared.Models;
+
+/// <summary>
+/// A Twitch user's display name.
+/// </summary>
+/// <remarks>
+/// This is the mixed case version of the <see cref="UserLogin"/> and can be customized by users.
+/// It can also be a localized name for users using Twitch in Arabic, Chinese, Korean, or Japanese.
+/// See <see href="https://help.twitch.tv/s/article/display-names-on-twitch">Display Names on Twitch</see> for more information.
+/// </remarks>
+/// <param name="Value">The string value of the display name.</param>
+[JsonConverter(typeof(WrapperJsonConverter<UserName, string>))]
+public readonly record struct UserName(string Value) : IWrapValue<string>
+{
+    public static implicit operator string(UserName name)
+        => name.Value;
+    public override string ToString()
+        => Value;
+}
