399c411cd84548ef4e097176f0bb03c9813c40f9
Yoyoyopo5 <118655115+Yoyoyopo5@users.noreply.github.com>
2026-01-21 03:32:27 -0600

    Add missing ID wrapper models.

---
+++ TwitchySharp.Shared/Models/ExtensionId.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.Json.Serialization;
+using TwitchySharp.Helpers;
+
+namespace TwitchySharp.Shared.Models;
+
+/// <summary>
+/// An id representing a specific Twitch extension.
+/// </summary>
+/// <param name="Value">The string value of the extension id.</param>
+[JsonConverter(typeof(WrapperJsonConverter<ExtensionId, string>))]
+public readonly record struct ExtensionId(string Value) : IWrapValue<string>
+{
+    public static implicit operator string(ExtensionId id)
+        => id.Value;
+    public override string ToString()
+        => Value;
+}
---
+++ TwitchySharp.Shared/Models/GameId.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using TwitchySharp.Helpers;
+
+namespace TwitchySharp.Shared.Models;
+
+/// <summary>
+/// An id representing a specific Twitch game or category.
+/// </summary>
+/// <param name="Value">The string value of the game id.</param>
+[JsonConverter(typeof(WrapperJsonConverter<GameId, string>))]
+public readonly record struct GameId(string Value) : IWrapValue<string>
+{
+    public static implicit operator string(GameId id)
+        => id.Value;
+    public override string ToString()
+        => Value;
+}
