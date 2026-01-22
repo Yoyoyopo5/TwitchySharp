d53fbe9af7ffc80bf5e22d550344ca8a3b5f4a69
Yoyoyopo5 <yoyoyopo5@DESKTOP-IT4LOU9>
2026-01-22 10:29:36 -0600

    Refactor HypeTrain api endpoint.

---
+++ TwitchySharp.Shared/Models/HypeTrainId.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.Json.Serialization;
+using TwitchySharp.Helpers;
+
+namespace TwitchySharp.Shared.Models;
+
+/// <summary>
+/// An id representing a specific Twitch chat Hype Train.
+/// </summary>
+/// <param name="Value">The string value of the id.</param>
+[JsonConverter(typeof(WrapperJsonConverter<HypeTrainId, string>))]
+public readonly record struct HypeTrainId(string Value) : IWrapValue<string>
+{
+    public static implicit operator string(HypeTrainId id)
+        => id.Value;
+    public override string ToString()
+        => Value;
+}
