1172fc1953045cd94665e0219757a8c3a7c94cb2
Yoyoyopo5 <yoyoyopo5@DESKTOP-IT4LOU9>
2026-01-22 09:21:21 -0600

    Refactor Goals api endpoints.

---
+++ TwitchySharp.Shared/Models/ChannelGoalId.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.Json.Serialization;
+using TwitchySharp.Helpers;
+
+namespace TwitchySharp.Shared.Models;
+
+/// <summary>
+/// An id representing a specific Twitch channel goal.
+/// </summary>
+/// <param name="Value">The string value of the id</param>
+[JsonConverter(typeof(WrapperJsonConverter<ChannelGoalId, string>))]
+public readonly record struct ChannelGoalId(string Value) : IWrapValue<string>
+{
+    public static implicit operator string(ChannelGoalId id)
+        => id.Value;
+    public override string ToString()
+        => Value;
+}
