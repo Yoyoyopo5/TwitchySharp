bef81019ca51be7b87fb9d14bcad158566d9a624
Yoyoyopo5 <118655115+Yoyoyopo5@users.noreply.github.com>
2026-01-23 08:14:11 -0600

    Refactor Raids api endpoints.

--- TwitchySharp.Api/Helix/Raids/Endpoints/StartRaid/StartRaidRequest.cs
+++ TwitchySharp.Api/Helix/Raids/Endpoints/StartRaid/StartRaidRequest.cs
@@ -1,6 +1,7 @@
 ﻿using System.Net.Http;
 using TwitchySharp.Api.Authorization;
 using TwitchySharp.Helpers;
+using TwitchySharp.Shared.Models;
 
 namespace TwitchySharp.Api.Helix.Raids;
 /// <summary>
@@ -15,25 +16,38 @@ public record StartRaidRequest : TwitchHelixRequest<StartRaidResponse>
 {
     /// <param name="clientId">The client id of the application.</param>
     /// <param name="accessToken">A user access token that includes <see cref="Scope.ChannelManageRaids"/>.</param>
-    /// <param name="fromBroadcasterId">
-    /// The user id of the broadcaster (channel) that is sending the raid.
-    /// This must be the same user that created the <paramref name="accessToken"/>.
-    /// </param>
-    /// <param name="toBroadcasterId">The user id of the broadcaster (channel) to send the raid to.</param>
+    /// <param name="parameters">The request parameters.</param>
     public StartRaidRequest(
-        string clientId,
-        string accessToken,
-        string fromBroadcasterId,
-        string toBroadcasterId
+        ClientId clientId,
+        UserAccessToken accessToken,
+        StartRaidRequestParameters parameters
         ) : base(
             "/raids",
             clientId,
             accessToken,
             new HttpQueryParameters()
-                .Add("from_broadcaster_id", fromBroadcasterId)
-                .Add("to_broadcaster_id", toBroadcasterId)
+                .Add("from_broadcaster_id", parameters.FromBroadcasterId)
+                .Add("to_broadcaster_id", parameters.ToBroadcasterId)
             )
     {
         Method = HttpMethod.Post;
     }
+}
+
+/// <summary>
+/// Request parameters for a <see cref="StartRaidRequest"/>.
+/// </summary>
+public record StartRaidRequestParameters
+{
+    /// <summary>
+    /// The user id of the broadcaster (channel) that is sending the raid.
+    /// </summary>
+    /// <remarks>
+    /// This must be the same user that created the access token in the request.
+    /// </remarks>
+    public required UserId FromBroadcasterId { get; set; }
+    /// <summary>
+    /// The user id of the broadcaster (channel) to send the raid to.
+    /// </summary>
+    public required UserId ToBroadcasterId { get; set; }
 }
