828fdf791aaa16a622dd1513a70d77729a8f006e
Yoyoyopo5 <118655115+Yoyoyopo5@users.noreply.github.com>
2026-01-26 09:24:39 -0600

    Refactor GetExtensionAnalyticsRequest to property-based pattern
    - Remove constructor and base call
    - Add Path, Method, DefaultIdentity, ValidScopes, QueryParameters properties
    - Move all parameters from GetExtensionAnalyticsRequestParameters to request class
    - Delete GetExtensionAnalyticsRequestParameters class
    - Add IPageableRequest interface to request class
    - Preserve WithinDateRange helper method
    - Use TwitchApiIdentity.Default as user context comes from access token
    
     Opus 4.5

--- TwitchySharp.Api/Helix/Analytics/Endpoints/GetExtensionAnalytics/GetExtensionAnalyticsRequest.cs
+++ TwitchySharp.Api/Helix/Analytics/Endpoints/GetExtensionAnalytics/GetExtensionAnalyticsRequest.cs
@@ -1,4 +1,5 @@
-﻿using System;
+using System;
+using System.Collections.Generic;
 using System.Net.Http;
 using TwitchySharp.Api.Authorization;
 using TwitchySharp.Helpers;
@@ -6,7 +7,7 @@ using TwitchySharp.Shared.Models;
 
 namespace TwitchySharp.Api.Helix.Analytics;
 /// <summary>
-/// Gets an analytics report for one or more extensions. 
+/// Gets an analytics report for one or more extensions.
 /// </summary>
 /// <remarks>
 /// The response contains the URLs used to download the reports (CSV files).
@@ -16,58 +17,43 @@ namespace TwitchySharp.Api.Helix.Analytics;
 /// See <see href="https://dev.twitch.tv/docs/api/reference/#get-extension-analytics">Get Extension Analytics</see> for more information.
 /// </remarks>
 public record GetExtensionAnalyticsRequest
-    : TwitchHelixRequest<GetExtensionAnalyticsResponse>
+    : TwitchHelixRequest<GetExtensionAnalyticsResponse>, IPageableRequest
 {
-    /// <param name="clientId">The client id of the application.</param>
-    /// <param name="accessToken">A user access token that includes <see cref="Scope.AnalyticsReadExtensions"/>.</param>
-    /// <param name="parameters">The request parameters.</param>
-    public GetExtensionAnalyticsRequest(
-        ClientId clientId,
-        UserAccessToken accessToken,
-        GetExtensionAnalyticsRequestParameters? parameters = null
-        )
-        : base(
-            "/analytics/extensions",
-            clientId,
-            accessToken,
-            new HttpQueryParameters()
-                .Add("extension_id", parameters?.ExtensionId)
-                .Add("type", parameters?.Type?.Value)
-                .Add("started_at", parameters?.StartedAt?.ToUniversalTwitchQueryString())
-                .Add("ended_at", parameters?.EndedAt?.ToUniversalTwitchQueryString())
-                .Add("first", parameters?.First?.ToString())
-                .Add("after", parameters?.After?.Value)
-            )
-    {
-        Method = HttpMethod.Get;
-    }
-}
+    protected override string Path => "/analytics/extensions";
+    public override HttpMethod Method => HttpMethod.Get;
+    protected override TwitchApiIdentity DefaultIdentity => TwitchApiIdentity.Default;
+    public override IEnumerable<Scope> ValidScopes => [ Scope.AnalyticsReadExtensions ];
+    protected override HttpQueryParameters QueryParameters
+        => new HttpQueryParameters()
+            .Add("extension_id", ExtensionId)
+            .Add("type", Type?.Value)
+            .Add("started_at", StartedAt?.ToUniversalTwitchQueryString())
+            .Add("ended_at", EndedAt?.ToUniversalTwitchQueryString())
+            .Add("first", First?.ToString())
+            .Add("after", After?.Value);
 
-/// <summary>
-/// Request parameters for a <see cref="GetExtensionAnalyticsRequest"/>.
-/// </summary>
-public record GetExtensionAnalyticsRequestParameters
-    : IPageableRequest
-{
     /// <summary>
-    /// The extension's client id. 
+    /// The extension's client id.
     /// </summary>
     /// <remarks>
-    /// If specified, the response contains a report for the specified extension. 
+    /// If specified, the response contains a report for the specified extension.
     /// If left <see langword="null"/>, the response includes a report for each extension that the authenticated user owns.
     /// </remarks>
     public ExtensionId? ExtensionId { get; set; }
+
     /// <summary>
     /// The type of analytics report to get.
     /// </summary>
     public ExtensionAnalyticsReportType? Type { get; set; }
+
     /// <summary>
-    /// The reporting window's start date. 
+    /// The reporting window's start date.
     /// </summary>
     /// <remarks>
     /// Use <see cref="WithinDateRange(DateTimeOffset, DateTimeOffset)"/> to set.
     /// </remarks>
     public DateTimeOffset? StartedAt { get; private set; }
+
     /// <summary>
     /// The reporting window's end date.
     /// </summary>
@@ -75,6 +61,7 @@ public record GetExtensionAnalyticsRequestParameters
     /// Use <see cref="WithinDateRange(DateTimeOffset, DateTimeOffset)"/> to set.
     /// </remarks>
     public DateTimeOffset? EndedAt { get; private set; }
+
     /// <summary>
     /// <inheritdoc cref="PaginationAmount"/>
     /// </summary>
@@ -82,6 +69,7 @@ public record GetExtensionAnalyticsRequestParameters
     /// While you may specify a maximum value of 100, the response will contain at most 20 URLs per page.
     /// </remarks>
     public PaginationAmount? First { get; set; }
+
     /// <summary>
     /// <inheritdoc cref="PaginationCursor"/>
     /// </summary>
@@ -91,24 +79,22 @@ public record GetExtensionAnalyticsRequestParameters
     public PaginationCursor? After { get; set; }
 
     /// <summary>
-    /// Sets the reporting window for the request (the <see cref="StartedAt"/> and <see cref="EndedAt"/> parameters). 
+    /// Sets the reporting window for the request (the <see cref="StartedAt"/> and <see cref="EndedAt"/> parameters).
     /// </summary>
     /// <param name="startedAt">
     /// The reporting window's start date.
-    /// The start date must be on or after January 31, 2018. 
+    /// The start date must be on or after January 31, 2018.
     /// If you specify an earlier date, the API ignores it and uses January 31, 2018.
     /// </param>
     /// <param name="endedAt">
     /// The reporting window's end date.
     /// The report is inclusive of the end date.
-    /// Because it can take up to two days for the data to be available, you must specify an end date that's earlier than today minus one to two days. 
+    /// Because it can take up to two days for the data to be available, you must specify an end date that's earlier than today minus one to two days.
     /// If not, the API ignores your end date and uses an end date that is today minus one to two days.
     /// </param>
-    /// <returns>This object.</returns>
-    public GetExtensionAnalyticsRequestParameters WithinDateRange(DateTimeOffset startedAt, DateTimeOffset endedAt)
+    /// <returns>This request with the date range set.</returns>
+    public GetExtensionAnalyticsRequest WithinDateRange(DateTimeOffset startedAt, DateTimeOffset endedAt)
     {
-        // I'm choosing not to throw runtime exceptions for invalid arguments here.
-        // Behavior seems well defined in the spec, so we'll let the users handle it.
         StartedAt = startedAt;
         EndedAt = endedAt;
         return this;
