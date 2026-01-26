e2b71bcfb30f090233b3afa6ed38cfe46a91b055
Yoyoyopo5 <118655115+Yoyoyopo5@users.noreply.github.com>
2026-01-26 09:48:48 -0600

    Refactor GetExtensionBitsProductsRequest to property-based pattern.
     Opus 4.5

--- TwitchySharp.Api/Helix/Extensions/Endpoints/GetExtensionBitsProduct/GetExtensionBitsProductsRequest.cs
+++ TwitchySharp.Api/Helix/Extensions/Endpoints/GetExtensionBitsProduct/GetExtensionBitsProductsRequest.cs
@@ -1,6 +1,7 @@
-﻿using System.Net.Http;
+using System.Collections.Generic;
+using System.Net.Http;
+using TwitchySharp.Api.Authorization;
 using TwitchySharp.Helpers;
-using TwitchySharp.Shared.Models;
 
 namespace TwitchySharp.Api.Helix.Extensions;
 /// <summary>
@@ -9,7 +10,7 @@ namespace TwitchySharp.Api.Helix.Extensions;
 /// <remarks>
 /// The client id identifies the extension (this must be the same application that created the access token).
 /// <br/>
-/// Requires an app access token. 
+/// Requires an app access token.
 /// The client id that created the app access token must be the extension's client id.
 /// <br/>
 /// See <see href="https://dev.twitch.tv/docs/api/reference/#get-extension-bits-products">Get Extension Bits Products</see> for more information.
@@ -17,32 +18,16 @@ namespace TwitchySharp.Api.Helix.Extensions;
 public record GetExtensionBitsProductsRequest
     : TwitchHelixRequest<GetExtensionBitsProductsResponse>
 {
-    /// <param name="clientId">The client id of the extension. This also identifies the extension to get products from.</param>
-    /// <param name="accessToken">An app access token.</param>
-    /// <param name="parameters">The request parameters.</param>
-    public GetExtensionBitsProductsRequest(
-        ClientId clientId,
-        AppAccessToken accessToken,
-        GetExtensionBitsProductsRequestParameters? parameters = null
-        ) : base(
-            "/bits/extensions",
-            clientId,
-            accessToken,
-            new HttpQueryParameters()
-                .Add("should_include_all", parameters?.ShouldIncludeAll?.ToString())
-            )
-    {
-        Method = HttpMethod.Get;
-    }
-}
+    protected override string Path => "/bits/extensions";
+    public override HttpMethod Method => HttpMethod.Get;
+    protected override TwitchApiIdentity DefaultIdentity => TwitchApiIdentity.Default;
+    public override IEnumerable<Scope> ValidScopes => [];
+    protected override HttpQueryParameters QueryParameters
+        => new HttpQueryParameters()
+            .Add("should_include_all", ShouldIncludeAll?.ToString());
 
-/// <summary>
-/// Request parameters for a <see cref="GetExtensionBitsProductsRequest"/>.
-/// </summary>
-public record GetExtensionBitsProductsRequestParameters
-{
     /// <summary>
-    /// Determines whether to include disabled or expired Bits products in the response. 
+    /// Determines whether to include disabled or expired Bits products in the response.
     /// </summary>
     /// <remarks>
     /// The default is <see langword="false"/>.
