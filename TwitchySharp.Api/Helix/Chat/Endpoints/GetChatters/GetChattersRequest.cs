1f70673e4221633fccad228d54d68ca5e991991a
Yoyoyopo5 <118655115+Yoyoyopo5@users.noreply.github.com>
2026-01-26 09:34:06 -0600

    Refactor GetChattersRequest to property-based pattern
    - Remove constructor and base call
    - Add Path, Method, DefaultIdentity, ValidScopes, QueryParameters properties
    - Move all parameters from GetChattersRequestParameters to request class
    - Delete GetChattersRequestParameters class
    - Add IPageableRequest interface to request class
    - Use UserIdentity(ModeratorId) for user context (access token must match moderator)
    
     Opus 4.5

--- TwitchySharp.Api/Helix/Chat/Endpoints/GetChatters/GetChattersRequest.cs
+++ TwitchySharp.Api/Helix/Chat/Endpoints/GetChatters/GetChattersRequest.cs
@@ -1,4 +1,5 @@
-﻿using System.Net.Http;
+using System.Collections.Generic;
+using System.Net.Http;
 using TwitchySharp.Api.Authorization;
 using TwitchySharp.Api.Helix.Moderation;
 using TwitchySharp.Api.Helix.Channels;
@@ -7,14 +8,14 @@ using TwitchySharp.Shared.Models;
 
 namespace TwitchySharp.Api.Helix.Chat;
 /// <summary>
-/// Gets the list of users that are connected to the broadcaster’s chat session.
+/// Gets the list of users that are connected to the broadcaster's chat session.
 /// </summary>
 /// <remarks>
-/// To determine whether a user is a moderator or VIP, use the <see cref="GetModeratorsRequest"/> and <see cref="GetVipsRequest"/> endpoints. 
+/// To determine whether a user is a moderator or VIP, use the <see cref="GetModeratorsRequest"/> and <see cref="GetVipsRequest"/> endpoints.
 /// You can check the roles of up to 100 users.
 /// <br/>
 /// <b>NOTE:</b> There is a delay between when users join and leave a chat and when the list is updated accordingly.
-/// <b>DEV NOTE:</b> The list is usually not very accurate (in real-time) for this reason. 
+/// <b>DEV NOTE:</b> The list is usually not very accurate (in real-time) for this reason.
 /// Often a user will not be in this list when they are active in chat.
 /// <br/>
 /// Requires a user access token that includes <see cref="Scope.ModeratorReadChatters"/>.
@@ -22,55 +23,42 @@ namespace TwitchySharp.Api.Helix.Chat;
 /// See <see href="https://dev.twitch.tv/docs/api/reference/#get-chatters">Get Chatters</see> for more information.
 /// </remarks>
 public record GetChattersRequest
-    : TwitchHelixRequest<GetChattersResponse>
+    : TwitchHelixRequest<GetChattersResponse>, IPageableRequest
 {
-    /// <param name="clientId">The client id of the application.</param>
-    /// <param name="accessToken">A user access token with <see cref="Scope.ModeratorReadChatters"/></param>
-    /// <param name="parameters">The request parameters.</param>
-    public GetChattersRequest(
-        ClientId clientId,
-        UserAccessToken accessToken,
-        GetChattersRequestParameters parameters
-        )
-        : base(
-            "/chat/chatters",
-            clientId,
-            accessToken,
-            new HttpQueryParameters()
-                .Add("broadcaster_id", parameters.BroadcasterId)
-                .Add("moderator_id", parameters.ModeratorId)
-                .Add("first", parameters.First?.ToString())
-                .Add("after", parameters.After?.Value)
-            )
-    {
-        Method = HttpMethod.Get;
-    }
-}
+    protected override string Path => "/chat/chatters";
+    public override HttpMethod Method => HttpMethod.Get;
+    protected override TwitchApiIdentity DefaultIdentity => new UserIdentity(ModeratorId);
+    public override IEnumerable<Scope> ValidScopes => [ Scope.ModeratorReadChatters ];
+    protected override HttpQueryParameters QueryParameters
+        => new HttpQueryParameters()
+            .Add("broadcaster_id", BroadcasterId)
+            .Add("moderator_id", ModeratorId)
+            .Add("first", First?.ToString())
+            .Add("after", After?.Value);
 
-/// <summary>
-/// Request parameters for a <see cref="GetChattersRequest"/>.
-/// </summary>
-public record GetChattersRequestParameters
-    : IPageableRequest
-{
     /// <summary>
     /// The user id of the broadcaster whose chatters you want to get.
     /// </summary>
     public required UserId BroadcasterId { get; set; }
+
     /// <summary>
     /// The user id of the broadcaster OR one of the broadcaster's moderators.
     /// </summary>
     /// <remarks>
     /// This must be the same user that created the access token used in the request.
+    /// Requires <see cref="Scope.ModeratorReadChatters"/>.
     /// </remarks>
     public required UserId ModeratorId { get; set; }
+
     /// <summary>
     /// <inheritdoc cref="PaginationAmount"/>
     /// </summary>
     /// <remarks>
-    /// The minimum page size is 1 item per page and the maximum is 1,000. 
+    /// The minimum page size is 1 item per page and the maximum is 1,000.
     /// The default is 100.
     /// </remarks>
     public PaginationAmount? First { get; set; }
+
+    /// <inheritdoc/>
     public PaginationCursor? After { get; set; }
 }
