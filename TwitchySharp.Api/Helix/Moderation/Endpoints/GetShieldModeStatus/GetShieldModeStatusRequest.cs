d2fa86407c5cf532996d44845dc766afdd656337
Yoyoyopo5 <yoyoyopo5@DESKTOP-IT4LOU9>
2026-01-26 10:03:51 -0600

    Refactor GetShieldModeStatusRequest to property-based pattern.

--- TwitchySharp.Api/Helix/Moderation/Endpoints/GetShieldModeStatus/GetShieldModeStatusRequest.cs
+++ TwitchySharp.Api/Helix/Moderation/Endpoints/GetShieldModeStatus/GetShieldModeStatusRequest.cs
@@ -1,11 +1,12 @@
-﻿using System.Net.Http;
+﻿using System.Collections.Generic;
+using System.Net.Http;
 using TwitchySharp.Api.Authorization;
 using TwitchySharp.Helpers;
 using TwitchySharp.Shared.Models;
 
 namespace TwitchySharp.Api.Helix.Moderation;
 /// <summary>
-/// Gets the broadcaster’s Shield Mode activation status.
+/// Gets the broadcaster's Shield Mode activation status.
 /// </summary>
 /// <remarks>
 /// Requires a user access token that includes <see cref="Scope.ModeratorReadShieldMode"/> or <see cref="Scope.ModeratorManageShieldMode"/>.
@@ -15,35 +16,20 @@ namespace TwitchySharp.Api.Helix.Moderation;
 public record GetShieldModeStatusRequest
     : TwitchHelixRequest<GetShieldModeStatusResponse>
 {
-    /// <param name="clientId">The client id of the application.</param>
-    /// <param name="accessToken">A user access token that includes <see cref="Scope.ModeratorReadShieldMode"/> or <see cref="Scope.ModeratorManageShieldMode"/>.</param>
-    /// <param name="parameters">The request parameters.</param>
-    public GetShieldModeStatusRequest(
-        ClientId clientId,
-        UserAccessToken accessToken,
-        GetShieldModeStatusRequestParameters parameters
-        ) : base(
-            "/moderation/shield_mode",
-            clientId,
-            accessToken,
-            new HttpQueryParameters()
-                .Add("broadcaster_id", parameters.BroadcasterId)
-                .Add("moderator_id", parameters.ModeratorId)
-            )
-    {
-        Method = HttpMethod.Get;
-    }
-}
+    protected override string Path => "/moderation/shield_mode";
+    public override HttpMethod Method => HttpMethod.Get;
+    protected override TwitchApiIdentity DefaultIdentity => new UserIdentity(ModeratorId);
+    public override IEnumerable<Scope> ValidScopes => [Scope.ModeratorReadShieldMode, Scope.ModeratorManageShieldMode];
+    protected override HttpQueryParameters QueryParameters
+        => new HttpQueryParameters()
+            .Add("broadcaster_id", BroadcasterId)
+            .Add("moderator_id", ModeratorId);
 
-/// <summary>
-/// Request parameters for a <see cref="GetShieldModeStatusRequest"/>.
-/// </summary>
-public record GetShieldModeStatusRequestParameters
-{
     /// <summary>
     /// The user id of the broadcaster (channel) to get Shield Mode status for.
     /// </summary>
     public required UserId BroadcasterId { get; set; }
+
     /// <summary>
     /// The user id of the broadcaster or a moderator of the broadcaster's channel.
     /// </summary>
