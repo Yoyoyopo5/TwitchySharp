0dcd69d136a1ed886fc780f8dd7bd14e86464191
Yoyoyopo5 <118655115+Yoyoyopo5@users.noreply.github.com>
2026-01-26 10:03:06 -0600

    Refactor GetModeratedChannelsRequest to property-based pattern.

--- TwitchySharp.Api/Helix/Moderation/Endpoints/GetModeratedChannels/GetModeratedChannelsRequest.cs
+++ TwitchySharp.Api/Helix/Moderation/Endpoints/GetModeratedChannels/GetModeratedChannelsRequest.cs
@@ -1,4 +1,5 @@
-﻿using System.Net.Http;
+﻿using System.Collections.Generic;
+using System.Net.Http;
 using TwitchySharp.Api.Authorization;
 using TwitchySharp.Helpers;
 using TwitchySharp.Shared.Models;
@@ -13,45 +14,32 @@ namespace TwitchySharp.Api.Helix.Moderation;
 /// <br/>
 /// See <see href="https://dev.twitch.tv/docs/api/reference/#get-moderated-channels">Get Moderated Channels</see> for more information.
 /// </remarks>
-public record GetModeratedChannelsRequest : TwitchHelixRequest<GetModeratedChannelsResponse>
+public record GetModeratedChannelsRequest
+    : TwitchHelixRequest<GetModeratedChannelsResponse>, IPageableRequest
 {
-    /// <param name="clientId">The client id of the application.</param>
-    /// <param name="accessToken">A user access token that includes <see cref="Scope.UserReadModeratedChannels"/>.</param>
-    /// <param name="parameters">The request parameters.</param>
-    public GetModeratedChannelsRequest(
-        ClientId clientId,
-        UserAccessToken accessToken,
-        GetModeratedChannelsRequestParameters parameters
-        ) : base(
-            "/moderation/channels",
-            clientId,
-            accessToken,
-            new HttpQueryParameters()
-                .Add("user_id", parameters.UserId)
-                .Add("after", parameters.After?.Value)
-                .Add("first", parameters.First?.ToString())
-            )
-    {
-        Method = HttpMethod.Get;
-    }
-}
+    protected override string Path => "/moderation/channels";
+    public override HttpMethod Method => HttpMethod.Get;
+    protected override TwitchApiIdentity DefaultIdentity => new UserIdentity(UserId);
+    public override IEnumerable<Scope> ValidScopes => [Scope.UserReadModeratedChannels];
+    protected override HttpQueryParameters QueryParameters
+        => new HttpQueryParameters()
+            .Add("user_id", UserId)
+            .Add("after", After?.Value)
+            .Add("first", First?.ToString());
 
-/// <summary>
-/// Request parameters for a <see cref="GetModeratedChannelsRequest"/>.
-/// </summary>
-public record GetModeratedChannelsRequestParameters
-    : IPageableRequest
-{
     /// <summary>
     /// The user id of the user to get moderated channels for.
     /// </summary>
     /// <remarks>
-    /// This must be the same user that created the <paramref name="accessToken"/>.
+    /// This must be the same user that created the access token.
     /// </remarks>
     public required UserId UserId { get; set; }
+
+    /// <inheritdoc/>
     public PaginationCursor? After { get; set; }
+
     /// <remarks>
-    /// Minimum page size is 1 item per page and the maximum is 100. 
+    /// Minimum page size is 1 item per page and the maximum is 100.
     /// The default is 20.
     /// </remarks>
     public PaginationAmount? First { get; set; }
