fb127f1680860ff7121239455d733025b4a375c4
Yoyoyopo5 <118655115+Yoyoyopo5@users.noreply.github.com>
2026-01-26 09:56:03 -0600

    Refactor SendGuestStarInviteRequest to property-based pattern.
     Opus 4.5

--- TwitchySharp.Api/Helix/GuestStar/Endpoints/SendGuestStarInvite/SendGuestStarInviteRequest.cs
+++ TwitchySharp.Api/Helix/GuestStar/Endpoints/SendGuestStarInvite/SendGuestStarInviteRequest.cs
@@ -1,4 +1,5 @@
-﻿using System.Net.Http;
+using System.Collections.Generic;
+using System.Net.Http;
 using TwitchySharp.Api.Authorization;
 using TwitchySharp.Helpers;
 using TwitchySharp.Shared.Models;
@@ -8,7 +9,6 @@ namespace TwitchySharp.Api.Helix.GuestStar;
 /// <b>BETA</b> Sends an invite to a specified guest on behalf of the broadcaster for a Guest Star session in progress.
 /// </summary>
 /// <remarks>
-/// <br/>
 /// Requires a user access token that includes <see cref="Scope.ChannelManageGuestStar"/> or <see cref="Scope.ModeratorManageGuestStar"/>.
 /// <br/>
 /// See <see href="https://dev.twitch.tv/docs/api/reference/#send-guest-star-invite">Send Guest Star Invite</see> for more information.
@@ -16,33 +16,17 @@ namespace TwitchySharp.Api.Helix.GuestStar;
 public record SendGuestStarInviteRequest
     : TwitchHelixRequest<SendGuestStarInviteResponse>
 {
-    /// <param name="clientId">The client id of the application.</param>
-    /// <param name="accessToken">A user access token that includes <see cref="Scope.ChannelManageGuestStar"/> or <see cref="Scope.ModeratorManageGuestStar"/>.</param>
-    /// <param name="parameters">The request parameters.</param>
-    public SendGuestStarInviteRequest(
-        ClientId clientId,
-        UserAccessToken accessToken,
-        SendGuestStarInviteRequestParameters parameters
-        ) : base(
-            "/guest_star/invites",
-            clientId,
-            accessToken,
-            new HttpQueryParameters()
-                .Add("broadcaster_id", parameters.BroadcasterId)
-                .Add("moderator_id", parameters.ModeratorId)
-                .Add("session_id", parameters.SessionId)
-                .Add("guest_id", parameters.GuestId)
-            )
-    {
-        Method = HttpMethod.Post;
-    }
-}
+    protected override string Path => "/guest_star/invites";
+    public override HttpMethod Method => HttpMethod.Post;
+    protected override TwitchApiIdentity DefaultIdentity => new UserIdentity(ModeratorId);
+    public override IEnumerable<Scope> ValidScopes => [ Scope.ChannelManageGuestStar, Scope.ModeratorManageGuestStar ];
+    protected override HttpQueryParameters QueryParameters
+        => new HttpQueryParameters()
+            .Add("broadcaster_id", BroadcasterId)
+            .Add("moderator_id", ModeratorId)
+            .Add("session_id", SessionId)
+            .Add("guest_id", GuestId);
 
-/// <summary>
-/// Request parameters for a <see cref="SendGuestStarInviteRequest"/>.
-/// </summary>
-public record SendGuestStarInviteRequestParameters
-{
     /// <summary>
     /// The user id of the broadcaster hosting the Guest Star session.
     /// </summary>
