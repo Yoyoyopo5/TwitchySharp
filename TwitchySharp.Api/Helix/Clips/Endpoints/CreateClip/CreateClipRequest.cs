ae6cb6fb8bcd8907305cb4372c9593bab338bb45
Yoyoyopo5 <118655115+Yoyoyopo5@users.noreply.github.com>
2026-01-26 09:39:54 -0600

    Refactor CreateClipRequest to property-based pattern
    - Remove constructor and base call
    - Add Path, Method, DefaultIdentity, ValidScopes, QueryParameters properties
    - Move all parameters from CreateClipRequestParameters to request class
    - Delete CreateClipRequestParameters class
    - Use TwitchApiIdentity.Default (user context from access token)
    
     Opus 4.5

--- TwitchySharp.Api/Helix/Clips/Endpoints/CreateClip/CreateClipRequest.cs
+++ TwitchySharp.Api/Helix/Clips/Endpoints/CreateClip/CreateClipRequest.cs
@@ -1,4 +1,5 @@
-﻿using System;
+using System;
+using System.Collections.Generic;
 using System.Net.Http;
 using TwitchySharp.Api.Authorization;
 using TwitchySharp.Helpers;
@@ -6,25 +7,25 @@ using TwitchySharp.Shared.Models;
 
 namespace TwitchySharp.Api.Helix.Clips;
 /// <summary>
-/// Creates a clip from the broadcaster’s stream.
+/// Creates a clip from the broadcaster's stream.
 /// </summary>
 /// <remarks>
 /// <para>
-/// This API captures up to 90 seconds of the broadcaster’s stream.
-/// The 90 seconds spans the point in the stream from when you called the API. 
-/// For example, if you call the API at the 4:00 minute mark, the API captures from approximately the 3:35 mark to approximately the 4:05 minute mark. 
-/// Twitch tries its best to capture 90 seconds of the stream, but the actual length may be less. 
+/// This API captures up to 90 seconds of the broadcaster's stream.
+/// The 90 seconds spans the point in the stream from when you called the API.
+/// For example, if you call the API at the 4:00 minute mark, the API captures from approximately the 3:35 mark to approximately the 4:05 minute mark.
+/// Twitch tries its best to capture 90 seconds of the stream, but the actual length may be less.
 /// This may occur if you begin capturing the clip near the beginning or end of the stream.
 /// </para>
 /// <para>
-/// By default, Twitch publishes up to the last 30 seconds of the 90 seconds window and provides a default title for the clip. 
-/// To specify the title and the portion of the 90 seconds window that’s used for the clip, use the URL in the response’s <see cref="CreateClipResponse.EditUrl"/> property. 
-/// You can specify a clip that’s from 5 seconds to 60 seconds in length. The URL is valid for up to 24 hours or until the clip is published, whichever comes first.
+/// By default, Twitch publishes up to the last 30 seconds of the 90 seconds window and provides a default title for the clip.
+/// To specify the title and the portion of the 90 seconds window that's used for the clip, use the URL in the response's <see cref="CreateClipResponse.EditUrl"/> property.
+/// You can specify a clip that's from 5 seconds to 60 seconds in length. The URL is valid for up to 24 hours or until the clip is published, whichever comes first.
 /// </para>
 /// <para>
-/// Creating a clip is an asynchronous process that can take a short amount of time to complete. 
-/// To determine whether the clip was successfully created, call Get Clips using the <see cref="CreateClipResponse.Id"/> that this request returned. 
-/// If Get Clips returns the clip, the clip was successfully created. If after 15 seconds Get Clips hasn’t returned the clip, assume it failed.
+/// Creating a clip is an asynchronous process that can take a short amount of time to complete.
+/// To determine whether the clip was successfully created, call Get Clips using the <see cref="CreateClipResponse.Id"/> that this request returned.
+/// If Get Clips returns the clip, the clip was successfully created. If after 15 seconds Get Clips hasn't returned the clip, assume it failed.
 /// </para>
 /// <br/>
 /// Requires a user access token that includes <see cref="Scope.ClipsEdit"/>.
@@ -34,41 +35,26 @@ namespace TwitchySharp.Api.Helix.Clips;
 public record CreateClipRequest
     : TwitchHelixRequest<CreateClipResponse>
 {
-    /// <param name="clientId">The client id of the application.</param>
-    /// <param name="accessToken">A user access token that includes <see cref="Scope.ClipsEdit"/>.</param>
-    /// <param name="parameters">The request parameters.</param>
-    public CreateClipRequest(
-        ClientId clientId,
-        UserAccessToken accessToken,
-        CreateClipRequestParameters parameters
-        )
-        : base(
-            "/clips",
-            clientId,
-            accessToken,
-            new HttpQueryParameters()
-                .Add("broadcaster_id", parameters.BroadcasterId)
-                .Add("title", parameters.Title)
-                .Add("duration", parameters.Duration?.TotalSeconds.ToString())
-            )
-    {
-        Method = HttpMethod.Post;
-    }
-}
+    protected override string Path => "/clips";
+    public override HttpMethod Method => HttpMethod.Post;
+    protected override TwitchApiIdentity DefaultIdentity => TwitchApiIdentity.Default;
+    public override IEnumerable<Scope> ValidScopes => [ Scope.ClipsEdit ];
+    protected override HttpQueryParameters QueryParameters
+        => new HttpQueryParameters()
+            .Add("broadcaster_id", BroadcasterId)
+            .Add("title", Title)
+            .Add("duration", Duration?.TotalSeconds.ToString());
 
-/// <summary>
-/// Parameters for a <see cref="CreateClipRequest"/>.
-/// </summary>
-public record CreateClipRequestParameters
-{
     /// <summary>
     /// The user id of the broadcaster (channel) to create a clip for.
     /// </summary>
     public required UserId BroadcasterId { get; set; }
+
     /// <summary>
     /// The title of the clip to create.
     /// </summary>
     public string? Title { get; set; }
+
     /// <summary>
     /// The length of the clip to create.
     /// </summary>
