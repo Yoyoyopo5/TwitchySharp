88797469088990629ea890bfdd48d4f2de8e5ac4
Yoyoyopo5 <118655115+Yoyoyopo5@users.noreply.github.com>
2026-01-26 09:44:30 -0600

    Refactor DeleteEventSubSubscriptionRequest to property-based pattern.
     Opus 4.5

--- TwitchySharp.Api/Helix/EventSub/Endpoints/DeleteEventSubSubscription/DeleteEventSubSubscriptionRequest.cs
+++ TwitchySharp.Api/Helix/EventSub/Endpoints/DeleteEventSubSubscription/DeleteEventSubSubscriptionRequest.cs
@@ -1,4 +1,5 @@
-﻿using System.Net.Http;
+using System.Collections.Generic;
+using System.Net.Http;
 using TwitchySharp.Api.Authorization;
 using TwitchySharp.Helpers;
 using TwitchySharp.Shared.Models;
@@ -11,41 +12,21 @@ namespace TwitchySharp.Api.Helix.EventSub;
 /// <para>
 /// Requires an app access token if you use the <see cref="WebhookSubscriptionTransport"/> or <see cref="ConduitSubscriptionTransport"/> as the subscription's transport.
 /// <br/>
-/// Requires a user access token if you use the <see cref="WebsocketSubscriptionTransport"/>, as the subscription's transport. No particular <see cref="Scope"/> is required.
+/// Requires a user access token if you use the <see cref="WebsocketSubscriptionTransport"/> as the subscription's transport. No particular <see cref="Scope"/> is required.
 /// </para>
 /// See <see href="https://dev.twitch.tv/docs/api/reference/#delete-eventsub-subscription">Delete EventSub Subscription</see> for more information.
 /// </remarks>
 public record DeleteEventSubSubscriptionRequest
     : TwitchHelixRequest<DeleteEventSubSubscriptionResponse>
 {
-    /// <param name="clientId">The client id of the application.</param>
-    /// <param name="accessToken">
-    /// If using <see cref="WebhookSubscriptionTransport"/> or <see cref="ConduitSubscriptionTransport"/>, an app access token. 
-    /// If using <see cref="WebsocketSubscriptionTransport"/>, a user access token.
-    /// </param>
-    /// <param name="parameters">The request parameters.</param>
-    public DeleteEventSubSubscriptionRequest(
-        ClientId clientId, 
-        AccessToken accessToken,
-        DeleteEventSubSubscriptionRequestParameters parameters
-        )
-        : base(
-            "/eventsub/subscriptions",
-            clientId,
-            accessToken,
-            new HttpQueryParameters()
-                .Add("id", parameters.SubscriptionId)
-            )
-    {
-        Method = HttpMethod.Delete;
-    }
-}
+    protected override string Path => "/eventsub/subscriptions";
+    public override HttpMethod Method => HttpMethod.Delete;
+    protected override TwitchApiIdentity DefaultIdentity => TwitchApiIdentity.Default;
+    public override IEnumerable<Scope> ValidScopes => [];
+    protected override HttpQueryParameters QueryParameters
+        => new HttpQueryParameters()
+            .Add("id", SubscriptionId);
 
-/// <summary>
-/// Request parameters for a <see cref="DeleteEventSubSubscriptionRequest"/>.
-/// </summary>
-public record DeleteEventSubSubscriptionRequestParameters
-{
     /// <summary>
     /// The id of the subscription to delete.
     /// </summary>
