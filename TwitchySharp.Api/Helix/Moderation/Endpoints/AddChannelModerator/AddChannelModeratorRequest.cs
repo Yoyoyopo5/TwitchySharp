9ca3ea083b17879e7561c2f5bbb2752024e215cc
Yoyoyopo5 <118655115+Yoyoyopo5@users.noreply.github.com>
2026-01-26 09:59:49 -0600

    Refactor AddChannelModeratorRequest to property-based pattern.

--- TwitchySharp.Api/Helix/Moderation/Endpoints/AddChannelModerator/AddChannelModeratorRequest.cs
+++ TwitchySharp.Api/Helix/Moderation/Endpoints/AddChannelModerator/AddChannelModeratorRequest.cs
@@ -1,11 +1,12 @@
-﻿using System.Net.Http;
+﻿using System.Collections.Generic;
+using System.Net.Http;
 using TwitchySharp.Api.Authorization;
 using TwitchySharp.Helpers;
 using TwitchySharp.Shared.Models;
 
 namespace TwitchySharp.Api.Helix.Moderation;
 /// <summary>
-/// Adds a moderator to the broadcaster’s chat room.
+/// Adds a moderator to the broadcaster's chat room.
 /// </summary>
 /// <remarks>
 /// <b>Rate Limits:</b> A broadcaster may add a maximum of 10 moderators within a 10-second window.
@@ -17,31 +18,15 @@ namespace TwitchySharp.Api.Helix.Moderation;
 public record AddChannelModeratorRequest
     : TwitchHelixRequest<AddChannelModeratorResponse>
 {
-    /// <param name="clientId">The client id of the application.</param>
-    /// <param name="accessToken">A user access token that includes <see cref="Scope.ChannelManageModerators"/>.</param>
-    /// <param name="parameters">The request parameters.</param>
-    public AddChannelModeratorRequest(
-        ClientId clientId,
-        UserAccessToken accessToken,
-        AddChannelModeratorRequestParameters parameters
-        ) : base(
-            "/moderation/moderators",
-            clientId,
-            accessToken,
-            new HttpQueryParameters()
-                .Add("broadcaster_id", parameters.BroadcasterId)
-                .Add("user_id", parameters.UserId)
-            )
-    {
-        Method = HttpMethod.Post;
-    }
-}
+    protected override string Path => "/moderation/moderators";
+    public override HttpMethod Method => HttpMethod.Post;
+    protected override TwitchApiIdentity DefaultIdentity => new UserIdentity(BroadcasterId);
+    public override IEnumerable<Scope> ValidScopes => [Scope.ChannelManageModerators];
+    protected override HttpQueryParameters QueryParameters
+        => new HttpQueryParameters()
+            .Add("broadcaster_id", BroadcasterId)
+            .Add("user_id", UserId);
 
-/// <summary>
-/// Request parameters for a <see cref="AddChannelModeratorRequest"/>.
-/// </summary>
-public record AddChannelModeratorRequestParameters
-{
     /// <summary>
     /// The user id of the broadcaster (channel) to add a moderator for.
     /// </summary>
