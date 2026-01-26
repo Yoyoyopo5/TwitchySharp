2b7748421721902195c4fe2cb1dab6401808c1df
Yoyoyopo5 <yoyoyopo5@DESKTOP-IT4LOU9>
2026-01-26 10:06:50 -0600

    Refactor ResolveUnbanRequestsRequest to property-based pattern.

--- TwitchySharp.Api/Helix/Moderation/Endpoints/ResolveUnbanRequests/ResolveUnbanRequestsRequest.cs
+++ TwitchySharp.Api/Helix/Moderation/Endpoints/ResolveUnbanRequests/ResolveUnbanRequestsRequest.cs
@@ -1,4 +1,5 @@
-﻿using System.Net.Http;
+﻿using System.Collections.Generic;
+using System.Net.Http;
 using TwitchySharp.Api.Authorization;
 using TwitchySharp.Helpers;
 using TwitchySharp.Shared.Models;
@@ -15,38 +16,23 @@ namespace TwitchySharp.Api.Helix.Moderation;
 public record ResolveUnbanRequestsRequest
     : TwitchHelixRequest<ResolveUnbanRequestsResponse>
 {
-    /// <param name="clientId">The client id of the application.</param>
-    /// <param name="accessToken">A user access token that includes <see cref="Scope.ModeratorManageUnbanRequests"/>.</param>
-    /// <param name="parameters">The request parameters.</param>
-    public ResolveUnbanRequestsRequest(
-        ClientId clientId,
-        UserAccessToken accessToken,
-        ResolveUnbanRequestsRequestParameters parameters
-        ) : base(
-            "/moderation/unban_requests",
-            clientId,
-            accessToken,
-            new HttpQueryParameters()
-                .Add("broadcaster_id", parameters.BroadcasterId)
-                .Add("moderator_id", parameters.ModeratorId)
-                .Add("unban_request_id", parameters.UnbanRequestId)
-                .Add("status", parameters.Status.Value)
-                .Add("resolution_text", parameters.ResolutionText)
-            )
-    {
-        Method = HttpMethod.Patch;
-    }
-}
+    protected override string Path => "/moderation/unban_requests";
+    public override HttpMethod Method => HttpMethod.Patch;
+    protected override TwitchApiIdentity DefaultIdentity => new UserIdentity(ModeratorId);
+    public override IEnumerable<Scope> ValidScopes => [Scope.ModeratorManageUnbanRequests];
+    protected override HttpQueryParameters QueryParameters
+        => new HttpQueryParameters()
+            .Add("broadcaster_id", BroadcasterId)
+            .Add("moderator_id", ModeratorId)
+            .Add("unban_request_id", UnbanRequestId)
+            .Add("status", Status.Value)
+            .Add("resolution_text", ResolutionText);
 
-/// <summary>
-/// Request parameters for a <see cref="ResolveUnbanRequestsRequest"/>.
-/// </summary>
-public record ResolveUnbanRequestsRequestParameters
-{
     /// <summary>
     /// The user id of the broadcaster (channel) to resolve the unban request for.
     /// </summary>
     public required UserId BroadcasterId { get; set; }
+
     /// <summary>
     /// The user id of the broadcaster or a moderator of the broadcaster's channel.
     /// </summary>
@@ -54,14 +40,17 @@ public record ResolveUnbanRequestsRequestParameters
     /// This must be the same user that created the access token in the request.
     /// </remarks>
     public required UserId ModeratorId { get; set; }
+
     /// <summary>
     /// The id of the unban request to resolve.
     /// </summary>
     public required UnbanRequestId UnbanRequestId { get; set; }
+
     /// <summary>
     /// The resolution status to set the unban request to.
     /// </summary>
     public required UnbanRequestResolutionStatus Status { get; set; }
+
     /// <summary>
     /// Caller-defined text that is added to the unban request.
     /// </summary>
