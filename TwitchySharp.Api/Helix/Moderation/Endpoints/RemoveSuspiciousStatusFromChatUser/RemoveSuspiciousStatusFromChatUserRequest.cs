4d7c3960ea099c0339706f82247908252b2949e4
Yoyoyopo5 <118655115+Yoyoyopo5@users.noreply.github.com>
2026-01-26 10:06:24 -0600

    Refactor RemoveSuspiciousStatusFromChatUserRequest to property-based pattern.

--- TwitchySharp.Api/Helix/Moderation/Endpoints/RemoveSuspiciousStatusFromChatUser/RemoveSuspiciousStatusFromChatUserRequest.cs
+++ TwitchySharp.Api/Helix/Moderation/Endpoints/RemoveSuspiciousStatusFromChatUser/RemoveSuspiciousStatusFromChatUserRequest.cs
@@ -1,11 +1,12 @@
-﻿using System.Net.Http;
+﻿using System.Collections.Generic;
+using System.Net.Http;
 using TwitchySharp.Api.Authorization;
 using TwitchySharp.Helpers;
 using TwitchySharp.Shared.Models;
 
 namespace TwitchySharp.Api.Helix.Moderation;
 /// <summary>
-/// <b>BETA</b> Remove a suspicious user status from a chatter on broadcaster’s channel.
+/// <b>BETA</b> Remove a suspicious user status from a chatter on broadcaster's channel.
 /// </summary>
 /// <remarks>
 /// Requires an app or user access token that includes <see cref="Scope.ModeratorManageSuspiciousUsers"/>.
@@ -15,41 +16,26 @@ namespace TwitchySharp.Api.Helix.Moderation;
 public record RemoveSuspiciousStatusFromChatUserRequest
     : TwitchHelixRequest<RemoveSuspiciousStatusFromChatUserResponse>
 {
-    /// <param name="clientId">The client id of the application.</param>
-    /// <param name="accessToken">An app or user access token that includes <see cref="Scope.ModeratorManageSuspiciousUsers"/>.</param>
-    /// <param name="parameters">The request parameters.</param>
-    public RemoveSuspiciousStatusFromChatUserRequest(
-        ClientId clientId,
-        AccessToken accessToken,
-        RemoveSuspiciousStatusFromChatUserRequestParameters parameters
-        )
-        : base(
-            "/moderation/suspicious_users",
-            clientId,
-            accessToken,
-            new HttpQueryParameters()
-                .Add("broadcaster_id", parameters.BroadcasterId)
-                .Add("moderator_id", parameters.ModeratorId)
-                .Add("user_id", parameters.UserId)
-            )
-    {
-        Method = HttpMethod.Delete;
-    }
-}
+    protected override string Path => "/moderation/suspicious_users";
+    public override HttpMethod Method => HttpMethod.Delete;
+    protected override TwitchApiIdentity DefaultIdentity => new UserIdentity(ModeratorId);
+    public override IEnumerable<Scope> ValidScopes => [Scope.ModeratorManageSuspiciousUsers];
+    protected override HttpQueryParameters QueryParameters
+        => new HttpQueryParameters()
+            .Add("broadcaster_id", BroadcasterId)
+            .Add("moderator_id", ModeratorId)
+            .Add("user_id", UserId);
 
-/// <summary>
-/// Request parameters for <see cref="RemoveSuspiciousStatusFromChatUserRequest"/>.
-/// </summary>
-public record RemoveSuspiciousStatusFromChatUserRequestParameters
-{
     /// <summary>
     /// The user id of the broadcaster (channel) in whose chat to remove the suspicious user status.
     /// </summary>
     public required UserId BroadcasterId { get; set; }
+
     /// <summary>
     /// The user id of the moderator (or the broadcaster) to remove the suspicious user status on behalf of.
     /// </summary>
     public required UserId ModeratorId { get; set; }
+
     /// <summary>
     /// The id of the user to remove the suspicious user status on.
     /// </summary>
