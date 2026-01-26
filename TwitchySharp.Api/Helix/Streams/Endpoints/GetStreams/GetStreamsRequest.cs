677f6ae71e77ea7e39e23e5936267cf7d3d5712f
Yoyoyopo5 <118655115+Yoyoyopo5@users.noreply.github.com>
2026-01-26 10:21:22 -0600

    Refactor GetStreamsRequest to property-based pattern.
     Opus 4.5

--- TwitchySharp.Api/Helix/Streams/Endpoints/GetStreams/GetStreamsRequest.cs
+++ TwitchySharp.Api/Helix/Streams/Endpoints/GetStreams/GetStreamsRequest.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
+using TwitchySharp.Api.Authorization;
 using TwitchySharp.Helpers;
 using TwitchySharp.Shared.Models;
 
@@ -9,63 +10,47 @@ namespace TwitchySharp.Api.Helix.Streams;
 /// Gets a list of all streams.
 /// </summary>
 /// <remarks>
-/// The returned list will be in descending order by the number of viewers watching the stream. 
-/// Because viewers come and go during a stream, it’s possible to find duplicate or missing streams in the list as you page through the results.
+/// The returned list will be in descending order by the number of viewers watching the stream.
+/// Because viewers come and go during a stream, it's possible to find duplicate or missing streams in the list as you page through the results.
 /// <br/>
 /// Requires an app or user access token.
 /// <br/>
 /// See <see href="https://dev.twitch.tv/docs/api/reference/#get-streams">Get Streams</see> for more information.
 /// </remarks>
-public record GetStreamsRequest : TwitchHelixRequest<GetStreamsResponse>
+public record GetStreamsRequest
+    : TwitchHelixRequest<GetStreamsResponse>, IPageableRequest
 {
-    /// <param name="clientId">The client id of the application.</param>
-    /// <param name="accessToken">An app or user access token.</param>
-    /// <param name="parameters">The request parameters. Leave this <see langword="null"/> to return all streams.</param>
-    public GetStreamsRequest(
-        ClientId clientId,
-        AccessToken accessToken,
-        GetStreamsRequestParameters? parameters = null
-        ) : base(
-            "/streams",
-            clientId,
-            accessToken,
-            new HttpQueryParameters()
-                .Add("user_id", parameters?.UserIds?.Select(x => x.Value))
-                .Add("user_login", parameters?.UserLogins?.Select(x => x.Value))
-                .Add("game_id", parameters?.GameIds?.Select(x => x.Value))
-                .Add("type", parameters?.Type?.Value)
-                .Add("language", parameters?.Languages?.Select(x => x.Value))
-                .Add("first", parameters?.First?.ToString())
-                .Add("before", parameters?.Before?.Value)
-                .Add("after", parameters?.After?.Value)
-            )
-    {
-        Method = HttpMethod.Get;
-    }
-}
+    protected override string Path => "/streams";
+    public override HttpMethod Method => HttpMethod.Get;
+    protected override TwitchApiIdentity DefaultIdentity => TwitchApiIdentity.Default;
+    public override IEnumerable<Scope> ValidScopes => [];
+    protected override HttpQueryParameters QueryParameters
+        => new HttpQueryParameters()
+            .Add("user_id", UserIds?.Select(x => x.Value))
+            .Add("user_login", UserLogins?.Select(x => x.Value))
+            .Add("game_id", GameIds?.Select(x => x.Value))
+            .Add("type", Type?.Value)
+            .Add("language", Languages?.Select(x => x.Value))
+            .Add("first", First?.ToString())
+            .Add("before", Before?.Value)
+            .Add("after", After?.Value);
 
-/// <summary>
-/// Request parameters for a <see cref="GetStreamsRequest"/>.
-/// </summary>
-public record GetStreamsRequestParameters
-    : IPageableRequest
-{
     /// <summary>
-    /// A list of user ids used to filter the list of streams. 
-    /// Returns only the streams of those users that are broadcasting. 
+    /// A list of user ids used to filter the list of streams.
+    /// Returns only the streams of those users that are broadcasting.
     /// You may specify a maximum of 100 ids.
     /// </summary>
     public IEnumerable<UserId>? UserIds { get; set; }
     /// <summary>
-    /// A list of user logins (usernames) used to filter the list of streams. 
-    /// Returns only the streams of those users that are broadcasting. 
+    /// A list of user logins (usernames) used to filter the list of streams.
+    /// Returns only the streams of those users that are broadcasting.
     /// You may specify a maximum of 100 login names.
     /// </summary>
     public IEnumerable<UserLogin>? UserLogins { get; set; }
     /// <summary>
-    /// A game (category) id used to filter the list of streams. 
-    /// Returns only the streams that are broadcasting the game (category). 
-    /// You may specify a maximum of 100 ids. 
+    /// A game (category) id used to filter the list of streams.
+    /// Returns only the streams that are broadcasting the game (category).
+    /// You may specify a maximum of 100 ids.
     /// </summary>
     public IEnumerable<GameId>? GameIds { get; set; }
     /// <summary>
@@ -74,8 +59,8 @@ public record GetStreamsRequestParameters
     /// </summary>
     public StreamType? Type { get; set; }
     /// <summary>
-    /// A language code used to filter the list of streams. 
-    /// Returns only streams that broadcast in the specified language. 
+    /// A language code used to filter the list of streams.
+    /// Returns only streams that broadcast in the specified language.
     /// Specify the language using an ISO 639-1 two-letter language code or other if the broadcast uses a language not in the list of <see href="https://help.twitch.tv/s/article/languages-on-twitch#streamlang">supported stream languages</see>.
     /// You may specify a maximum of 100 language codes.
     /// </summary>
@@ -84,10 +69,11 @@ public record GetStreamsRequestParameters
     /// <inheritdoc cref="PaginationAmount"/>
     /// </summary>
     /// <remarks>
-    /// The minimum page size is 1 item per page and the maximum is 100 items per page. 
+    /// The minimum page size is 1 item per page and the maximum is 100 items per page.
     /// The default is 20.
     /// </remarks>
     public PaginationAmount? First { get; set; }
+    /// <inheritdoc/>
     public PaginationCursor? After { get; set; }
     /// <summary>
     /// The cursor of the result to get results before.
