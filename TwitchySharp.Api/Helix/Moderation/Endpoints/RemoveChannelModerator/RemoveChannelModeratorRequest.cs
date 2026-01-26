e81f426e3c835db6788978075e2877104d504986
Yoyoyopo5 <118655115+Yoyoyopo5@users.noreply.github.com>
2026-01-26 10:05:45 -0600

    Refactor RemoveChannelModeratorRequest to property-based pattern.

--- TwitchySharp.Api/Helix/Moderation/Endpoints/RemoveChannelModerator/RemoveChannelModeratorRequest.cs
+++ TwitchySharp.Api/Helix/Moderation/Endpoints/RemoveChannelModerator/RemoveChannelModeratorRequest.cs
@@ -1,11 +1,12 @@
-﻿using System.Net.Http;
+﻿using System.Collections.Generic;
+using System.Net.Http;
 using TwitchySharp.Api.Authorization;
 using TwitchySharp.Helpers;
 using TwitchySharp.Shared.Models;
 
 namespace TwitchySharp.Api.Helix.Moderation;
 /// <summary>
-/// Removes a moderator from the broadcaster’s chat room.
+/// Removes a moderator from the broadcaster's chat room.
 /// </summary>
 /// <remarks>
 /// <b>Rate Limits:</b> A broadcaster may remove a maximum of 10 moderators within a 10-second window.
@@ -17,31 +18,15 @@ namespace TwitchySharp.Api.Helix.Moderation;
 public record RemoveChannelModeratorRequest
     : TwitchHelixRequest<RemoveChannelModeratorResponse>
 {
-    /// <param name="clientId">The client id of the application.</param>
-    /// <param name="accessToken">A user access token that includes <see cref="Scope.ChannelManageModerators"/>.</param>
-    /// <param name="parameters">The request parameters.</param>
-    public RemoveChannelModeratorRequest(
-        ClientId clientId,
-        UserAccessToken accessToken,
-        RemoveChannelModeratorRequestParameters parameters
-        ) : base(
-            "/moderation/moderators",
-            clientId,
-            accessToken,
-            new HttpQueryParameters()
-                .Add("broadcaster_id", parameters.BroadcasterId)
-                .Add("user_id", parameters.UserId)
-            )
-    {
-        Method = HttpMethod.Delete;
-    }
-}
+    protected override string Path => "/moderation/moderators";
+    public override HttpMethod Method => HttpMethod.Delete;
+    protected override TwitchApiIdentity DefaultIdentity => new UserIdentity(BroadcasterId);
+    public override IEnumerable<Scope> ValidScopes => [Scope.ChannelManageModerators];
+    protected override HttpQueryParameters QueryParameters
+        => new HttpQueryParameters()
+            .Add("broadcaster_id", BroadcasterId)
+            .Add("user_id", UserId);
 
-/// <summary>
-/// Request parameters for a <see cref="RemoveChannelModeratorRequest"/>.
-/// </summary>
-public record RemoveChannelModeratorRequestParameters
-{
     /// <summary>
     /// The user id of the broadcaster (channel) to remove a moderator for.
     /// </summary>
@@ -49,6 +34,7 @@ public record RemoveChannelModeratorRequestParameters
     /// This must be the same user that created the access token in the request.
     /// </remarks>
     public required UserId BroadcasterId { get; set; }
+
     /// <summary>
     /// The user id of the moderator to remove from the broadcaster's channel.
     /// </summary>
