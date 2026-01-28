2128406f7fa001822b35d5b121f28917a1b9a84f
Yoyoyopo5 <yoyoyopo5@DESKTOP-IT4LOU9>
2026-01-27 19:05:43 -0600

    References issue #32 - adds DeleteEventSubSubscriptionRequest Subscription property and constructor and implements DefaultIdentity logic for UserIdentity based Subscriptions.

--- TwitchySharp.Api/Helix/EventSub/Endpoints/DeleteEventSubSubscription/DeleteEventSubSubscriptionRequest.cs
+++ TwitchySharp.Api/Helix/EventSub/Endpoints/DeleteEventSubSubscription/DeleteEventSubSubscriptionRequest.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Net.Http;
 using TwitchySharp.Api.Authorization;
 using TwitchySharp.Helpers;
@@ -14,18 +15,61 @@ namespace TwitchySharp.Api.Helix.EventSub;
 /// <br/>
 /// Requires a user access token if you use the <see cref="WebsocketSubscriptionTransport"/> as the subscription's transport. No particular <see cref="Scope"/> is required.
 /// </para>
+/// <para>
+/// When using the <see cref="DeleteEventSubSubscriptionRequest(EventSubSubscription)"/> constructor or setting the <see cref="Subscription"/>, 
+/// the identity for the request introspected from the <see cref="Subscription"/>. It does not need to be manually configured unless you need to
+/// manually configure the <see cref="ClientIdentity"/>.
+/// </para>
 /// See <see href="https://dev.twitch.tv/docs/api/reference/#delete-eventsub-subscription">Delete EventSub Subscription</see> for more information.
 /// </remarks>
-public record DeleteEventSubSubscriptionRequest
+public record DeleteEventSubSubscriptionRequest()
     : TwitchHelixRequest<DeleteEventSubSubscriptionResponse>
 {
     protected override string Path => "/eventsub/subscriptions";
     public override HttpMethod Method => HttpMethod.Delete;
-    protected override TwitchApiIdentity DefaultIdentity => TwitchApiIdentity.Default;
+    protected override TwitchApiIdentity DefaultIdentity => Subscription switch
+    {
+        not null => Subscription.RequiresUserAccessToken() switch
+        {
+            true => Subscription.GetAuthorizingUser(),
+            _ => null
+        },
+        _ => null
+    } ?? TwitchApiIdentity.Default;
     public override IEnumerable<Scope> ValidScopes => [];
     protected override HttpQueryParameters QueryParameters
         => new HttpQueryParameters()
             .Add("id", SubscriptionId);
+
+    /// <summary>
+    /// The subscription to delete.
+    /// </summary>
+    /// <remarks>
+    /// Auto-sets <see cref="SubscriptionId"/> to support <see langword="with"/> syntax.
+    /// For first time initialization use the constructor.
+    /// </remarks>
+    public EventSubSubscription? Subscription
+    {
+        get => field;
+        init
+        {
+            field = value;
+            if (value is not null)
+                SubscriptionId = value.Id;
+        }
+    }
+    /// <summary>
+    /// Automatically sets the required <see cref="SubscriptionId"/>.
+    /// </summary>
+    /// <remarks>
+    /// You should use this in most cases, as it will set the correct <see cref="TwitchApiIdentity"/> to use with the request.
+    /// If you only set <see cref="SubscriptionId"/>, the default identity is <see cref="TwitchApiIdentity.Default"/>.
+    /// </remarks>
+    /// <param name="subscription">The subscription to delete.</param>
+    [SetsRequiredMembers]
+    public DeleteEventSubSubscriptionRequest(EventSubSubscription subscription)
+        : this()
+        => (Subscription, SubscriptionId) = (subscription, subscription.Id);
 
     /// <summary>
     /// The id of the subscription to delete.
