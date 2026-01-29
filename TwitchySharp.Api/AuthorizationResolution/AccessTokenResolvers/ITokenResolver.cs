0676d35ad7475acf977f5bd87f6fed4b79376367
Yoyoyopo5 <118655115+Yoyoyopo5@users.noreply.github.com>
2026-01-29 10:14:19 -0600

    Reconfigure authorization resolution pipeline.

---
+++ TwitchySharp.Api/AuthorizationResolution/AccessTokenResolvers/ITokenResolver.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace TwitchySharp.Api;
+
+public interface ITokenResolver
+{
+    /// <summary>
+    /// Resolves an <see cref="AccessToken"/> based on the provided <see cref="ITwitchRequest"/>.
+    /// </summary>
+    /// <param name="request">The request to get an access token for.</param>
+    /// <returns>An <see cref="AccessToken"/> resolved from the <see cref="ITwitchRequest"/>, if any.</returns>
+    ValueTask<AccessToken?> GetToken(ITwitchRequest request, CancellationToken ct = default);
+}
---
+++ TwitchySharp.Api/AuthorizationResolution/AccessTokenResolvers/SingleAccessTokenResolver.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace TwitchySharp.Api.AuthorizationResolution.AccessTokenResolvers;
+
+/// <summary>
+/// Resolves a pre-configured default <see cref="AccessToken"/> for every <see cref="ITwitchRequest"/>.
+/// </summary>
+/// <param name="Token">The <see cref="AccessToken"/> to use for all requests.</param>
+public record SingleAccessTokenResolver(AccessToken Token) : ITokenResolver
+{
+    public ValueTask<AccessToken?> GetToken(ITwitchRequest request, CancellationToken ct = default)
+        => ValueTask.FromResult<AccessToken?>(Token);
+}
