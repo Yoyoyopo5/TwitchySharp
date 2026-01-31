eedd1360f7f506b315d916212f80a626c4bbbaec
Yoyoyopo5 <118655115+Yoyoyopo5@users.noreply.github.com>
2026-01-31 13:29:55 -0600

    Refactor TwitchAuthorizationRequest, add documentation, and create stub test class.

---
+++ TwitchySharp.Api.Tests.Unit/Authorization/Test_TwitchAuthorizationRequest.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwitchySharp.Api.Tests.Unit.Authorization;
+public class Test_TwitchAuthorizationRequest
+{
+    // Implement tests here.
+}
