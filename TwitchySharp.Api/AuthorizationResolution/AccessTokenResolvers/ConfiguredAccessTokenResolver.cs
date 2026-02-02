540eee2264a6728f0ba932c1cff14769ad1072b7
Yoyoyopo5 <yoyoyopo5@DESKTOP-IT4LOU9>
2026-02-01 21:02:52 -0600

    Add default ITokenResolverand define interfaces for its dependencies. References issue #53

--- TwitchySharp.Api/AuthorizationResolution/AccessTokenResolvers/ConfiguredAccessTokenResolver.cs
+++ TwitchySharp.Api/AuthorizationResolution/AccessTokenResolvers/ConfiguredAccessTokenResolver.cs
@@ -7,7 +7,7 @@ namespace TwitchySharp.Api;
 /// Returns the <see cref="IRequireAuthorization.OverrideAccessToken"/> if the <see cref="ITwitchRequest"/>
 /// implements <see cref="IRequireAuthorization"/> and it is not <see langword="null"/>.
 /// </summary>
-public class ConfiguredAccessTokenResolver() : ITokenResolver
+public record ConfiguredAccessTokenResolver() : ITokenResolver
 {
     public ValueTask<AccessToken?> GetToken(ITwitchRequest request, CancellationToken ct = default)
         => ValueTask.FromResult((request as IRequireAuthorization)?.OverrideAccessToken);
