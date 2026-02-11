75fe7f802884bb6dc77c4a138ec43cee8f3fed96
Yoyoyopo5 <yoyoyopo5@DESKTOP-IT4LOU9>
2026-02-11 07:19:00 -0600

    Remove Functional Helpers projects (moved to Yoyoyopo5.Helpers.Functional nuget pkg).

--- TwitchySharp.Helpers.Functional/Combinators/Contract.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace TwitchySharp.Helpers.Functional;
-
-public static partial class FunctionalExtensions
-{
-    /// <summary>Converts a <see cref="Step{TIn, TOut}"/> (where both type parameters are the same) into a <see cref="Step{T}"/>.</summary>
-    /// <param name="step">The heteromorphic step to narrow.</param>
-    internal static Step<T> Contract<T>(this Step<T, T> step)
-        => (input, ct) => step(input, ct);
-}
--- TwitchySharp.Helpers.Functional/Combinators/Expand.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace TwitchySharp.Helpers.Functional;
-
-public static partial class FunctionalExtensions
-{
-    /// <summary>Converts a <see cref="Step{T}"/> into a <see cref="Step{TIn, TOut}"/> where both type parameters are the same.</summary>
-    /// <param name="step">The same-type step to widen.</param>
-    internal static Step<T, T> Expand<T>(this Step<T> step)
-        => (input, ct) => step(input, ct);
-}
