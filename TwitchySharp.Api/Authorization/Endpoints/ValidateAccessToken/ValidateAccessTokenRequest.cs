e5d3d95d8ae5dfff62f307acc6a86b85ba389044
Yoyoyopo5 <118655115+Yoyoyopo5@users.noreply.github.com>
2026-03-28 12:57:44 -0500

    Support authorization resolution on User Info and Validate Access Token authentication endpoints by adding optional UserId property and removing requirement on AccessToken property.

--- TwitchySharp.Api/Authorization/Endpoints/ValidateAccessToken/ValidateAccessTokenRequest.cs
+++ TwitchySharp.Api/Authorization/Endpoints/ValidateAccessToken/ValidateAccessTokenRequest.cs
@@ -1,6 +1,5 @@
-using System.Collections.Generic;
-using System.Collections.Immutable;
 using System.Net.Http;
+using TwitchySharp.Shared.Models;
 
 namespace TwitchySharp.Api.Authorization;
 /// <summary>
@@ -18,16 +17,25 @@ public record ValidateAccessTokenRequest
     protected override string Path => "/validate";
 
     /// <summary>
+    /// The id of the user to validate an access token for.
+    /// </summary>
+    /// <remarks>
+    /// You may set this or configure the <see cref="AccessToken"/> property manually 
+    /// (if not using authorization resolution).
+    /// </remarks>
+    public UserId? UserId { get; init; }
+
+    /// <summary>
     /// The user access token to validate.
     /// </summary>
     /// <remarks>
     /// This token is sent as the Bearer authorization header.
     /// </remarks>
-    public required UserAccessToken AccessToken { get; init; }
+    public UserAccessToken? AccessToken { get; init; }
 
     public TwitchRequestAuthorizationContext AuthorizationContext => new()
     {
-        Identity = TwitchIdentity.None.Instance,
+        Identity = UserId.HasValue ? new TwitchIdentity.User(UserId.Value) : TwitchIdentity.None.Instance,
         AccessToken = AccessToken
     };
 }
