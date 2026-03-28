b6de77a41298c9d5fef6886b4cbf08f6550c225c
Yoyoyopo5 <yoyoyopo5@DESKTOP-IT4LOU9>
2026-03-28 15:05:38 -0500

    Add implicit operator converting ExtensionId to ClientId.

--- TwitchySharp.Shared/Models/ExtensionId.cs
+++ TwitchySharp.Shared/Models/ExtensionId.cs
@@ -6,10 +6,15 @@ namespace TwitchySharp.Shared.Models;
 /// <summary>
 /// An id representing a specific Twitch extension.
 /// </summary>
+/// <remarks>
+/// This also functions as a <see cref="ClientId"/> and can be used to authenticate requests.
+/// </remarks>
 /// <param name="Value">The string value of the extension id.</param>
 [JsonConverter(typeof(WrapperJsonConverter<ExtensionId, string>))]
 public readonly record struct ExtensionId(string Value) : IWrapValue<string>
 {
+    public static implicit operator ClientId(ExtensionId id)
+        => new(id.Value);
     public static implicit operator string(ExtensionId id)
         => id.Value;
     public override string ToString()
