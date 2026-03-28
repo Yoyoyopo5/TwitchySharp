bded25c28e37508b51459ba5eacb77a83bb755c4
Yoyoyopo5 <118655115+Yoyoyopo5@users.noreply.github.com>
2026-03-28 12:59:35 -0500

    Add ExtensionSecret value wrapper struct and clean up ExtensionJwtPayload record to use required initializers and wrapper classes.

---
+++ TwitchySharp.Api/Authorization/Models/ExtensionSecret.cs
@@ -0,0 +1,18 @@
+﻿using TwitchySharp.Helpers;
+
+namespace TwitchySharp.Api.Authorization;
+
+/// <summary>
+/// An extension shared secret.
+/// </summary>
+/// <remarks>
+/// See <see href="https://dev.twitch.tv/docs/extensions/building/#managing-extension-secrets">Managing Extension Secrets</see> for more information.
+/// </remarks>
+/// <param name="Value">The string value of the shared secret.</param>
+public readonly record struct ExtensionSecret(string Value) : IWrapValue<string>
+{
+    public static implicit operator string(ExtensionSecret secret)
+        => secret.Value;
+    public override string ToString()
+        => Value;
+}
