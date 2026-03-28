ef2aa158b4f0dd9b44a3370fe9543ea6352bf1ab
Yoyoyopo5 <118655115+Yoyoyopo5@users.noreply.github.com>
2026-03-28 15:12:48 -0500

    Add a console app project used to generate a test user access token.

---
+++ TwitchySharp.Api.Tests.E2E.AcquireUserAccessToken/Options.cs
@@ -0,0 +1,8 @@
+﻿namespace TwitchySharp.Api.Tests.E2E.AcquireUserAccessToken;
+
+public class Options
+{
+    public required string ClientId { get; set; }
+    public required string ClientSecret { get; set; }
+    public required string RedirectUrl { get; set; }
+}
