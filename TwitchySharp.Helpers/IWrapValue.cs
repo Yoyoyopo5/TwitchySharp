d3f5e5191355e8d71c108a1f0794d6c51e52e1b9
Yoyoyopo5 <118655115+Yoyoyopo5@users.noreply.github.com>
2026-03-31 22:28:24 -0500

    Rename WrapperJsonConverter.cs to IWrapValue.cs for clarity. #67

--- TwitchySharp.Helpers/IWrapValue.cs
+++ TwitchySharp.Helpers/IWrapValue.cs
@@ -1,12 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Text.Json;
 using System.Text.Json.Serialization;
-using System.Threading.Tasks;
 
 namespace TwitchySharp.Helpers;
+
 /// <summary>
 /// Wraps a single value.
 /// </summary>
