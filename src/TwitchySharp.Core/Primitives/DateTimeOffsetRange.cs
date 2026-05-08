d0c346a2f10dfd647df0cf280677872c48375c20
Yoyoyopo5 <118655115+Yoyoyopo5@users.noreply.github.com>
2026-05-08 00:58:43 -0500

    Organize Api project and stage for Helix/Authentication project split.

---
+++ src/TwitchySharp.Core/Primitives/DateTimeOffsetRange.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Text.Json.Serialization;
+using TwitchySharp.Serialization;
+
+namespace TwitchySharp;
+
+public readonly record struct DateTimeOffsetRange
+{
+    [JsonConverter(typeof(EmptyDateTimeOffsetConverter))]
+    public DateTimeOffset? StartedAt { get; init; }
+    [JsonConverter(typeof(EmptyDateTimeOffsetConverter))]
+    public DateTimeOffset? EndedAt { get; init; }
+}
