028791def0638b1e9e73f85f5636fa05fbe61e51
Yoyoyopo5 <118655115+Yoyoyopo5@users.noreply.github.com>
2026-05-09 07:07:31 -0500

    Refactor TwitchySharp.EventSub.Tests.E2E project to match new solution structure.

--- tests/E2E/TwitchySharp.EventSub.Tests.E2E/WebsocketFixture.cs
+++ tests/E2E/TwitchySharp.EventSub.Tests.E2E/WebsocketFixture.cs
@@ -1,27 +1,27 @@
 ﻿using System.Reflection;
 using Microsoft.Extensions.Configuration;
 using TwitchySharp.Api;
-using TwitchySharp.Api.Authorization;
-using TwitchySharp.Api.AuthorizationResolution;
 using TwitchySharp.EventSub.Models;
 using TwitchySharp.EventSub.Models.Notifications;
 using TwitchySharp.EventSub.Websocket.Clients.Websocket.Client;
 using TwitchySharp.EventSub.Websocket.Messages.Payloads;
+using TwitchySharp.Tests.E2E;
 
 namespace TwitchySharp.EventSub.Websocket.Tests.E2E;
 
 public sealed class WebsocketFixture : IAsyncLifetime
 {
+    private static readonly HttpClient _httpClient = new();
     public TestHandler Handler { get; }
     public WebsocketClientEventSubWebsocketClient Websocket { get; }
-    public ITwitchClient Api { get; }
-
+    public ITwitchClient Client { get; }
     public WebsocketConfig Config { get; }
 
-    public TwitchIdentity.Client Client { get; }
+    public TwitchIdentity.Client ClientIdentity { get; }
     public TwitchIdentity.User AuthorizedBroadcaster { get; }
     private AccessTokenDetails.User _broadcasterAccessTokenDetails;
-    private readonly ITwitchClient _authClient = new TwitchClientBuilder().Build();
+    private AccessTokenDetails.App? _appAccessTokenDetails;
+    private readonly ITwitchClient _authClient = new TwitchClientBuilder() { HttpClient = _httpClient }.Build();
 
     private static readonly IConfiguration _config
         = new ConfigurationBuilder()
@@ -35,30 +35,37 @@ public sealed class WebsocketFixture : IAsyncLifetime
 
         Config = _config.GetRequiredSection("WebsocketFixture").Get<WebsocketConfig>() ?? throw new InvalidOperationException($"Could not bind configuration to {nameof(WebsocketConfig)}");
 
-        Client = new(Config.Client.Id);
+        ClientIdentity = new(Config.Client.Id);
         AuthorizedBroadcaster = Config.UserAccessTokenDetails.Identity;
         _broadcasterAccessTokenDetails = Config.UserAccessTokenDetails;
 
-        Api = new TwitchClientBuilder()
+        Client = new TwitchClientBuilder() { HttpClient = _httpClient }
             .WithAuthorizationResolution(new TwitchAuthorizationResolutionOptions()
             {
-                FallbackClientIdResolver = (ctx, _) => ValueTask.FromResult(Client.ClientId),
+                FallbackClientIdResolver = (ctx, _) => ValueTask.FromResult(ClientIdentity.ClientId),
             }
-            .ConfigureIdentityTokenResolution(new AppAccessTokenResolutionOptions()
+            .ConfigureIdentity<TwitchIdentity.User, AccessTokenDetails.User>(new TokenResolutionOptions<AccessTokenDetails.User>()
             {
-                AuthenticationClient = _authClient,
-                ClientSecretResolver = (ctx, _) => ValueTask.FromResult<ClientSecret?>(Config.Client.Secret)
-            })
-            .ConfigureIdentityTokenResolution(new UserAccessTokenResolutionOptions()
-            {
-                GetCachedToken = (ctx, _) => ValueTask.FromResult<AccessTokenDetails.User?>(_broadcasterAccessTokenDetails),
-                OnNewToken = (token, _) =>
+                GetCachedToken = (_, ct) => ValueTask.FromResult<AccessTokenDetails.User?>(_broadcasterAccessTokenDetails),
+                RefreshToken = (expiredToken, ct) => _authClient.RefreshUserAccessToken(expiredToken, Config.Client.Secret, ct),
+                OnNewToken = (newToken, ct) =>
                 {
-                    _broadcasterAccessTokenDetails = token;
+                    _broadcasterAccessTokenDetails = newToken;
                     return ValueTask.CompletedTask;
-                },
-                AuthenticationClient = _authClient,
-                ClientSecretResolver = (ctx, _) => ValueTask.FromResult<ClientSecret?>(Config.Client.Secret)
+                }
+            })
+            .ConfigureIdentity<TwitchIdentity.Client, AccessTokenDetails.App>(new TokenResolutionOptions<AccessTokenDetails.App>()
+            {
+                AcquireNewToken = (ctx, ct) => _authClient.GetNewAppAccessToken(ctx.Identity.ClientId, Config.Client.Secret, ct),
+                GetCachedToken = (_, ct) => ValueTask.FromResult(_appAccessTokenDetails),
+                RefreshToken = async (expiredToken, ct) => await _authClient.GetNewAppAccessToken(expiredToken.Identity.ClientId, Config.Client.Secret, ct) is { } newToken
+                    ? new AccessTokenRefreshResult.Refreshed<AccessTokenDetails.App>(newToken)
+                    : new AccessTokenRefreshResult.Expired<AccessTokenDetails.App>(expiredToken),
+                OnNewToken = (token, ct) =>
+                {
+                    _appAccessTokenDetails = token;
+                    return ValueTask.CompletedTask;
+                }
             }))
             .Build();
     }
@@ -86,12 +93,6 @@ public record WebsocketConfig
     {
         public required ClientId Id { get; set; }
         public required ClientSecret Secret { get; set; }
-    }
-
-    public record AuthorizedBroadcasterConfig
-    {
-        public required UserId Id { get; set; }
-        public required AccessTokenDetails.User AccessToken { get; set; }
     }
 
     public required ClientConfig Client { get; set; }
