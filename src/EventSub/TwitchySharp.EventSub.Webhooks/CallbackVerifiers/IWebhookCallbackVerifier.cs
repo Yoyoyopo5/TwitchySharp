930728dd0f68ce83a422989720c409d33966d029
Yoyoyopo5 <118655115+Yoyoyopo5@users.noreply.github.com>
2026-05-10 12:34:46 -0500

    Functionally refactor WebhookCallbackVerifier to use delegate instead of interface.

--- src/EventSub/TwitchySharp.EventSub.Webhooks/CallbackVerifiers/IWebhookCallbackVerifier.cs
+++
@@ -1,8 +0,0 @@
-﻿using TwitchySharp.EventSub.Webhooks.Responses;
-
-namespace TwitchySharp.EventSub.Webhooks.CallbackVerifiers;
-
-public interface IWebhookCallbackVerifier
-{
-    ValueTask<CallbackVerificationResponseData> VerifyCallback(string challenge, CancellationToken ct = default);
-}
