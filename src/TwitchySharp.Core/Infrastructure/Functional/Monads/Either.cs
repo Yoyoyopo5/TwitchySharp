df193811810cdf631e534148e7c1f158fee5e5b5
Yoyoyopo5 <118655115+Yoyoyopo5@users.noreply.github.com>
2026-05-10 12:18:14 -0500

    Add required functional monads Either and Validation.

---
+++ src/TwitchySharp.Core/Infrastructure/Functional/Monads/Either.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace TwitchySharp.Infrastructure.Functional;
+
+public readonly record struct Either<TL, TR>
+{
+    private readonly TL? _left;
+    private readonly TR? _right;
+    public Either(TL left) => _left = left;
+    public Either(TR right) => _right = right;
+    public static implicit operator Either<TL, TR>(TL left) => new(left);
+    public static implicit operator Either<TL, TR>(TR right) => new(right);
+    public Either<TL, TNextR> Bind<TNextR>(Func<TR, Either<TL, TNextR>> func)
+        => _left is not null ? _left : func(_right!);
+    public Either<TL, TNextR> Map<TNextR>(Func<TR, TNextR> func)
+        => _left is not null ? _left : func(_right!);
+    public T Match<T>(Func<TL, T> leftFunc, Func<TR, T> rightFunc)
+        => _left is not null ? leftFunc(_left) : rightFunc(_right!);
+}
---
+++ src/TwitchySharp.Core/Infrastructure/Functional/Monads/Validation.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace TwitchySharp.Infrastructure.Functional;
+
+public record Error()
+{
+    public Error(string message) : this() => Message = message;
+    public string Message { get; init; } = string.Empty;
+    public Error? InnerError { get; init; }
+};
+
+public readonly record struct Validation<T>
+{
+    private readonly Error? _error;
+    private readonly T? _valid;
+    public Validation(Error error) => _error = error;
+    public Validation(T right) => _valid = right;
+    public static implicit operator Validation<T>(Error error) => new(error);
+    public static implicit operator Validation<T>(T right) => new(right);
+    public Validation<TNextR> Bind<TNextR>(Func<T, Validation<TNextR>> func)
+        => _error is not null ?_error : func(_valid!);
+    public Validation<TNextR> Map<TNextR>(Func<T, TNextR> func)
+        => _error is not null ?_error : func(_valid!);
+    public TOut Match<TOut>(Func<Error, TOut> onError, Func<T, TOut> onValid)
+        => _error is not null ? onError(_error) : onValid(_valid!);
+}
