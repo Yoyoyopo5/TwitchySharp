d54296720e78a23ea194506c0e49ffc4f5d74caf
Yoyoyopo5 <118655115+Yoyoyopo5@users.noreply.github.com>
2026-05-10 18:11:04 -0500

    Functional rewrite of EventSub.Webhooks library

--- src/EventSub/TwitchySharp.EventSub.Webhooks/SecretResolvers/FixedSecretTwitchWebhookSecretsResolver.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace TwitchySharp.EventSub.Webhooks.SecretResolvers;
-
-public class FixedSecretTwitchWebhookSecretsResolver(string secret) : ITwitchEventSubWebhookSecretsResolver
-{
-    public ValueTask<string> GetSecret(EventSubWebhookRequestHeader requestHeaders, string body, CancellationToken ct = default)
-        => ValueTask.FromResult(secret);
-    public ValueTask<string> GetSecret(EventSubWebhookRequestHeader requestHeaders, Stream body, CancellationToken ct = default)
-        => ValueTask.FromResult(secret);
-}
---
+++ src/EventSub/TwitchySharp.EventSub.Webhooks/SecretResolvers/ResolveWebhookSecret.cs
@@ -0,0 +1,19 @@
+﻿using TwitchySharp.EventSub.Models;
+using Yoyoyopo5.ValueWrapper;
+
+namespace TwitchySharp.EventSub.Webhooks;
+
+/// <summary>
+/// An EventSub webhook secret.
+/// </summary>
+/// <param name="Value">The <see langword="string"/> value of the secret.</param>
+[Wrapper<string>]
+public readonly partial record struct WebhookSecret(string Value);
+
+/// <summary>
+/// A function resolving a specific webhook secret for an EventSub subscription.
+/// </summary>
+/// <param name="subscription">The subscription to resolve a secret for.</param>
+/// <param name="ct">Cancellation token.</param>
+/// <returns>The secret associated with the <paramref name="subscription"/>, if any.</returns>
+public delegate ValueTask<WebhookSecret?> ResolveWebhookSecret(EventSubSubscription subscription, CancellationToken ct);
