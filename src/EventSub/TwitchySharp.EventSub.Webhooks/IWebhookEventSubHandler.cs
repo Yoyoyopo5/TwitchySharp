1a3f2de8432d5e353a7a8765afbc3e730bd5bacd
Yoyoyopo5 <118655115+Yoyoyopo5@users.noreply.github.com>
2026-05-10 13:38:09 -0500

    Rewrite webhook request pipeline functionally.

--- src/EventSub/TwitchySharp.EventSub.Webhooks/IWebhookEventSubHandler.cs
+++ src/EventSub/TwitchySharp.EventSub.Webhooks/IWebhookEventSubHandler.cs
@@ -9,7 +9,7 @@ namespace TwitchySharp.EventSub.Webhooks;
 /// If you have no clue what you're doing, create a new class that implements this interface, 
 /// define what you want to happen when each of the events happen using the included methods,
 /// and register it as a service of <see cref="IWebhookEventSubHandler"/> (if using DI),
-/// or pass an instance of it directly to an <see cref="IEventSubWebhookMessageProcessor"/> (use the <see cref="DefaultEventSubWebhookMessageProcessor"/> or create your own processor).
+/// or pass an instance of it directly to an <see cref="IEventSubWebhookMessageProcessor"/> (use the <see cref="WebhookRequestProcessor"/> or create your own processor).
 /// </remarks>
 public interface IWebhookEventSubHandler : IEventSubHandler
 {
