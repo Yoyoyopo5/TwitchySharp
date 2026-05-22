1c3136dd6eaab26de962ac2a9b2356ebac8139e8
Yoyoyopo5 <118655115+Yoyoyopo5@users.noreply.github.com>
2026-05-21 21:03:14 -0500

    Add notes.

--- src/EventSub/TwitchySharp.EventSub.Webhooks/Http/NotificationRequestContent.cs
+++ src/EventSub/TwitchySharp.EventSub.Webhooks/Http/NotificationRequestContent.cs
@@ -2,6 +2,11 @@
 
 namespace TwitchySharp.EventSub.Webhooks.Http;
 
+// This is slightly weird because the notification payload is actually the Notification itself,
+// but we wrap it into a WebhookRequestContent so we can handle every request polymorphically.
+// When we pass the notification to the handler, we unwrap the notification.
+// This means that this object is not a 1:1 representation of the incoming request content like
+// the other request types.
 internal record NotificationRequestContent : WebhookRequestContent
 {
     public required IEventSubNotification Notification { get; init; }
