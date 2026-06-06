2a14aa3a21d6cfb90041a51840526745bed5126c
Yoyoyopo5 <118655115+Yoyoyopo5@users.noreply.github.com>
2026-06-06 08:21:29 -0500

    Add EventSub Websocket client core logic to TwitchySharp.EventSub.Websocket project, removing it from TwitchySharp.EventSub.Websocket.Clients.WebsocketClient. WebsocketClientEventSubWebsocketClient.cs remains as a usage example. It will be removed in a following commit.

--- src/EventSub/TwitchySharp.EventSub.Websocket.Clients.WebsocketClient/TwitchEventSubWebsocketUrl.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace TwitchySharp.EventSub.Websocket.Clients.WebsocketDotClient;
-
-internal static class TwitchEventSubWebsocketUrl
-{
-    /// <summary>
-    /// The default address of the Twitch EventSub Websocket server.
-    /// </summary>
-    public const string TWITCH_WEBSOCKET_URL = "wss://eventsub.wss.twitch.tv/ws";
-}
--- src/EventSub/TwitchySharp.EventSub.Websocket.Clients.WebsocketClient/WebsocketClientEventSubWebsocketClient.cs
+++ src/EventSub/TwitchySharp.EventSub.Websocket.Clients.WebsocketClient/WebsocketClientEventSubWebsocketClient.cs
@@ -1,101 +1,60 @@
-﻿using System.Collections.Concurrent;
-using System.Net.WebSockets;
-using System.Reactive.Linq;
-using System.Text;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using Microsoft.IO;
-using TwitchySharp.EventSub.Websocket.Functional;
-using TwitchySharp.Infrastructure.Functional;
+﻿using System.Reactive.Linq;
 using Websocket.Client;
 
 namespace TwitchySharp.EventSub.Websocket.Clients.WebsocketDotClient;
 
-internal interface IEventSubWebsocketClient
+// Example
+internal static class WebsocketDotClientEventSubWebsocketClient
 {
-    static abstract IEventSubWebsocketClient Create();
-    Task<Validation<IRunningEventSubWebsocketClient>> Start(CancellationToken ct);
-}
-
-internal interface IRunningEventSubWebsocketClient
-{
-    Task<Validation> Stop(CancellationToken ct);
-}
-
-internal interface IStoppedEventSubWebsocketClient
-{
-
-}
-
-internal static class EventSubWebsocketClient
-{
-    public record WebsocketClientError(string Message, Exception Exception) : Error(Message);
-
-    public static ListenToEventSubWebsocketClient Create(
-        Func<WebsocketClient> clientFactory,
-        ProcessWebsocketMessage pipeline,
-        Func<CancellationToken> messageCancellationTokenFactory
-        )
-        => async ct =>
-        {
-            WebsocketClient client = clientFactory();
-            IDisposable subscription = client.ConfigureWithPipeline(pipeline, messageCancellationTokenFactory);
-            try
-            {
-                await client.StartOrFail(); // does not take cancellation token.
-            }
-            catch (Exception ex)
-            {
-                return new WebsocketClientError("Websocket client threw exception on start.", ex);
-            }
-            return (StopEventSubWebsocketClient)(async ct =>
-            {
-                try
-                {
-                    bool stopResult = await client.StopOrFail(WebSocketCloseStatus.NormalClosure, "Connection closed");
-                    subscription.Dispose();
-                    client.Dispose();
-                    return stopResult;
-                }
-                catch (Exception ex)
-                {
-                    return new WebsocketClientError("Websocket client threw exception on stop.", ex);
-                }
-            });
-        };
-
-    public static Validation<Func<WebsocketClient>> CreateFactory(
-        EventSubWebsocketUrl url,
-        ILogger<WebsocketClient>? logger = null,
-        Func<Uri, CancellationToken, Task<WebSocket>>? connectionFactory = null,
-        RecyclableMemoryStreamManager? memoryStreamManager = null
-        )
-        => url.ToUri()
-            .Map<Func<WebsocketClient>>(uri => () => new WebsocketClient(uri, logger, connectionFactory, memoryStreamManager));
-
-    private static IDisposable ConfigureWithPipeline(
-        this WebsocketClient client,
-        ProcessWebsocketMessage pipeline,
-        Func<CancellationToken> messageCancellationTokenFactory
-        )
+    public static ListenToEventSubWebsocketClient Create()
     {
-        client.IsTextMessageConversionEnabled = false; // Disable text conversion, gives us raw stream access
-        client.IsStreamDisposedAutomatically = false; // Disable disposing the receive stream, we will handle that
-        IDisposable subscription = client.MessageReceived.Subscribe(onNext: CreateMessageProcessor(pipeline, messageCancellationTokenFactory));
-        return subscription;
-    }
-    
-    private static Action<ResponseMessage> CreateMessageProcessor(
-        ProcessWebsocketMessage pipeline,
-        Func<CancellationToken> cancellationTokenFactory
-        )
-        => async message =>
-        {
-            if (message.Stream is not Stream stream)
-                return;
+        return EventSubWebsocketClient.Create(
+            createClient: ctx =>
+            {
+                WebsocketClient client = new(ctx.Uri)
+                {
+                    IsTextMessageConversionEnabled = false,
+                    IsStreamDisposedAutomatically = false
+                };
 
-            CancellationToken ct = cancellationTokenFactory();
-            await pipeline(new(stream), ct);
-            await stream.DisposeAsync();
-        };
+                IDisposable messageHandler = client.MessageReceived.Subscribe(message =>
+                {
+                    if (message.Stream is not Stream messageStream)
+                        return;
+
+                    CancellationTokenSource cts = new();
+                    cts.CancelAfter(5000);
+
+                    try
+                    {
+                        _ = ctx.OnMessage(messageStream, cts.Token).AsTask();
+                    }
+                    catch (TaskCanceledException)
+                    {
+                        // Could log or whatever.
+                    }
+
+                    messageStream.Dispose();
+                });
+
+                IDisposable disconnectHandler = client.DisconnectionHappened.Subscribe(disconnect =>
+                {
+                    ctx.OnError(new Exception("Websocket.Client lost connection."));
+                    client.Dispose();
+                });
+
+                return async ct =>
+                {
+                    await client.StartOrFail();
+                    return () =>
+                    {
+                        messageHandler.Dispose();
+                        disconnectHandler.Dispose();
+                        client.Dispose();
+                        return Task.CompletedTask;
+                    };
+                };
+            }
+            );
+    }
 }
