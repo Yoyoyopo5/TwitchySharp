9b5e491832134106aed8bcbef99ae18e36dfaa62
Yoyoyopo5 <118655115+Yoyoyopo5@users.noreply.github.com>
2026-06-06 08:18:55 -0500

    Add default EventSubWebsocketUrl static definition.

--- src/EventSub/TwitchySharp.EventSub.Websocket/Primitives/EventSubWebsocketUrl.cs
+++ src/EventSub/TwitchySharp.EventSub.Websocket/Primitives/EventSubWebsocketUrl.cs
@@ -10,5 +10,10 @@ namespace TwitchySharp.EventSub.Websocket;
 [Wrapper<string>]
 public readonly partial record struct EventSubWebsocketUrl(string Value)
 {
+    private const string TWITCH_WEBSOCKET_URL = "wss://eventsub.wss.twitch.tv/ws";
+    /// <summary>
+    /// The default address of the Twitch EventSub Websocket server.
+    /// </summary>
+    public static EventSubWebsocketUrl Default { get; } = new(TWITCH_WEBSOCKET_URL);
     public Validation<Uri> ToUri(UriCreationOptions options = default) => Uri.TryCreate(Value, options, out Uri? uri) ? uri : new Error("Invalid Uri");
 }
