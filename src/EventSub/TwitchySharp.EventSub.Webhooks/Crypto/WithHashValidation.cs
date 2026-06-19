0a4b73bb848f5e3e9eeeb01c16734066cb933d02
Yoyoyopo5 <118655115+Yoyoyopo5@users.noreply.github.com>
2026-06-19 03:37:27 -0500

    Fix potential bugs in WithHashValidation for EventSub.Webhooks crypto.

--- src/EventSub/TwitchySharp.EventSub.Webhooks/Crypto/WithHashValidation.cs
+++ src/EventSub/TwitchySharp.EventSub.Webhooks/Crypto/WithHashValidation.cs
@@ -12,21 +12,28 @@ public static partial class ProcessWebhookRequestExtensions
         {
             RecyclableMemoryStreamManager memoryManager = _memoryManager;
 
-            return (request, ct) =>
+            return async (request, ct) =>
             {
-                using RecyclableMemoryStream cryptoStream = memoryManager.GetStream();
-                using TeeStream teeStream = new(request.Content, cryptoStream);
+                await using RecyclableMemoryStream cryptoStream = memoryManager.GetStream();
+                await using TeeStream teeStream = new(request.Content, cryptoStream, leaveOpen: true);
 
                 EventSubWebhookRequest nextRequest = request with { Content = new(teeStream) };
                 EventSubWebhookRequest toVerify = request with { Content = new(cryptoStream) };
 
-                return next(nextRequest, ct)
-                    .BindAsync((result, ct) => verifyHash(result.Subscription, toVerify, ct).MatchAsync(
+                return await next(nextRequest, ct)
+                    .BindAsync((result, ct) =>
+                    {
+                        // We assume the next function read the tee stream to completion,
+                        // which copies it into the cryptoStream, then we can reset it to position 0
+                        // for the verification step. This prevents the consumer from needing to
+                        // ensure that the request stream is buffered. i.e., we buffer it here.
+                        cryptoStream.Position = 0;
+                        return verifyHash(result.Subscription, toVerify, ct).MatchAsync(
                         // We have to wrap the unit validation from the verifier back into a WebhookRequestResult validation.
                         onError: (e, _) => ValueTask.FromResult(new Validation<WebhookRequestContent>(e)),
                         onValid: _ => ValueTask.FromResult<Validation<WebhookRequestContent>>(result),
-                        ct
-                        ), ct);
+                        ct);
+                    }, ct);
             };
         });
 }
