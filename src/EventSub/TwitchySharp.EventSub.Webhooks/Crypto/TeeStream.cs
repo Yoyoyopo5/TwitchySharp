e5ffdb7331cf57049941b68e3dbfdec3b43d65f2
Yoyoyopo5 <118655115+Yoyoyopo5@users.noreply.github.com>
2026-06-19 03:36:31 -0500

    Add TeeStream unit tests and fix namespace on TeeStream

--- src/EventSub/TwitchySharp.EventSub.Webhooks/Crypto/TeeStream.cs
+++ src/EventSub/TwitchySharp.EventSub.Webhooks/Crypto/TeeStream.cs
@@ -1,4 +1,4 @@
-﻿namespace TwitchySharp.EventSub.Webhooks.Crypto;
+﻿namespace TwitchySharp.EventSub.Webhooks;
 
 /// <summary>
 /// Helper class for splitting a <see cref="Stream"/>, allowing the original stream to be consumed once while writing to a second stream concurrently.
