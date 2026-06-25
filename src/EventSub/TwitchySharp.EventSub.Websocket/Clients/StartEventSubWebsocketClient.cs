b96596b4836e6737a6178d0066d8f5e5ce203991
Yoyoyopo5 <118655115+Yoyoyopo5@users.noreply.github.com>
2026-06-25 08:30:29 -0500

    Rewrite EventSub Websocket client delegates and helper functions to use a dedicated Stop function for lifecycle instead of CancellationToken.

---
+++ src/EventSub/TwitchySharp.EventSub.Websocket/Clients/StartEventSubWebsocketClient.cs
@@ -0,0 +1,14 @@
+﻿using TwitchySharp.EventSub.Websocket.Functional;
+
+namespace TwitchySharp.EventSub.Websocket.Clients;
+
+/// <summary>
+/// Start listening to EventSub websocket messages.
+/// </summary>
+/// <param name="pipeline">The message processing pipeline to use.</param>
+/// <param name="url">The websocket url to connect to.</param>
+/// <param name="ct">Cancellation token.</param>
+/// <returns>
+/// A <see cref="Task"/> containing a stop function that completes when the client has started listening.
+/// </returns>
+public delegate Task<StopWebsocketClient> StartEventSubWebsocketClient(ProcessWebsocketMessage pipeline, EventSubWebsocketUrl url, CancellationToken ct = default);
