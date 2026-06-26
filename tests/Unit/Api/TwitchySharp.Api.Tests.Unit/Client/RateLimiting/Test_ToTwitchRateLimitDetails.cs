2b88a70a4a631e7db452f60b36f2167726c003a8
Yoyoyopo5 <118655115+Yoyoyopo5@users.noreply.github.com>
2026-06-26 18:35:26 -0500

    Add unit tests for Api ToTwitchRateLimitDetails

---
+++ tests/Unit/Api/TwitchySharp.Api.Tests.Unit/Client/RateLimiting/Test_ToTwitchRateLimitDetails.cs
@@ -0,0 +1,35 @@
+﻿namespace TwitchySharp.Api.Tests.Unit.Client.RateLimiting;
+
+public class Test_ToTwitchRateLimitDetails
+{
+    [Fact]
+    public void ToTwitchRateLimitDetails_ValidHeaders_ReturnsExpectedTwitchRateLimitDetails()
+    {
+        const int LIMIT = 100;
+        const int REMAINING = 99;
+        const int RESET = 1782516706; // Unix Timestamp (seconds)
+
+        HttpResponseMessage stubMessage = new();
+        stubMessage.Headers.Add("Ratelimit-Limit", LIMIT.ToString());
+        stubMessage.Headers.Add("Ratelimit-Remaining", REMAINING.ToString());
+        stubMessage.Headers.Add("Ratelimit-Reset", RESET.ToString());
+
+        TwitchRateLimitDetails result = stubMessage.Headers.ToTwitchRateLimitDetails();
+
+        Assert.Equal(LIMIT, result.Limit);
+        Assert.Equal(REMAINING, result.Remaining);
+        Assert.Equal(DateTimeOffset.FromUnixTimeSeconds(RESET), result.Reset);
+    }
+
+    [Fact]
+    public void ToTwitchRateLimitDetails_MissingHeaders_ReturnsTwitchRateLimitDetailsWithNullValues()
+    {
+        HttpResponseMessage stubMessage = new();
+
+        TwitchRateLimitDetails result = stubMessage.Headers.ToTwitchRateLimitDetails();
+
+        Assert.Null(result.Limit);
+        Assert.Null(result.Remaining);
+        Assert.Null(result.Reset);
+    }
+}
