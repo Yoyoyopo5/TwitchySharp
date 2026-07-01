444507ad3329e3cf3d8593865bf8677a8358007c
Yoyoyopo5 <118655115+Yoyoyopo5@users.noreply.github.com>
2026-06-30 20:03:09 -0500

    Write new integration tests for EventSub.Webhooks

---
+++ tests/Integration/EventSub/TwitchySharp.EventSub.Webhooks.Tests.Integration/Test_HashValidation.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace TwitchySharp.EventSub.Webhooks.Tests.Integration;
+
+public class Test_HashValidation(WebhooksFixture fixture) : IClassFixture<WebhooksFixture>
+{
+    private readonly WebhooksFixture _fixture = fixture;
+
+    [Fact]
+    public async Task Receive_NotificationWithInvalidSignature_IgnoreRequest()
+    {
+        const string FAKE_MESSAGE_ID = "1234567890";
+        const string FAKE_BODY = $$"""
+                        {
+              "subscription": {
+                "id": "{{FAKE_MESSAGE_ID}}",
+                "status": "enabled",
+                "type": "channel.follow",
+                "version": "1",
+                "cost": 1,
+                "condition": {
+                  "broadcaster_user_id": "12826"
+                },
+                "transport": {
+                  "method": "webhook",
+                  "callback": "https://example.com/webhooks/callback"
+                },
+                "created_at": "2019-11-16T10:11:12.634234626Z"
+              },
+              "event": {
+                "user_id": "1337",
+                "user_login": "awesome_user",
+                "user_name": "Awesome_User",
+                "broadcaster_user_id":     "12826",
+                "broadcaster_user_login":  "twitch",
+                "broadcaster_user_name":   "Twitch",
+                "followed_at": "2020-07-15T18:16:11.17106713Z"
+              }
+            }
+            """;
+        const string FAKE_INVALID_SECRET = "bad_secret";
+        DateTimeOffset fakeTimestamp = new(2026, 6, 30, 19, 42, 0, TimeSpan.Zero);
+
+        HttpContext request = await WebhookRequest.Create(
+            new(FAKE_INVALID_SECRET),
+            new(FAKE_MESSAGE_ID),
+            Functional.EventSubWebhookMessageType.Notification,
+            EventSubSubscriptionType.ChannelFollow,
+            fakeTimestamp,
+            FAKE_BODY
+            );
+
+        await _fixture.SimulateRequest(request, TestContext.Current.CancellationToken);
+
+        Assert.Null(_fixture.Handler.LastNotification);
+    }
+}
---
+++ tests/Integration/EventSub/TwitchySharp.EventSub.Webhooks.Tests.Integration/WebhookRequest.cs
@@ -0,0 +1,43 @@
+﻿using System.Text;
+using Microsoft.AspNetCore.Http;
+using TwitchySharp.EventSub.Webhooks.Crypto;
+using TwitchySharp.EventSub.Webhooks.Functional;
+
+namespace TwitchySharp.EventSub.Webhooks.Tests.Integration;
+
+public static class WebhookRequest
+{
+    public static async ValueTask<HttpContext> Create(WebhookSecret secret, WebhookMessageId messageId, EventSubWebhookMessageType messageType, EventSubSubscriptionType subscriptionType, DateTimeOffset timestamp, string body)
+    {
+        DefaultHttpContext context = new();
+        context.Request.Headers.Append("Twitch-Eventsub-Message-Id", messageId.Value);
+        context.Request.Headers.Append("Twitch-Eventsub-Message-Type", messageType.Value);
+        context.Request.Headers.Append("Twitch-Eventsub-Subscription-Type", subscriptionType.Type);
+        context.Request.Headers.Append("Twitch-Eventsub-Subscription-Version", subscriptionType.Version);
+        context.Request.Headers.Append("Twitch-Eventsub-Message-Timestamp", timestamp.ToUnixTimeSeconds().ToString());
+        context.Request.Headers.Append("Twitch-Eventsub-Message-Signature", await ComputeSignature(secret, messageId, timestamp, body));
+
+        context.Request.Method = "POST";
+        context.Request.Host = new("https://twitch.tv");
+        context.Request.Path = new(WebhooksFixture.WEBHOOKS_PATH);
+        context.Request.Body = body.ToStream();
+
+        return context;
+    }
+
+    private static MemoryStream ToStream(this string data)
+        => new(Encoding.UTF8.GetBytes(data));
+
+    private static async ValueTask<string> ComputeSignature(WebhookSecret secret, WebhookMessageId messageId, DateTimeOffset timestamp, string body)
+    {
+        using Stream bodyStream = body.ToStream();
+        byte[] signatureBytes = await EventSubWebhookCrypto.ComputeSignature(
+            secretBytes: secret.ToBytes(),
+            messageId,
+            timestamp.ToUnixTimeSeconds().ToString(),
+            bodyStream,
+            TestContext.Current.CancellationToken
+            );
+        return Encoding.UTF8.GetString(signatureBytes);
+    }
+}
