b3b5061640b808afab057f8d5b59748ce1301836
Yoyoyopo5 <118655115+Yoyoyopo5@users.noreply.github.com>
2026-07-10 02:28:53 -0500

    Add integration tests for EventSub.Websocket

--- tests/Integration/EventSub/TwitchySharp.EventSub.Websocket.Tests.Integration/WebsocketFixture.cs
+++ tests/Integration/EventSub/TwitchySharp.EventSub.Websocket.Tests.Integration/WebsocketFixture.cs
@@ -1,7 +1,9 @@
 ﻿using System.Collections.Concurrent;
 using System.Net.WebSockets;
 using System.Text;
+using System.Text.Json;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.TestHost;
 using Microsoft.Extensions.DependencyInjection;
 using TwitchySharp.EventSub.Notifications;
 using TwitchySharp.EventSub.Websocket.Clients;
@@ -9,14 +11,19 @@ using TwitchySharp.EventSub.Websocket.Functional;
 using TwitchySharp.EventSub.Websocket.Idempotency;
 using TwitchySharp.EventSub.Websocket.Serialization;
 using TwitchySharp.Infrastructure.Functional;
+using TwitchySharp.Serialization;
 using Websocket.Client;
 
 namespace TwitchySharp.EventSub.Websocket.Tests.Integration;
 
-public class WebsocketFixture
+public class WebsocketFixture : IAsyncLifetime
 {
     private readonly ConcurrentDictionary<EventSubWebsocketSessionId, WebSocket> _sockets = [];
+    public IReadOnlyDictionary<EventSubWebsocketSessionId, WebSocket> OpenWebsockets => _sockets;
     public WebApplication Host { get; }
+
+    public IServiceScope NewServiceScope()
+        => Host.Services.CreateScope();
 
     public WebsocketFixture()
     {
@@ -24,6 +31,7 @@ public class WebsocketFixture
         Host.UseWebSockets();
         Host.Map("/ws", async ctx =>
         {
+            CancellationToken ct = TestContext.Current.CancellationToken;
             if (!ctx.WebSockets.IsWebSocketRequest)
             {
                 ctx.Response.StatusCode = 400;
@@ -32,12 +40,37 @@ public class WebsocketFixture
 
             using WebSocket ws = await ctx.WebSockets.AcceptWebSocketAsync();
 
-            // TODO: Create and send welcome message with new session id
-            // Add to _sockets with session id key
+            EventSubWebsocketSessionId sessionId = new(Guid.NewGuid().ToString());
 
-            await ws.WaitForClientClose();
+            _sockets.AddOrUpdate(sessionId, ws, (_, w) => w);
 
-            // Remove from _sockets
+            await ws.SendEventSubMessage(
+                new EventSubWebsocketMessage<WelcomeMessagePayload>()
+                {
+                    Metadata = new()
+                    {
+                        MessageId = new(Guid.NewGuid().ToString()),
+                        MessageTimestamp = DateTimeOffset.UtcNow,
+                        MessageType = WebsocketMessageType.Welcome
+                    },
+                    Payload = new()
+                    {
+                        Session = new()
+                        {
+                            Id = sessionId,
+                            Status = EventSubSessionStatus.Connected,
+                            ConnectedAt = DateTimeOffset.UtcNow,
+                            KeepaliveTimeout = TimeSpan.FromSeconds(10)
+                        }
+                    }
+                },
+                JsonConfig.ApiOptions,
+                ct
+                );
+
+            await ws.WaitForClientClose(ct);
+
+            _sockets.TryRemove(new(sessionId, ws));
         });
     }
 
@@ -46,51 +79,85 @@ public class WebsocketFixture
             ? throw new KeyNotFoundException("The websocket connection with the specified key was not found.")
             : ws.SendAsync(message, ct);
 
-    public Task<StopWebsocketClient> StartNewClient(CancellationToken ct = default)
+    public Task SendTestMessageAsync<TPayload>(EventSubWebsocketSessionId sessionId, EventSubWebsocketMessage<TPayload> message, CancellationToken ct = default)
+        => SendTestMessageAsync(sessionId, JsonSerializer.Serialize(message, JsonConfig.ApiOptions), ct);
+
+    public async Task<StopWebsocketClient> StartNewClient(IServiceProvider sp, CancellationToken ct = default)
     {
-        using IServiceScope scope = Host.Services.CreateScope();
-        return Host.Services.GetRequiredService<StartEventSubWebsocketClient>()(
-            Host.Services.GetRequiredService<ProcessWebsocketMessage>(),
-            new("ws://localhost/ws"),
-            ct
-            );
+        StartEventSubWebsocketClient start = EventSubWebsocketClient.Create(ctx =>
+        {
+            Host.GetTestServer().CreateWebSocketClient();
+            WebsocketClient client = new(
+                url: ctx.Uri,
+                connectionFactory: (uri, ct) => Host.GetTestServer().CreateWebSocketClient().ConnectAsync(uri, ct),
+                logger: null
+                )
+            {
+                IsStreamDisposedAutomatically = false,
+                IsTextMessageConversionEnabled = false
+            };
+
+            IDisposable messageHandler = client.MessageReceived.Subscribe(async message =>
+            {
+                if (message.Stream is not Stream stream)
+                    return;
+
+                await ctx.OnMessage(stream, TestContext.Current.CancellationToken);
+                await stream.DisposeAsync();
+            });
+
+            return async ct =>
+            {
+                await client.StartOrFail();
+                return async ct =>
+                {
+                    messageHandler.Dispose();
+                    try
+                    {
+                        await client.StopOrFail(WebSocketCloseStatus.NormalClosure, string.Empty);
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                };
+            };
+        }).WithReconnects(error => throw error);
+
+        return await start(
+                sp.GetRequiredService<ProcessWebsocketMessage>(),
+                new("ws://localhost/ws"),
+                ct
+                );
     }
 
     private static WebApplicationBuilder ConfigureWebHost(WebApplicationBuilder builder)
     {
-        builder.Services.AddScoped<IWebsocketEventSubHandler, TestHandler>();
+        builder.WebHost.UseTestServer();
+        builder.Services.AddScoped<TestHandler>();
         builder.Services.AddScoped<IdempotencyCache>();
-        builder.Services.AddScoped<StartEventSubWebsocketClient>(sp
-            => EventSubWebsocketClient.Create(ctx =>
-            {
-                WebsocketClient client = new(ctx.Uri)
-                {
-                    IsTextMessageConversionEnabled = false,
-                    IsStreamDisposedAutomatically = false
-                };
-                IDisposable messageHandler = client.MessageReceived.Subscribe(async message =>
-                {
-                    if (message.Stream is not Stream stream)
-                        return;
-
-                    await ctx.OnMessage(stream, TestContext.Current.CancellationToken);
-                    await stream.DisposeAsync();
-                });
-                return async ct =>
-                {
-                    await client.StartOrFail();
-                    return async ct =>
-                    {
-                        messageHandler.Dispose();
-                        await client.StopOrFail(WebSocketCloseStatus.NormalClosure, string.Empty);
-                    };
-                };
-            }).WithReconnects(error => throw error));
         builder.Services.AddScoped<ProcessWebsocketMessage>(sp
             => WebsocketMessageDeserializer.Create()
                 .WithIdempotentMessages((id, ct) => sp.GetRequiredService<IdempotencyCache>().IsRepeated(id, ct))
-                .WithHandler(sp.GetRequiredService<IWebsocketEventSubHandler>()));
+                .WithHandler(sp.GetRequiredService<TestHandler>())
+                .With(next => async (message, ct) =>
+                {
+                    Validation<EventSubWebsocketMessage> result = await next(message, ct);
+                    return result.Match<Validation<EventSubWebsocketMessage>>(
+                        e => { TestContext.Current.AddAttachment("pipeline-error", $"{e.GetType().FullName}: {e.Message}"); return e; },
+                        message => message
+                        );
+                })
+                );
         return builder;
+    }
+
+    public async ValueTask InitializeAsync()
+        => await Host.StartAsync(TestContext.Current.CancellationToken);
+    public async ValueTask DisposeAsync()
+    {
+        await Host.StopAsync();
+        await Host.DisposeAsync();
     }
 }
 
@@ -103,39 +170,54 @@ public class TestHandler : IWebsocketEventSubHandler
     public EventSubReconnectSession? LastReconnect { get; private set; }
     public Error? LastError { get; private set; }
 
+    private TaskCompletionSource MessageReceived = new();
+
+    public async Task WaitForMessage(CancellationToken ct)
+    {
+        // Not exactly thread safe but the use case shouldn't cause an issue.
+        await MessageReceived.Task.WaitAsync(ct);
+        MessageReceived = new();
+    }
+
     public ValueTask OnWelcome(EventSubWebsocketSession session, CancellationToken ct = default)
     {
         Session = session;
+        MessageReceived.TrySetResult();
         return ValueTask.CompletedTask;
     }
 
     public ValueTask OnKeepalive(CancellationToken ct = default)
     {
         KeepaliveCounter++;
+        MessageReceived.TrySetResult();
         return ValueTask.CompletedTask;
     }
 
     public ValueTask OnNotified(IEventSubNotification notification, CancellationToken ct = default)
     {
         LastNotification = notification;
+        MessageReceived.TrySetResult();
         return ValueTask.CompletedTask;
     }
 
     public ValueTask OnSubscriptionRevoked(EventSubSubscription subscription, CancellationToken ct = default)
     {
         LastRevokedSubscription = subscription;
+        MessageReceived.TrySetResult();
         return ValueTask.CompletedTask;
     }
 
     public ValueTask OnReconnect(EventSubReconnectSession reconnect, CancellationToken ct = default)
     {
         LastReconnect = reconnect;
+        MessageReceived.TrySetResult();
         return ValueTask.CompletedTask;
     }
 
     public ValueTask OnError(Error error, CancellationToken ct = default)
     {
         LastError = error;
+        MessageReceived.TrySetResult();
         return ValueTask.CompletedTask;
     }
 }
@@ -155,6 +237,14 @@ public class IdempotencyCache
 
 public static class WebsocketTestExtensions
 {
+    public static Task SendEventSubMessage<TPayload>(
+        this WebSocket ws,
+        EventSubWebsocketMessage<TPayload> message,
+        JsonSerializerOptions serializerOptions,
+        CancellationToken ct = default
+        )
+        => ws.SendAsync(JsonSerializer.Serialize(message, serializerOptions), ct);
+
     public static Task SendAsync(this WebSocket ws, string message, CancellationToken ct = default)
         => ws.SendAsync(new ArraySegment<byte>(Encoding.UTF8.GetBytes(message)),
             WebSocketMessageType.Text,
