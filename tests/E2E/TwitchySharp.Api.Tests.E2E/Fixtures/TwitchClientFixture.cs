885f30ec46f1bdea6a5ec27322cf8cf8e25b20ce
Yoyoyopo5 <118655115+Yoyoyopo5@users.noreply.github.com>
2026-07-12 16:56:43 -0500

    Rewrite E2E Api test fixture and move common logic into shared E2E test project. WIP commit does not build

--- tests/E2E/TwitchySharp.Api.Tests.E2E/Fixtures/TwitchClientFixture.cs
+++ tests/E2E/TwitchySharp.Api.Tests.E2E/Fixtures/TwitchClientFixture.cs
@@ -1,139 +1,61 @@
-﻿using System.Collections.Concurrent;
-using Microsoft.Extensions.Configuration;
+﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using TwitchySharp.Api.Authorization;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
+using TwitchySharp.Api.Tests.E2E;
 using TwitchySharp.Tests.E2E;
+
+[assembly: AssemblyFixture(typeof(TwitchClientFixture))]
 
 namespace TwitchySharp.Api.Tests.E2E;
 
-public static class TestServiceProvider
-{
-    private static readonly Lazy<IServiceProvider> _serviceProvider = new(() => new ServiceCollection()
-        .AddHttpClient()
-        .AddSingleton<ConcurrentBag<AccessTokenDetails>>()
-        .BuildServiceProvider()
-    );
-    public static IServiceProvider Instance => _serviceProvider.Value;
-}
-
-public class TestConfiguration
-{
-    private static readonly Lazy<IConfiguration> _config = new(() => new ConfigurationBuilder()
-        .SetBasePath(Directory.GetCurrentDirectory())
-        .AddJsonFile("appsettings.json", true)
-        .AddUserSecrets<TestConfiguration>()
-        .AddEnvironmentVariables()
-        .Build());
-
-    public static IConfiguration Instance => _config.Value;
-}
-
-public sealed class TokenStoreAcquisition(SemaphoreSlim semaphore, ConcurrentDictionary<TwitchIdentity, AccessTokenDetails> store) : IDisposable
-{
-    public ConcurrentDictionary<TwitchIdentity, AccessTokenDetails> Store { get; } = store;
-    private readonly SemaphoreSlim _semaphore = semaphore;
-    private int _isDisposed;
-
-    public void Dispose()
-    {
-        if (Interlocked.Exchange(ref _isDisposed, 1) == 0)
-            _semaphore.Release();
-    }
-}
-
 public class TwitchClientFixture
 {
-    private static readonly IConfiguration _configuration = TestConfiguration.Instance;
-    private static readonly IServiceProvider _serviceProvider = TestServiceProvider.Instance;
-
-    private static readonly SemaphoreSlim _tokenStoreSemaphore = new(1, 1);
-    public static async Task<TokenStoreAcquisition> AcquireTokenStore(CancellationToken ct)
+    public IHost ApplicationHost { get; } = ConfigureApplication(Host.CreateApplicationBuilder()).Build();
+    private static HostApplicationBuilder ConfigureApplication(HostApplicationBuilder builder)
     {
-        await _tokenStoreSemaphore.WaitAsync(ct);
-        return new(_tokenStoreSemaphore, TokenStore);
+        builder.Configuration
+            .AddJsonFile("appsettings.json")
+            .AddUserSecrets<TwitchClientFixture>()
+            .AddEnvironmentVariables();
+
+        builder.Services
+            .Configure<ClientConfiguration>(builder.Configuration.GetRequiredSection("Client"))
+            .Configure<ExtensionConfiguration>(builder.Configuration.GetRequiredSection("Extension"))
+            .Configure<UserConfiguration[]>(builder.Configuration.GetRequiredSection("Users"))
+            .AddSingleton<TokenStore>(sp
+                => new(sp.GetRequiredService<IOptions<UserConfiguration[]>>().Value.Select(user
+                    => user.ToAccessTokenDetails(sp.GetRequiredService<IOptions<ClientConfiguration>>().Value.ClientId))))
+            .AddHttpClient()
+            .AddAppAccessTokens()
+            .AddUserAccessTokens()
+            .AddExtensionJwts()
+            .AddTransient<ITwitchClient>(sp
+                => TwitchClient.CreateDefault(sp.GetRequiredService<HttpClient>())
+                    .WithRateLimiting()
+                    .WithAuthentication(new TwitchAuthorizationResolutionOptions()
+                        {
+                            FallbackClientIdResolver = (_, _) => ValueTask.FromResult<ClientId?>(sp.GetRequiredService<IOptions<ClientConfiguration>>().Value.ClientId)
+                        }
+                        .AddTokens(sp)
+                        )
+                );
+
+        return builder;
     }
-    public TwitchIdentity.User UserIdentity { get; } = TokenStore.Values
-        .OfType<AccessTokenDetails.User>()
-        .FirstOrDefault()?
-        .Identity ?? throw new InvalidOperationException("A user access token must be configured.");
-
-    private static ConcurrentDictionary<TwitchIdentity, AccessTokenDetails> TokenStore { get; } = _configuration
-        .GetRequiredSection("Authorization")
-        .GetRequiredSection("UserAccessTokenDetails")
-        .Get<UserTokenConfiguration>()?
-        .ToAccessTokenDetails() switch
-    {
-        { } token => new ConcurrentDictionary<TwitchIdentity, AccessTokenDetails>(
-            [
-                new KeyValuePair<TwitchIdentity, AccessTokenDetails>(token.Identity, token)
-            ]),
-        _ => []
-    };
-    private static ValueTask<TDetails?> GetCachedDetails<TDetails>(TwitchRequestAuthorizationContext context, CancellationToken ct = default)
-        where TDetails : AccessTokenDetails
-        => ValueTask.FromResult(TokenStore.GetValueOrDefault(context.Identity) as TDetails);
-    private static ValueTask SetCachedDetails<TDetails>(TDetails details, CancellationToken ct)
-        where TDetails : AccessTokenDetails
-    {
-        TokenStore.AddOrUpdate(details.Identity, details, (_, newDetails) => newDetails);
-        return ValueTask.CompletedTask;
-    }
-
-    public static ClientConfiguration ClientConfig { get; } = _configuration
-        .GetRequiredSection("Authorization")
-        .GetRequiredSection("Client")
-        .Get<ClientConfiguration>()!;
-
-    public static ExtensionConfiguration ExtensionConfig { get; } = _configuration
-        .GetRequiredSection("Authorization")
-        .GetRequiredSection("Extension")
-        .Get<ExtensionConfiguration>()!;
-
-    public TwitchIdentity.Extension ExtensionIdentity => new(UserIdentity.UserId, ExtensionId: ExtensionConfig.ExtensionId);
-
-    private static readonly ITwitchClient AuthenticationClient = new TwitchClientBuilder() { HttpClient = _serviceProvider.GetRequiredService<HttpClient>() }.Build();
-
-    private static readonly TwitchAuthorizationResolutionOptions authorizationOptions = new TwitchAuthorizationResolutionOptions()
-    {
-        FallbackClientIdResolver = (_, _) => ValueTask.FromResult<ClientId?>(ClientConfig.ClientId)
-    }
-        .ConfigureIdentity<TwitchIdentity.User, AccessTokenDetails.User>(new TokenResolutionOptions<AccessTokenDetails.User>()
-        {
-            AcquireNewToken = null,
-            GetCachedToken = GetCachedDetails<AccessTokenDetails.User>,
-            RefreshToken = async (expiredToken, ct) => await AuthenticationClient.RefreshUserAccessToken(expiredToken, ClientConfig.ClientSecret, ct),
-            OnNewToken = SetCachedDetails
-        })
-        .ConfigureIdentity<TwitchIdentity.Client, AccessTokenDetails.App>(new TokenResolutionOptions<AccessTokenDetails.App>()
-        {
-            AcquireNewToken = async (context, ct) => await AuthenticationClient.GetNewAppAccessToken(context.Identity.ClientId, ClientConfig.ClientSecret, ct),
-            GetCachedToken = GetCachedDetails<AccessTokenDetails.App>,
-            RefreshToken = async (expiredToken, ct) => await AuthenticationClient.GetNewAppAccessToken(expiredToken.Identity.ClientId, ClientConfig.ClientSecret, ct) is { } newToken
-                ? new AccessTokenRefreshResult.Refreshed<AccessTokenDetails.App>(newToken)
-                : new AccessTokenRefreshResult.Expired<AccessTokenDetails.App>(expiredToken),
-            OnNewToken = SetCachedDetails
-        })
-        .ConfigureIdentity<TwitchIdentity.Extension, AccessTokenDetails.ExtensionJwt>(new TokenResolutionOptions<AccessTokenDetails.ExtensionJwt>()
-        {
-            // Not sure how we sign these just yet.
-            AcquireNewToken = async (context, ct) => context.Identity switch
-            {
-                TwitchIdentity.Extension identity => await identity.SignNewJwt(ExtensionConfig.Secret),
-                _ => null
-            },
-            GetCachedToken = GetCachedDetails<AccessTokenDetails.ExtensionJwt>,
-            RefreshToken = async (expired, ct) => new AccessTokenRefreshResult.Refreshed<AccessTokenDetails.ExtensionJwt>(await expired.Identity.SignNewJwt(ExtensionConfig.Secret)),
-            OnNewToken = SetCachedDetails
-        });
-
-    private static readonly ITwitchClientBuilder _clientBuilder = new TwitchClientBuilder()
-    {
-        HttpClient = _serviceProvider.GetRequiredService<HttpClient>()
-    }
-        .WithAuthorizationResolution(authorizationOptions)
-        .WithRateLimiting();
-    public static ITwitchClient Client { get; } = _clientBuilder.Build();
 }
 
-[CollectionDefinition("twitch")]
-public class TwitchClientCollection : ICollectionFixture<TwitchClientFixture> { }
+public static class TwitchClientFixtureExtensions
+{
+    public static ClientConfiguration GetClientConfig(this TwitchClientFixture fixture)
+        => fixture.ApplicationHost.Services.GetRequiredService<IOptions<ClientConfiguration>>().Value;
+
+    public static UserConfiguration? GetUserConfigFor(this TwitchClientFixture fixture, EndpointName endpoint)
+        => fixture.ApplicationHost.Services.GetService<IOptions<UserConfiguration[]>>()?.Value.WithEndpointName(endpoint);
+
+    public static ITwitchClient GetTwitchApiClient(this TwitchClientFixture fixture)
+        => fixture.ApplicationHost.Services.GetRequiredService<ITwitchClient>();
+
+    public static TokenStore GetTokenStore(this TwitchClientFixture fixture)
+        => fixture.ApplicationHost.Services.GetRequiredService<TokenStore>();
+}
---
+++ tests/E2E/TwitchySharp.Tests.E2E.Toolkit/TestContextExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Xunit;
+
+namespace TwitchySharp.Tests.E2E;
+
+public static class TestContextExtensions
+{
+    public static ITestContext AddSkippedEndpointWarning(this ITestContext ctx, EndpointName endpointName)
+    {
+        ctx.AddWarning($"No user configuration found for endpoint {endpointName}. Skipping test.");
+        return ctx;
+    }
+}
