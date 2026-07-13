a8d256c4c8f34e29f1f31b61d59cf2ea4b23a924
Yoyoyopo5 <118655115+Yoyoyopo5@users.noreply.github.com>
2026-07-13 05:12:56 -0500

    Refactor existing Authentication API E2E tests

---
+++ tests/E2E/TwitchySharp.Api.Tests.E2E/Tests/Authentication/Test_ClientCredentialsRequest.cs
@@ -0,0 +1,27 @@
+﻿using TwitchySharp.Api.Authorization;
+using TwitchySharp.Tests.E2E;
+
+namespace TwitchySharp.Api.Tests.E2E.Tests.Authorization;
+
+public class Test_ClientCredentialsRequest(TwitchClientFixture fixture)
+{
+    private readonly TwitchClientFixture _fixture = fixture;
+
+    [Fact]
+    public async Task Send_ClientCredentialsRequest_ReturnSuccessResponse()
+    {
+        ClientConfiguration clientConfig = _fixture.GetClientConfig();
+
+        ClientCredentialsRequest request = new()
+        {
+            ClientId = new(clientConfig.ClientId),
+            ClientSecret = new(clientConfig.ClientSecret)
+        };
+        ITwitchClient client = _fixture.GetTwitchApiClient();
+        TwitchResponse<ClientCredentialsResponse> response = await client.SendAsync(request, TestContext.Current.CancellationToken);
+
+        Assert.False(string.IsNullOrEmpty(response.Content.AccessToken.Value));
+        Assert.Equal("bearer", response.Content.TokenType);
+        Assert.True(response.Content.ExpiresIn > TimeSpan.Zero);
+    }
+}
---
+++ tests/E2E/TwitchySharp.Api.Tests.E2E/Tests/Authentication/Test_DeviceCodeRequest.cs
@@ -0,0 +1,31 @@
+﻿using TwitchySharp.Api.Authorization;
+using TwitchySharp.Tests.E2E;
+
+namespace TwitchySharp.Api.Tests.E2E.Tests.Authorization;
+
+public class Test_DeviceCodeRequest(TwitchClientFixture fixture)
+{
+    private readonly TwitchClientFixture _fixture = fixture;
+
+    [Fact]
+    public async Task Send_DeviceCodeRequest_ReturnSuccessResponse()
+    {
+        ClientConfiguration clientConfig = _fixture.GetClientConfig();
+
+        HashSet<Scope> scopes = [];
+        DeviceCodeRequest request = new()
+        {
+            ClientId = new(clientConfig.ClientId),
+            Scopes = scopes
+        };
+        ITwitchClient client = _fixture.GetTwitchApiClient();
+
+        TwitchResponse<DeviceCodeResponse> response = await client.SendAsync(request, TestContext.Current.CancellationToken);
+
+        Assert.False(string.IsNullOrEmpty(response.Content.DeviceCode.Value));
+        Assert.True(response.Content.ExpiresIn > TimeSpan.Zero);
+        Assert.True(response.Content.Interval > TimeSpan.Zero);
+        Assert.False(string.IsNullOrEmpty(response.Content.UserCode));
+        Assert.False(string.IsNullOrEmpty(response.Content.VerificationUri.AbsoluteUri));
+    }
+}
