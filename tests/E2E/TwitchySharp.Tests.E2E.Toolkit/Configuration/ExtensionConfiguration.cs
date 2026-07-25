ff77c08b6d82bcd84c83ff1cf481b60f82a26ef2
Yoyoyopo5 <118655115+Yoyoyopo5@users.noreply.github.com>
2026-07-25 02:52:39 -0500

    Add owner user id proeprty to the E2E test ExtensionConfiguration

--- tests/E2E/TwitchySharp.Tests.E2E.Toolkit/Configuration/ExtensionConfiguration.cs
+++ tests/E2E/TwitchySharp.Tests.E2E.Toolkit/Configuration/ExtensionConfiguration.cs
@@ -5,6 +5,7 @@ namespace TwitchySharp.Tests.E2E;
 public class ExtensionConfiguration : ITestIdentity
 {
     public required ExtensionId ExtensionId { get; set; }
+    public required UserId ExtensionOwnerUserId { get; set; }
     public required ClientSecret ApiSecret { get; set; }
     public required ExtensionSecret Secret { get; set; }
     public required ExtensionVersion Version { get; set; }
@@ -16,4 +17,13 @@ public class ExtensionConfiguration : ITestIdentity
 public class ExtensionBitsProductConfiguration
 {
     public required ExtensionProductSku Sku { get; set; }
+}
+
+public static class ExtensionConfigurationExtensions
+{
+    public static TwitchIdentity.Extension ToIdentity(this ExtensionConfiguration config)
+        => new(
+            OwnerId: config.ExtensionOwnerUserId,
+            ExtensionId: config.ExtensionId
+            );
 }
