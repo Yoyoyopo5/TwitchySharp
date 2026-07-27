c940bdeb946791981c7ff8822eddee5d8fdb5f3a
Yoyoyopo5 <118655115+Yoyoyopo5@users.noreply.github.com>
2026-07-27 01:00:56 -0500

    Abstracted out TwitchTestApplication from TwitchClientFixture to share extension methods with EventSub E2E test project.

---
+++ tests/E2E/TwitchySharp.Tests.E2E.Toolkit/TwitchClientExtensions.cs
@@ -0,0 +1,21 @@
+﻿using TwitchySharp.Api;
+using Xunit;
+
+namespace TwitchySharp.Tests.E2E;
+
+public static class TwitchClientExtensions
+{
+    public static TwitchClient WithExceptionsAsTestAttachments(this TwitchClient client)
+        => client.With(next => async (request, ct) =>
+        {
+            try
+            {
+                return await next(request, ct);
+            }
+            catch (TwitchApiException apiEx)
+            {
+                TestContext.Current.AddAttachment("twitch-api-exception", apiEx.ToReportString());
+                throw;
+            }
+        });
+}
