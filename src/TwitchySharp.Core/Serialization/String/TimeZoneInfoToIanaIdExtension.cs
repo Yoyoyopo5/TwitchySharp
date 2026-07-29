3f98d9c1597eede80b2a0b1e2067843ac3cb9491
Yoyoyopo5 <118655115+Yoyoyopo5@users.noreply.github.com>
2026-07-29 13:27:47 -0500

    Add TimeZoneInfo .ToIanaId() extension method and change IanaTimeZoneJsonConverter and UpdateChannelStreamScheduleRequest to use it.

---
+++ src/TwitchySharp.Core/Serialization/String/TimeZoneInfoToIanaIdExtension.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Text.Json;
+
+namespace TwitchySharp.Serialization;
+
+public static class TimeZoneInfoToIanaIdExtension
+{
+    public static string ToIanaId(this TimeZoneInfo tzi)
+        => TimeZoneInfo.TryConvertWindowsIdToIanaId(tzi.Id, out string? ianaId) switch
+        {
+            true => ianaId,
+            false => tzi.HasIanaId switch
+            {
+                true => tzi.Id,
+                false => throw new JsonException($"Cannot convert timezone id '{tzi.Id}' to IANA id format.")
+            }
+        };
+}
