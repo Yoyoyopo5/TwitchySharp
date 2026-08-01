21205ebd0801b3ee0403106eb3cef7cc7cb8eccd
Yoyoyopo5 <118655115+Yoyoyopo5@users.noreply.github.com>
2026-08-01 16:57:12 -0500

    Refactor the EventSub Webhooks AspNetCore project to support the refactored delegate-based IWebhookEventSubHandler design

--- src/EventSub/TwitchySharp.EventSub.Webhooks.AspNetCore/TwitchWebhooksRouteExtensions.cs
+++ src/EventSub/TwitchySharp.EventSub.Webhooks.AspNetCore/TwitchWebhooksRouteExtensions.cs
@@ -6,6 +6,12 @@ namespace TwitchySharp.EventSub.Webhooks.AspNetCore;
 
 public static class TwitchWebhooksRouteExtensions
 {
+    /// <summary>
+    /// Map an endpoint for Twitch EventSub Webhooks requests.
+    /// </summary>
+    /// <param name="endpoints">The endpoint route builder.</param>
+    /// <param name="pattern">The endpoint path.</param>
+    /// <returns>The endpoint route builder.</returns>
     public static IEndpointConventionBuilder MapTwitchWebhooks(this IEndpointRouteBuilder endpoints, string pattern)
         => endpoints.MapPost(pattern, (HandleAspNetWebhookRequest process, HttpContext context, CancellationToken ct = default) => process(context, ct));
 }
