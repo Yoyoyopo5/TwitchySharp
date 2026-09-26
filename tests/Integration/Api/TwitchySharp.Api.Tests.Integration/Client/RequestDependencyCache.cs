b61a580e00ba340d8d2959ed955748282abdebab
Yoyoyopo5 <118655115+Yoyoyopo5@users.noreply.github.com>
2026-09-26 00:20:29 -0500

    Add static factory helper for integration tests using the IRequestDependencyCache

---
+++ tests/Integration/Api/TwitchySharp.Api.Tests.Integration/Client/RequestDependencyCache.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.DataProtection.KeyManagement;
+using Newtonsoft.Json.Linq;
+using TwitchySharp.Api.Authentication;
+
+namespace TwitchySharp.Api.Tests.Integration.Client;
+
+public static class RequestDependencyCache
+{
+    public static ValueTask<IRequestDependencyCache<TKey, TValue>> Create<TKey, TValue>(
+            IEnumerable<KeyValuePair<TKey, TValue>>? tokens = null
+            )
+            where TKey : notnull
+            where TValue : IAccessTokenDetails<TwitchIdentity>
+            => tokens is null
+                ? ValueTask.FromResult<IRequestDependencyCache<TKey, TValue>>(new InMemoryConcurrentCache<TKey, TValue>())
+                : tokens.Aggregate(
+                ValueTask.FromResult<IRequestDependencyCache<TKey, TValue>>(new InMemoryConcurrentCache<TKey, TValue>()),
+                async (current, next) => await (await current).Set(
+                    next.Key,
+                    next.Value,
+                    TestContext.Current.CancellationToken
+                    ));
+}
